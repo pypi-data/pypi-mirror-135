14731e1395c823e364638c2ec0be3e4edcc61835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:31 +0000

    Add package IslanderDataPreprocessing/1.0.9/IslanderDataPreprocessing-1.0.9-py3.9.egg

---
+++ packages/IslanderDataPreprocessing/IslanderDataPreprocessing-1.0.9-py3.9.egg/IslanderDataPreprocessing/__init__.py
@@ -0,0 +1,9 @@
+'''there are 7 modules in this package you have teh DataBase class, DataDiscovery,
+DataVisulization, DataCorrelation, Encoder, MissingValues, FeatureScaler'''
+from .database import DataBase
+from .datadiscovery import DataDiscovery
+from .DataVisulization import DataVisulization
+from .datacorrelation import DataCorrelation
+from .encoder2 import Encoder
+from .missingvalues import MissingValues
+from .featur_scaling import FeatureScaler
