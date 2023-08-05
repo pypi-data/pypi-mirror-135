ca8434857137de65bf6c6162b24c9912790dc614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:59 +0000

    Add package trytrtretgjkgbjkbl/9999999999.999999999.999999999/trytrtretgjkgbjkbl-9999999999.999999999.999999999.tar.gz

---
+++ packages/trytrtretgjkgbjkbl/trytrtretgjkgbjkbl-9999999999.999999999.999999999.tar.gz/trytrtretgjkgbjkbl-9999999999.999999999.999999999/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='trytrtretgjkgbjkbl',
+    version='9999999999.999999999.999999999',
+    license='MIT',
+    author="Giorgos Myrianthous",
+    author_email='email@example.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/gmyrianthous/example-publish-pypi',
+    keywords='example project',
+    install_requires=[
+          'scikit-learn',
+      ],
+
+)
