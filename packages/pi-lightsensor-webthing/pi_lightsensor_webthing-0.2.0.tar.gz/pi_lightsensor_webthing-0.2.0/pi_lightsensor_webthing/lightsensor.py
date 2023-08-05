d87c4130783673acbd8942860b8f9cd48c0cc9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:39 +0000

    Add package pi-lightsensor-webthing/0.2.0/pi_lightsensor_webthing-0.2.0.tar.gz

---
+++ packages/pi-lightsensor-webthing/pi_lightsensor_webthing-0.2.0.tar.gz/pi_lightsensor_webthing-0.2.0/pi_lightsensor_webthing/lightsensor.py
@@ -0,0 +1,29 @@
+import board
+import adafruit_bh1750
+from threading import Thread
+from time import sleep
+
+
+class LightSensor:
+
+    def __init__(self):
+        i2c = board.I2C()
+        self.sensor = adafruit_bh1750.BH1750(i2c)
+
+    def listen(self, listener):
+        Thread(target=self.__listen, args=(listener,), daemon=True).start()
+
+    def __listen(self, listener):
+        while True:
+            listener(self.sensor.lux)
+            sleep(1)
+
+
+
+def listener(brightness: float):
+    print(brightness)
+
+sensor = LightSensor()
+sensor.listen(listener)
+
+sleep(10000)
