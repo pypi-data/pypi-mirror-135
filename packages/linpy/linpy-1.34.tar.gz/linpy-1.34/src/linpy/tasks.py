662e342c9d6bfd5019c6ddf7b790539f76234739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:21 +0000

    Add package linpy/1.34/linpy-1.34.tar.gz

---
+++ packages/linpy/linpy-1.34.tar.gz/linpy-1.34/src/linpy/tasks.py
@@ -0,0 +1,18 @@
+IFrame = None
+
+
+def init(iframe):
+    global IFrame
+    IFrame = iframe
+
+
+
+def open(q, ex):
+    """
+    Сначала нужно вызвать init(IFrame)
+
+    from IPython.display import IFrame
+
+    open(2, 10) открывает q2.10
+    """
+    return IFrame('1.pdf', width=600, height=300)
