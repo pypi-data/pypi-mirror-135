e9111f9a12c40afc61c2782c7212c66bac3ec93c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:53 +0000

    Add package django-jsignature/0.11/django-jsignature-0.11.tar.gz

---
+++ packages/django-jsignature/django-jsignature-0.11.tar.gz/django-jsignature-0.11/tests/settings.py
@@ -0,0 +1,36 @@
+import os
+
+DEBUG = True
+
+USE_TZ = True
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+SECRET_KEY = 'thisisntactuallysecretatall'
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+# ROOT_URLCONF = 'tests.urls'
+
+INSTALLED_APPS = [
+    'jsignature',
+    'tests',
+]
+
+PASSWORD_HASHERS = {
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+}
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+    },
+]
