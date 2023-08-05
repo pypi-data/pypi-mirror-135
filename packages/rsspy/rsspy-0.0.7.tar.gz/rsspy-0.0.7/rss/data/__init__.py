a63f2900a26c670c932b702390e27992d3c9030e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:13 +0000

    Add package rsspy/0.0.7/rsspy-0.0.7.tar.gz

---
+++ packages/rsspy/rsspy-0.0.7.tar.gz/rsspy-0.0.7/rss/data/__init__.py
@@ -0,0 +1,29 @@
+WECHAT_PUBLIC = {
+    'almosthuman': {
+        'main_url': 'https://www.wxkol.com/show/almosthuman2014.html',
+        'source': '机器之心',
+        'redis_subkey': 'almosthuman',
+    },
+    'yuntoutiao': {
+        'main_url': 'https://www.wxkol.com/show/yuntoutiao.html',
+        'source': '云头条',
+        'redis_subkey': 'yuntoutiao',
+    },
+    'aifront': {
+        'main_url': 'https://www.wxkol.com/show/ai-front.html',
+        'source': 'AI前线',
+        'redis_subkey': 'aifront',
+    },
+    'huxiu': {
+        'main_url': 'https://www.wxkol.com/show/huxiu_com.html',
+        'source': '虎嗅网',
+        'redis_subkey': 'huxiu',
+    },
+    'infoq': {
+        'main_url': 'https://www.wxkol.com/show/infoqchina.html',
+        'source': 'InfoQ',
+        'redis_subkey': 'infoq',
+    },
+}
+
+TELEGRAM = {'bot_name': 'hema_bot', 'channel_name': 'global_news_podcast'}
