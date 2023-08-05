396b8c5604d2eaa8455317541713f78602107f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:24 +0000

    Add package django-clite/0.16.0/django-clite-0.16.0.tar.gz

---
+++ packages/django-clite/django-clite-0.16.0.tar.gz/django-clite-0.16.0/cli/commands/__init__.py
@@ -0,0 +1,6 @@
+# cli:commands
+from cli.commands.destroy import destroy
+from cli.commands.docker import docker
+from cli.commands.generate import generate
+from cli.commands.new import new
+from cli.commands.run import run
