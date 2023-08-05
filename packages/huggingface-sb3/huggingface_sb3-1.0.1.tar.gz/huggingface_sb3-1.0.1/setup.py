323d2c6b839c21592bc51d89d7fef788f143fcdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:55 +0000

    Add package huggingface-sb3/1.0.1/huggingface_sb3-1.0.1.tar.gz

---
+++ packages/huggingface-sb3/huggingface_sb3-1.0.1.tar.gz/huggingface_sb3-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+install_requires = [
+    "huggingface_hub",
+]
+
+setup(
+    name='huggingface_sb3',
+    version='1.0.1',
+    packages=['huggingface_sb3'],
+    url='https://github.com/huggingface/huggingface_sb3',
+    license='',
+    author='Thomas Simonini',
+    author_email='thomas.simonini@huggingface.co',
+    description='Additional code for Stable-baselines3 to load and upload models from the Hub.',
+    install_requires=install_requires,
+
+)
