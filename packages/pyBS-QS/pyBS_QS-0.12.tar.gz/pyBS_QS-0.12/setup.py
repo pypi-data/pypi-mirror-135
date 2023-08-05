553cd93d8be76b1347566898972075d92b75a4af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:16 +0000

    Add package pyBS-QS/0.12/pyBS_QS-0.12.tar.gz

---
+++ packages/pyBS-QS/pyBS_QS-0.12.tar.gz/pyBS_QS-0.12/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(name='pyBS_QS', 
+version='0.12', 
+description='this package has been prepared a binarysearch allgorithm using quicksort',
+packages=['BS','QS'],
+zip_safe=False)
