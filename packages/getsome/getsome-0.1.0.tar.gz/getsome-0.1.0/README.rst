c53ac240ec883efa8b5dcd72caa4db5214fbca02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:08 +0000

    Add package getsome/0.1.0/getsome-0.1.0.tar.gz

---
+++ packages/getsome/getsome-0.1.0.tar.gz/getsome-0.1.0/README.rst
@@ -0,0 +1,41 @@
+=======
+getsome
+=======
+
+
+.. image:: https://img.shields.io/pypi/v/getsome.svg
+        :target: https://pypi.python.org/pypi/getsome
+
+.. image:: https://img.shields.io/travis/ranuzz/getsome.svg
+        :target: https://travis-ci.com/ranuzz/getsome
+
+.. image:: https://readthedocs.org/projects/getsome/badge/?version=latest
+        :target: https://getsome.readthedocs.io/en/latest/?version=latest
+        :alt: Documentation Status
+
+
+.. image:: https://pyup.io/repos/github/ranuzz/getsome/shield.svg
+     :target: https://pyup.io/repos/github/ranuzz/getsome/
+     :alt: Updates
+
+
+
+Get something interesting to consume on internet
+
+
+* Free software: MIT license
+* Documentation: https://getsome.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ packages/getsome/getsome-0.1.0.tar.gz/getsome-0.1.0/getsome/models/rss.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Sequence
+import datetime
+
+from getsome import AppConfig, SqlBase
+
+
+class Rss(SqlBase):
+    __tablename__ = 'rss'
+    id = Column(Integer, Sequence('rss_id_seq'), primary_key=True)
+    uri = Column(String(1024))
+    lastsync = Column(DateTime)
+
+
+def add_rss(session, uri):
+    rss = Rss(uri=uri, lastsync=datetime.datetime.fromtimestamp(0))
+    session.add(rss)
+    session.commit()
+
+
+def add_rss_init(session):
+    with open(AppConfig.rss_feed_path, 'r') as fp:
+        for line in fp.readlines():
+            add_rss(session, line)
+
+
+def all_rss(session):
+    cur_datetime = datetime.datetime.now()
+    rsss = []
+    for row in session.query(Rss).all():
+        time_delta = cur_datetime - row.lastsync
+        if time_delta.days >= 2:
+            rsss.append(row.uri)
+            update_rss_synctime(session, row)
+    return rsss
+
+
+def update_rss_synctime(session, rss):
+    rss.lastsync = datetime.datetime.now()
+    session.add(rss)
+    session.commit()
