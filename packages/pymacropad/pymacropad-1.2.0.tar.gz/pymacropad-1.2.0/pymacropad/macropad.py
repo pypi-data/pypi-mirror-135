93dc609a6a86412d0ab3aa83f24932f45ca8b652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:37 +0000

    Add package pymacropad/1.2.0/pymacropad-1.2.0.tar.gz

---
+++ packages/pymacropad/pymacropad-1.2.0.tar.gz/pymacropad-1.2.0/pymacropad/macropad.py
@@ -0,0 +1,18 @@
+from subprocess import Popen
+
+from .config import Config
+from .daemon import Daemon, KeyEvent
+
+
+def start():
+    config = Config(use_default=True)
+
+    def handle(event: KeyEvent):
+        print(f"Handling key: {event.key}, state: {event.state}")
+        command = config.get_command(event)
+        if command is not None:
+            Popen(command, shell=True)
+
+    daemon = Daemon(config.device_id)
+    daemon.handlers.add(handle)
+    daemon.start()
