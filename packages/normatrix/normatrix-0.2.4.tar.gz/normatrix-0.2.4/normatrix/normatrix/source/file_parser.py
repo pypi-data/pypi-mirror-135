e766712966ce0791cccfddff4a26814e96fa4f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:39 +0000

    Add package normatrix/0.2.4/normatrix-0.2.4.tar.gz

---
+++ packages/normatrix/normatrix-0.2.4.tar.gz/normatrix-0.2.4/normatrix/normatrix/source/file_parser.py
@@ -0,0 +1,79 @@
+import re
+from enum import Enum
+
+class TypeLine(Enum):
+    FUNCTION = 1
+    MACRO = 2
+    STRUCT = 3
+    ENUM = 4
+    GLOBAL = 5
+    COMMENT = 6
+    NONE = 7
+
+class CFileParse:
+    def __init__(self, filepath, name):
+        """An object of a file 'parsed'."""
+        # the relative path
+        # : str
+        self.basename = filepath[len(name) + 1:]
+        # the absolute path
+        # : str
+        self.filepath = filepath
+        # list of each line
+        # : list[str]
+        self.real_filelines = []
+        # list of each line without str ("text here will be removed")
+        # : list[str]
+        self.sub_filelines = []
+        # sub_filelines but with the type line
+        # : list[tuple[TypeLine, str]]
+        self.sub_parsedline = []
+        # real_filelines but with the type line
+        # : list[tuple[TypeLine, str]]
+        self.real_parsedline = []
+
+    def get_filelines(self):
+        with open(self.filepath) as fd:
+            lines = fd.read()
+        self.real_filelines = lines.split('\n')
+        lines = re.sub(r'".+?" ', '', lines)
+        lines = re.sub(r'".+?"', '', lines)
+        self.sub_filelines = lines.split('\n')
+
+def get_status(lines: str) -> (TypeLine, str):
+    reg = [
+        '^( ){0,}?\/\*(.*?\n{0,}){0,}\*\/',
+        '^( ){0,}?\/\/.*',
+        '((\w{1,}(\*{0,}) ){1,}(\*){0,}\w{1,})\(((void)|((\w{1,} {0,}){1,}(\*){0,} (\*){0,}\w{1,}(\[\d{0,}\]){0,}(, {0,}(\n {0,}){0,1}){0,}){1,4})\)\n\{((.*)\n){1,}?\}',
+        '^ {0,}#\w{1,}.*',
+        '^(typedef ){0,1}(struct )(\w{1,} ){0,1}{\n {4}\w{1,} \w{1,};(\n {4}\w{1,} \w{1,};){0,}\n}( \w{1,}){0,1};',
+        '^(typedef ){0,1}(enum )(\w{1,} ){0,1}{\n {4}\w{1,} \w{1,};(\n {4}\w{1,} \w{1,};){0,}\n}( \w{1,}){0,1};',
+        '^(static ){0,1}(const ){0,1}\w{1,} \*{0,}\w{1,}(\[[0-9]{0,}\]){0,1} = ((\w{0,})|({\n{0,1} {0,}\w{0,}(,\n{0,1} {0,}\w{0,}){0,})|)\n{0,1}}{0,1};'
+    ]
+    status = [
+        TypeLine.COMMENT,
+        TypeLine.COMMENT,
+        TypeLine.FUNCTION,
+        TypeLine.MACRO,
+        TypeLine.STRUCT,
+        TypeLine.ENUM,
+        TypeLine.GLOBAL
+    ]
+    for i, regex in enumerate(reg):
+        res = re.search(regex, lines, re.MULTILINE)
+        if res != None and res.start() <= len(lines.split('\n')[0]):
+            return (status[i], lines[res.start():res.end()])
+    return (TypeLine.NONE, lines.split('\n')[0])
+
+def parse(filepath: str, dirname: str) -> CFileParse:
+    obj = CFileParse(filepath, dirname)
+    obj.get_filelines()
+    i = 0
+    while i < len(obj.sub_filelines):
+        rest = "\n".join(obj.sub_filelines[i:])
+        (status, lines) = get_status(rest)
+        for line in lines.split('\n'):
+            obj.sub_parsedline.append((status, line))
+            obj.real_parsedline.append((status, obj.real_filelines[i]))
+            i += 1
+    return obj
---
+++ packages/normatrix/normatrix-0.2.4.tar.gz/normatrix-0.2.4/normatrix/normatrix/source/main.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import argparse
+
+try:
+    from normatrix.source import color
+    from normatrix.source import get_file_to_check
+    from normatrix.source import print_stats
+    from normatrix.source import call_plugged
+    from normatrix import plugged
+except ModuleNotFoundError:
+    from normatrix.normatrix.source import color
+    from normatrix.normatrix.source import get_file_to_check
+    from normatrix.normatrix.source import print_stats
+    from normatrix.normatrix.source import call_plugged
+    from normatrix.normatrix import plugged
+
+def call_argparse():
+    parser = argparse.ArgumentParser(prog='python -m normatrix',
+            description='The C Epitech Coding Style Norm Checker',
+            epilog='source: https://github.com/Saverio976/NorMatrix')
+    parser.add_argument('paths', metavar='paths', nargs='*',
+            help='list of path to check (default: the current working directory)')
+    parser.add_argument('--tests-run', action='store_const', dest='action',
+            const='tests', default='norm',
+            help='if you want to execute the tests (default: execute the norm checker)')
+    parser.add_argument('--no-operators-pluggin', action='store_const',
+            dest='plug_operator_activ', const='no', default='yes',
+            help='remove the operators pluggin (because it print some false positiv for now)')
+    result = parser.parse_args()
+    if result.paths == []:
+        result.paths.append(os.getcwd())
+    return result
+
+def check_norm_path(pwd: str, plug_operator_activ: bool) -> int:
+    list_checkers = plugged.__all__
+    if plug_operator_activ == False:
+        list_checkers.remove('operators')
+
+    color.print_color("green", "\nNorMatrix!")
+    color.print_color("cyan", f"directory to check: {pwd}\n")
+
+    stats, files_to_check = get_file_to_check.get_file_to_check(pwd)
+
+    STATS, NB_ERROR = call_plugged.call_plugged(files_to_check, list_checkers, pwd)
+
+    STATS.extend(stats)
+    NB_ERROR += len(stats)
+
+    print_stats.print_stats(STATS, files_to_check)
+
+    if NB_ERROR == 0:
+        color.print_color("green", "OK BRO")
+        return 0
+    else:
+        color.print_color("red", f"DUMB BRO ({NB_ERROR} error{'' if NB_ERROR == 0 else 's'})")
+        return NB_ERROR
+
+def switch_between_status(result):
+    if result.action == 'tests':
+        try:
+            from tests.fn_tests import tests
+            exit(tests.main())
+        except ModuleNotFoundError:
+            print("You cannot perform this action")
+            exit(1)
+    if result.action == 'norm':
+        ret_code = 0
+        for path in result.paths:
+            if check_norm_path(path, result.plug_operator_activ == 'yes') != 0:
+                ret_code += 1
+        if ret_code != 0:
+            if len(result.paths) == 1:
+                exit(1)
+            print(f'\nYou Have {ret_code} folder that dont respect the norm')
+            exit(1)
+        exit(0)
+
+def main():
+    args = call_argparse()
+    switch_between_status(args)
