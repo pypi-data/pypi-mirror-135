add4f797f5ab5ab3749580ac84a10bbb4a9cfc0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:31 +0000

    Add package cait/1.1.0/cait-1.1.0-py3-none-any.whl

---
+++ packages/cait/cait-1.1.0-py3-none-any.whl/cait/filter/__init__.py
@@ -0,0 +1,11 @@
+from ._ma import *
+from ._of import *
+
+__all__ = ['normalization_constant',
+           'box_car_smoothing',
+           'optimal_transfer_function',
+           'filter_event',
+           'get_amplitudes',
+           'rem_off',
+           'linregfit',
+           ]
