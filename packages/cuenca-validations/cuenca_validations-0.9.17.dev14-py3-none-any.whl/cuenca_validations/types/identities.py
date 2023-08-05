2988df2adbdf1569561b9df95d54f483dc6ae738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:12 +0000

    Add package cuenca-validations/0.9.17.dev14/cuenca_validations-0.9.17.dev14-py3-none-any.whl

---
+++ packages/cuenca-validations/cuenca_validations-0.9.17.dev14-py3-none-any.whl/cuenca_validations/types/identities.py
@@ -0,0 +1,62 @@
+import datetime as dt
+import re
+from typing import Optional
+
+from pydantic import BaseModel
+from pydantic.types import StrictStr
+
+from .enums import EntidadFederativa, KYCFileType
+
+
+class Address(BaseModel):
+    calle: str
+    numero_ext: str
+    codigo_postal: str
+    estado: EntidadFederativa
+    colonia: str
+    ciudad: Optional[str] = None
+    numero_int: Optional[str] = None
+
+
+class Beneficiary(BaseModel):
+    name: str
+    birth_date: dt.datetime
+    phone_number: str
+    user_relationship: str
+    percentage: int
+
+
+class KYCFile(BaseModel):
+    type: KYCFileType
+    is_mx: bool
+    feedme_uri_front: str
+    feedme_uri_back: Optional[str] = None
+    data: Optional[dict] = None
+
+
+class TOSAgreement(BaseModel):
+    version: int
+    ip: str
+    location: str
+    type: str  # hay que definir bien
+
+
+class PhoneNumber(StrictStr):
+    min_length = 10
+    max_length = 13
+    regex = re.compile(r'^\+{0,1}[0-9]{10,12}$')
+
+
+class CurpType(StrictStr):
+    min_length = 18
+    max_length = 18
+    regex = re.compile(r'^[A-Z]{4}[0-9]{6}[A-Z]{6}[A-Z|0-9][0-9]$')
+
+
+class Rfc(StrictStr):
+    min_length = 12
+    max_length = 13
+
+
+class Curp(BaseModel):
+    value: CurpType
