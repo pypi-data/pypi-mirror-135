666485e4a0deb7b71f9a56ad3d4cebbdda020a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:39 +0000

    Add package gdsclient/0.0.6/gdsclient-0.0.6-py3-none-any.whl

---
+++ packages/gdsclient/gdsclient-0.0.6-py3-none-any.whl/gdsclient/graph/graph_proc_runner.py
@@ -0,0 +1,194 @@
+from typing import Any, Dict, List, Optional, Union
+
+from ..error.illegal_attr_checker import IllegalAttrChecker
+from ..error.uncallable_namespace import UncallableNamespace
+from ..query_runner.query_runner import QueryResult, QueryRunner
+from .graph_export_runner import GraphExportRunner
+from .graph_object import Graph
+from .graph_project_runner import GraphProjectRunner
+
+Strings = Union[str, List[str]]
+
+
+class GraphProcRunner(UncallableNamespace, IllegalAttrChecker):
+    def __init__(self, query_runner: QueryRunner, namespace: str):
+        self._query_runner = query_runner
+        self._namespace = namespace
+
+    @property
+    def project(self) -> GraphProjectRunner:
+        self._namespace += ".project"
+        return GraphProjectRunner(self._query_runner, self._namespace)
+
+    @property
+    def export(self) -> GraphExportRunner:
+        self._namespace += ".export"
+        return GraphExportRunner(self._query_runner, self._namespace)
+
+    def drop(
+        self,
+        G: Graph,
+        failIfMissing: bool = False,
+        dbName: str = "",
+        username: Optional[str] = None,
+    ) -> QueryResult:
+        self._namespace += ".drop"
+
+        params = {
+            "graph_name": G.name(),
+            "fail_if_missing": failIfMissing,
+            "db_name": dbName,
+        }
+        if username:
+            query = f"CALL {self._namespace}($graph_name, $fail_if_missing, $db_name, $username)"
+            params["username"] = username
+        else:
+            query = f"CALL {self._namespace}($graph_name, $fail_if_missing, $db_name)"
+
+        result = self._query_runner.run_query(query, params)
+
+        return result
+
+    def exists(self, graph_name: str) -> QueryResult:
+        self._namespace += ".exists"
+        return self._query_runner.run_query(
+            f"CALL {self._namespace}($graph_name)", {"graph_name": graph_name}
+        )
+
+    def list(self, G: Optional[Graph] = None) -> QueryResult:
+        self._namespace += ".list"
+
+        if G:
+            query = f"CALL {self._namespace}($graph_name)"
+            params = {"graph_name": G.name()}
+        else:
+            query = "CALL gds.graph.list()"
+            params = {}
+
+        return self._query_runner.run_query(query, params)
+
+    def get(self, graph_name: str) -> Graph:
+        if self._namespace != "gds.graph":
+            raise SyntaxError(f"There is no {self._namespace + '.get'} to call")
+
+        if not self.exists(graph_name)[0]["exists"]:
+            raise ValueError(f"No projected graph named '{graph_name}' exists")
+
+        return Graph(graph_name, self._query_runner)
+
+    def _handle_properties(
+        self,
+        G: Graph,
+        properties: Strings,
+        entities: Strings,
+        config: Dict[str, Any],
+    ) -> QueryResult:
+        query = f"CALL {self._namespace}($graph_name, $properties, $entities, $config)"
+        params = {
+            "graph_name": G.name(),
+            "properties": properties,
+            "entities": entities,
+            "config": config,
+        }
+
+        return self._query_runner.run_query(query, params)
+
+    def streamNodeProperties(
+        self,
+        G: Graph,
+        node_properties: List[str],
+        node_labels: Strings = ["*"],
+        **config: Any,
+    ) -> QueryResult:
+        self._namespace += ".streamNodeProperties"
+
+        return self._handle_properties(G, node_properties, node_labels, config)
+
+    def streamNodeProperty(
+        self,
+        G: Graph,
+        node_properties: str,
+        node_labels: Strings = ["*"],
+        **config: Any,
+    ) -> QueryResult:
+        self._namespace += ".streamNodeProperty"
+
+        return self._handle_properties(G, node_properties, node_labels, config)
+
+    def streamRelationshipProperties(
+        self,
+        G: Graph,
+        relationship_properties: List[str],
+        relationship_types: Strings = ["*"],
+        **config: Any,
+    ) -> QueryResult:
+        self._namespace += ".streamRelationshipProperties"
+
+        return self._handle_properties(
+            G, relationship_properties, relationship_types, config
+        )
+
+    def streamRelationshipProperty(
+        self,
+        G: Graph,
+        relationship_properties: str,
+        relationship_types: Strings = ["*"],
+        **config: Any,
+    ) -> QueryResult:
+        self._namespace += ".streamRelationshipProperty"
+
+        return self._handle_properties(
+            G, relationship_properties, relationship_types, config
+        )
+
+    def writeNodeProperties(
+        self,
+        G: Graph,
+        node_properties: List[str],
+        node_labels: Strings = ["*"],
+        **config: Any,
+    ) -> QueryResult:
+        self._namespace += ".writeNodeProperties"
+
+        return self._handle_properties(G, node_properties, node_labels, config)
+
+    def writeRelationship(
+        self,
+        G: Graph,
+        relationship_type: str,
+        relationship_property: str = "",
+        **config: Any,
+    ) -> QueryResult:
+        self._namespace += ".writeRelationship"
+
+        query = f"CALL {self._namespace}($graph_name, $relationship_type, $relationship_property, $config)"
+        params = {
+            "graph_name": G.name(),
+            "relationship_type": relationship_type,
+            "relationship_property": relationship_property,
+            "config": config,
+        }
+
+        return self._query_runner.run_query(query, params)
+
+    def removeNodeProperties(
+        self,
+        G: Graph,
+        node_properties: List[str],
+        node_labels: Strings = ["*"],
+        **config: Any,
+    ) -> QueryResult:
+        self._namespace += ".removeNodeProperties"
+
+        return self._handle_properties(G, node_properties, node_labels, config)
+
+    def deleteRelationships(self, G: Graph, relationship_type: str) -> QueryResult:
+        self._namespace += ".deleteRelationships"
+
+        query = f"CALL {self._namespace}($graph_name, $relationship_type)"
+        params = {
+            "graph_name": G.name(),
+            "relationship_type": relationship_type,
+        }
+
+        return self._query_runner.run_query(query, params)
---
+++ packages/gdsclient/gdsclient-0.0.6-py3-none-any.whl/gdsclient/graph_data_science.py
@@ -0,0 +1,16 @@
+from .call_builder import CallBuilder
+from .direct_endpoints import DirectEndpoints
+from .error.uncallable_namespace import UncallableNamespace
+from .query_runner.query_runner import QueryRunner
+
+
+class GraphDataScience(DirectEndpoints, UncallableNamespace):
+    def __init__(self, query_runner: QueryRunner):
+        super().__init__(query_runner, "gds")
+        self._query_runner = query_runner
+
+    def __getattr__(self, attr: str) -> CallBuilder:
+        return CallBuilder(self._query_runner, f"gds.{attr}")
+
+    def set_database(self, db: str) -> None:
+        self._query_runner.set_database(db)
