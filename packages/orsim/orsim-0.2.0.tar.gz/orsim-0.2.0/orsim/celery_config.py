1d682f625f4419974821693de12b1704c8fc012b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:34 +0000

    Add package orsim/0.2.0/orsim-0.2.0.tar.gz

---
+++ packages/orsim/orsim-0.2.0.tar.gz/orsim-0.2.0/orsim/celery_config.py
@@ -0,0 +1,11 @@
+
+class CeleryConfig:
+
+    # List of modules to import when the Celery worker starts.
+    imports = ('apps.tasks',)
+
+    ## Broker settings.
+    broker_url = 'amqp://'
+    ## Disable result backent and also ignore results.
+    task_ignore_result = True
+
---
+++ packages/orsim/orsim-0.2.0.tar.gz/orsim-0.2.0/orsim/utils/__init__.py
@@ -0,0 +1,7 @@
+from .utils import (id_generator,
+                    is_success,
+                    deep_update,
+                    time_to_str, str_to_time,
+                    )
+
+
