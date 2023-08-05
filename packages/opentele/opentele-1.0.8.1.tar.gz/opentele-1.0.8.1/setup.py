c2ae42ca60d0a476adea80d181ccc84c5349bc18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:30 +0000

    Add package opentele/1.0.8.1/opentele-1.0.8.1.tar.gz

---
+++ packages/opentele/opentele-1.0.8.1.tar.gz/opentele-1.0.8.1/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="opentele",
+    version="1.0.8.1",
+    description="A python library created to make life easier for Telegram API Developers.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thedemons/opentele",
+    author="thedemons",
+    author_email="thedemons@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["src", "src/td", "src/tl"],
+    include_package_data=True,
+    install_requires=["pyqt5", "telethon", "tgcrypto"],
+)
