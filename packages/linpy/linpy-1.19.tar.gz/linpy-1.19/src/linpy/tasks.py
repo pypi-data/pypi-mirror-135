f5bfedc17f2b0de8604adabff73a4f81f23d1272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:56 +0000

    Add package linpy/1.19/linpy-1.19.tar.gz

---
+++ packages/linpy/linpy-1.19.tar.gz/linpy-1.19/src/linpy/tasks.py
@@ -0,0 +1,17 @@
+IFrame = None
+
+
+def init_opening(iframe_from_jupyter):
+    """
+    from IPython.display import IFrame
+    init_opening(IFrame)
+    """
+    global IFrame
+    IFrame = iframe_from_jupyter
+
+
+def open(task):
+    """
+    Например open(10)
+    """
+    return IFrame(f'./data/{task}.pdf', width=600, height=300)
