df721d5a7ae507cf713af84ee050b30093930e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:32 +0000

    Add package hek/0.1.7/hek-0.1.7-py3-none-any.whl

---
+++ packages/hek/hek-0.1.7-py3-none-any.whl/hek/__init__.py
@@ -0,0 +1,7 @@
+from .ipstuff import ipstuff
+from .proxy import proxy
+from .image import Image
+from .wireless import wifi
+from .network import net, wireshark, arp
+from .devicemanger import system
+from .torrequests import tor
