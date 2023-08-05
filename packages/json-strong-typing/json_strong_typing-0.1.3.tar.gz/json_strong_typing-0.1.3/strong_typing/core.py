cee50e695514ac5f7a843135e8eacfd5344806e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:07 +0000

    Add package json-strong-typing/0.1.3/json_strong_typing-0.1.3.tar.gz

---
+++ packages/json-strong-typing/json_strong_typing-0.1.3.tar.gz/json_strong_typing-0.1.3/strong_typing/core.py
@@ -0,0 +1,17 @@
+from typing import (
+    Dict,
+    List,
+    Union,
+)
+
+
+class JsonObject:
+    "Placeholder type for an unrestricted JSON object."
+
+
+class JsonArray:
+    "Placeholder type for an unrestricted JSON array."
+
+
+JsonType = Union[None, bool, int, float, str, Dict[str, "JsonType"], List["JsonType"]]
+Schema = JsonType
