6ca9c98de770b72214094713302d291d193fdbae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:43 +0000

    Add package flex-format/0.2.18/flex-format-0.2.18.tar.gz

---
+++ packages/flex-format/flex-format-0.2.18.tar.gz/flex-format-0.2.18/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+import versioneer
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="flex-format",
+    url="https://github.com/AWehrhahn/flex-format",
+    author="Ansgar Wehrhahn",
+    author_email="ansgar.wehrhahn@physics.uu.se",
+    description="A flexible file format for astronomical (and other) data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
---
+++ packages/flex-format/flex-format-0.2.18.tar.gz/flex-format-0.2.18/test/test_tabledata.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import pytest
+
+from flex.extensions.tabledata import (
+    AsciiTableExtension,
+    JSONTableExtension,
+    TableExtension,
+)
+from flex.flex import FlexFile
+
+
+@pytest.fixture
+def table():
+    df = pd.DataFrame(np.ones((10, 2)), columns=["A", "B"])
+    return df
+
+
+@pytest.fixture(
+    params=[TableExtension, AsciiTableExtension, JSONTableExtension],
+    ids=["table", "ascii", "json"],
+)
+def tableclass(request):
+    return request.param
+
+
+def test_read_write(tableclass, tmp_fname, table):
+    file = FlexFile()
+    ext = tableclass(data=table)
+    file.extensions["tab"] = ext
+
+    file.write(tmp_fname)
+
+    del file
+    f2 = FlexFile.read(tmp_fname)
+
+    assert f2["tab"].data.size == 10 * 2
+    assert np.all(f2["tab"].data == 1)
+    assert len(f2["tab"].data.columns) == 2
+    assert "A" in f2["tab"].data.columns
+    assert "B" in f2["tab"].data.columns
+
+
+def test_json(tableclass, tmp_fname, table):
+    file = FlexFile()
+    ext = tableclass(data=table)
+    file.extensions["tab"] = ext
+
+    file.to_json(tmp_fname)
+
+    del file
+    f2 = FlexFile.from_json(tmp_fname)
+
+    assert f2["tab"].data.size == 10 * 2
+    assert np.all(f2["tab"].data == 1)
+    assert len(f2["tab"].data.columns) == 2
+    assert "A" in f2["tab"].data.columns
+    assert "B" in f2["tab"].data.columns
