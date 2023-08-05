b6c8193afba4e3458a069a4f1ea611e97d2caec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:32 +0000

    Add package hek/0.1.7/hek-0.1.7.tar.gz

---
+++ packages/hek/hek-0.1.7.tar.gz/hek-0.1.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+  name='hek',
+  version='0.1.7',
+  description='A python library mostly used for pentesting and automation some tasks.',
+  long_description=open('README.txt').read(),
+  url='https://github.com/greedalbadi/hek',
+  author='greed albadi',
+  author_email='greedalbadi@gmail.com',
+  license='MIT',
+  keywords='none',
+  packages=find_packages(),
+  install_requires=['pillow', "psutil", "requests", "requests[socks]"]
+)
