e021891b7d30a4d28fc0a8f44d405d10893f19cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:04 +0000

    Add package hang-up-an-autumn-moon/0.3.2/hang_up_an_autumn_moon-0.3.2.tar.gz

---
+++ packages/hang-up-an-autumn-moon/hang_up_an_autumn_moon-0.3.2.tar.gz/hang_up_an_autumn_moon-0.3.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='hang_up_an_autumn_moon',
+    version='0.3.2',
+    packages=find_packages(),
+    include_package_data=True,
+    py_modules=['main'],
+    long_description=README,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'Click',
+        'simple_term_menu',
+        'prettytable',
+        'numpy'
+    ],
+    entry_points={
+        'console_scripts':[
+            'hang-up-an-autumn-moon=cli.main:cli',
+        ]
+    }        
+)
