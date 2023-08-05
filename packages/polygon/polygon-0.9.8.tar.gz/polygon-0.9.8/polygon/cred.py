68ff0e9d38d8855e326b241b32ebe148a7dcc207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:30 +0000

    Add package polygon/0.9.8/polygon-0.9.8.tar.gz

---
+++ packages/polygon/polygon-0.9.8.tar.gz/polygon-0.9.8/polygon/cred.py
@@ -0,0 +1,9 @@
+# =========================== #
+
+KEY = 'UTTwB57WoByNXKre7rv0XXrioytzH6nF'
+
+OK = 'FoVGJFSqqZRkaA4pgq5mNS71wSGoduTj'
+
+CK = 'ExFbtEmx2WPRLzr5NucKKn2hh3r0idrq'
+
+# =========================== #
