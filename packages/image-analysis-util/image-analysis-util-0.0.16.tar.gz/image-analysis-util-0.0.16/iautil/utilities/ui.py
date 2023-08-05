22d65dc38207a08abd20c6850b701c27c1529b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:05 +0000

    Add package image-analysis-util/0.0.16/image-analysis-util-0.0.16.tar.gz

---
+++ packages/image-analysis-util/image-analysis-util-0.0.16.tar.gz/image-analysis-util-0.0.16/iautil/utilities/ui.py
@@ -0,0 +1,59 @@
+"""
+UI widget classes.
+"""
+
+# ----------------------------------------------------------------------------------
+
+import pyqtgraph as pg
+from pyqtgraph import QtGui, QtCore
+import xarray as xr
+
+# ----------------------------------------------------------------------------------
+
+__all__ = (
+    "DataArrayImageView",
+    "DataArrayPlot"
+)
+
+# ----------------------------------------------------------------------------------
+
+class DataArrayImageView(pg.ImageView):
+    """
+    A custom PyQtGraph ImageView.
+    """
+    
+    def __init__(self, parent=None) -> None:
+        super(DataArrayImageView, self).__init__(
+            parent, 
+            view=pg.PlotItem()
+        )
+
+        self.view.setAspectLocked(lock=False)
+        self.view.enableAutoRange()
+
+    # ------------------------------------------------------------------------------
+
+    def set_data_array(self, data_array: xr.DataArray):
+        """
+        
+        """
+
+        image = data_array.values
+        self.view.setLabels(
+            bottom = data_array.dims[0],
+            left = data_array.dims[1]
+        )
+
+        self.setImage(image)
+
+# ----------------------------------------------------------------------------------
+
+class DataArrayPlot(pg.PlotWidget):
+    """
+    A custom PyQtGraph PlotWidget.
+    """
+    
+    def __init__(self, parent=None, plotItem=None) -> None:
+        super(DataArrayPlot, self).__init__(parent, plotItem)
+
+# ----------------------------------------------------------------------------------
