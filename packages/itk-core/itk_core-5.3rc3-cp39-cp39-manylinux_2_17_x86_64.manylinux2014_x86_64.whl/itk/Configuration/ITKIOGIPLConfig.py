7097bbcc2010a9557b0e9b76d576c970cba858b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:02 +0000

    Add package itk-core/5.3rc3/itk_core-5.3rc3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/itk-core/itk_core-5.3rc3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/itk/Configuration/ITKIOGIPLConfig.py
@@ -0,0 +1,4 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (  ('GiplImageIO', 'itk::GiplImageIO', 'itkGiplImageIO', True),
+  ('GiplImageIOFactory', 'itk::GiplImageIOFactory', 'itkGiplImageIOFactory', True),
+)
