99cb0fa4e4e3ed1b62804b20c531f133b3290051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:58 +0000

    Add package cressp/0.2.10/cressp-0.2.10-py3-none-any.whl

---
+++ packages/cressp/cressp-0.2.10-py3-none-any.whl/cressp/__init__.py
@@ -0,0 +1,9 @@
+# import top-level functions
+from .__main__ import *
+
+# Version of cressp package
+__version__ = "0.2.10"
+
+# import modules
+
+__all__ = [ "structural_property_estimation", "cross_reactivity_prediction", "web_application" ]
