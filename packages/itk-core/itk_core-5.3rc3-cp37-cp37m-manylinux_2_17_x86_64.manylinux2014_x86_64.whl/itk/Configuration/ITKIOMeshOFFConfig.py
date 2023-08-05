1f6f7ba275358a72c74884f5c6cc0bb953470aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:03 +0000

    Add package itk-core/5.3rc3/itk_core-5.3rc3-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/itk-core/itk_core-5.3rc3-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/itk/Configuration/ITKIOMeshOFFConfig.py
@@ -0,0 +1,4 @@
+depends = ('ITKPyBase', 'ITKMesh', 'ITKIOMeshBase', 'ITKCommon', )
+templates = (  ('OFFMeshIO', 'itk::OFFMeshIO', 'itkOFFMeshIO', True),
+  ('OFFMeshIOFactory', 'itk::OFFMeshIOFactory', 'itkOFFMeshIOFactory', True),
+)
