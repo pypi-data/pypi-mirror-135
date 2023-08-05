ddcae6ebc723ca8636d57b8dfb3cf2f97fb70f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:51 +0000

    Add package ufy/0.1.16/ufy-0.1.16.tar.gz

---
+++ packages/ufy/ufy-0.1.16.tar.gz/ufy-0.1.16/setup.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   setup.py
+@Time    :   2022/01/17 11:04:08
+@Author  :   ufy
+@Contact :   antarm@outlook.com
+@Version :   v1.0
+@Desc    :   None
+'''
+
+# here put the import lib
+from setuptools import setup,find_packages
+
+
+setup(
+    name = 'ufy',
+    version = '0.1.16',
+    description= 'some tools for ufy',
+    author='ufy',
+    author_eamil='antarm@outlook.com',
+    packages=find_packages('.'),
+)
+
+# print(find_packages('.'),)
