fba9f738de02081b57179d70422c9acff1f8f7cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:46 +0000

    Add package onedrivedownloader/1.0.2/onedrivedownloader-1.0.2.tar.gz

---
+++ packages/onedrivedownloader/onedrivedownloader-1.0.2.tar.gz/onedrivedownloader-1.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
+setup(
+    name='onedrivedownloader',
+    packages=find_packages(),
+    version='1.0.2',
+    license='MIT',
+    description='Python utility to download files through OneDrive',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Lorenzo Bonicelli',
+    author_email='loribonna@gmail.com',
+    url='https://github.com/loribonna/onedrivedownloader',
+    download_url='https://github.com/loribonna/onedrivedownloader/archive/refs/tags/v1.0.2.zip',
+    keywords=['onedrive', 'downloader', 'python', 'utility'],
+    install_requires=[requirements],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
