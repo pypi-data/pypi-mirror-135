625ef057427f11b1d954500acd057162912cae37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:29 +0000

    Add package python-optix/0.1.0/python-optix-0.1.0.tar.gz

---
+++ packages/python-optix/python-optix-0.1.0.tar.gz/python-optix-0.1.0/optix/optix_includes.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <optix.h>
+#include <optix_stubs.h>
+#include <optix_function_table_definition.h>
+#include <optix_stack_size.h>
+#include <sstream>
+
+inline void optix_check_return(OptixResult result) {
+        if( result != OPTIX_SUCCESS ) {
+            std::stringstream ss;
+            ss << optixGetErrorName(result);
+            ss << ": " << optixGetErrorString(result);
+            throw std::runtime_error(ss.str());
+        }
+}
