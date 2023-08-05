4cc9795df14cb023b84721777127f360423482d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:34 +0000

    Add package ipycytoscape/1.3.2/ipycytoscape-1.3.2-py2.py3-none-any.whl

---
+++ packages/ipycytoscape/ipycytoscape-1.3.2-py2.py3-none-any.whl/ipycytoscape/_version.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) 2020, QuantStack, Mariana Meireles and ipycytoscape Contributors
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+
+version_info = (1, 3, 2)
+__version__ = ".".join(map(str, version_info))
