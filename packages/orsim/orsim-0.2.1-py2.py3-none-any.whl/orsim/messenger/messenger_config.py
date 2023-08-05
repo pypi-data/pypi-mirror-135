98d8aff2e5bcd9748a227cd961a9131dd409e666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:43 +0000

    Add package orsim/0.2.1/orsim-0.2.1-py2.py3-none-any.whl

---
+++ packages/orsim/orsim-0.2.1-py2.py3-none-any.whl/orsim/messenger/messenger_config.py
@@ -0,0 +1,12 @@
+# # import logging
+
+# messenger_backend_settings = {
+
+#     'RABBITMQ_MANAGEMENT_SERVER': "http://localhost:15672/api", # "http://192.168.10.135:15672/api", # "http://localhost:15672/api",
+#     'RABBITMQ_ADMIN_USER': 'guest', # 'test', # 'guest',
+#     'RABBITMQ_ADMIN_PASSWORD': 'guest', #'test', # 'guest',
+
+#     'MQTT_BROKER': "localhost", # "192.168.10.115", # "localhost",
+
+# }
+
