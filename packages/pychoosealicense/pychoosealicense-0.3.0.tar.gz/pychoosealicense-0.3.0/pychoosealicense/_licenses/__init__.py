29dd011e915fbcd8aab773d69e6b571847ff4d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:02 +0000

    Add package pychoosealicense/0.3.0/pychoosealicense-0.3.0.tar.gz

---
+++ packages/pychoosealicense/pychoosealicense-0.3.0.tar.gz/pychoosealicense-0.3.0/pychoosealicense/_licenses/__init__.py
@@ -0,0 +1,14 @@
+"""
+License metadata used by ``pychoosealicense``.
+"""
+
+CHOOSEALICENSE_COMMIT_INFO = """
+commit 20d853259a9412aa7a0d6f6d9bc0f098f38266d9
+Author: Mike Linksvayer <mlinksva@github.com>
+Date:   Wed Jan 5 17:50:10 2022 -0800
+
+    rubocop suggestions
+"""
+
+CHOOSEALICENSE_COMMIT_HASH = "20d853259a9412aa7a0d6f6d9bc0f098f38266d9"
+CHOOSEALICENSE_COMMIT_SHORTHASH = "20d8532"
