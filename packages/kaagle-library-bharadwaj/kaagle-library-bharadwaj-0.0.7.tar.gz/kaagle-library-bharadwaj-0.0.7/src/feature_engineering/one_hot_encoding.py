dfefca15308de361f238e6d5344daaea4aee5708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:53 +0000

    Add package kaagle-library-bharadwaj/0.0.7/kaagle-library-bharadwaj-0.0.7.tar.gz

---
+++ packages/kaagle-library-bharadwaj/kaagle-library-bharadwaj-0.0.7.tar.gz/kaagle-library-bharadwaj-0.0.7/src/feature_engineering/one_hot_encoding.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+class OneHotEncoding:
+    def __init__(self, target_column:str):
+        self.target_column = target_column
+    def perform_one_hot_encoding(self, dataframesource):
+        # Get one hot encoding of column
+        one_hot_column_encoding = pd.get_dummies(dataframesource[self.target_column])
+        # Drop column as it is now encoded
+        dataframesource = dataframesource.drop(self.target_column,axis = 1)
+        # Join the encoded df
+        dataframesource = dataframesource.join(one_hot_column_encoding)
+        return dataframesource
