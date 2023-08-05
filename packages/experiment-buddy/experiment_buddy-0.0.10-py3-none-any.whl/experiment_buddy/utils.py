0b045eeecc182f09fa10c01e800500a661e0df31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:50 +0000

    Add package experiment-buddy/0.0.10/experiment_buddy-0.0.10-py3-none-any.whl

---
+++ packages/experiment-buddy/experiment_buddy-0.0.10-py3-none-any.whl/experiment_buddy/utils.py
@@ -0,0 +1,47 @@
+import enum
+import os
+import subprocess
+
+import fabric
+import git
+import invoke
+
+
+class Backend(enum.Enum):
+    GENERAL: str = "general"
+    SLURM: str = "slurm"
+    DOCKER: str = "docker"
+
+
+def get_backend(ssh_session: fabric.connection.Connection, project_dir: str) -> str:
+    try:
+        ssh_session.run("/opt/slurm/bin/scontrol ping")
+        return Backend.SLURM
+    except invoke.exceptions.UnexpectedExit:
+        pass
+
+    if not os.path.exists(os.path.join(project_dir, "Dockerfile")):
+        return Backend.GENERAL
+    try:
+        ssh_session.run("docker -v")
+        ssh_session.run("docker-compose -v")
+        return Backend.DOCKER
+    except invoke.exceptions.UnexpectedExit:
+        pass
+    return Backend.GENERAL
+
+
+def get_project_name(git_repo: git.Repo) -> str:
+    git_repo_remotes = git_repo.remotes
+    assert isinstance(git_repo_remotes, list)
+    if not len(git_repo_remotes):
+        raise Exception("No remote url found, have you pushed the new repo?")
+    remote_url = git_repo_remotes[0].config_reader.get("url")
+    project_name, _ = os.path.splitext(os.path.basename(remote_url))
+    return project_name
+
+
+def _get_job_info(jid):
+    result = subprocess.check_output(f"/opt/slurm/bin/sacct --brief -j {jid}".split()).decode()
+    jid, state, exit_code = result.split("\n")[2].split()
+    return state
