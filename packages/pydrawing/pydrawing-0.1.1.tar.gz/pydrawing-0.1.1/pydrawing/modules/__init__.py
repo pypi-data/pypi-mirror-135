f6f5aa943625f7f1d04ad10ff87d17923f2e4e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:07 +0000

    Add package pydrawing/0.1.1/pydrawing-0.1.1.tar.gz

---
+++ packages/pydrawing/pydrawing-0.1.1.tar.gz/pydrawing-0.1.1/pydrawing/modules/__init__.py
@@ -0,0 +1,6 @@
+'''initialize'''
+from .utils import Logger
+from .beautifiers import (
+    CartooniseBeautifier, PencilDrawingBeautifier, CartoonGanBeautifier, FastNeuralStyleTransferBeautifier,
+    DouyinEffectBeautifier, CharacterizeBeautifier, PhotomosaicBeautifier, GlitchBeautifier
+)
