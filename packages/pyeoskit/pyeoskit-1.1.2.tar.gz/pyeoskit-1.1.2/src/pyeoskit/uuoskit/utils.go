d4a3d75cc54d506046cd89183e3e58519b702a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:19 +0000

    Add package pyeoskit/1.1.2/pyeoskit-1.1.2.tar.gz

---
+++ packages/pyeoskit/pyeoskit-1.1.2.tar.gz/pyeoskit-1.1.2/src/pyeoskit/uuoskit/utils.go
@@ -0,0 +1,58 @@
+package uuoskit
+
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+
+	traceable_errors "github.com/go-errors/errors"
+)
+
+var (
+	DEBUG = true
+)
+
+func SetDebug(debug bool) {
+	DEBUG = debug
+}
+
+func GetDebug() bool {
+	return DEBUG
+}
+
+func DecodeHash256(hash string) ([]byte, error) {
+	_hash, err := hex.DecodeString(hash)
+	if err != nil {
+		return nil, newError(err)
+	}
+	if len(_hash) != 32 {
+		return nil, newErrorf("invalid hash")
+	}
+	return _hash, nil
+}
+
+func newError(err error) error {
+	if DEBUG {
+		if _, ok := err.(*traceable_errors.Error); ok {
+			return err
+		}
+		return traceable_errors.New(err.Error())
+	} else {
+		return err
+	}
+}
+
+func newErrorf(format string, args ...interface{}) error {
+	errMsg := fmt.Sprintf(format, args...)
+	if DEBUG {
+		return traceable_errors.New(errMsg)
+	} else {
+		return errors.New(errMsg)
+	}
+}
+
+func reverseBytes(aa []byte) {
+	for i, j := 0, len(aa)-1; i < j; i, j = i+1, j-1 {
+		aa[i], aa[j] = aa[j], aa[i]
+	}
+}
