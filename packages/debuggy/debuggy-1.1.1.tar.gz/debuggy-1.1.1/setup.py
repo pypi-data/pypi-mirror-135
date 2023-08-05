424a5dca4756187bd8bac896e3e9e728ed6479f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:20 +0000

    Add package debuggy/1.1.1/debuggy-1.1.1.tar.gz

---
+++ packages/debuggy/debuggy-1.1.1.tar.gz/debuggy-1.1.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+# with open('README.md') as f:
+#      readme = f.read()
+
+with open('LICENSE') as f:
+     license = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()  
+
+
+setup(
+        name ='debuggy',
+        version ='1.1.1',
+        author ='Eje Daniel',
+        author_email ='ejedenials@gmail.com',
+        url ='https://github.com/derhnyel/deBuggy',
+        description ='Stalk StackOverflow.',
+        license =license,
+        py_modules=['debuggy','stalkoverflow'],
+        packages=find_packages(exclude=('tests', 'docs','bin','assets','.circleci')),
+        #long_description_content_type ="text/markdown",
+        #long_description = readme,
+        entry_points ={
+            'console_scripts': [
+                'debuggy = stalkoverflow.__main__:main'
+            ]
+        },
+        classifiers =[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        keywords ='debuggy python package stalkoverflow',
+        install_requires = requirements,
+        zip_safe = False)
