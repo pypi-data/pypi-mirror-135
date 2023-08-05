384fa302b5168260068ca47d12a4efb8fd2ad9f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:37 +0000

    Add package docarray/0.3.1/docarray-0.3.1.tar.gz

---
+++ packages/docarray/docarray-0.3.1.tar.gz/docarray-0.3.1/docarray/document/pydantic_model.py
@@ -0,0 +1,49 @@
+from typing import Optional, List, Dict, Any, TYPE_CHECKING, Union
+
+from pydantic import BaseModel, validator
+
+from ..math.ndarray import to_list
+
+if TYPE_CHECKING:
+    from ..types import ArrayType
+
+# this order must be preserved: https://pydantic-docs.helpmanual.io/usage/types/#unions
+_ProtoValueType = Optional[Union[bool, float, str]]
+_StructValueType = Union[
+    _ProtoValueType, List[_ProtoValueType], Dict[str, _ProtoValueType]
+]
+
+
+def _convert_ndarray_to_list(v: 'ArrayType'):
+    if v is not None:
+        return to_list(v)
+
+
+class PydanticDocument(BaseModel):
+    id: str
+    parent_id: Optional[str]
+    granularity: Optional[int]
+    adjacency: Optional[int]
+    blob: Optional[bytes]
+    tensor: Optional[Any]
+    mime_type: Optional[str]
+    text: Optional[str]
+    weight: Optional[float]
+    uri: Optional[str]
+    tags: Optional[Dict[str, '_StructValueType']]
+    offset: Optional[float]
+    location: Optional[List[float]]
+    embedding: Optional[Any]
+    modality: Optional[str]
+    evaluations: Optional[Dict[str, Dict[str, '_StructValueType']]]
+    scores: Optional[Dict[str, Dict[str, '_StructValueType']]]
+    chunks: Optional[List['PydanticDocument']]
+    matches: Optional[List['PydanticDocument']]
+
+    _tensor2list = validator('tensor', allow_reuse=True)(_convert_ndarray_to_list)
+    _embedding2list = validator('embedding', allow_reuse=True)(_convert_ndarray_to_list)
+
+
+PydanticDocument.update_forward_refs()
+
+PydanticDocumentArray = List[PydanticDocument]
