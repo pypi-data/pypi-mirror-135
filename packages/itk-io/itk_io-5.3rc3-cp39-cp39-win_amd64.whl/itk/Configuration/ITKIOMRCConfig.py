ff27a6639a6a19bf8b31611fd03cf291d77f7a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:45 +0000

    Add package itk-io/5.3rc3/itk_io-5.3rc3-cp39-cp39-win_amd64.whl

---
+++ packages/itk-io/itk_io-5.3rc3-cp39-cp39-win_amd64.whl/itk/Configuration/ITKIOMRCConfig.py
@@ -0,0 +1,4 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (  ('MRCImageIO', 'itk::MRCImageIO', 'itkMRCImageIO', True),
+  ('MRCImageIOFactory', 'itk::MRCImageIOFactory', 'itkMRCImageIOFactory', True),
+)
---
+++ packages/itk-io/itk_io-5.3rc3-cp39-cp39-win_amd64.whl/itk/Configuration/ITKIOSiemensConfig.py
@@ -0,0 +1,4 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', 'ITKIOIPL', )
+templates = (  ('SiemensVisionImageIO', 'itk::SiemensVisionImageIO', 'itkSiemensVisionImageIO', True),
+  ('SiemensVisionImageIOFactory', 'itk::SiemensVisionImageIOFactory', 'itkSiemensVisionImageIOFactory', True),
+)
