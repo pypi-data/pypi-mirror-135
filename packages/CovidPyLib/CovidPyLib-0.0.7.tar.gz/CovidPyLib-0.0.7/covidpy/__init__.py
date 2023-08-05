3eafd89c8326cd255ec20ddef8cf42ad83ab0689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:41 +0000

    Add package CovidPyLib/0.0.7/CovidPyLib-0.0.7.tar.gz

---
+++ packages/CovidPyLib/CovidPyLib-0.0.7.tar.gz/CovidPyLib-0.0.7/covidpy/__init__.py
@@ -0,0 +1,13 @@
+
+# This file is part of CovidPy v0.0.7.
+#
+# The project has been distributed in the hope it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# You can use it and/or modify it under the terms of the GNU General Public License v3.0 or later.
+# You should have received a copy of the GNU General Public License along with the project.
+from .lib import CovidPy
+from .types import QRCode, VerifyResult
+from .verifier import DCCVerifier
+from .errors import InvalidDCC
