85ebf4f55c9c133a725aac94f6152ec93c44f28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:00 +0000

    Add package sledo/0.2.1/sledo-0.2.1.tar.gz

---
+++ packages/sledo/sledo-0.2.1.tar.gz/sledo-0.2.1/sledo/generate/field_generators/base.py
@@ -0,0 +1,40 @@
+from abc import ABCMeta, abstractmethod
+from typing import Any, Dict, List, Tuple
+
+from .reference import ReferenceFieldGenerator
+
+
+class FieldGenerator(object, metaclass=ABCMeta):
+    def __init__(self, options: Dict[str, str] = {}, type: str = None):
+        super(FieldGenerator, self).__init__()
+        self.options = options
+        self.type = type
+        self.validate()
+
+    @abstractmethod
+    def generate(self):
+        raise NotImplementedError()
+
+    def validate(self):
+        pass
+
+    def val_to_str(self, value: Any) -> str:
+        return str(value)
+
+    def prepare_options(self, schema_name: str = None, res: Dict[str, Tuple[Tuple, List[List]]] = {}, iter_res={}):
+        options = self.options.copy()
+        for (key, value) in options.items():
+            if not isinstance(value, ReferenceFieldGenerator):
+                continue
+
+            schema = iter_res.get(value.type, res.get(value.type))
+            if schema is None:
+                raise Exception(
+                    f"Cannot find schema: '{value.type}' in current result")
+
+            entry = list(filter(
+                lambda header: header[1][0] == value.options["field_attr"], enumerate(schema[0])))[0]
+
+            options[key] = schema[1][-1][entry[0]]
+
+        return options
---
+++ packages/sledo/sledo-0.2.1.tar.gz/sledo-0.2.1/sledo/main.py
@@ -0,0 +1,38 @@
+
+from io import TextIOWrapper
+import os
+import shutil
+import click
+from .generate import main as generate_main
+
+
+@click.group()
+def cli():  # pragma: no cover
+    pass
+
+
+@click.command()
+@click.argument("file", type=click.File("r"))
+@click.option("-O", "--outdir", default="out", help="The directory where the generated CSVs will be stored", type=click.Path(file_okay=False, writable=True), show_default=True)
+def generate(file: TextIOWrapper, outdir: str):
+    # check if the 'out'-directory already exists
+    if os.path.isdir(outdir):
+        # TODO: reenable confirmation
+        click.confirm(f"The output directory '{outdir}' and ALL ITS CONTENTS will be REMOVED! Continue?",
+                      default=False, abort=True, prompt_suffix=": ", show_default=True, err=False)
+        # delete the existing directory
+        shutil.rmtree(outdir)
+
+    # create the directory
+    try:
+        os.mkdir(outdir)
+    except OSError as e:
+        raise click.UsageError(e)
+
+    generate_main(file, outdir)
+
+
+cli.add_command(generate)
+
+if __name__ == '__main__':  # pragma: no cover
+    cli()
