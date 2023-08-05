ede475e721a668ccf1ef02ef0c62f365eb1254d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:19 +0000

    Add package pylayer/1.0.0/pylayer-1.0.0.tar.gz

---
+++ packages/pylayer/pylayer-1.0.0.tar.gz/pylayer-1.0.0/pylayer.py
@@ -0,0 +1,98 @@
+import os
+import sys
+import time
+
+class Check:
+	print("Ent-to-end encrypted.")
+	writed = input("Enter a key: ")
+
+	token = "ePBfRsT23Le7C4DbM1WoIoo7PUr6VhuFqTOp"
+
+	if writed == token:
+		print("Unlocked.")
+	else: 
+		print("Already locked.")
+		time.sleep(5)
+		exit()
+
+	# token encrypt.
+
+class protocol:
+	write = input("Confirm a key: ")
+
+	token = "ePBfRsT23Le7C4DbM1WoIoo7PUr6VhuFqTOp"
+
+	locked = ('INVALID!')
+	unlocked = ('OKAY!')
+
+	def mMXvcBH6HmwNmukA():
+		token = "ePBfRsT23Le7C4DbM1WoIoo7PUr6VhuFqTOp"
+		locked = ('INVALID!')
+		unlocked = ('OKAY!')
+		write = input("#1 reConfirm a key: ")
+		# end to end success or decline.
+		if write == token:
+			print("CHECK #1: " + unlocked)
+		else:
+			print("CHECK #1: " + locked)
+
+	mMXvcBH6HmwNmukA()
+
+	def srY7EqBMIO6wiUGT():
+		token = "ePBfRsT23Le7C4DbM1WoIoo7PUr6VhuFqTOp"
+		locked = ('INVALID!')
+		unlocked = ('OKAY!')
+		write = input("#2 reConfirm a key: ")
+		# end to end success or decline.
+		if write == token:
+			print("CHECK #2: " + unlocked)
+		else:
+			print("CHECK #2: " + locked)
+
+	srY7EqBMIO6wiUGT()
+
+	def b1iT6swR7v919DAk():
+		token = "ePBfRsT23Le7C4DbM1WoIoo7PUr6VhuFqTOp"
+		locked = ('INVALID!')
+		unlocked = ('OKAY!')
+		write = input("#3 reConfirm a key: ")
+		# end to end success or decline.
+		if write == token:
+			print("CHECK #3: " + unlocked)
+		else:
+			print("CHECK #3: " + locked)
+			exit()
+
+	b1iT6swR7v919DAk()
+
+class flood:
+	http = "HTTP"
+	pack = "package{"
+	wlan = "WLAN"
+	bad = "bad"
+	att = "attention"
+
+	c1 = "HTTP"
+	c2 = "HTTP"
+	c3 = "HTTP"
+	udp = "package{"
+	icnp = "WLAN"
+	ddos = "bad"
+	bot = "bad"
+	ms = "attention"
+
+	if c1 == http:
+		http = "okay"
+	else:
+		exit()
+
+	if c2 == http:
+		http = "good"
+	else:
+		exit()
+
+	if c3 == http:
+		http = "primary"
+	else:
+		exit()
+
---
+++ packages/pylayer/pylayer-1.0.0.tar.gz/pylayer-1.0.0/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['pylayer']
+setup_kwargs = {
+    'name': 'pylayer',
+    'version': '1.0.0',
+    'description': 'Make a end-to-end encryption pass. PASSWORD FROM MODULE: ePBfRsT23Le7C4DbM1WoIoo7PUr6VhuFqTOp',
+    'long_description': None,
+    'author': 'OwoNicoo',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
