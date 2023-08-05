e5f1aa05f5e4f89dd8549f366bb30ebdbcbbf844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:21 +0000

    Add package kraken-extract-from-html/0.0.5/kraken-extract-from-html-0.0.5.tar.gz

---
+++ packages/kraken-extract-from-html/kraken-extract-from-html-0.0.5.tar.gz/kraken-extract-from-html-0.0.5/kraken_extract_from_html/kraken_extract_from_html.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup
+
+from kraken_extract_from_html.extractors import extract_feeds
+from kraken_extract_from_html.extractors import extract_images
+from kraken_extract_from_html.extractors import extract_links
+from kraken_extract_from_html.extractors import extract_schemas
+from kraken_extract_from_html.extractors import extract_src
+from kraken_extract_from_html.extractors import extract_text
+from kraken_extract_from_html.extractors import extract_title
+
+
+
+def get(url, html):
+    return process_extraction(url, html)
+
+
+
+def process_extraction(url, html, image_urls = None):
+
+    records = []
+
+    
+    text = extract_text.get(url, html)
+    records+=text
+    
+    soup = _get_soup(html)
+
+
+    links = extract_links.get(url, soup)
+    records+=links
+
+
+    images = extract_images.get(url, soup)
+    records += images
+
+
+    feeds = extract_feeds.get(url, soup)
+    records += feeds
+
+    schemas = extract_schemas.get(url, html)
+    records += schemas
+
+    src = extract_src.get(url, soup)
+    records += src
+
+    texts = extract_text.get(url, html)
+    records += texts
+
+    titles = extract_title.get(url, soup)
+    records += titles
+
+
+    return records
+
+
+
+
+def _get_soup(html):
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    return soup
+
+
+
+
