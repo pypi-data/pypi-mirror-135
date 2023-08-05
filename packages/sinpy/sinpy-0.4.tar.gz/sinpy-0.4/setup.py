dc854396355f22fd9e1cd13088ecf8544d75c1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:29 +0000

    Add package sinpy/0.4/sinpy-0.4.tar.gz

---
+++ packages/sinpy/sinpy-0.4.tar.gz/sinpy-0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sinpy',
+    version='0.4',
+    license='MIT',
+    author="sinpy",
+    author_email='sinpy@example.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    keywords='',
+    install_requires=[
+          'sympy',
+          'numpy'
+      ],
+
+)
