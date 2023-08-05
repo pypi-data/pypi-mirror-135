d68537eb2d87f7457fc5268187eaac856587d1b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:15 +0000

    Add package rubrix/0.8.0/rubrix-0.8.0.tar.gz

---
+++ packages/rubrix/rubrix-0.8.0.tar.gz/rubrix-0.8.0/src/rubrix/server/static/_nuxt/ff8ae68.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[103],{1047:function(n,t,e){"use strict";e.r(t);var o={mounted:function(){this.$router.push("/ws")}},r=e(42),component=Object(r.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
