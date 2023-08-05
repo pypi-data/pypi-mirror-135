61f5cd48848b1c04507b959c77ff14e3ca8e26a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:10 +0000

    Add package pyfort/0.0.3/pyfort-0.0.3.tar.gz

---
+++ packages/pyfort/pyfort-0.0.3.tar.gz/pyfort-0.0.3/pyfort/pyfort.py
@@ -0,0 +1,26 @@
+from pyfort.menu import *
+
+
+def main():
+    try:
+        print("""
+        `*kMdMkr`        
+       l@@#MeMB@@c       
+      l@@L     x@@V      
+      E@@       #@0      
+    `vB@@xxxxxxx@@Bv`      __         _
+    V@@@@@@3}K@@@@@@y     / _|___ _ _| |_
+    V@@@@@B` `Q@@@@@w    |  _/ _ \ '_|  _| 
+    V@@@@@@] x@@@@@@w    |_| \___/_|  \__|  Password Manager
+    V@@@@@#_--#@@@@@w    
+    !dRRRRRRRRRRRRRd!""")
+        
+        print(colored( """
+Program Created By a-tharva
+Link to Original: github.com/a-tharva/
+Info: - This is written in Python
+      - Still in development""", 'red'))
+        show_menu()
+        
+    except KeyboardInterrupt:
+        print('\nExiting...')
