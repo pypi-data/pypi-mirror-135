fd44a466fe982333117eda4b26e199ef33307dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:15 +0000

    Add package coldtype/0.9.0/coldtype-0.9.0.tar.gz

---
+++ packages/coldtype/coldtype-0.9.0.tar.gz/coldtype-0.9.0/coldtype/interpolation/__init__.py
@@ -0,0 +1,18 @@
+
+def norm(value, start, stop):
+    return start + (stop-start) * value
+
+def lerp(start, stop, amt):
+    return float(amt-start) / float(stop-start)
+
+def interp_dict(v, a, b=None):
+    if b is None:
+        a, b = a[0], a[1]
+    
+    out = dict()
+    for k, _v in a.items():
+        out[k] = norm(v, a[k], b[k])
+    return out
+
+def loopidx(lst, idx):
+    return lst[idx % len(lst)]
