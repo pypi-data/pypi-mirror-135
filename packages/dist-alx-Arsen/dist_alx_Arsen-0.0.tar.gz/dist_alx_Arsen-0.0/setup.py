68e1baf6d600a49a15629fa6f055deafcd8ffcf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:24 +0000

    Add package dist-alx-Arsen/0.0/dist_alx_Arsen-0.0.tar.gz

---
+++ packages/dist-alx-Arsen/dist_alx_Arsen-0.0.tar.gz/dist_alx_Arsen-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dist_alx_Arsen',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_alx_Arsen'],
+      author_email='rasen13@mail.ru',
+      zip_safe=False)
