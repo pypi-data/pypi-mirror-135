df6d54efe59a3d43394d24e61cb27735deebf1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:38 +0000

    Add package mtpiclick/0.0.1/mtpiclick-0.0.1.tar.gz

---
+++ packages/mtpiclick/mtpiclick-0.0.1.tar.gz/mtpiclick-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    author="Patrick Toohey",
+    description="HMI automation tools.",
+    name="mtpiclick",
+    version="0.0.1",
+    packages=find_packages(include=["mtpiclick","mtpiclick.*"]),
+    install_requires=['pyautogui'],
+    python_requires='>=3.7',
+)
