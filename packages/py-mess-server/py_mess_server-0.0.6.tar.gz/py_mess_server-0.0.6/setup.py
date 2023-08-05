8be016b5935d53f44a0b097c4a3db19bdd42e8ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:36 +0000

    Add package py-mess-server/0.0.6/py_mess_server-0.0.6.tar.gz

---
+++ packages/py-mess-server/py_mess_server-0.0.6.tar.gz/py_mess_server-0.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="py_mess_server",
+      version="0.0.6",
+      description="Mess Client",
+      author="Ivan Ivanov",
+      author_email="iv.iv@yandex.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex'],
+      scripts=['server/server.py']
+      )
