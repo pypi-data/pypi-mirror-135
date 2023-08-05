186317f1219a2bc3926f1362f7ab7b9ec4b34943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:07 +0000

    Add package straxen/1.2.6/straxen-1.2.6.tar.gz

---
+++ packages/straxen/straxen-1.2.6.tar.gz/straxen-1.2.6/straxen/__init__.py
@@ -0,0 +1,37 @@
+__version__ = '1.2.6'
+
+from utilix import uconfig
+from .common import *
+# contexts.py below
+from .corrections_services import *
+from .get_corrections import *
+from .hitfinder_thresholds import *
+from .itp_map import *
+from .matplotlib_utils import *
+from .mini_analysis import *
+from .misc import *
+from .mongo_storage import *
+from .online_monitor import *
+from .rundb import *
+from .scada import *
+from .bokeh_utils import *
+from .rucio import *
+from .url_config import *
+
+from . import plugins
+from .plugins import *
+
+from . import analyses
+
+# Do not make all contexts directly available under straxen.
+# Otherwise we have straxen.demo() etc.
+from . import contexts
+
+from . import test_utils
+from .test_utils import *
+
+try:
+    from . import holoviews_utils
+    from .holoviews_utils import *
+except ModuleNotFoundError:
+    pass
