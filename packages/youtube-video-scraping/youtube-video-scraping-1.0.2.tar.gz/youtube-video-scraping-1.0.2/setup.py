7e5934e114e9d4db4feeb468f800f04c002441cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:39 +0000

    Add package youtube-video-scraping/1.0.2/youtube-video-scraping-1.0.2.tar.gz

---
+++ packages/youtube-video-scraping/youtube-video-scraping-1.0.2.tar.gz/youtube-video-scraping-1.0.2/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+VERSION = '1.0.2'
+PACKAGE_NAME = 'youtube-video-scraping'
+AUTHOR = 'DataKund'
+AUTHOR_EMAIL = 'datakund@gmail.com'
+URL = 'https://youtube-api.datakund.com/en/latest/'
+KEYWORDS='youtube python bot_studio video info scraper data web-scraping'
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'A python library to scrape video data from youtube automatically.'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'requests','bot-studio'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages(),
+      url=URL,
+      keywords = KEYWORDS
+      )
