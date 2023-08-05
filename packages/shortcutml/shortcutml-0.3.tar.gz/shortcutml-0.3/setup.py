4aeb365cc2bf39b1900f927f62685a6d8b85825c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:54 +0000

    Add package shortcutml/0.3/shortcutml-0.3.tar.gz

---
+++ packages/shortcutml/shortcutml-0.3.tar.gz/shortcutml-0.3/setup.py
@@ -0,0 +1,46 @@
+import pathlib
+from setuptools import setup
+from distutils.core import setup
+
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='shortcutml',
+    packages=['shortcutml'],
+    version='0.3',
+
+    license='MIT',
+    # Give a short description about your library
+    description='Machine learning baseline prototyping tools',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Sulthan Abiyyu Hakim',
+    author_email='sabiyyuhakim@student.ub.ac.id',
+    url='https://github.com/SulthanAbiyyu/ShortcutML',
+
+    download_url='https://github.com/SulthanAbiyyu/ShortcutML/archive/refs/tags/0.3.tar.gz',
+
+    keywords=['machine learning', 'summary'],
+    install_requires=[
+        "joblib",
+        "lightgbm",
+        "matplotlib",
+        "nltk",
+        "numpy",
+        "pandas",
+        "PySastrawi",
+        "scikit-learn",
+        "seaborn",
+        "xgboost"
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
