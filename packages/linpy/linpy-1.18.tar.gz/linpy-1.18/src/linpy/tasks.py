85c17347c34322643b9f0e2a16807fa8d5247ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:53 +0000

    Add package linpy/1.18/linpy-1.18.tar.gz

---
+++ packages/linpy/linpy-1.18.tar.gz/linpy-1.18/src/linpy/tasks.py
@@ -0,0 +1,13 @@
+from IPython.display import IFrame
+
+# IFrame = None
+
+# def init_opening(IFrame_from_jupyter):
+#     global IFrame
+#     IFrame = IFrame_tmp
+
+def open(task):
+    """
+    Например open(10)
+    """
+    return IFrame(f'./data/2.pdf', width=600, height=300)
