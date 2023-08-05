1ff9f38f73f495c50c665dc2d726a26a9c1835f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:05 +0000

    Add package access-outlook-email/0.1.3/access_outlook_email-0.1.3-py3-none-any.whl

---
+++ packages/access-outlook-email/access_outlook_email-0.1.3-py3-none-any.whl/accessOutlookEmail/use_cases/saveSteubing.py
@@ -0,0 +1,15 @@
+"""
+Created on 10.05.2021
+
+@author: baier
+"""
+from accessOutlookEmail import create_account, save_attachment
+
+
+def main():
+    account = create_account('handel@orcacapital.de', 'Jar54Cet$cosh45')
+    save_attachment('Steubing', r'W:\01. ORCA Depot\Z InputConfirms\Steubing', account)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/access-outlook-email/access_outlook_email-0.1.3-py3-none-any.whl/accessOutlookEmail/use_cases/test.py
@@ -0,0 +1,5 @@
+from accessOutlookEmail import create_account
+
+robert = create_account('handel@orcacapital.de', 'Jar54Cet$cosh45')
+
+print(robert)
