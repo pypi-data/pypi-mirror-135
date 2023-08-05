6f0e4a8e0ca41bacd3319670903edcb76143f85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:26 +0000

    Add package mtpiclick/0.0.6/mtpiclick-0.0.6-py3-none-any.whl

---
+++ packages/mtpiclick/mtpiclick-0.0.6-py3-none-any.whl/mtpiclick/cseries/xga/coordinates.py
@@ -0,0 +1,20 @@
+from mtpiclick.point import point
+
+bottom = 725
+left = 108
+top = 41
+submenu = 307
+subsubmenu = 510
+
+product = point(left, top)
+home = point(141, bottom)
+login = point(865, top)
+menu = point(225, bottom)
+start = point(52, bottom)
+messages = point(512, bottom)
+login_password = point(622, 323)
+key_3 = point(244, 347)
+key_ok = point(927, 466)
+login_ok = point(389, 424)
+capture = point(807,864)
+frame = point(760, 0)
---
+++ packages/mtpiclick/mtpiclick-0.0.6-py3-none-any.whl/mtpiclick/cseries/xga/hmi.py
@@ -0,0 +1,53 @@
+import time
+import pyautogui as gui
+from .coordinates import home, login, login_password, key_3, key_ok, login_ok, menu, capture, frame
+from mtpiclick.click import moveClick
+
+def MoveHome():
+    gui.moveTo(home.x, home.y, 1)
+
+def Home():
+    moveClick(home)
+
+def Login():
+    moveClick(login)
+
+def Logout():
+    moveClick(login)
+
+def Login_Password():
+    moveClick(login_password)
+
+def Keyboard_3():
+    moveClick(key_3)
+
+def Keyboard_OK():
+    moveClick(key_ok)
+
+def Login_OK():
+    moveClick(login_ok)
+
+def Menu():
+    moveClick(menu)
+
+def RecordStart():
+    moveClick(capture)
+    time.sleep(2.5)
+    moveClick(frame)
+    Home()
+    gui.keyDown('shift')
+    gui.press('f9')
+    gui.keyUp('shift')
+    time.sleep(5)
+
+def RecordStop():
+    Home()
+    time.sleep(2)
+    gui.keyDown('shift')
+    gui.press('f9')
+    gui.keyUp('shift')
+
+def Record(navigation):
+    RecordStart()
+    navigation()
+    RecordStop()
