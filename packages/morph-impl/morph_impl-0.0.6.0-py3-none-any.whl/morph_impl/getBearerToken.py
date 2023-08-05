0a05db79122b2a3b89599eb422af5a600310f67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:09 +0000

    Add package morph-impl/0.0.6.0/morph_impl-0.0.6.0-py3-none-any.whl

---
+++ packages/morph-impl/morph_impl-0.0.6.0-py3-none-any.whl/morph_impl/getBearerToken.py
@@ -0,0 +1,13 @@
+import json
+import requests
+import os
+from . import morph_log
+
+def bearerToken(baseURL, ADMIN_USERNAME, ADMIN_PASSWORD):
+  logger = morph_log.get_logger('getg')
+  url = baseURL+"/oauth/token?grant_type=password&scope=write&client_id=morph-api"
+  payload={'username': ADMIN_USERNAME,'password': ADMIN_PASSWORD}
+  result = requests.request("POST", url, verify=False, data=payload)
+  jsonDump = json.loads(result.text)
+  logger.debug(jsonDump["access_token"])
+  return jsonDump["access_token"]  
---
+++ packages/morph-impl/morph_impl-0.0.6.0-py3-none-any.whl/morph_impl/morph_log.py
@@ -0,0 +1,31 @@
+import logging
+import sys
+from logging.handlers import TimedRotatingFileHandler
+FORMATTER = logging.Formatter('%(asctime)s:%(lineno)d:%(module)s:%(funcName)s:%(name)s:%(levelname)s:%(message)s')
+LOG_FILE = "morph_impl.log"
+
+def get_console_handler():
+   """
+   Logging console handler
+   """
+   console_handler = logging.StreamHandler(sys.stdout)
+   console_handler.setFormatter(FORMATTER)
+   return console_handler
+def get_file_handler():
+   """
+   Logging file handler
+   """
+   file_handler = TimedRotatingFileHandler(LOG_FILE, when='midnight')
+   file_handler.setFormatter(FORMATTER)
+   return file_handler
+def get_logger(logger_name):
+   """
+   Create the logger - call this function from other scripts. 
+   """
+   logger = logging.getLogger(logger_name)
+   logger.setLevel(logging.INFO) # better to have too much log than not enough
+   logger.addHandler(get_console_handler())
+   logger.addHandler(get_file_handler())
+   # with this pattern, it's rarely necessary to propagate the error up to parent
+   logger.propagate = False
+   return logger
