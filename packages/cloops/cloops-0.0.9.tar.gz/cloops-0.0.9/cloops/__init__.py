144280b5c6893b5e9bfab9660eabc778baf01a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:08 +0000

    Add package cloops/0.0.9/cloops-0.0.9.tar.gz

---
+++ packages/cloops/cloops-0.0.9.tar.gz/cloops-0.0.9/cloops/__init__.py
@@ -0,0 +1,22 @@
+_version_ = '0.1.0'
+import random
+def whp(what_do_you_want_to_print):
+  while True:
+    print(what_do_you_want_to_print)
+def fop(how_many_times, what_do_you_want_to_print):
+  for i in range(how_many_times):
+    print(what_do_you_want_to_print)
+def foc(range_):
+  for i in range(range_):
+    print(i)
+def fori(what_do_you_want_to_print_random_in_range_0_100):
+  a = random.randint(0, 100)
+  for i in range(a):
+    print(what_do_you_want_to_print_random_in_range_0_100)
+def wht():
+    while True:
+        True
+def cal(put_math_problem):
+    while True:
+        z = eval(put_math_problem)
+        print(z)
