14ef8f5c8b021eeb98c18708d975778c1018c1a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:26 +0000

    Add package pyMelt/1.95/pyMelt-1.95-py3-none-any.whl

---
+++ packages/pyMelt/pyMelt-1.95-py3-none-any.whl/pyMelt/lithologies/__init__.py
@@ -0,0 +1,17 @@
+"""
+===========
+Lithologies
+===========
+
+The lithologies module provides lithology objects representing different bulk compositions, with
+models published by a number of authors.
+
+"""
+
+from pyMelt.lithologies import matthews
+from pyMelt.lithologies import katz
+from pyMelt.lithologies import pertermann
+from pyMelt.lithologies import shorttle
+from pyMelt.lithologies import mckenzie
+
+__all__ = ['matthews', 'katz', 'pertermann', 'shorttle', 'mckenzie']
