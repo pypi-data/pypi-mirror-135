cea878d31f8dc7e7e9a830d4cea1155d55d8cb4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:24 +0000

    Add package quickspider/0.1.2.1/quickspider-0.1.2.1.tar.gz

---
+++ packages/quickspider/quickspider-0.1.2.1.tar.gz/quickspider-0.1.2.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+with open("./README.md", "rt") as f:
+    long_description = f.read()
+
+setup(
+        name='quickspider',
+        version='0.1.2.1',
+        description='help you build a small spider quickly.',
+        long_description="see github.",
+        author='Ivory',
+        author_email='1070642565@qq.com',
+        url='https://github.com/tiwe0/quickspider.git',
+        packages=['quickspider', 'quickspider.core', 'quickspider.template'],
+        package_dir={'quickspider': 'quickspider'},
+        package_data={'quickspider': ['template/*.toml']},
+        scripts=['quickspider/bin/quickspidercommand']
+        )
