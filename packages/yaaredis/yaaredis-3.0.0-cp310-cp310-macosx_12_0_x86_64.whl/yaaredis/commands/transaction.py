8f15cfb937ad3b37bf3b24692bfd31043da53e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:02 +0000

    Add package yaaredis/3.0.0/yaaredis-3.0.0-cp310-cp310-macosx_12_0_x86_64.whl

---
+++ packages/yaaredis/yaaredis-3.0.0-cp310-cp310-macosx_12_0_x86_64.whl/yaaredis/commands/transaction.py
@@ -0,0 +1,61 @@
+import asyncio
+
+from ..exceptions import WatchError
+from ..utils import bool_ok
+from ..utils import string_keys_to_dict
+
+
+class TransactionCommandMixin:
+
+    RESPONSE_CALLBACKS = string_keys_to_dict(
+        'WATCH UNWATCH',
+        bool_ok,
+    )
+
+    async def transaction(self, func, *watches, **kwargs):
+        """
+        Convenience method for executing the callable `func` as a transaction
+        while watching all keys specified in `watches`. The 'func' callable
+        should expect a single argument which is a Pipeline object.
+        """
+        shard_hint = kwargs.pop('shard_hint', None)
+        value_from_callable = kwargs.pop('value_from_callable', False)
+        watch_delay = kwargs.pop('watch_delay', None)
+        async with await self.pipeline(True, shard_hint) as pipe:
+            while True:
+                try:
+                    if watches:
+                        await pipe.watch(*watches)
+                    func_value = await func(pipe)
+                    exec_value = await pipe.execute()
+                    return func_value if value_from_callable else exec_value
+                except WatchError:
+                    if watch_delay is not None and watch_delay > 0:
+                        await asyncio.sleep(watch_delay)
+                    continue
+
+
+class ClusterTransactionCommandMixin(TransactionCommandMixin):
+
+    async def transaction(self, func, *watches, **kwargs):
+        """
+        Convenience method for executing the callable `func` as a transaction
+        while watching all keys specified in `watches`. The 'func' callable
+        should expect a single argument which is a Pipeline object.
+
+        cluster transaction can only be run with commands in the same node,
+        otherwise error will be raised.
+        """
+        shard_hint = kwargs.pop('shard_hint', None)
+        value_from_callable = kwargs.pop('value_from_callable', False)
+        watch_delay = kwargs.pop('watch_delay', None)
+        async with await self.pipeline(True, shard_hint, watches=watches) as pipe:
+            while True:
+                try:
+                    func_value = await func(pipe)
+                    exec_value = await pipe.execute()
+                    return func_value if value_from_callable else exec_value
+                except WatchError:
+                    if watch_delay is not None and watch_delay > 0:
+                        await asyncio.sleep(watch_delay)
+                    continue
---
+++ packages/yaaredis/yaaredis-3.0.0-cp310-cp310-macosx_12_0_x86_64.whl/yaaredis/exceptions.py
@@ -0,0 +1,142 @@
+class RedisError(Exception):
+    pass
+
+
+class AuthenticationFailureError(RedisError):
+    pass
+
+
+class AuthenticationRequiredError(RedisError):
+    pass
+
+
+class NoPermissionError(RedisError):
+    pass
+
+
+class ConnectionError(RedisError):  # pylint: disable=redefined-builtin
+    pass
+
+
+class TimeoutError(RedisError):  # pylint: disable=redefined-builtin
+    pass
+
+
+class BusyLoadingError(ConnectionError):
+    pass
+
+
+class InvalidResponse(RedisError):
+    pass
+
+
+class ResponseError(RedisError):
+    pass
+
+
+class DataError(RedisError):
+    pass
+
+
+class PubSubError(RedisError):
+    pass
+
+
+class WatchError(RedisError):
+    pass
+
+
+class NoScriptError(ResponseError):
+    pass
+
+
+class ExecAbortError(ResponseError):
+    pass
+
+
+class ReadOnlyError(ResponseError):
+    pass
+
+
+class LockError(RedisError, ValueError):
+    """Errors acquiring or releasing a lock"""
+    # NOTE: For backwards compatability, this class derives from ValueError.
+    # This was originally chosen to behave like threading.Lock.
+
+
+class CacheError(RedisError):
+    """Basic error of yaaredis.cache"""
+
+
+class SerializeError(CacheError):
+    pass
+
+
+class CompressError(CacheError):
+    pass
+
+
+class RedisClusterException(Exception):
+    pass
+
+
+class RedisClusterError(Exception):
+    pass
+
+
+class ClusterUnreachableError(Exception):
+    pass
+
+
+class ClusterDownException(Exception):
+    pass
+
+
+class ClusterError(RedisError):
+    pass
+
+
+class ClusterCrossSlotError(ResponseError):
+    message = "Keys in request don't hash to the same slot"
+
+
+class ClusterDownError(ClusterError, ResponseError):
+    def __init__(self, resp):
+        super().__init__(resp)
+        self.args = (resp,)
+        self.message = resp
+
+
+class ClusterTransactionError(ClusterError):
+    def __init__(self, msg):
+        super().__init__(msg)
+        self.msg = msg
+
+
+class AskError(ResponseError):
+    """
+    src node: MIGRATING to dst node
+        get > ASK error
+        ask dst node > ASKING command
+    dst node: IMPORTING from src node
+        asking command only affects next command
+        any op will be allowed after asking command
+    """
+
+    def __init__(self, resp):
+        """should only redirect to master node"""
+        super().__init__(resp)
+        self.args = (resp,)
+        self.message = resp
+        slot_id, new_node = resp.split(' ')
+        host, port = new_node.rsplit(':', 1)
+        self.slot_id = int(slot_id)
+        self.node_addr = self.host, self.port = host, int(port)
+
+
+class TryAgainError(ResponseError):
+    pass
+
+
+class MovedError(AskError):
+    pass
