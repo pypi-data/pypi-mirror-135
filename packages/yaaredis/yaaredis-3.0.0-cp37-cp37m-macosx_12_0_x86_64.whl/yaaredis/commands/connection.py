c57da00e843ef3c7b44e0adced323d64919b35fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:02 +0000

    Add package yaaredis/3.0.0/yaaredis-3.0.0-cp37-cp37m-macosx_12_0_x86_64.whl

---
+++ packages/yaaredis/yaaredis-3.0.0-cp37-cp37m-macosx_12_0_x86_64.whl/yaaredis/commands/connection.py
@@ -0,0 +1,33 @@
+from ..utils import bool_ok
+from ..utils import nativestr
+from ..utils import NodeFlag
+
+
+class ConnectionCommandMixin:
+
+    RESPONSE_CALLBACKS = {
+        'AUTH': bool,
+        'PING': lambda r: nativestr(r) == 'PONG',
+        'SELECT': bool_ok,
+    }
+
+    async def echo(self, value):
+        'Echo the string back from the server'
+        return await self.execute_command('ECHO', value)
+
+    async def ping(self):
+        'Ping the Redis server'
+        return await self.execute_command('PING')
+
+
+class ClusterConnectionCommandMixin(ConnectionCommandMixin):
+
+    NODES_FLAGS = {
+        'PING': NodeFlag.ALL_NODES,
+        'ECHO': NodeFlag.ALL_NODES,
+    }
+
+    RESULT_CALLBACKS = {
+        'ECHO': lambda res: res,
+        'PING': lambda res: res,
+    }
