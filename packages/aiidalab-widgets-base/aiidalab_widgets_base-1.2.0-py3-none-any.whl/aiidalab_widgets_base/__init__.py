ad042cd4c852f1715ae7ea17acecb103402ec708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:40 +0000

    Add package aiidalab-widgets-base/1.2.0/aiidalab_widgets_base-1.2.0-py3-none-any.whl

---
+++ packages/aiidalab-widgets-base/aiidalab_widgets_base-1.2.0-py3-none-any.whl/aiidalab_widgets_base/__init__.py
@@ -0,0 +1,82 @@
+"""Reusable widgets for AiiDAlab applications."""
+from aiida import load_profile
+
+load_profile()
+
+from .codes import AiiDACodeSetup, CodeDropdown
+from .computers import AiidaComputerSetup, ComputerDropdown, SshComputerSetup
+from .databases import (
+    CodeDatabaseWidget,
+    CodQueryWidget,
+    ComputerDatabaseWidget,
+    OptimadeQueryWidget,
+)
+from .elns import ElnConfigureWidget, ElnExportWidget, ElnImportWidget
+from .export import ExportButtonWidget
+from .nodes import NodesTreeWidget, OpenAiidaNodeInAppWidget
+from .process import (
+    ProcessCallStackWidget,
+    ProcessFollowerWidget,
+    ProcessInputsWidget,
+    ProcessListWidget,
+    ProcessMonitor,
+    ProcessNodesTreeWidget,
+    ProcessOutputsWidget,
+    ProcessReportWidget,
+    ProgressBarWidget,
+    RunningCalcJobOutputWidget,
+    SubmitButtonWidget,
+)
+from .structures import (
+    BasicStructureEditor,
+    SmilesWidget,
+    StructureBrowserWidget,
+    StructureExamplesWidget,
+    StructureManagerWidget,
+    StructureUploadWidget,
+)
+from .viewers import AiidaNodeViewWidget, register_viewer_widget, viewer
+from .wizard import WizardAppWidget, WizardAppWidgetStep
+
+__all__ = [
+    "AiiDACodeSetup",
+    "AiidaComputerSetup",
+    "AiidaNodeViewWidget",
+    "BasicStructureEditor",
+    "CodQueryWidget",
+    "CodeDatabaseWidget",
+    "CodeDropdown",
+    "ComputerDatabaseWidget",
+    "ComputerDropdown",
+    "ElnConfigureWidget",
+    "ElnExportWidget",
+    "ElnImportWidget",
+    "ExportButtonWidget",
+    "MultiStructureUploadWidget",
+    "NodesTreeWidget",
+    "OpenAiidaNodeInAppWidget",
+    "OptimadeQueryWidget",
+    "ProcessCallStackWidget",
+    "ProcessFollowerWidget",
+    "ProcessInputsWidget",
+    "ProcessListWidget",
+    "ProcessMonitor",
+    "ProcessNodesTreeWidget",
+    "ProcessOutputsWidget",
+    "ProcessReportWidget",
+    "ProgressBarWidget",
+    "RunningCalcJobOutputWidget",
+    "SmilesWidget",
+    "SshComputerSetup",
+    "StructureBrowserWidget",
+    "StructureExamplesWidget",
+    "StructureManagerWidget",
+    "StructureUploadWidget",
+    "SubmitButtonWidget",
+    "WizardAppWidget",
+    "WizardAppWidgetStep",
+    "register_viewer_widget",
+    "viewer",
+]
+
+__version__ = "1.2.0"
