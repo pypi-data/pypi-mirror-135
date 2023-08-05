e0cfb7154f9915675654c36cce8a159ecfdffae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:54 +0000

    Add package rdpc/0.0.0/rdpc-0.0.0.tar.gz

---
+++ packages/rdpc/rdpc-0.0.0.tar.gz/rdpc-0.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="rdpc",
+    version="0.0.0",
+    description="Data Producer and consumer Library for Kafka.",
+    packages=setuptools.find_packages("src"),
+    package_dir={'' : 'src'},
+    author="Yulong",
+    author_email="yulong.wang@rakuten.com",
+    licenses="",
+    install_requires=['']
+)
