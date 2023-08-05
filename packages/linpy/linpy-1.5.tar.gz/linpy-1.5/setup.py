c15fd3e2bab610b620d7817827150ee8e4b545a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:33 +0000

    Add package linpy/1.5/linpy-1.5.tar.gz

---
+++ packages/linpy/linpy-1.5.tar.gz/linpy-1.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='linpy',
+    version='1.05',
+    license='MIT',
+    author="linpy",
+    author_email='linpy@example.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    keywords='',
+    install_requires=[
+          'sympy',
+          'numpy'
+      ],
+
+)
