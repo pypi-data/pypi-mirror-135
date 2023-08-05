4e78dedaa0b0ce733ee0a4044770768e740c2bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:26 +0000

    Add package mandown/0.3.1/mandown-0.3.1.tar.gz

---
+++ packages/mandown/mandown-0.3.1.tar.gz/mandown-0.3.1/mandown/mandown.py
@@ -0,0 +1,48 @@
+import os
+from typing import Iterable
+
+from mandown import iohandler, sources
+from mandown.sources.base_source import BaseSource, Chapter
+
+
+def query(url: str, populate: bool = False) -> BaseSource:
+    """
+    Return the source file for a URL.
+    """
+    source = sources.get_class_for(url)(url)
+    if populate:
+        # these statements are to trigger their getters to
+        # fetch the data
+        if source.metadata:
+            pass
+        if source.chapters:
+            pass
+    return source
+
+
+def download_chapter(
+    chapter: Chapter, dest_folder: str, maxthreads: int = 1
+) -> Iterable[None]:
+    """
+    Download the images of a chapter to a destination folder.
+    Returns an iterable that increments whenever an item has finished
+    downloading.
+    Raises ValueError if the folder does not exist.
+    """
+    if not chapter.images:
+        raise ValueError("No images available to download")
+
+    if not os.path.isdir(dest_folder):
+        raise ValueError(f"Folder path {dest_folder} does not exist")
+
+    download_folder = os.path.join(dest_folder, chapter.title)
+    if not os.path.isdir(download_folder):
+        os.mkdir(download_folder)
+
+    for i in iohandler.download(
+        chapter.images,
+        os.path.join(dest_folder, chapter.title),
+        chapter.headers,
+        maxthreads,
+    ):
+        yield i
