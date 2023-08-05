a78eb577366435fff15daf8d3fdc51ba1169679a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:02 +0000

    Add package ml-init/0.5.2/ml_init-0.5.2-py3-none-any.whl

---
+++ packages/ml-init/ml_init-0.5.2-py3-none-any.whl/ml_init/__init__.py
@@ -0,0 +1,12 @@
+'''
+Author: Alberto Rubiales Borrego
+Mail: al.rubiales.b@gmail.com
+Github: arubiales
+Created Date: 16-Jan-2022
+-----
+Last Modified: 17-Jan-2022
+Modified By: Alberto Rubiales Borrego
+-----
+'''
+
+__version__ = '0.5.2'
