1f1c0e43e0f42a86d05b1daa1029a67887095eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:43 +0000

    Add package itk-io/5.3rc3/itk_io-5.3rc3-cp310-cp310-win_amd64.whl

---
+++ packages/itk-io/itk_io-5.3rc3-cp310-cp310-win_amd64.whl/itk/Configuration/ITKIOBrukerConfig.py
@@ -0,0 +1,4 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (  ('Bruker2dseqImageIO', 'itk::Bruker2dseqImageIO', 'itkBruker2dseqImageIO', True),
+  ('Bruker2dseqImageIOFactory', 'itk::Bruker2dseqImageIOFactory', 'itkBruker2dseqImageIOFactory', True),
+)
