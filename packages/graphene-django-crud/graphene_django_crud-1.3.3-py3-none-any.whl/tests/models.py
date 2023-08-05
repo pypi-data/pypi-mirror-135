fb61fb249e00bbc4ea1ba347c32f855b095df232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:52 +0000

    Add package graphene-django-crud/1.3.3/graphene_django_crud-1.3.3-py3-none-any.whl

---
+++ packages/graphene-django-crud/graphene_django_crud-1.3.3-py3-none-any.whl/tests/models.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from .relationship.models import *
+from .generate_schema.models import *
+from .choices_converter.models import *
+from .relay.models import *
+from .connection.models import *
+from .file_field.models import *
