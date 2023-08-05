716ed1bc61f2a657f2d909b125e5329aefb70c6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:26 +0000

    Add package aoc-lib/1.1.3/aoc_lib-1.1.3-py3-none-any.whl

---
+++ packages/aoc-lib/aoc_lib-1.1.3-py3-none-any.whl/aoc/utils.py
@@ -0,0 +1,25 @@
+import re
+from itertools import chain
+
+
+def get_ints(s: str) -> [int]:
+    """
+    Get all int (including sign) values from the given string
+    """
+    return list(map(int, re.findall(r"((?:-|\+)?\d+)", s)))
+
+
+def fetch(iterable, n: int, fillvalue=None) -> [any]:
+    """
+    fetches n values from iterable - filled with fillvalue if needed;
+    great for destructuring data with different number of arguments etc
+    e.g.: a, b = fetch(args, 2) - fetches 2 elements from args and fills with None if needed
+    :param iterable: the iterable to fetch from
+    :param n: the number of elements to fetch; align that with your destructuring
+    :param fillvalue: optional fill value if less than n elements are available (None as default)
+    :return: list with the n elements; filled with fillvalue if needed
+    """
+    if len(iterable) >= n:
+        return iterable[0:n]
+    fill = [fillvalue] * (n - len(iterable))
+    return chain(iterable, fill)
