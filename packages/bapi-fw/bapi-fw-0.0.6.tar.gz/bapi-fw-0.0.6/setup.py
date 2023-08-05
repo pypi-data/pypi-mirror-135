f00fb8d07c6f81d9917864489180cafb844f7b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:50 +0000

    Add package bapi-fw/0.0.6/bapi-fw-0.0.6.tar.gz

---
+++ packages/bapi-fw/bapi-fw-0.0.6.tar.gz/bapi-fw-0.0.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.6'
+DESCRIPTION = ''
+LONG_DESCRIPTION = ''
+
+# Setting up
+setup(
+    name="bapi-fw",
+    version=VERSION,
+    author="Christophe Kafrouni",
+    author_email="<chris.kafrouni@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pydantic'],
+    keywords=['python', 'api', 'framework', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
