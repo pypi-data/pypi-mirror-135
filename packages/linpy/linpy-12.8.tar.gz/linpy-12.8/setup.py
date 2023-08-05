b234e2c1ebde23635afa0ebc364bc7353c6d5f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:46 +0000

    Add package linpy/12.8/linpy-12.8.tar.gz

---
+++ packages/linpy/linpy-12.8.tar.gz/linpy-12.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+# python setup.py sdist
+# pip install twine
+# twine upload dist/*
+
+
+setup(
+    name='linpy',
+    version='12.8',
+    license='MIT',
+    author="linpy",
+    author_email='linpy@example.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    keywords='',
+    install_requires=[
+
+      ],
+)
