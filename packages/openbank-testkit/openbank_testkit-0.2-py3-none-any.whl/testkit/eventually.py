9d3cb642d04a04007399dbc40d22df072da471e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:56 +0000

    Add package openbank-testkit/0.2/openbank_testkit-0.2-py3-none-any.whl

---
+++ packages/openbank-testkit/openbank_testkit-0.2-py3-none-any.whl/testkit/eventually.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+
+
+class eventually():
+
+  def __init__(self, timeout=2):
+    self.__inblock = False
+    self.__last_exception = None
+    self.__timeout = timeout
+    self.__block = lambda *args: None
+
+  def __get__(self, instance, *args):
+    return partial(self.__call__, instance)
+
+  def __call__(self, *args, **kwargs):
+    if not self.__inblock:
+      self.__block = args[0]
+      self.__inblock = True
+      return self
+
+    deadline = time.monotonic() + self.__timeout
+    while deadline > time.monotonic():
+      try:
+        return self.__block(*args, **kwargs)
+      except Exception as ex:
+        self.__last_exception = ex
+        time.sleep(0.5)
+
+    if self.__last_exception:
+      raise self.__last_exception
+    else:
+      raise AssertionError('timeout')
