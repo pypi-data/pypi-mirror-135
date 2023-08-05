51a364c555340d497c9c802408cd7b606e57f732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:12 +0000

    Add package datarobot-early-access/2.28.0.2022.1.17/datarobot_early_access-2.28.0.2022.1.17-py3-none-any.whl

---
+++ packages/datarobot-early-access/datarobot_early_access-2.28.0.2022.1.17-py3-none-any.whl/datarobot/_experimental/models/custom_training_model.py
@@ -0,0 +1,16 @@
+#
+# Copyright 2021 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+#
+# DataRobot, Inc.
+#
+# This is proprietary source code of DataRobot, Inc. and its
+# affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
+from datarobot.models.custom_model import _CustomModelBase
+
+
+class CustomTrainingModel(_CustomModelBase):
+    _model_type = "training"
