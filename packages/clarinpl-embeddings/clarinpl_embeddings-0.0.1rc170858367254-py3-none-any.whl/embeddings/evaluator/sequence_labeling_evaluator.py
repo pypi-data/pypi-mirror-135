33b07bb604e157a837213eb3c260a063172e670b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:10 +0000

    Add package clarinpl-embeddings/0.0.1rc170858367254/clarinpl_embeddings-0.0.1rc170858367254-py3-none-any.whl

---
+++ packages/clarinpl-embeddings/clarinpl_embeddings-0.0.1rc170858367254-py3-none-any.whl/embeddings/evaluator/sequence_labeling_evaluator.py
@@ -0,0 +1,70 @@
+from enum import Enum
+from typing import Any, Dict, Final, List, Optional, Sequence, Set, Union
+
+import torch
+from numpy import typing as nptyping
+
+from embeddings.evaluator.metrics_evaluator import MetricsEvaluator
+from embeddings.metric.hugging_face_metric import HuggingFaceMetric
+from embeddings.metric.metric import Metric
+from embeddings.metric.unit_seqeval_metric import UnitSeqevalMetric
+
+
+class SequenceLabelingEvaluator(MetricsEvaluator):
+    class EvaluationMode(str, Enum):
+        UNIT = "unit"
+        CONLL = "conll"
+        STRICT = "strict"
+
+    class TaggingScheme(str, Enum):
+        IOB1 = "IOB1"
+        IOB2 = "IOB2"
+        IOE1 = "IOE1"
+        IOE2 = "IOE2"
+        IOBES = "IOBES"
+        BILOU = "BILOU"
+
+    SEQEVAL_EVALUATION_MODES: Final[Set[str]] = {EvaluationMode.CONLL, EvaluationMode.STRICT}
+
+    def __init__(
+        self,
+        evaluation_mode: EvaluationMode = EvaluationMode.CONLL,
+        tagging_scheme: Optional[TaggingScheme] = None,
+    ) -> None:
+        super().__init__()
+        self.metric = self._get_metric(evaluation_mode, tagging_scheme)
+
+    def _get_metric(
+        self,
+        evaluation_mode: EvaluationMode,
+        tagging_scheme: Optional[TaggingScheme] = None,
+    ) -> Union[HuggingFaceMetric, UnitSeqevalMetric]:
+        if evaluation_mode in SequenceLabelingEvaluator.SEQEVAL_EVALUATION_MODES:
+            if evaluation_mode == "strict" and not tagging_scheme:
+                raise ValueError("Tagging scheme must be set, when using strict evaluation mode!")
+            elif evaluation_mode == "conll" and tagging_scheme:
+                raise ValueError("Tagging scheme can be set only in strict mode!")
+            return HuggingFaceMetric(
+                name="seqeval",
+                compute_kwargs={
+                    "mode": evaluation_mode if evaluation_mode == "strict" else None,
+                    "scheme": tagging_scheme,
+                },
+            )
+        elif evaluation_mode == "unit":
+            return UnitSeqevalMetric()
+        else:
+            raise ValueError(
+                f"Evaluation mode {evaluation_mode} not supported. Must be one of "
+                f"[unit, conll, strict]."
+            )
+
+    @property
+    def metrics(
+        self,
+    ) -> Sequence[Metric[Union[List[Any], nptyping.NDArray[Any], torch.Tensor], Dict[Any, Any]]]:
+        return [self.metric]
+
+
+EvaluationMode = SequenceLabelingEvaluator.EvaluationMode
+TaggingScheme = SequenceLabelingEvaluator.TaggingScheme
---
+++ packages/clarinpl-embeddings/clarinpl_embeddings-0.0.1rc170858367254-py3-none-any.whl/embeddings/pipeline/pipelines_metadata.py
@@ -0,0 +1,79 @@
+from typing import Any, Dict, Optional, Tuple, TypeVar
+
+from typing_extensions import Literal, TypedDict
+
+from embeddings.data.io import T_path
+
+
+class PathMetadata(TypedDict, total=False):
+    output_path: T_path
+
+
+class EmbeddingPipelineBaseMetadata(PathMetadata):
+    embedding_name: str
+    task_model_kwargs: Optional[Dict[str, Any]]
+    task_train_kwargs: Optional[Dict[str, Any]]
+
+
+class FlairEmbeddingPipelineMetadata(EmbeddingPipelineBaseMetadata):
+    dataset_name: str
+    load_dataset_kwargs: Optional[Dict[str, Any]]
+
+
+class FlairClassificationPipelineMetadata(FlairEmbeddingPipelineMetadata):
+    input_column_name: str
+    target_column_name: str
+    document_embedding_cls: str
+    load_model_kwargs: Optional[Dict[str, Any]]
+
+
+class FlairPairClassificationPipelineMetadata(FlairEmbeddingPipelineMetadata):
+    input_columns_names_pair: Tuple[str, str]
+    target_column_name: str
+    document_embedding_cls: str
+    load_model_kwargs: Optional[Dict[str, Any]]
+
+
+class FlairSequenceLabelingPipelineMetadata(FlairEmbeddingPipelineMetadata):
+    input_column_name: str
+    target_column_name: str
+    hidden_size: int
+    evaluation_mode: str
+    tagging_scheme: Optional[str]
+
+
+class FlairEvaluationPipelineMetadata(EmbeddingPipelineBaseMetadata):
+    dataset_path: str
+    persist_path: Optional[str]
+    predict_subset: Literal["dev", "test"]
+
+
+class FlairSequenceLabelingEvaluationPipelineMetadata(FlairEvaluationPipelineMetadata):
+    hidden_size: int
+    evaluation_mode: str
+    tagging_scheme: Optional[str]
+
+
+class FlairClassificationEvaluationPipelineMetadata(FlairEvaluationPipelineMetadata):
+    document_embedding_cls: str
+    load_model_kwargs: Optional[Dict[str, Any]]
+
+
+class LightningClassificationPipelineMetadata(EmbeddingPipelineBaseMetadata):
+    dataset_name_or_path: str
+    input_column_name: str
+    target_column_name: str
+    train_batch_size: int
+    eval_batch_size: int
+    finetune_last_n_layers: int
+    tokenizer_name: Optional[str]
+    load_dataset_kwargs: Optional[Dict[str, Any]]
+    datamodule_kwargs: Optional[Dict[str, Any]]
+    tokenizer_kwargs: Optional[Dict[str, Any]]
+    batch_encoding_kwargs: Optional[Dict[str, Any]]
+    model_config_kwargs: Optional[Dict[str, Any]]
+    predict_subset: Literal["dev", "test"]
+
+
+Metadata = TypeVar("Metadata", bound=EmbeddingPipelineBaseMetadata)
+EvaluationMetadata = TypeVar("EvaluationMetadata", bound=EmbeddingPipelineBaseMetadata)
