2c5861e2ca1a48d508887e2a8582706265c7ed96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:00 +0000

    Add package cloops/0.0.8/cloops-0.0.8.tar.gz

---
+++ packages/cloops/cloops-0.0.8.tar.gz/cloops-0.0.8/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cloops', 'cloops.tests']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'cloops',
+    'version': '0.0.8',
+    'description': 'sdasd',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
