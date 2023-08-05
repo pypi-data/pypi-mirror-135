ecf371bc7dc65ea012d2290659becece2927bcbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:49 +0000

    Add package ikologikapi/1.1.4/ikologikapi-1.1.4-py3-none-any.whl

---
+++ packages/ikologikapi/ikologikapi-1.1.4-py3-none-any.whl/ikologikapi/services/ReportService.py
@@ -0,0 +1,30 @@
+from types import SimpleNamespace
+import json
+import requests
+
+from ikologikapi.IkologikApiCredentials import IkologikApiCredentials
+from ikologikapi.services.AbstractIkologikInstallationService import AbstractIkologikInstallationService
+
+
+class ReportService(AbstractIkologikInstallationService):
+
+    def __init__(self, jwtHelper: IkologikApiCredentials):
+        super().__init__(jwtHelper)
+
+    # CRUD Actions
+
+    def get_url(self, customer: str, installation: str, reportType: str):
+        return f'{self.jwtHelper.get_url()}/api/v2/customer/{customer}/installation/{installation}/reporttype/{reportType}/report'
+
+    def create(self, customer: str, installation: str, reportType: str, o: object) -> object:
+        try:
+            data = json.dumps(o, default=lambda o: o.__dict__)
+            response = requests.post(
+                self.get_url(customer, installation),
+                data=data,
+                headers=self.get_headers()
+            )
+            result = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
+            return result
+        except requests.exceptions.HTTPError as error:
+            print(error)
