aa2378221b84dfcebec4bebb6264edc6c0c50c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:20 +0000

    Add package itk-core/5.3rc3/itk_core-5.3rc3-cp310-cp310-win_amd64.whl

---
+++ packages/itk-core/itk_core-5.3rc3-cp310-cp310-win_amd64.whl/itk/Configuration/ITKIOJPEGConfig.py
@@ -0,0 +1,4 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (  ('JPEGImageIO', 'itk::JPEGImageIO', 'itkJPEGImageIO', True),
+  ('JPEGImageIOFactory', 'itk::JPEGImageIOFactory', 'itkJPEGImageIOFactory', True),
+)
