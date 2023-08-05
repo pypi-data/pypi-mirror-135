98301bebac6bdf195b9d3e9961eeb226cb9b7d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:47 +0000

    Add package pystatacons/1.2.0/pystatacons-1.2.0.tar.gz

---
+++ packages/pystatacons/pystatacons-1.2.0.tar.gz/pystatacons-1.2.0/src/pystatacons/__init__.py
@@ -0,0 +1,2 @@
+__version__ = "1.2.0"
+from .sconstruct_aux import decider_str_lookup, dependency_newer_then_content_changed, changed_timestamp_then_dependency_newer_then_content_content, special_sig_fns, init_env
