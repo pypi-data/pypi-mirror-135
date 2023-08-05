232a580e7b11eee3caa3508a6249bf60e561f8cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:09 +0000

    Add package weconnect/0.33.0/weconnect-0.33.0.tar.gz

---
+++ packages/weconnect/weconnect-0.33.0.tar.gz/weconnect-0.33.0/weconnect/elements/plug_status.py
@@ -0,0 +1,83 @@
+from enum import Enum
+import logging
+
+from weconnect.addressable import AddressableAttribute
+from weconnect.elements.generic_status import GenericStatus
+
+LOG = logging.getLogger("weconnect")
+
+
+class PlugStatus(GenericStatus):
+    def __init__(
+        self,
+        vehicle,
+        parent,
+        statusId,
+        fromDict=None,
+        fixAPI=True,
+    ):
+        self.plugConnectionState = AddressableAttribute(
+            localAddress='plugConnectionState', parent=self, value=None, valueType=PlugStatus.PlugConnectionState)
+        self.plugLockState = AddressableAttribute(
+            localAddress='plugLockState', value=None, parent=self, valueType=PlugStatus.PlugLockState)
+        super().__init__(vehicle=vehicle, parent=parent, statusId=statusId, fromDict=fromDict, fixAPI=fixAPI)
+
+    def update(self, fromDict, ignoreAttributes=None):
+        ignoreAttributes = ignoreAttributes or []
+        LOG.debug('Update Plug status from dict')
+
+        if 'value' in fromDict:
+            if 'plugConnectionState' in fromDict['value'] and fromDict['value']['plugConnectionState']:
+                try:
+                    self.plugConnectionState.setValueWithCarTime(
+                        PlugStatus.PlugConnectionState(fromDict['value']['plugConnectionState']), lastUpdateFromCar=None,
+                        fromServer=True)
+                except ValueError:
+                    self.plugConnectionState.setValueWithCarTime(PlugStatus.PlugConnectionState.UNKNOWN,
+                                                                 lastUpdateFromCar=None, fromServer=True)
+                    LOG.warning('An unsupported plugConnectionState: %s was provided,'
+                                ' please report this as a bug', fromDict['value']['plugConnectionState'])
+            else:
+                self.plugConnectionState.enabled = False
+
+            if 'plugLockState' in fromDict['value'] and fromDict['value']['plugLockState']:
+                try:
+                    self.plugLockState.setValueWithCarTime(PlugStatus.PlugLockState(fromDict['value']['plugLockState']),
+                                                           lastUpdateFromCar=None, fromServer=True)
+                except ValueError:
+                    self.plugLockState.setValueWithCarTime(PlugStatus.PlugLockState.UNKNOWN,
+                                                           lastUpdateFromCar=None, fromServer=True)
+                    LOG.warning('An unsupported plugLockState: %s was provided,'
+                                ' please report this as a bug', fromDict['value']['plugLockState'])
+            else:
+                self.plugLockState.enabled = False
+
+        else:
+            self.plugConnectionState.enabled = False
+            self.plugLockState.enabled = False
+
+        super().update(fromDict=fromDict, ignoreAttributes=(
+            ignoreAttributes + ['plugConnectionState', 'plugLockState']))
+
+    def __str__(self):
+        string = super().__str__()
+        string += '\n\tPlug:'
+        if self.plugConnectionState.enabled:
+            string += f' {self.plugConnectionState.value.value}, '  # pylint: disable=no-member
+        if self.plugLockState.enabled:
+            string += f'{self.plugLockState.value.value}'  # pylint: disable=no-member
+        return string
+
+    class PlugConnectionState(Enum,):
+        CONNECTED = 'connected'
+        DISCONNECTED = 'disconnected'
+        INVALID = 'invalid'
+        UNSUPPORTED = 'unsupported'
+        UNKNOWN = 'unknown unlock plug state'
+
+    class PlugLockState(Enum,):
+        LOCKED = 'locked'
+        UNLOCKED = 'unlocked'
+        INVALID = 'invalid'
+        UNSUPPORTED = 'unsupported'
+        UNKNOWN = 'unknown unlock plug state'
