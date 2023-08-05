4146e612db782a2c8cb5be79505c6cbd6d640526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:25 +0000

    Add package mindsdb-sql/0.0.44/mindsdb_sql-0.0.44-py3-none-any.whl

---
+++ packages/mindsdb-sql/mindsdb_sql-0.0.44-py3-none-any.whl/mindsdb_sql/utils.py
@@ -0,0 +1,59 @@
+from mindsdb_sql.exceptions import ParsingException
+
+
+def indent(level):
+    return '  ' * level
+
+
+def ensure_select_keyword_order(select, operation):
+    op_to_attr = {
+        'FROM': select.from_table,
+        'WHERE': select.where,
+        'GROUP BY': select.group_by,
+        'HAVING': select.having,
+        'ORDER BY': select.order_by,
+        'LIMIT': select.limit,
+        'OFFSET': select.offset,
+        'MODE': select.mode,
+    }
+
+    requirements = {
+        'WHERE': ['FROM'],
+        'GROUP BY': ['FROM'],
+        'ORDER BY': ['FROM'],
+        'HAVING': ['GROUP BY'],
+    }
+
+    precedence = ['FROM', 'WHERE', 'GROUP BY', 'HAVING', 'ORDER BY', 'LIMIT', 'OFFSET', 'MODE']
+
+    if op_to_attr[operation]:
+        raise ParsingException(f"Duplicate {operation} clause. Only one {operation} allowed per SELECT.")
+
+    op_requires = requirements.get(operation, [])
+
+    for req in op_requires:
+        if not op_to_attr[req]:
+            raise ParsingException(f"{operation} requires {req}")
+
+    op_precedence_pos = precedence.index(operation)
+
+    for next_op in precedence[op_precedence_pos:]:
+        if op_to_attr[next_op]:
+            raise ParsingException(f"{operation} must go after {next_op}")
+
+
+class JoinType:
+    JOIN = 'JOIN'
+    INNER_JOIN = 'INNER JOIN'
+    OUTER_JOIN = 'OUTER JOIN'
+    CROSS_JOIN = 'CROSS JOIN'
+    LEFT_JOIN = 'LEFT JOIN'
+    RIGHT_JOIN = 'RIGHT JOIN'
+    FULL_JOIN = 'FULL JOIN'
+
+
+def to_single_line(text):
+    text = '\t'.join([line.strip() for line in text.split('\n')])
+    text = text.replace('\t', ' ')
+    text = ' '.join(text.split())
+    return text
