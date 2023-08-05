2ece0fba7a7f2122e58a16d3837188acdce3935f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:46 +0000

    Add package jax-models/0.0.3/jax_models-0.0.3-py3-none-any.whl

---
+++ packages/jax-models/jax_models-0.0.3-py3-none-any.whl/jax_models/models/__init__.py
@@ -0,0 +1,8 @@
+from . import poolformer
+from . import patchconvnet
+from . import mpvit
+from . import mlp_mixer
+from . import conv_mixer
+from . import segformer
+from . import masked_autoencoder
+from .model_registry import list_models, load_model
