e73f91b935b77ab39bdabe317fea42045dda4ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:27 +0000

    Add package itk-core/5.3rc3/itk_core-5.3rc3-cp37-cp37m-win_amd64.whl

---
+++ packages/itk-core/itk_core-5.3rc3-cp37-cp37m-win_amd64.whl/itk/Configuration/ITKPyUtilsConfig.py
@@ -0,0 +1,9 @@
+depends = ('ITKPyBase', 'ITKCommon', )
+templates = (  ('PyCommand', 'itk::PyCommand', 'itkPyCommand', False),
+  ('PyImageFilter', 'itk::PyImageFilter', 'itkPyImageFilterIUC2IUC2', False, 'itk::Image< unsigned char,2 >, itk::Image< unsigned char,2 >'),
+  ('PyImageFilter', 'itk::PyImageFilter', 'itkPyImageFilterIUC3IUC3', False, 'itk::Image< unsigned char,3 >, itk::Image< unsigned char,3 >'),
+  ('PyImageFilter', 'itk::PyImageFilter', 'itkPyImageFilterIUC4IUC4', False, 'itk::Image< unsigned char,4 >, itk::Image< unsigned char,4 >'),
+  ('PyImageFilter', 'itk::PyImageFilter', 'itkPyImageFilterIUS2IUS2', False, 'itk::Image< unsigned short,2 >, itk::Image< unsigned short,2 >'),
+  ('PyImageFilter', 'itk::PyImageFilter', 'itkPyImageFilterIUS3IUS3', False, 'itk::Image< unsigned short,3 >, itk::Image< unsigned short,3 >'),
+  ('PyImageFilter', 'itk::PyImageFilter', 'itkPyImageFilterIUS4IUS4', False, 'itk::Image< unsigned short,4 >, itk::Image< unsigned short,4 >'),
+)
