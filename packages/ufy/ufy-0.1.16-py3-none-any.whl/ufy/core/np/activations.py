d40ed5ab499486551526df0cdd9e6017aec294c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:51 +0000

    Add package ufy/0.1.16/ufy-0.1.16-py3-none-any.whl

---
+++ packages/ufy/ufy-0.1.16-py3-none-any.whl/ufy/core/np/activations.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def softmax(x:np.ndarray):
+    x_row_max = x.max(axis=-1)
+    x_row_max = x_row_max.reshape(list(x.shape)[:-1]+[1])
+    x = x - x_row_max
+    x_exp = np.exp(x)
+    x_exp_row_sum = x_exp.sum(axis=-1).reshape(list(x.shape)[:-1]+[1])
+    out = x_exp / x_exp_row_sum
+    return out
