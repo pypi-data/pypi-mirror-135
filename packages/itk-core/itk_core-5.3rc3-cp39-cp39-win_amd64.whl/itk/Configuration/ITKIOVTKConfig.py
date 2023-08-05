cc347814538aed8718a3aaa0454aaf326041c710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:42 +0000

    Add package itk-core/5.3rc3/itk_core-5.3rc3-cp39-cp39-win_amd64.whl

---
+++ packages/itk-core/itk_core-5.3rc3-cp39-cp39-win_amd64.whl/itk/Configuration/ITKIOVTKConfig.py
@@ -0,0 +1,4 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (  ('VTKImageIO', 'itk::VTKImageIO', 'itkVTKImageIO', True),
+  ('VTKImageIOFactory', 'itk::VTKImageIOFactory', 'itkVTKImageIOFactory', True),
+)
