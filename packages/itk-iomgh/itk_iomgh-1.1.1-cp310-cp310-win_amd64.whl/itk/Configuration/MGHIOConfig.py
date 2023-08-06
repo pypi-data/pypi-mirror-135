103b04e2ce568a6b68cb6611a9d50d2aa4a5c789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:51 +0000

    Add package itk-iomgh/1.1.1/itk_iomgh-1.1.1-cp310-cp310-win_amd64.whl

---
+++ packages/itk-iomgh/itk_iomgh-1.1.1-cp310-cp310-win_amd64.whl/itk/Configuration/MGHIOConfig.py
@@ -0,0 +1,4 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (  ('MGHImageIO', 'itk::MGHImageIO', 'itkMGHImageIO', True),
+  ('MGHImageIOFactory', 'itk::MGHImageIOFactory', 'itkMGHImageIOFactory', True),
+)
