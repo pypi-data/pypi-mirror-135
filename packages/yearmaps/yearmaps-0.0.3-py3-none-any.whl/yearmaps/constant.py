bc7f85d83f1af359fa56831c261198d26a3d5bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:52 +0000

    Add package yearmaps/0.0.3/yearmaps-0.0.3-py3-none-any.whl

---
+++ packages/yearmaps/yearmaps-0.0.3-py3-none-any.whl/yearmaps/constant.py
@@ -0,0 +1,15 @@
+from datetime import timedelta
+from enum import Enum, unique
+
+
+@unique
+class Config(Enum):
+    DATA_DIR = "data_dir"
+    OUTPUT_DIR = "output_dir"
+    MODE = "mode"
+    YEAR = "year"
+    FILE_TYPE = "file_type"
+    COLOR = "color"
+
+
+ONE_DAY = timedelta(days=1)
