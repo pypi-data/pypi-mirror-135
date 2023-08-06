bc94d9bf7982f37accaab30967d03af1534db87f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:51 +0000

    Add package descarteslabs/1.10.0/descarteslabs-1.10.0.tar.gz

---
+++ packages/descarteslabs/descarteslabs-1.10.0.tar.gz/descarteslabs-1.10.0/descarteslabs/discover/__init__.py
@@ -0,0 +1,23 @@
+from .client import (
+    AccessGrant,
+    Asset,
+    Blob,
+    Discover,
+    Folder,
+    Organization,
+    SymLink,
+    UserEmail,
+    Table,
+)
+
+__all__ = [
+    "AccessGrant",
+    "Asset",
+    "Blob",
+    "Discover",
+    "Folder",
+    "Organization",
+    "SymLink",
+    "UserEmail",
+    "Table",
+]
