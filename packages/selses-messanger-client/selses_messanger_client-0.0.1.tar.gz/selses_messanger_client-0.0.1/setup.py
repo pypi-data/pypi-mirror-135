57dc745cca207ef9f4f4e5bebe9650edee61fcd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:09 +0000

    Add package selses-messanger-client/0.0.1/selses_messanger_client-0.0.1.tar.gz

---
+++ packages/selses-messanger-client/selses_messanger_client-0.0.1.tar.gz/selses_messanger_client-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="selses_messanger_client",
+      version="0.0.1",
+      description="Mess Client",
+      author="SomeoneElse",
+      author_email="fatal.critical.error@gmail.com",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
