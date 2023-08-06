aeb2ab155f1cfc1ecb325026e210bad3e26f3db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:11 +0000

    Add package u8darts/0.16.1/u8darts-0.16.1-py3-none-any.whl

---
+++ packages/u8darts/u8darts-0.16.1-py3-none-any.whl/darts/utils/data/__init__.py
@@ -0,0 +1,53 @@
+"""
+TimeSeries Datasets
+-------------------
+"""
+
+try:
+    # Base classes for training datasets:
+    from .training_dataset import (
+        TrainingDataset,
+        PastCovariatesTrainingDataset,
+        FutureCovariatesTrainingDataset,
+        DualCovariatesTrainingDataset,
+        MixedCovariatesTrainingDataset,
+        SplitCovariatesTrainingDataset,
+    )
+
+    # Base class and implementations for inference datasets:
+    from .inference_dataset import (
+        InferenceDataset,
+        PastCovariatesInferenceDataset,
+        FutureCovariatesInferenceDataset,
+        DualCovariatesInferenceDataset,
+        MixedCovariatesInferenceDataset,
+        SplitCovariatesInferenceDataset,
+    )
+
+    # Implementations (sequential)
+    from .sequential_dataset import (
+        PastCovariatesSequentialDataset,
+        FutureCovariatesSequentialDataset,
+        DualCovariatesSequentialDataset,
+        MixedCovariatesSequentialDataset,
+        SplitCovariatesSequentialDataset,
+    )
+
+    # Implementations (shifted)
+    from .shifted_dataset import (
+        PastCovariatesShiftedDataset,
+        FutureCovariatesShiftedDataset,
+        DualCovariatesShiftedDataset,
+        MixedCovariatesShiftedDataset,
+        SplitCovariatesShiftedDataset,
+    )
+
+    # Implementation (horizon-based)
+    from .horizon_based_dataset import HorizonBasedDataset
+
+    # Sequence Encoder
+    from .encoders import SequentialEncoder
+
+except ImportError:
+    # Torch is not available
+    pass
