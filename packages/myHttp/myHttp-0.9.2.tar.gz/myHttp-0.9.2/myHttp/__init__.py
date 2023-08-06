263934e4a9885d0c58c0f1f2e6537400f9b63946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:55 +0000

    Add package myHttp/0.9.2/myHttp-0.9.2.tar.gz

---
+++ packages/myHttp/myHttp-0.9.2.tar.gz/myHttp-0.9.2/myHttp/__init__.py
@@ -0,0 +1,16 @@
+from .main import http,testInternet
+
+__all__=[
+    'http',
+    'testInternet',
+]
+
+
+__version__='0.9.2'
+__license__='GPL v2'
+__author__='Tiancheng Jiao'
+__github__='https://github.com/jtc1246/easy-http'
+__author_email__='jtc1246@outlook.com'
+__description__='A very easy http tool'
+
+
