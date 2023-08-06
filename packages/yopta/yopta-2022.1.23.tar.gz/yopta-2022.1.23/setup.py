c658bf1d60fb0ab3811714175d122697ca05f500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:35 +0000

    Add package yopta/2022.1.23/yopta-2022.1.23.tar.gz

---
+++ packages/yopta/yopta-2022.1.23.tar.gz/yopta-2022.1.23/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='yopta',
+      version='2022.1.23',
+      author="aqur1n",
+      url=r"https://github.com/aqur1n/yopta.py",
+      description='Перевод методов, атрибутов и функций на пацанский для python!',
+      packages=['yopta'],
+      zip_safe=False)
