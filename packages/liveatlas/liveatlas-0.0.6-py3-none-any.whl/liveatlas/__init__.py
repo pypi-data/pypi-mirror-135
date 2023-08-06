e0154211dcce4b670675f95586991b82deda463a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:59 +0000

    Add package liveatlas/0.0.6/liveatlas-0.0.6-py3-none-any.whl

---
+++ packages/liveatlas/liveatlas-0.0.6-py3-none-any.whl/liveatlas/__init__.py
@@ -0,0 +1,9 @@
+"""
+To update:
+1. Delete contents of ./dist
+2. increment version in setup.py
+3. Save changes to file
+4. run $python setup.py sdist bdist_wheel
+5. run $twine upload dist/*
+
+"""
