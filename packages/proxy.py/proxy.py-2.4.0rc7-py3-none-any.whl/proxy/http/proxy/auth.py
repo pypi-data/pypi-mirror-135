c045594e8786f1357d506de68fb0944f0b3e57b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:48 +0000

    Add package proxy.py/2.4.0rc7/proxy.py-2.4.0rc7-py3-none-any.whl

---
+++ packages/proxy.py/proxy.py-2.4.0rc7-py3-none-any.whl/proxy/http/proxy/auth.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+    proxy.py
+    ~~~~~~~~
+    ⚡⚡⚡ Fast, Lightweight, Pluggable, TLS interception capable proxy server focused on
+    Network monitoring, controls & Application development, testing, debugging.
+
+    :copyright: (c) 2013-present by Abhinav Singh and contributors.
+    :license: BSD, see LICENSE for more details.
+
+    .. spelling::
+
+       auth
+       http
+"""
+from typing import Optional
+
+from ...http import httpHeaders
+from ..exception import ProxyAuthenticationFailed
+from ...http.proxy import HttpProxyBasePlugin
+from ...common.flag import flags
+from ...http.parser import HttpParser
+from ...common.constants import DEFAULT_BASIC_AUTH
+
+
+flags.add_argument(
+    '--basic-auth',
+    type=str,
+    default=DEFAULT_BASIC_AUTH,
+    help='Default: No authentication. Specify colon separated user:password '
+    'to enable basic authentication.',
+)
+
+
+class AuthPlugin(HttpProxyBasePlugin):
+    """Performs proxy authentication."""
+
+    def before_upstream_connection(
+            self, request: HttpParser,
+    ) -> Optional[HttpParser]:
+        if self.flags.auth_code and request.headers:
+            if httpHeaders.PROXY_AUTHORIZATION not in request.headers:
+                raise ProxyAuthenticationFailed()
+            parts = request.headers[httpHeaders.PROXY_AUTHORIZATION][1].split()
+            if len(parts) != 2 \
+                    or parts[0].lower() != b'basic' \
+                    or parts[1] != self.flags.auth_code:
+                raise ProxyAuthenticationFailed()
+        return request
