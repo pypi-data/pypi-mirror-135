51c3d5825e752e56a53f2389afaddf619eb80a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:22 +0000

    Add package little-dinosaur/0.0.28/little_dinosaur-0.0.28.tar.gz

---
+++ packages/little-dinosaur/little_dinosaur-0.0.28.tar.gz/little_dinosaur-0.0.28/little_dinosaur/__init__.py
@@ -0,0 +1,9 @@
+from little_dinosaur.eat import (
+    eat,
+    self_learning
+)
+
+from little_dinosaur.nlp_classification_model import (
+    train_classification_model,
+    predict_classification_model
+)
