a7ae4d083c712e9fce0995b012c8fa3b8084d975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:42 +0000

    Add package ProjZ.py/1.5/ProjZ.py-1.5.tar.gz

---
+++ packages/ProjZ.py/ProjZ.py-1.5.tar.gz/ProjZ.py-1.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "httpx[all]",
+    "typing",
+    "websockets", 
+    "ujson",
+    "aiofiles"
+]
+
+setup(name = "ProjZ.py",
+      version = "1.5",
+      description = "Library for project z",
+      packages = find_packages(),
+      author_email = "ktoya170214@gmail.com",
+      install_requires = requirements,
+      zip_safe = False)
