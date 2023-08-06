3f3552dbe9e0e15f54a10d1cf3c04e455b4ec867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:34 +0000

    Add package cft-analysis/0.1.0/cft-analysis-0.1.0.tar.gz

---
+++ packages/cft-analysis/cft-analysis-0.1.0.tar.gz/cft-analysis-0.1.0/cft_analysis/__init__.py
@@ -0,0 +1,7 @@
+"""A package to work with data from the CFT Study."""
+
+__version__ = "0.1.0"
+
+from cft_analysis import datasets, feature_extraction, utils
+
+__all__ = ["datasets", "feature_extraction", "utils"]
