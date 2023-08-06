16ef47904e697aadc18fbe669fd378dd13fd35bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:48 +0000

    Add package twitter-demographer/0.1.4/twitter_demographer-0.1.4.tar.gz

---
+++ packages/twitter-demographer/twitter_demographer-0.1.4.tar.gz/twitter_demographer-0.1.4/twitter_demographer/geolocation/geonames.py
@@ -0,0 +1,40 @@
+from tqdm import tqdm
+import geocoder
+from twitter_demographer.components import Component
+import logging
+
+
+
+class GeoNamesDecoder(Component):
+    """
+    Wrappers on the geocoder API to disambiguate users' locations
+    """
+
+    def __init__(self, key):
+        super().__init__()
+        self.key = key
+
+    def outputs(self):
+        return ["geo_location_country", "geo_location_address"]
+
+    def inputs(self):
+        return ["location"]
+
+    def infer(self, data):
+        logger = logging.StreamHandler()
+        logger.setLevel(logging.ERROR)
+        geo = self.initialize_return_dict()
+        pbar = tqdm(total=len(data), position=1)
+        pbar.set_description("Geocoder")
+
+        for val in data["location"]:
+            pbar.update(1)
+            if val is None:
+                geo["geo_location_country"].append(None)
+                geo["geo_location_address"].append(None)
+            else:
+                g = geocoder.geonames(val, key=self.key)
+                geo["geo_location_country"].append(g.country)
+                geo["geo_location_address"].append(g.address)
+        pbar.close()
+        return geo
