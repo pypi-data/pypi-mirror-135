79de5f7a65e912a93f73b164f9717ea04f97467b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:41 +0000

    Add package dr-data/1.0.1/dr_data-1.0.1-py3-none-any.whl

---
+++ packages/dr-data/dr_data-1.0.1-py3-none-any.whl/dr_data/biopsy/__init__.py
@@ -0,0 +1,220 @@
+import sys
+import psycopg2
+import logging
+from progress.bar import Bar
+from dr_data.static_strings import *
+from dr_data.sql import Sql
+
+__author__ = AUTHOR
+__copyright__ = COPYRIGHT
+__license__ = LICENSE
+
+_logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
+
+
+class Biopsy:
+    """
+    Explicitly exports a schema and table insertion-order JSON files
+    """
+    def __init__(self, configuration):
+        """
+        Constructor for Biopsy
+        :param configuration: configuration file
+        :type configuration: JSON
+        """
+        self.configuration = configuration
+        conn_info = self.configuration['db']
+
+        self.connection = psycopg2.connect(**conn_info)
+        self.database = conn_info['database']
+        self.cursor = self.connection.cursor()
+        self.schema = dict()
+        self.insertion_order = dict()
+        self.table_count = 0
+        self.insert_order = 1
+
+    def execute_cmd(self):
+        """
+        Executes the biopsy command, main entry point
+        :return: tuple of db schema and insertion order schema
+        :rtype: tuple
+        """
+        return (
+            self.build_schema(),
+            self.build_insertion_order_schema()
+        )
+
+    def build_schema(self, table_schema_name='public'):
+        """
+        Builds a JSON schema from the database tables and columns
+        :param table_schema_name:  The schema name, default is public
+        :type table_schema_name: str
+        :return: a dictionary of tables and columns
+        :rtype: dict
+        """
+        tables = self.build_tables(table_schema_name)
+        no_foreign_keys = []
+        has_foreign_keys = []
+        progress_bar = Bar('- Generating schema for {database}...'.format(database=self.database), max=len(tables))
+        for table in tables:
+            columns = self.build_columns(table)
+            self.schema[table] = columns
+            if columns['@table_metadata']['has_foreign_keys']:
+                has_foreign_keys.append(table)
+            else:
+                no_foreign_keys.append(table)
+            progress_bar.next()
+        progress_bar.finish()
+        self.schema["@database_metadata"] = {
+            "no_foreign_key_tables": no_foreign_keys,
+            "foreign_key_tables": has_foreign_keys
+        }
+        return self.schema
+
+    def build_insertion_order_schema(self):
+        """
+        Builds a JSON insertion schema from the JSON schema
+        :return:  a insertion order dictionary of tables and columns
+        :rtype: dict
+        """
+        if len(self.schema) == 0:
+            self.build_schema()
+        insertion_table_order = self.get_insertion_table_order()
+        progress_bar = Bar('- Generating insertion order for {database}....'.format(database=self.database),
+                           max=len(insertion_table_order))
+        for table in insertion_table_order:
+            table = table.replace("\"", "")
+            self.insertion_order[self.insert_order] = {
+                table: self.schema[table]
+            }
+            self.insert_order += 1
+            progress_bar.next()
+        progress_bar.finish()
+        return self.insertion_order
+
+    def get_insertion_table_order(self):
+        """
+        This queries the database and gets the table insertion order
+        :return: ordered insetion list of table names
+        :rtype: list[str]
+        """
+        query = Sql.build_insertion_table_order()
+        self.cursor.execute(query)
+        insertion_data = self.cursor.fetchall()
+        output = []
+        for insertion_name in insertion_data:
+            output.append(insertion_name[0])
+        return output
+
+    def build_tables(self, table_schema_name='public'):
+        """
+        This queries the database and gets the list of tables and related table data
+        :param table_schema_name: The schema name, default is public
+        :type table_schema_name: str
+        :return: list of tables and table schema information
+        :rtype: list[str]
+        """
+        query = Sql.build_tables_query(table_schema_name)
+        self.cursor.execute(query)
+        table_data = self.cursor.fetchall()
+        data = []
+        for table in table_data:
+            data.append(table[1])
+            self.table_count += 1
+        return data
+
+    def build_columns(self, table_name, table_schema_name='public'):
+        """
+        This queries the database and build a list of columns per table.
+        :param table_name: Name of the table
+        :type table_name: str
+        :param table_schema_name:  The schema name, default is public
+        :type table_schema_name: str
+        :return: Dictionary of column data per table.
+        :rtype: dict
+        """
+        query = Sql.build_columns_query(table_schema_name, table_name)
+        self.cursor.execute(query)
+        column_data = self.cursor.fetchall()
+        column_data_list = []
+
+        foreign_key_tables = []
+        has_foreign_keys = False
+        has_user_defined_keys = False
+        for column in column_data:
+            constraint = self.get_column_constraint(table_name, column[2])
+            insert_object = {
+                "name": column[2],
+                "data_type": column[5],
+                "column_default": column[3],
+                "is_nullable": column[4],
+            }
+
+            if column[5] == 'USER-DEFINED':
+                has_user_defined_keys = True
+                insert_object["user_defined_type"] = {
+                    "name": column[6],
+                    "values": self.get_values_from_type(column[6])
+                }
+
+            if constraint:
+                insert_object["constraint"] = constraint
+
+                for key, value in constraint.items():
+                    if value['type'] == 'FOREIGN KEY':
+                        foreign_key_tables.append(value['referenced_table'])
+                        has_foreign_keys = True
+            column_data_list.append(insert_object)
+
+        output = {
+            "columns": column_data_list,
+            "@table_metadata": {
+                "column_count": len(column_data),
+                "has_foreign_keys": has_foreign_keys,
+                "has_user_defined_keys": has_user_defined_keys,
+                "foreign_constraint_tables": foreign_key_tables
+            }
+        }
+        return output
+
+    def get_column_constraint(self, table_name, column_name, table_schema_name='public'):
+        """
+        This queries the database getting column constraints per table.
+        :param table_name: The name of the table
+        :type table_name: str
+        :param column_name: The name of the column
+        :type column_name: str
+        :param table_schema_name: The schema name, default is public
+        :type table_schema_name: str
+        :return: Dictionary of colum constraints.
+        :rtype: dict
+        """
+        query = Sql.build_column_constraints(table_schema_name, table_name, column_name)
+        self.cursor.execute(query)
+        constraint_data = self.cursor.fetchall()
+        data = dict()
+
+        for constraint in constraint_data:
+            data[constraint[4]] = {
+                "type": constraint[5],
+                "referenced_table": constraint[8],
+                "referenced_column": constraint[9]
+            }
+        return data
+
+    def get_values_from_type(self, type):
+        """
+        This queries the database gettting the `types` from value
+        :param type: The name of the type
+        :type type: str
+        :return: List of types
+        :rtype: list[str]
+        """
+        query = Sql.build_values_from_type(type)
+        self.cursor.execute(query)
+        types = []
+        type_data = self.cursor.fetchall()
+        for tdata in type_data:
+            types.append(tdata[1])
+        return types
---
+++ packages/dr-data/dr_data-1.0.1-py3-none-any.whl/dr_data/main.py
@@ -0,0 +1,212 @@
+import os
+import argparse
+import logging
+import sys
+import json
+from dr_data.utilities.file import FileUtility
+from dr_data.utilities.db import DatabaseUtility
+from dr_data.static_strings import *
+from dr_data.biopsy import Biopsy
+from dr_data.inject import Inject
+from dr_data.transplant import Transplant
+
+from dr_data import __version__
+
+__author__ = AUTHOR
+__copyright__ = COPYRIGHT
+__license__ = LICENSE
+
+_logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.DEBUG)
+
+
+class Main:
+    """
+    The main entry point class for the CLI
+    """
+    def __init__(self, args):
+        """
+        Constructor for the main entry point class
+        :param args: CLI argument
+        :type args: dict
+        """
+        self.parser = argparse.ArgumentParser(
+            description=MAIN_DESCRIPTION,
+            epilog="Version: {version}".format(version=__version__))
+        self.arguments = self.parse_args(args)
+
+        if self.arguments.config:
+            self.configuration = self.load_configuration(self.arguments.config)
+        else:
+            self.configuration = self.load_configuration(os.getenv(ENV_CONFIG_NAME))
+
+        self.default_rows = self.configuration['defaults']['rows']
+        self.database_name = self.configuration['db']['database']
+        self.db_util = DatabaseUtility(self.configuration['db'])
+        self.schema_data = None
+
+    def load_configuration(self, config_path):
+        """
+        Loads the configuration file into memory.
+        :param config_path: Path to the configuration file
+        :type config_path: str
+        :return: JSON
+        :rtype: JSON
+        """
+        configuration_file = os.getenv(ENV_CONFIG_NAME)
+        if not config_path and not configuration_file:
+            raise argparse.ArgumentTypeError(NO_CONFIG_ARGUMENT.format(env_name=ENV_CONFIG_NAME))
+        if config_path:
+            json_config = json.loads(FileUtility.read_file(config_path))
+            return json_config
+
+    def parse_args(self, args):
+        """
+        Sets up CLI arguments and helper messages
+        :param args: Arguments to parse
+        :type args: dict
+        :return: Parser
+        :rtype: Arguments Parser
+        """
+        # transplant arguments
+        self.parser.add_argument('-transplant', help=TRANSPLANT_ARG, action='store_true')
+        self.parser.add_argument('-source', help=TRANSPLANT_SOURCE_ARG, type=str)
+        self.parser.add_argument('-destination', help=TRANSPLANT_DESTINATION_ARG, type=str)
+
+        # inject arguments
+        self.parser.add_argument('-inject', help=INJECT_ARG, action='store_true')
+        self.parser.add_argument('-rows', help=INJECT_ROW_ARG, type=int)
+
+        # biopsy arguments
+        self.parser.add_argument('-biopsy', help=BIOPSY_ARG, action='store_true')
+        self.parser.add_argument('-export', help=BIOPSY_EXPORT_ARG, type=str)
+
+        # cleanse (truncate) arguments
+        self.parser.add_argument('-cleanse', help=CLEANSE_ARG, action='store_true')
+
+        # config argument
+        self.parser.add_argument('-config', help=CONFIG_ARG.format(env_name=ENV_CONFIG_NAME), type=str)
+
+        return self.parser.parse_args(args)
+
+    def setup_logging(self):
+        """
+        Sets up logging for the CLI
+        :return: None
+        :rtype: None
+        """
+        log_format = "[%(asctime)s] %(levelname)s:%(name)s: %(message)s"
+        logging.basicConfig(
+            level=logging.INFO, stream=sys.stdout, format=log_format, datefmt="%Y-%m-%d %H:%M:%S"
+        )
+
+    def execute_biopsy(self):
+        """
+        Executes the biopsy command
+        :return: None
+        :rtype: None
+        """
+        if not self.arguments.export:
+            sys.tracebacklimit=0
+            raise argparse.ArgumentTypeError(BIOPSY_NO_EXPORT)
+        if not os.path.isdir(self.arguments.export):
+            sys.tracebacklimit=0
+            raise argparse.ArgumentTypeError(BIOPSY_EXPORT_NOT_EXIST.format(path=self.arguments.export))
+
+        logging.info(BIOPSY_START_MESSAGE.format(database=self.database_name))
+        self.schema_data = Biopsy(self.configuration).execute_cmd()
+        schema_filename = "{db_name}_schema".format(db_name=self.database_name)
+        insertion_order_filename = "{db_name}_biopsy_insertion_order".format(db_name=self.database_name)
+
+        FileUtility.generate_json_file(schema_filename, self.arguments.export, self.schema_data[0])
+        logging.info(BIOPSY_GENERATED_SCHEMA.format(filename=schema_filename))
+
+        FileUtility.generate_json_file(insertion_order_filename, self.arguments.export, self.schema_data[1])
+        logging.info(BIOPSY_GENERATED_INSERT_ORDER_SCHEMA.format(filename=insertion_order_filename))
+        logging.info(BIOPSY_COMPLETE_MESSAGE.format(database=self.database_name, export_path=self.arguments.export))
+
+    def execute_cleanse(self):
+        """
+        Executes the cleanse command
+        :return: None
+        :rtype: None
+        """
+        self.db_util .truncate_db()
+        logging.info(CLEANSE_COMPLETE_MESSAGE.format(database=self.database_name))
+
+    def execute_inject(self):
+        """
+        Executes the inject command
+        :return: None
+        :rtype: None
+        """
+        if not self.arguments.rows:
+            self.arguments.rows = self.default_rows
+            logging.info(INJECT_NO_ROWS.format(rows=self.default_rows))
+        self.schema_data = Biopsy(self.configuration).execute_cmd()
+        Inject(self.schema_data[1], self.configuration).execute_cmd(self.arguments.rows)
+        logging.info(INJECT_COMPLETE_MESSAGE.format(database=self.database_name, rows=self.arguments.rows))
+
+    def execute_transplant(self):
+        """
+        Executes the transplant command
+        :return: None
+        :rtype: None
+        """
+        logging.info(TRANSPLANT_START_MESSAGE.format(database=self.database_name))
+        if not self.arguments.source:
+            sys.tracebacklimit=0
+            raise argparse.ArgumentTypeError(TRANSPLANT_NO_SOURCE)
+        transplant = Transplant(self.configuration)
+
+        if os.path.isfile(self.arguments.source):
+            logging.info('- Transplanting file {file}'.format(file=self.arguments.source))
+            if not self.arguments.destination:
+                sys.tracebacklimit=0
+                raise argparse.ArgumentTypeError(TRANSPLANT_NO_DESTINATION)
+            if not FileUtility.is_csv_file(self.arguments.source):
+                raise argparse.ArgumentTypeError(TRANSPLANT_NOT_CSV)
+            transplant.execute_file_cmd(self.arguments.source, self.arguments.destination)
+            logging.info(TRANSPLANT_COMPLETE_MESSAGE.format(database=self.database_name))
+            sys.exit()
+        elif os.path.isdir(self.arguments.source):
+            self.schema_data = Biopsy(self.configuration).execute_cmd()
+            logging.info('- Transplanting directory {file}'.format(file=self.arguments.source))
+            transplant.execute_directory_cmd(self.arguments.source, self.schema_data)
+            logging.info(TRANSPLANT_COMPLETE_MESSAGE.format(database=self.database_name))
+            sys.exit()
+        else:
+            sys.tracebacklimit=0
+            raise argparse.ArgumentTypeError(TRANSPLANT_FILE_FOLDER_ERROR.format(file=self.arguments.source))
+
+    def execute_cmd(self):
+        """
+        Sets up logging and determines incoming arguments
+        :return: None
+        :rtype: None
+        """
+        self.setup_logging()
+        if self.arguments.transplant:
+            self.execute_transplant()
+        elif self.arguments.inject:
+            self.execute_inject()
+        elif self.arguments.biopsy:
+            self.execute_biopsy()
+        elif self.arguments.cleanse:
+            self.execute_cleanse()
+        else:
+            logging.info(NO_ARGUMENTS + "\n")
+            self.parser.print_help()
+
+
+def run():
+    """
+    Run the main entry point class
+    :return: None
+    :rtype: None
+    """
+    Main(sys.argv[1:]).execute_cmd()
+
+
+if __name__ == '__main__':
+    run()
