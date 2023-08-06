9a3f87808cf6b81ea61cf08c5a043080ef54ae1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:23 +0000

    Add package py3-django-enriched-carton/0.1.0/py3_django_enriched_carton-0.1.0-py3-none-any.whl

---
+++ packages/py3-django-enriched-carton/py3_django_enriched_carton-0.1.0-py3-none-any.whl/carton/__init__.py
@@ -0,0 +1,9 @@
+"""
+django-enriched-carton is a simple and lightweight application for shopping
+carts and wish lists.
+Forked of django-carton.
+Python3 and Django 3.2LTS are supported.
+"""
+
+__author__ = 'smirnov.ev'
+__version__ = '0.1.0'
