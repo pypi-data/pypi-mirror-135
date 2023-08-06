e7892eb6e0c6280b82b2f06f3d125270c13cfe69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:21 +0000

    Add package throttled/0.1.16a3/throttled-0.1.16a3-py3-none-any.whl

---
+++ packages/throttled/throttled-0.1.16a3-py3-none-any.whl/throttled/fastapi/limiters/base.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+from typing import Callable, Sequence
+
+from starlette import status
+from starlette.middleware.base import RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
+
+from throttled.exceptions import RateLimitExceeded
+from throttled.strategy import Strategy
+
+
+def response_from_exception(exc: RateLimitExceeded) -> Response:
+    return Response(
+        status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+        content=f"Rate exceeded for key={exc.key}.",
+        headers={"Retry-After": str(exc.retry_after)},
+    )
+
+
+async def catcher_middleware(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except RateLimitExceeded as exc:
+        return response_from_exception(exc)
+
+
+class Middleware(ABC):
+    _ignored_paths: Sequence[str] = ("/docs", "/redoc", "/favicon.ico", "/openapi.json")
+
+    def ignore_path(self, path: str):
+        ignored = list(self._ignored_paths)
+        ignored.append(path)
+        self._ignored_paths = ignored
+
+    async def dispatch(
+        self, request: Request, call_next: RequestResponseEndpoint
+    ) -> Response:
+        try:
+            print(request.path_params)
+            self._maybe_block_request(request)
+            return await call_next(request)
+        except RateLimitExceeded as exc:
+            return response_from_exception(exc)
+
+    @abstractmethod
+    def _maybe_block_request(self, request: Request):
+        ...
+
+
+class Limiter(Callable, ABC):
+    def __init__(self, strategy: Strategy):
+        self._strategy = strategy
+
+    def __call__(self, *args, **kwargs):
+        ...
