c50ba3029b245336a8c949ff8d19e7852c8b143f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:10 +0000

    Add package jaws-libp/2.7.0a0/jaws_libp-2.7.0a0-py3-none-any.whl

---
+++ packages/jaws-libp/jaws_libp-2.7.0a0-py3-none-any.whl/jlab_jaws/avro/__init__.py
@@ -0,0 +1,8 @@
+"""
+   AVRO schemas (.avsc data files) and corresponding Python entities (classes) for JAWS are included in this package.
+
+   This package also includes JAWS clients and serializers/deserializers (serde) corresponding to the provided entities.
+
+   See Also:
+       - `Apache AVRO <https://avro.apache.org/>`_
+"""
