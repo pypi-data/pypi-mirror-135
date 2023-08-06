b304eb2d48a832598a722b47af831346320bd97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:07 +0000

    Add package courseSelector/0.9.2/courseSelector-0.9.2.tar.gz

---
+++ packages/courseSelector/courseSelector-0.9.2.tar.gz/courseSelector-0.9.2/courseSelector/__init__.py
@@ -0,0 +1,20 @@
+from .main import help,check,printCourseList,waitEmptySpace,fastSelect,luckyDraw
+
+__all__=[
+    'help',
+    'check',
+    'printCourseList',
+    'waitEmptySpace',
+    'fastSelect',
+    'luckyDraw'
+]
+
+
+__version__='0.9.2'
+__license__='GPL v2'
+__author__='Tiancheng Jiao'
+__url__='https://github.com/jtc1246/courseSelector'
+__author_email__='jtc1246@outlook.com'
+__description__='An automatic course selection program for SJTU-JI'
+
+
