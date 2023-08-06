2b7a29ff279c0e25a2e86340d6fc7cb7acb97e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:17 +0000

    Add package linestats/1.2.1/linestats-1.2.1-py3-none-any.whl

---
+++ packages/linestats/linestats-1.2.1-py3-none-any.whl/linestats/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=C0114
+'''
+linestats: Python module to count the number of scripted, commented, docstringed, and empty lines in
+Python code.
+
+Copyright (c) 2020-2022 F.P.A. Vogt; frederic.vogt@alumni.anu.edu.au
+
+Distributed under the terms of the GNU General Public License v3.0 or later.
+
+SPDX-License-identifier: GPL-3.0-or-later
+'''
+
+# Make it easier to access the linestats functions
+
+from .version import __version__
+from .linestats import *
