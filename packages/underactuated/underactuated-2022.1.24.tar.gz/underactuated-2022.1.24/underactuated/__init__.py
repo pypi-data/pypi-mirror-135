9e598886719ca9b5ba37626f05cc268a27f81117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:29 +0000

    Add package underactuated/2022.1.24/underactuated-2022.1.24.tar.gz

---
+++ packages/underactuated/underactuated-2022.1.24.tar.gz/underactuated-2022.1.24/underactuated/__init__.py
@@ -0,0 +1,5 @@
+from .jupyter import running_as_notebook
+from .pyplot_visualizer import SliderSystem
+from .multibody import ManipulatorDynamics
+from .utils import FindResource
+from .plot_utils import plot_2d_phase_portrait
