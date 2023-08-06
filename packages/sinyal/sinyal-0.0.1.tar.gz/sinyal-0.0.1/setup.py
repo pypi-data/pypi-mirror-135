e447d6ac3cd1e3cd609668fdbb1a039970496e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:04 +0000

    Add package sinyal/0.0.1/sinyal-0.0.1.tar.gz

---
+++ packages/sinyal/sinyal-0.0.1.tar.gz/sinyal-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='sinyal',
+      version='0.0.1',
+      description="""A library for processing and analysing physiological
+      signals such as ECG, BP, EMG, EEG, Pulse and Sp02""",
+      url='http://github.com/faruqsandi/sinyal',
+      author='Faruq Sandi',
+      author_email='faruqsandi@gmail.com',
+      license='MIT',
+      packages=['sinyal'],
+      zip_safe=False)
