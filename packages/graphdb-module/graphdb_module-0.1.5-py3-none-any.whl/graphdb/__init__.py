62adfcae7f073f94657b15a5ab902a6a490f2078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:47 +0000

    Add package graphdb-module/0.1.5/graphdb_module-0.1.5-py3-none-any.whl

---
+++ packages/graphdb-module/graphdb_module-0.1.5-py3-none-any.whl/graphdb/__init__.py
@@ -0,0 +1,8 @@
+# Version of the graphdb module
+__version__ = "0.1.5"
+
+from graphdb.connection import GraphDbConnection
+from graphdb.graph import GraphDb
+from graphdb.interface import *
+from graphdb.schema import *
+from graphdb.utils import *
