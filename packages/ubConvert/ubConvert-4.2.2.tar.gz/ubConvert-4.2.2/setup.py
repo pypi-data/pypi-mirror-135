b90377cce1797002358118d93d8bb0636d5a4f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:34 +0000

    Add package ubConvert/4.2.2/ubConvert-4.2.2.tar.gz

---
+++ packages/ubConvert/ubConvert-4.2.2.tar.gz/ubConvert-4.2.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ubConvert',
+    version='4.2.2',
+    author='ZennDogg',
+    author_email='zenndogg@outlook.com.com',
+    url='http://pypi.python.org/pypi/ubConvert/',
+    license='LICENSE.txt',
+    description='Units Converter Package',
+    description_file='README.txt',
+    python_requires = '>=3.8',
+)
