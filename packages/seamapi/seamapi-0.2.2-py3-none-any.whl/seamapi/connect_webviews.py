f9b17b3e3e15aa337172712b218bb187f9ce6724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:09 +0000

    Add package seamapi/0.2.2/seamapi-0.2.2-py3-none-any.whl

---
+++ packages/seamapi/seamapi-0.2.2-py3-none-any.whl/seamapi/connect_webviews.py
@@ -0,0 +1,54 @@
+from seamapi.types import (
+    AbstractConnectWebviews,
+    ConnectWebview,
+    AbstractSeam as Seam,
+    AcceptedProvider,
+)
+import requests
+from typing import List, Optional
+
+
+class ConnectWebviews(AbstractConnectWebviews):
+    seam: Seam
+
+    def __init__(self, seam: Seam):
+        self.seam = seam
+
+    def list(self) -> List[ConnectWebview]:
+        raise NotImplementedError
+
+    def get(self, connect_webview_id: str) -> ConnectWebview:
+        res = requests.post(
+            f"{self.seam.api_url}/connect_webviews/get",
+            headers={"Authorization": f"Bearer {self.seam.api_key}"},
+            params={"connect_webview_id": connect_webview_id},
+        )
+        if not res.ok:
+            raise Exception(res.text)
+        json_webview = res.json()["connect_webview"]
+        return ConnectWebview(
+            connect_webview_id=json_webview["connect_webview_id"],
+            status=json_webview["status"],
+            url=json_webview["url"],
+            login_successful=json_webview["login_successful"],
+            third_party_account_id=json_webview["third_party_account_id"],
+        )
+
+    def create(
+        self, accepted_providers: Optional[List[AcceptedProvider]] = []
+    ) -> ConnectWebview:
+        res = requests.post(
+            f"{self.seam.api_url}/connect_webviews/create",
+            headers={"Authorization": f"Bearer {self.seam.api_key}"},
+            json={"accepted_providers": accepted_providers},
+        )
+        if not res.ok:
+            raise Exception(res.text)
+        json_webview = res.json()["connect_webview"]
+        return ConnectWebview(
+            connect_webview_id=json_webview["connect_webview_id"],
+            status=json_webview["status"],
+            url=json_webview["url"],
+            login_successful=json_webview["login_successful"],
+            third_party_account_id=None,
+        )
