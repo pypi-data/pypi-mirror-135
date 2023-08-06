e77df04b657fbd7af0acae3d888507dec13f760d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:41 +0000

    Add package timeseriesencoder/0.1.2/timeseriesencoder-0.1.2.tar.gz

---
+++ packages/timeseriesencoder/timeseriesencoder-0.1.2.tar.gz/timeseriesencoder-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+import os
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+setup(name="timeseriesencoder", install_requires=install_requires)
