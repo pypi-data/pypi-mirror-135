e8da066b67f8775f1812f96468fc0f52c31cf615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:17 +0000

    Add package PF-Flask-DB/1.0.1/PF-Flask-DB-1.0.1.tar.gz

---
+++ packages/PF-Flask-DB/PF-Flask-DB-1.0.1.tar.gz/PF-Flask-DB-1.0.1/pf_flask_db/pf_app_model.py
@@ -0,0 +1,17 @@
+from pf_py_common.py_common import PyCommon
+from sqlalchemy import Integer
+from pf_flask_db.pf_app_database import app_db
+from pf_flask_db.helper.pf_db_model import PFModel
+
+
+class BaseModel(PFModel):
+    __abstract__ = True
+
+
+class AppModel(BaseModel):
+    __abstract__ = True
+    id = app_db.Column("id", app_db.BigInteger().with_variant(Integer, "sqlite"), primary_key=True)
+    created = app_db.Column("created", app_db.DateTime, default=app_db.func.now())
+    updated = app_db.Column("updated", app_db.DateTime, default=app_db.func.now(), onupdate=app_db.func.now())
+    isDeleted = app_db.Column("is_deleted", app_db.Boolean, default=False)
+    uuid = app_db.Column("uuid", app_db.String(50), default=PyCommon.uuid())
---
+++ packages/PF-Flask-DB/PF-Flask-DB-1.0.1.tar.gz/PF-Flask-DB-1.0.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+import pathlib
+
+CURRENT_DIR = pathlib.Path(__file__).parent
+README = (CURRENT_DIR / "readme.md").read_text()
+
+env = os.environ.get('dev')
+
+
+def get_dependencies():
+    dependency = [
+        'Flask',
+        'Flask-SQLAlchemy'
+    ]
+
+    if env and env == "dev":
+        return dependency
+
+    return dependency + ["PF-PY-Common"]
+
+
+setup(
+    name='PF-Flask-DB',
+    version='1.0.1',
+    url='https://github.com/problemfighter/pf-flask-db',
+    license='Apache 2.0',
+    author='Problem Fighter',
+    author_email='problemfighter.com@gmail.com',
+    description='Flask Database Manager by Problem Fighter Library',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=get_dependencies(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+    ]
+)
