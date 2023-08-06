9affdfd10f0f0a9aaa906edb67eea4a05c2b9670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:50 +0000

    Add package superMathBeta/1.0/superMathBeta-1.0.tar.gz

---
+++ packages/superMathBeta/superMathBeta-1.0.tar.gz/superMathBeta-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='superMathBeta',
+    version='1.0',
+    description="testing",
+    author='collins',
+    author_email='CollinsLiu21@126.com',
+    py_modules=['superMath.mathAdd', 'superMath.mathMultiply']
+)
