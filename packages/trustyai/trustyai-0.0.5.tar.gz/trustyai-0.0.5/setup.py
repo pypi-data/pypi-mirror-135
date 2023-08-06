aed3d0ee3d1ef716da1e9824fb6b398cd02f15b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:01 +0000

    Add package trustyai/0.0.5/trustyai-0.0.5.tar.gz

---
+++ packages/trustyai/trustyai-0.0.5.tar.gz/trustyai-0.0.5/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="trustyai",
+    version="0.0.5",
+    description="Python bindings to the TrustyAI explainability library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/trustyai-python/module",
+    author="Rui Vieira",
+    author_email="rui@redhat.com",
+    license='Apache License 2.0',
+    platforms='any',
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Java",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Java Libraries"
+    ],
+    packages=['trustyai', 'trustyai.model', 'trustyai.utils', 'trustyai.local'],
+    include_package_data=False,
+    install_requires=['Jpype1'],
+)
