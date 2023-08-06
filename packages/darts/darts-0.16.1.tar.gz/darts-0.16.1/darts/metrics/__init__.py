d79068c23cb3a23eda89446354351919357e3137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:13 +0000

    Add package darts/0.16.1/darts-0.16.1.tar.gz

---
+++ packages/darts/darts-0.16.1.tar.gz/darts-0.16.1/darts/metrics/__init__.py
@@ -0,0 +1,20 @@
+"""
+Metrics
+-------
+"""
+
+from .metrics import (
+    mae,
+    mse,
+    rmse,
+    rmsle,
+    mape,
+    smape,
+    mase,
+    ope,
+    marre,
+    r2_score,
+    coefficient_of_variation,
+    dtw_metric,
+    rho_risk,
+)
