93611023107475b7f4f930c553f5c67ec8eb44f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:23 +0000

    Add package ProjZ.py/1.7.1/ProjZ.py-1.7.1.tar.gz

---
+++ packages/ProjZ.py/ProjZ.py-1.7.1.tar.gz/ProjZ.py-1.7.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "httpx[all]",
+    "typing",
+    "websockets==8.0.2", 
+    "ujson",
+    "aiofiles"
+]
+
+setup(name = "ProjZ.py",
+      version = "1.7.1",
+      description = "Library for project z",
+      packages = find_packages(),
+      author_email = "ktoya170214@gmail.com",
+      install_requires = requirements,
+      zip_safe = False)
