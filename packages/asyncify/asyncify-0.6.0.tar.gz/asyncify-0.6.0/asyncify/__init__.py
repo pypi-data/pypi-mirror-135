fab8efb0318973e888fd5f1d23c3c7021a3766b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:34 +0000

    Add package asyncify/0.6.0/asyncify-0.6.0.tar.gz

---
+++ packages/asyncify/asyncify-0.6.0.tar.gz/asyncify-0.6.0/asyncify/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Asyncify"""
+from asyncify._meta import __version__
+from asyncify.core import asyncify, await_or_return, is_async, run
+from funkify import funkify
+
+funkify(asyncify, key="asyncify")
+
+__all__ = ("__version__", "asyncify", "run", "await_or_return", "is_async")
