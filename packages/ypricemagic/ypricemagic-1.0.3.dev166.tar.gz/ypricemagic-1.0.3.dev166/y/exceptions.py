6e5002754e02ff8353ccde9a85ded901469da1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:37 +0000

    Add package ypricemagic/1.0.3.dev166/ypricemagic-1.0.3.dev166.tar.gz

---
+++ packages/ypricemagic/ypricemagic-1.0.3.dev166.tar.gz/ypricemagic-1.0.3.dev166/y/exceptions.py
@@ -0,0 +1,19 @@
+
+class PriceError(Exception):
+    pass
+
+class UnsupportedNetwork(Exception):
+    pass
+
+class NonStandardERC20(Exception):
+    pass
+
+class CalldataPreparationError(Exception):
+    pass
+
+class ContractNotVerified(Exception):
+    pass
+
+def contract_not_verified(e: Exception) -> bool:
+    message = 'Contract source code not verified'
+    return True if message not in str(e) else False
