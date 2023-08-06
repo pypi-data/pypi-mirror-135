8bdaa2d98d9a737f7d7aa66c4df493b23004d54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:03 +0000

    Add package ase-grain/0.1.4/ase-grain-0.1.4.tar.gz

---
+++ packages/ase-grain/ase-grain-0.1.4.tar.gz/ase-grain-0.1.4/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+exec(open("ase_grain/_version.py").read())
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ase-grain",
+    version=__version__,
+    author="Harry Zhang",
+    author_email="zhanghar@iu.edu",
+    description="An async wrapper for ASE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Contextualist/ase-grain",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "ase >= 3.22.0, < 3.23.0",
+        "grain-scheduler >= 0.12.1",
+        "trio",
+    ],
+    tests_require=[
+        "pytest",
+        "pytest-trio",
+    ],
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Framework :: Trio",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+    ],
+)
