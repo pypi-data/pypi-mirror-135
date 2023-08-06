0eb4796062884bc1f5af28321d81133149102522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:49 +0000

    Add package ctof/0.2.0/ctof-0.2.0-py3-none-any.whl

---
+++ packages/ctof/ctof-0.2.0-py3-none-any.whl/ctof/__init__.py
@@ -0,0 +1,20 @@
+"""
+ctof 0.2.0 - 01/21/2022
+
+ctof is a simple library that performs temperature unit conversion.
+The common units Celsius, Fahrenheit and Kelvin are supported.
+Valid input values can be an int, a float, or a list/tuple/set of
+ints and/or floats.
+
+NOTICE:
+ctof versions 0.2.0 and above are INCOMPATIBLE with any previous version.
+
+Compatible with Python 3.6+ (Python 3.7+ required to run the tests)
+This work is released under the terms of the MIT License.
+
+Author:     David Costell <davidcostell44@gmail.com>
+Repository: https://github.com/DontEatThemCookies/ctof
+"""
+
+from convert import *
+from version import *
