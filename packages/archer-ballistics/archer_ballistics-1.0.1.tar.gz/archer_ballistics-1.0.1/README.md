9ac53bb5136d2566959289b86ff14fc9b90ef9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:02 +0000

    Add package archer-ballistics/1.0.1/archer_ballistics-1.0.1.tar.gz

---
+++ packages/archer-ballistics/archer_ballistics-1.0.1.tar.gz/archer_ballistics-1.0.1/README.md
@@ -0,0 +1,13 @@
+Archer Ballistics Calculator
+=========
+
+Installation
+------------
+    pip install archer_ballistics
+
+Usage
+-----
+
+```python
+    
+```
---
+++ packages/archer-ballistics/archer_ballistics-1.0.1.tar.gz/archer_ballistics-1.0.1/archer_ballistics/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from . import archer_ballistics
+
+assert archer_ballistics
+
+__author__ = "o-murphy"
+__copyright__ = ("",)
+
+__credits__ = ["o-murphy"]
+__version__ = "1.0.1"
+
+__all__ = ['archer_ballistics']
