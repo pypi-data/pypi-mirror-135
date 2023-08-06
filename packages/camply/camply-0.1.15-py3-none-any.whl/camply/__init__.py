bf042f9ae7b71c688f497bc316bcb40eaca48327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:54 +0000

    Add package camply/0.1.15/camply-0.1.15-py3-none-any.whl

---
+++ packages/camply/camply-0.1.15-py3-none-any.whl/camply/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author::    Justin Flannery  (mailto:juftin@juftin.com)
+
+"""
+camply __init__ file
+"""
+
+from ._version import __version__
+
+__all__ = [
+    "__version__"
+]
