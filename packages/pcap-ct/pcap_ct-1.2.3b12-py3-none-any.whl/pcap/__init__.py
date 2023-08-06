d7e07480162aa96abd5a45a216a0f50cdb92c32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:41 +0000

    Add package pcap-ct/1.2.3b12/pcap_ct-1.2.3b12-py3-none-any.whl

---
+++ packages/pcap-ct/pcap_ct-1.2.3b12-py3-none-any.whl/pcap/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2016-2022, Adam Karpierz
+# Licensed under the BSD license
+# https://opensource.org/licenses/BSD-3-Clause
+
+from .__about__ import * ; del __about__  # noqa
+__url__ = __uri__
+
+from ._pcap import *  # noqa
