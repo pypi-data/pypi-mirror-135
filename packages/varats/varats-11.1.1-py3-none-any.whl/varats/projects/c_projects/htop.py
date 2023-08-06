a4bdc2a62b7ad34095bf4fe5fd6d68aa0907f5f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:11 +0000

    Add package varats/11.1.1/varats-11.1.1-py3-none-any.whl

---
+++ packages/varats/varats-11.1.1-py3-none-any.whl/varats/projects/c_projects/htop.py
@@ -0,0 +1,76 @@
+"""Project file for htop."""
+import typing as tp
+
+import benchbuild as bb
+from benchbuild.utils.cmd import make
+from benchbuild.utils.settings import get_number_of_jobs
+from plumbum import local
+
+from varats.containers.containers import get_base_image, ImageBase
+from varats.paper_mgmt.paper_config import project_filter_generator
+from varats.project.project_domain import ProjectDomains
+from varats.project.project_util import (
+    ProjectBinaryWrapper,
+    wrap_paths_to_binaries,
+    BinaryType,
+    verify_binaries,
+    get_local_project_git_path,
+)
+from varats.project.varats_project import VProject
+from varats.utils.git_util import ShortCommitHash, RevisionBinaryMap
+from varats.utils.settings import bb_cfg
+
+
+class Htop(VProject):
+    """Process visualization tool (fetched by Git)"""
+
+    NAME = 'htop'
+    GROUP = 'c_projects'
+    DOMAIN = ProjectDomains.UNIX_TOOLS
+
+    SOURCE = [
+        bb.source.Git(
+            remote="https://github.com/htop-dev/htop.git",
+            local="htop",
+            refspec="origin/HEAD",
+            limit=None,
+            shallow=False,
+            version_filter=project_filter_generator("htop")
+        )
+    ]
+
+    CONTAINER = get_base_image(ImageBase.DEBIAN_10).run(
+        'apt', 'install', '-y', 'autoconf', 'automake', 'autotools-dev',
+        'libtool'
+    )
+
+    @staticmethod
+    def binaries_for_revision(
+        revision: ShortCommitHash
+    ) -> tp.List[ProjectBinaryWrapper]:
+        binary_map = RevisionBinaryMap(get_local_project_git_path(Htop.NAME))
+
+        binary_map.specify_binary('htop', BinaryType.EXECUTABLE)
+
+        return binary_map[revision]
+
+    def run_tests(self) -> None:
+        pass
+
+    def compile(self) -> None:
+        """Compile the project."""
+        htop_source = local.path(self.source_of_primary)
+
+        clang = bb.compiler.cc(self)
+        with local.cwd(htop_source):
+            with local.env(CC=str(clang)):
+                bb.watch(local["./autogen.sh"])()
+                bb.watch(local["./configure"])()
+
+            bb.watch(make)("-j", get_number_of_jobs(bb_cfg()))
+
+            verify_binaries(self)
+
+    @classmethod
+    def get_cve_product_info(cls) -> tp.List[tp.Tuple[str, str]]:
+        return [("Htop", "Htop")]
