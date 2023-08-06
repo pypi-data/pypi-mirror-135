ce4f7c7982b9938c804cfa4a9841996a2a1af038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:22 +0000

    Add package pykin/0.5.1/pykin-0.5.1.tar.gz

---
+++ packages/pykin/pykin-0.5.1.tar.gz/pykin-0.5.1/pykin/collision/collision_manager.py
@@ -0,0 +1,383 @@
+import numpy as np
+import collections 
+import itertools
+import copy
+
+try:
+    import fcl
+    import trimesh
+except BaseException:
+    fcl = None
+    trimesh = None
+
+from pykin.utils.error_utils import CollisionError
+from pykin.utils.transform_utils import get_h_mat
+from pykin.utils.log_utils import create_logger
+from pykin.collision.contact_data import ContactData
+logger = create_logger('Collision Manager', "debug",)
+
+class CollisionManager:
+    """
+    A rigid body collision manager.
+    """
+
+    def __init__(self, mesh_path=None):
+        self.mesh_path = mesh_path
+        self._objs = {}
+        self._names = collections.defaultdict(lambda: None)
+        self._manager = fcl.DynamicAABBTreeCollisionManager()
+        self._manager.setup()
+        self._filter_names = set()
+
+    def __repr__(self):
+        return 'pykin.collision.collision_manager.{}()'.format(type(self).__name__)
+
+    def setup_robot_collision(self, robot, fk=None, geom="visual"):
+        if fk is None:
+            fk = robot.init_transformations
+        self._filter_contact_names(robot, fk, geom)
+
+    def setup_object_collision(self, objects):
+        for name, info in objects:
+            self.add_object(name, info[0], info[1], info[2])
+
+    def _filter_contact_names(self, robot, fk, geom):            
+        is_collision = False
+
+        def _get_collision_datas():
+            nonlocal is_collision
+            result = []
+            for (name1, name2) in self._filter_names:
+                index_name1 = list(self._objs.keys()).index(name1)
+                index_name2 = list(self._objs.keys()).index(name2)
+
+                if abs(index_name1-index_name2) > 1:
+                    for joint in robot.joints.values():
+                        if name1 == joint.parent:
+                            if joint.dtype == "revolute":
+                                print(name1, name2)
+                                print(index_name1, index_name2)
+                                is_collision = True
+                                result.append((name1, name2))
+            return result
+
+        for link, transformation in fk.items():
+            if geom == "visual":
+                if robot.links[link].visual.gtype == "mesh":
+                    mesh_name = robot.links[link].visual.gparam.get('filename')
+                    file_name = self.mesh_path + mesh_name
+                    mesh = trimesh.load_mesh(file_name)
+                    A2B = np.dot(transformation.h_mat, robot.links[link].visual.offset.h_mat)
+                    self.add_object(robot.links[link].name, "mesh", mesh, A2B)
+
+            if geom == "collision":
+                if robot.links[link].collision.gtype == "mesh":
+                    mesh_name = robot.links[link].collision.gparam.get('filename')
+                    file_name = self.mesh_path + mesh_name
+                    mesh = trimesh.load_mesh(file_name)
+                    A2B = np.dot(transformation.h_mat, robot.links[link].collision.offset.h_mat)
+                    self.add_object(robot.links[link].name, "mesh", mesh, A2B)
+
+        _, names = self.in_collision_internal(return_names=True)
+        self._filter_names = copy.deepcopy(names)
+
+        if robot.robot_name == "ur5e":
+            return
+
+        collision_datas = _get_collision_datas()
+        if is_collision:
+            for name1, name2 in collision_datas:
+                logger.error(f"{name1} and {name2} is Collision..")
+            raise CollisionError(f"Conflict confirmed. Check the joint settings again") 
+    
+    def add_object(self, 
+                   name, 
+                   gtype=None,
+                   gparam=None,
+                   transform=None):
+        """
+        Add an object to the collision manager.
+        If an object with the given name is already in the manager, replace it.
+
+        Args:
+            name (str): An identifier for the object
+            gtype (str): object type (cylinder, sphere, box)
+            gparam (float or tuple): object parameter (radius, length, size)
+            transform (np.array): Homogeneous transform matrix for the object
+        """
+        if gtype is None:
+            return
+
+        if transform is None:
+            transform = np.eye(4)
+        transform = np.asanyarray(transform, dtype=np.float32)
+        if transform.shape != (4, 4):
+            if transform.shape == (3,):
+                transform = get_h_mat(position=transform)
+            else:
+                raise ValueError('transform must be (4,4)!')
+
+        if gtype == "mesh":
+            geom = self._get_BVH(gparam)
+        else:
+            geom = self._get_geom(gtype, gparam)
+        
+        t = fcl.Transform(transform[:3, :3], transform[:3, 3])
+        o = fcl.CollisionObject(geom, t)
+
+        # Add collision object to set
+        if name in self._objs:
+            self._manager.unregisterObject(self._objs[name])
+        self._objs[name] = {'obj': o,
+                            'geom': geom}
+        # store the name of the geometry
+        self._names[id(geom)] = name
+
+        self._manager.registerObject(o)
+        self._manager.update()
+
+    def set_transform(self, name=None, transform=np.eye(4)):
+        """
+        Set the transform for one of the manager's objects.
+        This replaces the prior transform.
+        
+        Args:
+            name (str): An identifier for the object already in the manager
+            transform (np.array): A new homogeneous transform matrix for the object
+        """
+        if name is None:
+            return
+            
+        if name in self._objs:
+            o = self._objs[name]['obj']
+            o.setRotation(transform[:3, :3])
+            o.setTranslation(transform[:3, 3])
+            self._manager.update(o)
+        else:
+            raise ValueError('{} not in collision manager!'.format(name))
+
+    def remove_object(self, name):
+        """
+        Delete an object from the collision manager.
+        
+        Args:
+            name (str): The identifier for the object
+        """
+        if name in self._objs:
+            self._manager.unregisterObject(self._objs[name]['obj'])
+            self._manager.update(self._objs[name]['obj'])
+            # remove objects from _objs
+            geom_id = id(self._objs.pop(name)['geom'])
+            # remove names
+            self._names.pop(geom_id)
+        else:
+            logger.warn('{} not in collision manager!'.format(name))
+
+    def reset_all_object(self):
+        """
+        Reset all object from the collision manager.
+        """
+        self._objs = {}
+        self._names = collections.defaultdict(lambda: None)
+        self._manager = fcl.DynamicAABBTreeCollisionManager()
+        self._manager.setup()
+
+    def in_collision_internal(self, return_names=False, return_data=False):
+        cdata = fcl.CollisionData()
+        if return_names or return_data:
+            cdata = fcl.CollisionData(request=fcl.CollisionRequest(
+                num_max_contacts=100000, enable_contact=False))
+
+        self._manager.collide(cdata, fcl.defaultCollisionCallback)
+
+        result = cdata.result.is_collision
+
+        objs_in_collision = set()
+        contact_data = []
+
+        if return_names or return_data:
+            for contact in cdata.result.contacts:
+                coll_names = (self._extract_name(contact.o1),self._extract_name(contact.o2))
+                coll_names = tuple(sorted(coll_names))
+
+                if (coll_names[0], coll_names[1]) in self._filter_names:
+                    continue
+
+                if ("obstacle" in coll_names[0] and "obstacle" in coll_names[1]):
+                    continue
+
+                if return_names:
+                    objs_in_collision.add(coll_names)
+                if return_data:
+                    contact_data.append(ContactData(coll_names, contact))
+
+        if not objs_in_collision:
+            result = False
+            objs_in_collision = "No object collided.."
+
+        return self._get_returns(return_names, return_data, result, objs_in_collision, contact_data)
+
+    def in_collision_other(self, other_manager=None, return_names=False, return_data=False):
+        if other_manager is None:
+            return 
+            
+        cdata = fcl.CollisionData()
+        if return_names or return_data:
+            cdata = fcl.CollisionData(request=fcl.CollisionRequest(
+                num_max_contacts=100000, enable_contact=False))
+
+        self._manager.collide(other_manager._manager, cdata, fcl.defaultCollisionCallback)
+
+        result = cdata.result.is_collision
+
+        objs_in_collision = set()
+        contact_data = []
+
+        if return_names or return_data:
+            for contact in cdata.result.contacts:
+                reverse = False
+                coll_names = (self._extract_name(contact.o1), other_manager._extract_name(contact.o2))
+
+                if (coll_names[0], coll_names[1]) in self._filter_names:
+                    continue
+
+                if ("obstacle" in coll_names[0] and "obstacle" in coll_names[1]):
+                    continue
+
+                if coll_names[0] is None:
+                    coll_names = (self._extract_name(contact.o2), other_manager._extract_name(contact.o1))
+                    reverse = True
+
+                if return_names:
+                    objs_in_collision.add(coll_names)
+                if return_data:
+                    if reverse:
+                        coll_names = reversed(coll_names)
+                    contact_data.append(ContactData(coll_names, contact))
+
+            if not objs_in_collision:
+                result = False
+                objs_in_collision = "No object collided.."
+
+        return self._get_returns(return_names, return_data, result, objs_in_collision, contact_data)
+
+    def get_distances_internal(self):
+        req = fcl.DistanceRequest()
+        res = fcl.DistanceResult()
+
+        result = collections.defaultdict(float)
+        for (o1, o2) in list(itertools.combinations(self._objs, 2)):
+            if (o1, o2) in self._filter_names:
+                continue
+            distance = np.round(fcl.distance(self._objs[o1]['obj'],self._objs[o2]['obj'], req, res), 6)
+            result[(o1, o2)] = distance
+        
+        return result
+
+    def get_distances_other(self, other_manager):
+        def _mix_objects():
+            for o1 in self._objs:
+                for o2 in other_manager._objs:
+                    yield (o1, o2)
+
+        req = fcl.DistanceRequest()
+        res = fcl.DistanceResult()
+        
+        result = collections.defaultdict(float)
+        for (o1, o2) in _mix_objects():
+            distance = np.round(fcl.distance(self._objs[o1]['obj'], other_manager._objs[o2]['obj'], req, res), 6)
+            result[(o1, o2)] = distance
+        
+        return result
+
+    @staticmethod
+    def _get_returns(return_names, return_data, *args):
+        if return_names and return_data:
+            return args[0], args[1], args[2]
+        elif return_names:
+            return args[0], args[1]
+        elif return_data:
+            return args[0], args[2]
+        else:
+            return args[0]
+
+    def _get_BVH(self, mesh):
+        """
+        Get a BVH for a mesh.
+
+        Parameters
+        -------------
+        mesh : Trimesh
+          Mesh to create BVH for
+
+        Returns
+        --------------
+        bvh : fcl.BVHModel
+          BVH object of source mesh
+        """
+        bvh = self.mesh_to_BVH(mesh)
+        return bvh
+
+    @staticmethod
+    def mesh_to_BVH(mesh):
+        """
+        Create a BVHModel object from a Trimesh object
+
+        Parameters
+        -----------
+        mesh : Trimesh
+        Input geometry
+
+        Returns
+        ------------
+        bvh : fcl.BVHModel
+        BVH of input geometry
+        """
+        bvh = fcl.BVHModel()
+        bvh.beginModel(num_tris_=len(mesh.faces),
+                    num_vertices_=len(mesh.vertices))
+        bvh.addSubModel(verts=mesh.vertices,
+                        triangles=mesh.faces)
+        bvh.endModel()
+        return bvh
+
+    @staticmethod
+    def _get_geom(gtype, gparam):
+        """
+        Get fcl geometry from robot's geometry type or params
+        
+        Args:
+            geom (CollisionObject): Input model
+        
+        Returns:
+            names (hashable): Name of input geometry
+        """
+        geom = None
+        if gtype == "cylinder":
+            radius = gparam[0]
+            length = gparam[1]
+            geom = fcl.Cylinder(radius, length)
+        elif gtype == "sphere":
+            radius = float(gparam)
+            geom = fcl.Sphere(radius)
+        elif gtype == "box":
+            size = gparam
+            geom = fcl.Box(*size)
+        elif gtype == "mesh":
+            pass
+        return geom
+
+    def _extract_name(self, geom):
+        """
+        Retrieve the name of an object from the manager by its
+        CollisionObject, or return None if not found.
+        
+        Args:
+            geom (CollisionObject): Input model
+        
+        Returns:
+            names (hashable): Name of input geometry
+        """
+        return self._names[id(geom)]
+
+
---
+++ packages/pykin/pykin-0.5.1.tar.gz/pykin-0.5.1/pykin/planners/cartesian_planner.py
@@ -0,0 +1,227 @@
+import numpy as np
+
+
+import pykin.utils.transform_utils as t_utils
+import pykin.utils.kin_utils as k_utils
+import pykin.kinematics.jacobian as jac
+
+from pykin.planners.planner import Planner
+from pykin.utils.error_utils import OriValueError, CollisionError
+from pykin.utils.kin_utils import ShellColors as sc, logging_time
+from pykin.utils.log_utils import create_logger
+from pykin.utils.transform_utils import get_linear_interpoation, get_quaternion_slerp
+
+logger = create_logger('Cartesian Planner', "debug",)
+
+class CartesianPlanner(Planner):
+    """
+    path planner in Cartesian space
+
+    Args:
+        robot(SingleArm or Bimanual): The manipulator robot type is SingleArm or Bimanual
+        self_collision_manager: CollisionManager for robot's self collision check
+        object_collision_manager: CollisionManager for collision check between robot and object
+        n_step(int): Number of waypoints
+        dimension(int): robot arm's dof
+        waypoint_type(str): Type of waypoint ex) "Linear", "Cubic", "Circular"
+    """
+    def __init__(
+        self,
+        robot,
+        self_collision_manager=None,
+        object_collision_manager=None,
+        n_step=500,
+        dimension=7,
+        waypoint_type="Linear"
+    ):
+        super(CartesianPlanner, self).__init__(
+            robot, 
+            self_collision_manager, 
+            object_collision_manager,
+            dimension)
+        self.n_step = n_step
+        self.waypoint_type = waypoint_type
+        self.eef_name = self.robot.eef_name
+        self.arm = None
+        self._dimension = dimension
+
+        super()._setup_q_limits()
+        super()._setup_eef_name()
+    
+    def __repr__(self):
+        return 'pykin.planners.cartesian_planner.{}()'.format(type(self).__name__)
+    
+    @logging_time
+    def get_path_in_joinst_space(
+        self, 
+        current_q=None,
+        goal_pose=None,
+        waypoints=None,
+        resolution=1, 
+        damping=0.5,
+        epsilon=1e-12,
+        pos_sensitivity=0.03,
+        is_slerp=False
+    ):
+        self._cur_qpos = super()._change_types(current_q)
+        self._goal_pose = super()._change_types(goal_pose)
+        init_fk = self.robot.kin.forward_kinematics(self.robot.desired_frames, self._cur_qpos)
+        self._cur_pose = self.robot.get_eef_pose(init_fk)
+
+        self._resolution = resolution
+        self._damping = damping
+        self._pos_sensitivity = pos_sensitivity
+        self._is_slerp = is_slerp
+
+        if waypoints is None:
+            waypoints = self.generate_waypoints(is_slerp)
+
+        paths, target_positions = self._compute_path_and_target_pose(waypoints, epsilon)
+        
+        return paths, target_positions
+
+    def _compute_path_and_target_pose(self, waypoints, epsilon):
+        cnt = 0
+        total_cnt = 10
+        while True:
+            cnt += 1
+            collision_pose = {}
+            cur_fk = self.robot.kin.forward_kinematics(self.robot.desired_frames, self._cur_qpos)
+
+            current_transform = cur_fk[self.eef_name].h_mat
+            eef_position = cur_fk[self.eef_name].pos
+
+            paths = [self._cur_qpos]
+            target_positions = [eef_position]
+
+            for step, (pos, ori) in enumerate(waypoints):
+                target_transform = t_utils.get_h_mat(pos, ori)
+                err_pose = k_utils.calc_pose_error(target_transform, current_transform, epsilon) 
+                J = jac.calc_jacobian(self.robot.desired_frames, cur_fk, self._dimension)
+                J_dls = np.dot(J.T, np.linalg.inv(np.dot(J, J.T) + self._damping**2 * np.identity(6)))
+
+                dq = np.dot(J_dls, err_pose)
+                self._cur_qpos = np.array([(self._cur_qpos[i] + dq[i]) for i in range(self._dimension)]).reshape(self._dimension,)
+
+                is_collision_free = self._collision_free(self._cur_qpos)
+
+                if not is_collision_free:
+                    _, name = self.self_c_manager.in_collision_other(other_manager=self.object_c_manager, return_names=True) 
+                    
+                    collision_pose[step] = (name, np.round(target_transform[:3,3], 6))
+
+                    continue
+
+                if not self._check_q_in_limits(self._cur_qpos):
+                    continue
+
+                cur_fk = self.robot.kin.forward_kinematics(self.robot.desired_frames, self._cur_qpos)
+                current_transform = cur_fk[self.robot.eef_name].h_mat
+
+                if step % (1/self._resolution) == 0 or step == len(waypoints)-1:
+                    paths.append(self._cur_qpos)
+                    target_positions.append(pos)
+
+            err = t_utils.compute_pose_error(self._goal_pose[:3], cur_fk[self.eef_name].pos)
+            
+            if collision_pose.keys():
+                logger.error(f"Failed Generate Path.. Collision may occur.")
+                
+                for name, pose in collision_pose.values():
+                    logger.warning(f"\n\tCollision Names : {name} \n\tCollision Position : {pose}")
+                    # logger.warning(f"Collision Position : {pose}")
+                raise CollisionError("Conflict confirmed. Check the object position!")
+                
+            if err < self._pos_sensitivity:
+                logger.info(f"Generate Path Successfully!! Error is {err:6f}")
+                break
+
+            if cnt > total_cnt:
+                logger.error(f"Failed Generate Path.. The number of retries of {cnt} exceeded")
+                paths, target_positions = None, None
+                break
+
+            logger.error(f"Failed Generate Path.. Position Error is {err:6f}")
+            print(f"{sc.BOLD}Retry Generate Path, the number of retries is {cnt}/{total_cnt} {sc.ENDC}\n")
+            
+        return paths, target_positions
+
+    # TODO
+    # generate cubic, circular waypoints
+    def generate_waypoints(self, is_slerp):
+        if self.waypoint_type == "Linear":
+            waypoints = [path for path in self._get_linear_path(self._cur_pose, self._goal_pose, is_slerp)]
+        if self.waypoint_type == "Cubic":
+            pass
+        if self.waypoint_type == "Circular":
+            pass
+        return waypoints
+
+    def get_waypoints(self):
+        return self.waypoints
+
+    def _change_pose_type(self, pose):
+        ret = np.zeros(7)
+        ret[:3] = pose[:3]
+        
+        if isinstance(pose, (list, tuple)):
+            pose = np.asarray(pose)
+        ori = pose[3:]
+
+        if ori.shape == (3,):
+            ori = t_utils.get_quaternion_from_rpy(ori)
+            ret[3:] = ori
+        elif ori.shape == (4,):
+            ret[3:] = ori
+        else:
+            raise OriValueError(ori.shape)
+
+        return ret
+
+    def _get_linear_path(self, init_pose, goal_pose, is_slerp):
+        for step in range(1, self.n_step + 1):
+            delta_t = step / self.n_step
+            pos = get_linear_interpoation(init_pose[:3], goal_pose[:3], delta_t)
+            ori = init_pose[3:]
+            if is_slerp:
+                ori = get_quaternion_slerp(init_pose[3:], goal_pose[3:], delta_t)
+
+            yield (pos, ori)
+
+    def _get_cubic_path(self):
+        pass
+
+    def _get_cicular_path(self):
+        pass
+
+    @property
+    def resolution(self):
+        return self._resolution
+    
+    @resolution.setter
+    def resolution(self, resolution):
+        self._resolution = resolution
+
+    @property
+    def damping(self):
+        return self._damping
+    
+    @damping.setter
+    def damping(self, damping):
+        self._damping = damping
+
+    @property
+    def pos_sensitivity(self):
+        return self._pos_sensitivity
+    
+    @pos_sensitivity.setter
+    def pos_sensitivity(self, pos_sensitivity):
+        self._pos_sensitivity = pos_sensitivity
+
+    @property
+    def is_slerp(self):
+        return self._is_slerp
+    
+    @is_slerp.setter
+    def is_slerp(self, is_slerp):
+        self._is_slerp = is_slerp
