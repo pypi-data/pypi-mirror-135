da5ff992c11b8f4c902c088a8f76369b22ce7140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:33 +0000

    Add package fikkie/0.3.5/fikkie-0.3.5-py3-none-any.whl

---
+++ packages/fikkie/fikkie-0.3.5-py3-none-any.whl/fikkie/notifiers/telegram.py
@@ -0,0 +1,26 @@
+import logging
+
+
+__all__ = ["TelegramNotifier"]
+
+
+class TelegramNotifier:
+    """A telegram notifier."""
+
+    TYPE = "telegram"
+    ENCODING = "UTF-8"
+
+    def __init__(self, token: str, chat_id: str):
+        # Only import the telegram dependency when it's needed
+        try:
+            from telegram import Bot
+        except ModuleNotFoundError:  # pragma: no cover - no complexity here
+            logging.error("Please install the `python-telegram-bot` package.")
+            exit(1)
+
+        self._bot = Bot(token=token)
+        self._chat_id = chat_id
+
+    def notify(self, text: str) -> None:
+        """Sends a message."""
+        self._bot.sendMessage(chat_id=self._chat_id, text=text)
