2ee0d4c0a612328a756064200f58318d6ecbedbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:47 +0000

    Add package pyguru/0.0.2/pyguru-0.0.2.tar.gz

---
+++ packages/pyguru/pyguru-0.0.2.tar.gz/pyguru-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import codeop
+from ctypes.wintypes import LONG
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = "0.0.2"
+DESCRIPTION = "Labguru API python support"
+LONG_DESCRIPTION = "Python wrapper for the Labguru RESTful API"
+
+# Setting up
+setup(
+    name = "pyguru",
+    version = VERSION,
+    author = "jteske54",
+    author_email="teske.jacob@gmail.com",
+    url="https://github.com/jteske54/pyguru",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests','json','pandas','numpy'],
+    keywords=['python','labguru','pyguru','lab','guru'],
+
+
+
+)
