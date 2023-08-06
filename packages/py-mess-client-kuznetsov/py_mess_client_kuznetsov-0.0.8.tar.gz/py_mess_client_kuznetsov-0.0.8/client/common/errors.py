594fcaac14714d1b2a4c62e09553c387e65b4dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:57 +0000

    Add package py-mess-client-kuznetsov/0.0.8/py_mess_client_kuznetsov-0.0.8.tar.gz

---
+++ packages/py-mess-client-kuznetsov/py_mess_client_kuznetsov-0.0.8.tar.gz/py_mess_client_kuznetsov-0.0.8/client/common/errors.py
@@ -0,0 +1,31 @@
+"""Exception classes"""
+
+
+class IncorrectDataRecivedError(Exception):
+    """Exception - incorrect data received from the socket"""
+    def __str__(self):
+        return 'Принято некорректное сообщение от удалённого компьютера.'
+
+
+class ServerError(Exception):
+    """Exception - server error"""
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return self.text
+
+
+class NonDictInputError(Exception):
+    """Exception - the function argument is not a dictionary"""
+    def __str__(self):
+        return 'Аргумент функции должен быть словарём.'
+
+
+class ReqFieldMissingError(Exception):
+    """Error - a required field is missing in the accepted dictionary"""
+    def __init__(self, missing_field):
+        self.missing_field = missing_field
+
+    def __str__(self):
+        return f'В принятом словаре отсутствует обязательное поле {self.missing_field}.'
