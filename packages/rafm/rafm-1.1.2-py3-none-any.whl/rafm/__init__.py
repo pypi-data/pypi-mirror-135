61cf8444af7c90cc21ed3151a15919584d8343eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:39 +0000

    Add package rafm/1.1.2/rafm-1.1.2-py3-none-any.whl

---
+++ packages/rafm/rafm-1.1.2-py3-none-any.whl/rafm/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""rafm -- reliability of AlphaFold measures.
+
+\b
+For complete help visit https://github.com/joelb123/rafm
+\b
+License: MIT
+Copyright © 2021, The University of New Mexico.
+Copyright © 2021, GenerisBio, LLC.
+All rights reserved.
+"""  # noqa: D301
+from importlib import metadata
+
+NAME = "rafm"
+VERSION: str = metadata.version(NAME)
