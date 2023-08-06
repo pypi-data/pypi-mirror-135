19a60b48ee2d7e36489221cc8832518f6da1af65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:50:58 +0000

    Add package blahblah/1.4.1/blahblah-1.4.1.tar.gz

---
+++ packages/blahblah/blahblah-1.4.1.tar.gz/blahblah-1.4.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+
+
+def find_required():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+def find_dev_required():
+    with open("requirements-dev.txt") as f:
+        return f.read().splitlines()
+
+
+setup(
+    name="blahblah",
+    version="1.4.1",
+    description="Fake data generator for district42 schema",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Nikita Tsvetkov",
+    author_email="nikitanovosibirsk@yandex.com",
+    python_requires=">=3.8",
+    url="https://github.com/nikitanovosibirsk/blahblah",
+    license="Apache-2.0",
+    packages=find_packages(exclude=("tests",)),
+    package_data={"blahblah": ["py.typed"]},
+    install_requires=find_required(),
+    tests_require=find_dev_required(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development",
+        "Typing :: Typed",
+    ],
+)
