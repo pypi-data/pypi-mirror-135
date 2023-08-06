cbc3fb47501407b7ce2e81dc07b3e7ca3552967c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:19 +0000

    Add package BetterJSONStorage/0.2/BetterJSONStorage-0.2.tar.gz

---
+++ packages/BetterJSONStorage/BetterJSONStorage-0.2.tar.gz/BetterJSONStorage-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BetterJSONStorage',
+    version='0.2',
+    license='MIT',
+    author="Thomas Eeckout",
+    author_email='thomas.eeckout@outlook.be',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    keywords='tinyDB python orjson compressed database',
+    install_requires=[
+          'orjson',
+          'blosc'
+      ],
+)
