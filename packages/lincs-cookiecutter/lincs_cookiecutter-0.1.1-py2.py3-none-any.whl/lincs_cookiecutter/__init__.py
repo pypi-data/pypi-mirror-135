b26fd92bae6e4c96d8258f7d045e2487cd5fca32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:12 +0000

    Add package lincs-cookiecutter/0.1.1/lincs_cookiecutter-0.1.1-py2.py3-none-any.whl

---
+++ packages/lincs-cookiecutter/lincs_cookiecutter-0.1.1-py2.py3-none-any.whl/lincs_cookiecutter/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for lincs_cookiecutter."""
+
+__author__ = """Marc-Olivier Buob"""
+__email__ = 'marc-olivier.buob@nokia-bell-labs.com'
+__version__ = '0.1.1'
+
+
+from lincs_cookiecutter.sub_package_1.my_class_1 import MyClass1
+from lincs_cookiecutter.sub_package_2.my_class_2 import MyClass2
+from lincs_cookiecutter.sub_package_2.my_class_3 import MyClass3
