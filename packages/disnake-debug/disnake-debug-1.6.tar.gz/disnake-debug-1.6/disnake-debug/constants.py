14d3396a3f1f78bd694506d4e21f7420858b691d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:38 +0000

    Add package disnake-debug/1.6/disnake-debug-1.6.tar.gz

---
+++ packages/disnake-debug/disnake-debug-1.6.tar.gz/disnake-debug-1.6/disnake-debug/constants.py
@@ -0,0 +1,28 @@
+import pathlib
+
+# emojis
+THUMBS_UP = "👍"
+THUMBS_DOWN = "👎"
+ERROR = "❌"
+
+# info
+
+ROOT = pathlib.Path(__file__).parent
+VERSION = "1.6"
+
+BLACKLIST_HELP = """
+Send the name of the commands you want to blacklist the user from.
+    => Seperate the command names by a comma
+    => Send 'all' to blacklist the user from every command
+"""
+DESCRIPTION = f"""
+Welcome to disnake-debug v{VERSION}
+-----------------------------
+waiting:
+    - when the bot sends a question (non-embed message) it is waiting for you to send a response to that question
+        => to stop this just type q
+"""
+PATH_HELP = """
+Type the path you want to go to (eg /blacklist/user)
+Not all paths are available because some require additional info (eg which guild to leave in leave_guild/leave)
+"""
---
+++ packages/disnake-debug/disnake-debug-1.6.tar.gz/disnake-debug-1.6/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+import pathlib
+
+from hawkey import VERSION
+
+ROOT = pathlib.Path(__file__).parent
+with open(ROOT / "README.md", "r", encoding="utf-8") as f:
+    README = f.read()
+
+VERSION = "1.6"
+setup(
+    name="disnake-debug",
+    packages=["disnake-debug", "disnake-debug.dialogs"],
+    version=VERSION,
+    license="MIT",
+    description="A visual debugging extension for disnake discord bots including easy blacklisting, evaluation, echoing and much more!",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Caeden PH",
+    author_email="caedenperelliharris@gmail.com",
+    url="https://github.com/CaedenPH/disnake-debug",
+    download_url=f'https://github.com/CaedenPH/disnake-debug/archive/refs/tags/v_{VERSION.replace(".", "")}.tar.gz',
+    keywords=["disnake", "discord", "debugging", "visual", "extension", "cog"],
+    project_urls={
+        "Code": "https://github.com/CaedenPH/disnake-debug",
+    },
+    install_requires=["disnake", "aiosqlite",
+                      "fuzzywuzzy", "python-Levenshtein"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
