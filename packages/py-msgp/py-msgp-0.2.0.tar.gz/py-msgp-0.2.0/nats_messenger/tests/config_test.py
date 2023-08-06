c7f8af8a2f16d0eb2bb2f4c5d1ab06c717d6b38e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:35 +0000

    Add package py-msgp/0.2.0/py-msgp-0.2.0.tar.gz

---
+++ packages/py-msgp/py-msgp-0.2.0.tar.gz/py-msgp-0.2.0/nats_messenger/tests/config_test.py
@@ -0,0 +1,8 @@
+"""Common configuration parameters for test cases"""
+
+URL = ""
+CREDENTIALS = ""
+CLUSTER_ID = "py-msgp-cluster"
+CLIENT_ID = "test-client"
+TEST_TOPIC = "test-topic-name"
+TEST_PAYLOAD = b"test payload..."
