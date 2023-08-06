b4dbe9ff94f2b7fad395da055180ba3b5368301f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:49 +0000

    Add package tfhub-styletransfer-wrapper/1.0.5/tfhub_styletransfer_wrapper-1.0.5-py3-none-any.whl

---
+++ packages/tfhub-styletransfer-wrapper/tfhub_styletransfer_wrapper-1.0.5-py3-none-any.whl/tfhub_styletransfer_wrapper/imgFnc.py
@@ -0,0 +1,36 @@
+from matplotlib import gridspec
+import matplotlib.pylab as plt
+import tensorflow as tf
+
+
+def crop_center(image):
+    # Returns a cropped square image.
+    shape = image.shape
+    new_shape = min(shape[1], shape[2])
+    offset_y = max(shape[1] - shape[2], 0) // 2
+    offset_x = max(shape[2] - shape[1], 0) // 2
+    image = tf.image.crop_to_bounding_box(image, offset_y, offset_x, new_shape, new_shape)
+    return image
+
+
+def load_image(image_path, image_size=(256, 256), preserve_aspect_ratio=True):
+    # Loads and preprocesses images.
+    # Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
+    img = tf.io.decode_image(tf.io.read_file(image_path), channels=3, dtype=tf.float32)[tf.newaxis, ...]
+    img = crop_center(img)
+    img = tf.image.resize(img, image_size, preserve_aspect_ratio=preserve_aspect_ratio)
+    return img
+
+
+def show_images(images, titles=('',)):
+    n = len(images)
+    image_sizes = [image.shape[1] for image in images]
+    w = (image_sizes[0] * 6) // 320
+    plt.figure(figsize=(w * n, w))
+    gs = gridspec.GridSpec(1, n, width_ratios=image_sizes)
+    for i in range(n):
+        plt.subplot(gs[i])
+        plt.imshow(images[i][0], aspect='equal')
+        plt.axis('off')
+        plt.title(titles[i] if len(titles) > i else '')
+    plt.show()
