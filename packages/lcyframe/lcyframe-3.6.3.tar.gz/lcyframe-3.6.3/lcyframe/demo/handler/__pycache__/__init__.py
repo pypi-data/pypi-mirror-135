e2c7d1572ff7c99c48d7a2149d76b70d423f3e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:51 +0000

    Add package lcyframe/3.6.3/lcyframe-3.6.3.tar.gz

---
+++ packages/lcyframe/lcyframe-3.6.3.tar.gz/lcyframe-3.6.3/lcyframe/demo/handler/__pycache__/__init__.py
@@ -0,0 +1,17 @@
+# /usr/env/bin python
+# -*- coding:utf-8 -*-
+import os
+import re
+import logging
+dir_path = os.listdir(os.path.abspath(os.path.dirname(__file__)))
+
+
+# xxx_handler xx leng must > 1.
+hander_files = [x for x in dir_path if re.findall('[A-Za-z]\w+\.py$', x)]
+
+for hander_file in hander_files:
+    model_name = hander_file[:-3]
+    if model_name == "__init__":
+        continue
+    logging.warn("handler [%s] register success!" % hander_file)
+    __import__(model_name, globals(), locals(), [model_name], -1)
