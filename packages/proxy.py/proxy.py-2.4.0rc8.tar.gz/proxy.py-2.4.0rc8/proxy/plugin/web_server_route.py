40a6b1263cd187e4cf4c338f68a869500102bad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:47 +0000

    Add package proxy.py/2.4.0rc8/proxy.py-2.4.0rc8.tar.gz

---
+++ packages/proxy.py/proxy.py-2.4.0rc8.tar.gz/proxy.py-2.4.0rc8/proxy/plugin/web_server_route.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+    proxy.py
+    ~~~~~~~~
+    ⚡⚡⚡ Fast, Lightweight, Pluggable, TLS interception capable proxy server focused on
+    Network monitoring, controls & Application development, testing, debugging.
+
+    :copyright: (c) 2013-present by Abhinav Singh and contributors.
+    :license: BSD, see LICENSE for more details.
+"""
+import logging
+from typing import List, Tuple
+
+from ..http.parser import HttpParser
+from ..http.server import HttpWebServerBasePlugin, httpProtocolTypes
+from ..http.responses import okResponse
+
+
+logger = logging.getLogger(__name__)
+
+HTTP_RESPONSE = okResponse(content=b'HTTP route response')
+HTTPS_RESPONSE = okResponse(content=b'HTTPS route response')
+
+
+class WebServerPlugin(HttpWebServerBasePlugin):
+    """Demonstrates inbuilt web server routing using plugin."""
+
+    def routes(self) -> List[Tuple[int, str]]:
+        return [
+            (httpProtocolTypes.HTTP, r'/http-route-example$'),
+            (httpProtocolTypes.HTTPS, r'/https-route-example$'),
+            (httpProtocolTypes.WEBSOCKET, r'/ws-route-example$'),
+        ]
+
+    def handle_request(self, request: HttpParser) -> None:
+        if request.path == b'/http-route-example':
+            self.client.queue(HTTP_RESPONSE)
+        elif request.path == b'/https-route-example':
+            self.client.queue(HTTPS_RESPONSE)
