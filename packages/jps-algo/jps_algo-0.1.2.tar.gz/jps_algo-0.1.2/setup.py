75bb24f6a48be5195d43041874583bc4f1a82adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:22 +0000

    Add package jps-algo/0.1.2/jps_algo-0.1.2.tar.gz

---
+++ packages/jps-algo/jps_algo-0.1.2.tar.gz/jps_algo-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['jps_algo', 'jps_algo.utils']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'jps-algo',
+    'version': '0.1.2',
+    'description': "Jackson's Preemptive Scheduleを解くためのアルゴリズムが実装されている",
+    'long_description': None,
+    'author': '鈴木貴大',
+    'author_email': 'merioda.seven.24@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/tkp0331/jps-algo',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7.9,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
