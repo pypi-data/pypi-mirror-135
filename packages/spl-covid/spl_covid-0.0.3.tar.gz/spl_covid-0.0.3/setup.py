7750d2f9ffdbe8d8dfd13ffdc734f3160b721a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:56 +0000

    Add package spl-covid/0.0.3/spl_covid-0.0.3.tar.gz

---
+++ packages/spl-covid/spl_covid-0.0.3.tar.gz/spl_covid-0.0.3/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="spl_covid",
+    version="0.0.3",
+    author="Colin Simon-Fellowes",
+    author_email="colin.tsf@gmail.com",
+    description="Covid Statistics mini-widget for the amusement of Robert Remez",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=['tableauscraper', 'xlsxwriter', 'pandas'],
+    python_requires=">=3.6",
+)
