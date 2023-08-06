13a51d4d8622e50010e0cb2a9963d750b4289c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:03 +0000

    Add package INF367-chen/1.0.1/INF367-chen-1.0.1.tar.gz

---
+++ packages/INF367-chen/INF367-chen-1.0.1.tar.gz/INF367-chen-1.0.1/src/topological_neural_networks.py
@@ -0,0 +1,49 @@
+"""Description here!"""
+
+
+class SelfOrganizingMaps(object):
+    """Self organizing maps"""
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def fit(self):
+        raise NotImplementedError
+
+    def transform(self):
+        raise NotImplementedError
+
+    def predict(self):
+        raise NotImplementedError
+
+
+class GrowingNeuralGas(object):
+    """Growing Neural Gas"""
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def fit(self):
+        raise NotImplementedError
+
+    def transform(self):
+        raise NotImplementedError
+
+    def predict(self):
+        raise NotImplementedError
+
+
+class GenerativeGaussianGraphs(object):
+    """Generative Gaussian Graphs"""
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def fit(self):
+        raise NotImplementedError
+
+    def transform(self):
+        raise NotImplementedError
+
+    def predict(self):
+        raise NotImplementedError
