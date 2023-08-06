1e805e681f0786232c904529ad82ec511cd34c40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:45 +0000

    Add package imitate-client-snapshot/2022.0.0/imitate-client-snapshot-2022.0.0.tar.gz

---
+++ packages/imitate-client-snapshot/imitate-client-snapshot-2022.0.0.tar.gz/imitate-client-snapshot-2022.0.0/imitate/producer/producer.py
@@ -0,0 +1,21 @@
+
+
+class ImitateProducer:
+    """ImitateProducer
+
+    ATTRS:
+        prop (dict): producer config
+    """
+
+    def __init__(self, prop):
+        self.prop = prop
+
+
+
+    def send(self, message_record):
+        """ send a message record
+
+        :param message_record:  (MessageRecord)
+        :return: (MessageRecord)
+        """
+        pass
