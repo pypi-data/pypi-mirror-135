311567f450cda33cfff79ec469a03b47b1077c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:09 +0000

    Add package BeeDrive/0.1.0.1/BeeDrive-0.1.0.1-py3-none-any.whl

---
+++ packages/BeeDrive/BeeDrive-0.1.0.1-py3-none-any.whl/beedrive/__init__.py
@@ -0,0 +1,6 @@
+
+__name__ = "BeeDrive"
+__version__ = "0.1.0.1"
+__date__ = 20220122
+__author__ = "Xuansheng Wu"
+__update__ = "The first version of BeeDrive, including cloud, proxy, upload, and download."
