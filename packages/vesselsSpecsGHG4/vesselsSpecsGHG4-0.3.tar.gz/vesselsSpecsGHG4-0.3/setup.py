cf1a89140d6341f23f5693a14a8580ea40f0fb14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:10 +0000

    Add package vesselsSpecsGHG4/0.3/vesselsSpecsGHG4-0.3.tar.gz

---
+++ packages/vesselsSpecsGHG4/vesselsSpecsGHG4-0.3.tar.gz/vesselsSpecsGHG4-0.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 19 00:57:05 2022
+
+@author: gabri
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='vesselsSpecsGHG4',
+    version='0.3',
+    license='MIT',
+    author="Gabriel Fuentes",
+    author_email='gabriel.fuentes@snf.no',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url="https://github.com/gabrielfuenmar/vessels_transformation_for_GHG4",
+    keywords='GHG4adapter',
+    install_requires=[
+          'scikit-learn',
+          'pandas'
+      ],
+
+)
