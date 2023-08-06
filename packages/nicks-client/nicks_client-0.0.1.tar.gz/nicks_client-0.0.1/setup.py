66a8783f58bf1ca3444a329f89a5af62bc3edaf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:21 +0000

    Add package nicks-client/0.0.1/nicks_client-0.0.1.tar.gz

---
+++ packages/nicks-client/nicks_client-0.0.1.tar.gz/nicks_client-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="nicks_client",
+      version="0.0.1",
+      description="GB Client",
+      author="Nick",
+      author_email="nezgovorov.nikita@gmail.com",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
