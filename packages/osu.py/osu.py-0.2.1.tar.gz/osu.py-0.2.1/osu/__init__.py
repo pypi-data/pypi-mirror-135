4e2da78c6d860ff6dcc4d0e82c48b8bb94310d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:29 +0000

    Add package osu.py/0.2.1/osu.py-0.2.1.tar.gz

---
+++ packages/osu.py/osu.py-0.2.1.tar.gz/osu.py-0.2.1/osu/__init__.py
@@ -0,0 +1,8 @@
+from .auth import AuthHandler
+from .objects import *
+from .client import Client
+from .exceptions import *
+from .notification import NotificationWebsocket
+
+
+__version__ = '0.2.1'
