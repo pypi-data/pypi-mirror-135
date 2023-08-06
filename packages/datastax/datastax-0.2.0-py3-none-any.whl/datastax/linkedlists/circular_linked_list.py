aa5184c9d342c31633a8147e5251a54f6a25c7e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:29 +0000

    Add package datastax/0.2.0/datastax-0.2.0-py3-none-any.whl

---
+++ packages/datastax/datastax-0.2.0-py3-none-any.whl/datastax/linkedlists/circular_linked_list.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from typing import Any
+
+from datastax.linkedlists.linked_list import Node, LinkedList
+
+
+class CircularLinkedList(LinkedList):
+    def append(self, data: Any) -> None:
+        super().append(data)
+        self.tail.next = self.head
+
+    def insert(self, data: Any):
+        super().insert(data)
+        self.tail.next = self.head
+
+    def __str__(self, node: Node = None):
+        head = node or self.head
+        if not head:
+            return "NULL"
+        string = "┌->"
+        ref = head
+        while True:
+            string += f' Node[{str(ref.data)}] ->'
+            ref = ref.next
+            if ref is head:
+                break
+        string += f"┐\n└{'<'.center(len(string) - 1, '─')}┘"
+        return string
