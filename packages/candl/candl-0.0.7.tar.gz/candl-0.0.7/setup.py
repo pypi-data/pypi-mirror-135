a096f563a394e8940961acc8823d1af4a4c71dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:12 +0000

    Add package candl/0.0.7/candl-0.0.7.tar.gz

---
+++ packages/candl/candl-0.0.7.tar.gz/candl-0.0.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="candl",
+    version="0.0.7",
+    author="Sri Pranav Kunda",
+    author_email="sripkunda@gmail.com",
+    description="A tiny, pedagogical neural network library with a pytorch-like API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sripkunda/candl/",
+    project_urls={
+        "Bug Tracker": "https://github.com/sripkunda/candl/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
