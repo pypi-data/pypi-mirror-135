84f7af8c4bbaad61ceec95de35f351f624c57a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:41 +0000

    Add package capturejob/0.1.3/capturejob-0.1.3-py3-none-any.whl

---
+++ packages/capturejob/capturejob-0.1.3-py3-none-any.whl/capturejob/__init__.py
@@ -0,0 +1,46 @@
+import os
+
+from loguru import logger
+from dyndebug import Debug
+from typing import Callable, Any
+from dotmap import DotMap
+from capturejob.service.fileservice import FileService
+from datetime import datetime
+
+def checkPrerequsites(variable):
+    if os.getenv(variable) is None:
+        return True
+    return False
+
+
+def CaptureJob():
+    debug = Debug('capturejob')
+
+    issues = list(
+        filter(
+            checkPrerequsites,
+            ["CAPTURE_CONNECTION_STRING","CAPTURE_CONTAINER_NAME","JOB"],
+        )
+    )
+    if len(issues) > 0:
+        raise Exception(f"Missing environment variables {issues}")
+        
+    config = DotMap({
+        "connectionString": os.environ.get("CAPTURE_CONNECTION_STRING"),
+        "containerName": os.environ.get("CAPTURE_CONTAINER_NAME"),
+    })
+
+    print(config.containerName)
+    fileservice =FileService(config)
+
+    job = os.environ.get("JOB")
+
+    now = datetime.now() 
+    date_time = now.strftime("%Y%m%d_%H%M%S")    
+
+    cwd = os.getcwd()
+    
+    folder = f"{date_time}_{job}"
+    fileservice.upload(os.path.join(cwd,"stdout.txt"),folder,"stdout.txt")
+    fileservice.upload(os.path.join(cwd,"stderr.txt"),folder,"stderr.txt")
+ 
