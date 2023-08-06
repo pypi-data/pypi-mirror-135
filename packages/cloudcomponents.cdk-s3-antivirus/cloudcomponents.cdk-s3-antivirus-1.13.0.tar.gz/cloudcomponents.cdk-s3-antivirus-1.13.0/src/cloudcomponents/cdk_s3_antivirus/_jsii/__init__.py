f2142dbf003beeb0470c4aff832da2621f61bfcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:03 +0000

    Add package cloudcomponents.cdk-s3-antivirus/1.13.0/cloudcomponents.cdk-s3-antivirus-1.13.0.tar.gz

---
+++ packages/cloudcomponents.cdk-s3-antivirus/cloudcomponents.cdk-s3-antivirus-1.13.0.tar.gz/cloudcomponents.cdk-s3-antivirus-1.13.0/src/cloudcomponents/cdk_s3_antivirus/_jsii/__init__.py
@@ -0,0 +1,34 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_efs._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_event_sources._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cloudcomponents/cdk-s3-antivirus",
+    "1.13.0",
+    __name__[0:-6],
+    "cdk-s3-antivirus@1.13.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
