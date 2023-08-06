fd559519a70fb6a04c83a7e3d6fa8e14212a6f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:55 +0000

    Add package CSUMMDET/1.0.21/CSUMMDET-1.0.21.tar.gz

---
+++ packages/CSUMMDET/CSUMMDET-1.0.21.tar.gz/CSUMMDET-1.0.21/mmdet/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Fri Dec 18 14:39:30 2020
+
+__version__ = '1.0.rc0+HEAD'
+short_version = '1.0.rc0'
---
+++ packages/CSUMMDET/CSUMMDET-1.0.21.tar.gz/CSUMMDET-1.0.21/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages  # 这个包没有的可以pip一下
+
+setup(
+    name="CSUMMDET",  # 这里是pip项目发布的名称
+    version="1.0.21",  # 版本号，数值大的会优先被pip
+    keywords=["pip"],
+    description="person or car",
+    license="MIT Licence",
+    author="csu_ywj",
+    packages=find_packages(),
+    data_files=["mmdet/ops/dcn/deform_conv_cuda.cpython-36m-x86_64-linux-gnu.so",
+                "mmdet/ops/dcn/deform_pool_cuda.cpython-36m-x86_64-linux-gnu.so"],
+    include_package_data=True,
+    platforms="any",
+)
