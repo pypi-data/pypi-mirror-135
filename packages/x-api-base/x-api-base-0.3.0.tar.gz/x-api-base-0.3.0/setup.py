0b290b8445f484cce6b9297b5f12606c927e091b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:17 +0000

    Add package x-api-base/0.3.0/x-api-base-0.3.0.tar.gz

---
+++ packages/x-api-base/x-api-base-0.3.0.tar.gz/x-api-base-0.3.0/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import sys
+
+from setuptools import setup, find_packages
+
+PACKAGE = 'basex'
+NAME = 'x-api-base'
+PY_VER = sys.version_info
+REQUIREMENTS = [
+    'orjson',
+    'loguru',
+    'PyYAML',
+    'pydantic',
+    'sqlalchemy>=1.4'
+]
+package = __import__(PACKAGE)
+VERSION = package.__version__
+AUTHOR = package.__author__
+DESCRIBE = package.__describe__
+
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    'Operating System :: OS Independent',
+    'Environment :: Web Environment',
+    'Development Status :: 3 - Alpha',
+    'Topic :: Internet',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+]
+
+KEYWORDS = ["api", "x-api", "x-api-base"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email="408856732@qq.com",
+    description=DESCRIBE,
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="BSD 2-Clause",
+    url=f"https://github.com/yinziyan1206/x-base",
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+    keywords=KEYWORDS,
+    classifiers=CLASSIFIERS
+)
