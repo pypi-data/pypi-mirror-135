f5734c3b7b8a3a975cc9d3a4734f638fff72287f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:07 +0000

    Add package EASGen/0.1.2/EASGen-0.1.2.tar.gz

---
+++ packages/EASGen/EASGen-0.1.2.tar.gz/EASGen-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='EASGen',
+    packages=find_packages(include=['EASGen']),
+    version='0.1.2',
+    description='A Python library to generate EAS SAME Audio using Raw Data',
+    author='A-c0rN',
+    license='ODbL-1.0',
+    install_requires=['pydub'],
+)
+
