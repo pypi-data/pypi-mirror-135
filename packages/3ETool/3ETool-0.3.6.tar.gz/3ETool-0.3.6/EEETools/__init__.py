c6e3d58d330bd9b40afc75aeba438c68c7f09ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:22 +0000

    Add package 3ETool/0.3.6/3ETool-0.3.6.tar.gz

---
+++ packages/3ETool/3ETool-0.3.6.tar.gz/3ETool-0.3.6/EEETools/__init__.py
@@ -0,0 +1,20 @@
+import inspect
+
+called_from_setup = False
+
+for frame in inspect.stack():
+
+    if "setup.py" in frame.filename:
+
+        called_from_setup = True
+        break
+
+if not called_from_setup:
+
+    from EEETools.Tools.API.terminal_api import paste_default_excel_file, paste_components_documentation, paste_user_manual
+    from EEETools.Tools.API.terminal_api import calculate, launch_connection_debug, launch_network_display
+
+else:
+
+    from EEETools.Tools.Other.resource_downloader import update_resource_folder
+    update_resource_folder(force_update=True)
