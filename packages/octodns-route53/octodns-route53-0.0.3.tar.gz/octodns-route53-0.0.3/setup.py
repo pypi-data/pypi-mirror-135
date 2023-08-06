0155fb7b852ed893648d120fb0deba761d511582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:49:36 +0000

    Add package octodns-route53/0.0.3/octodns-route53-0.0.3.tar.gz

---
+++ packages/octodns-route53/octodns-route53-0.0.3.tar.gz/octodns-route53-0.0.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+
+
+def descriptions():
+    with open('README.md') as fh:
+        ret = fh.read()
+        first = ret.split('\n', 1)[0].replace('#', '')
+        return first, ret
+
+
+def version():
+    with open('octodns_route53/__init__.py') as fh:
+        for line in fh:
+            if line.startswith('__VERSION__'):
+                return line.split("'")[1]
+
+
+description, long_description = descriptions()
+
+setup(
+    author='Ross McFarland',
+    author_email='rwmcfa1@gmail.com',
+    description=description,
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    name='octodns-route53',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=('octodns>=0.9.14', 'boto3>=1.20.26'),
+    url='https://github.com/octodns/octodns-route53',
+    version=version(),
+    tests_require=[
+        'mock>=4.0.3',
+        'pytest',
+        'pytest-network',
+    ],
+)
