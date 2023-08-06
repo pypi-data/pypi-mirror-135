de896233980b64f623f4655ba26528698db518bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:36 +0000

    Add package Colorfully/0.0.0/Colorfully-0.0.0.tar.gz

---
+++ packages/Colorfully/Colorfully-0.0.0.tar.gz/Colorfully-0.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Colorfully",
+    version='',
+    author="Suzuya",
+    author_email="suzuyagvng@protobmail.com",
+    description='To make a beautiful script in python',
+    long_description_content_type="text/markdown",
+    long_description='To make a beautiful script in python',
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'color', 'colorama'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
