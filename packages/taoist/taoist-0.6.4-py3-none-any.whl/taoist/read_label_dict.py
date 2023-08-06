7bf660bbfaf8f34d56a7ef8823b7609a1ad6df78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:25 +0000

    Add package taoist/0.6.4/taoist-0.6.4-py3-none-any.whl

---
+++ packages/taoist/taoist-0.6.4-py3-none-any.whl/taoist/read_label_dict.py
@@ -0,0 +1,29 @@
+""""read_label_dict.py"""
+
+from typing import Dict
+from configparser import ConfigParser
+from todoist_api_python.api_async import TodoistAPIAsync
+
+
+async def read_label_dict(config: ConfigParser) -> Dict:
+    """
+    Read label list into dictionary
+    """
+
+    # Initialize Todoist API
+    api = TodoistAPIAsync(config['Default']['token'])
+
+    # Initialize label dictionary
+    label_dict = {}
+
+   # Get labels
+    try:
+        labels = await api.get_labels()
+    except Exception as error:
+        raise error
+    
+    # Construct label dictionary
+    for label in labels:
+        label_dict[label.id] = label
+    
+    return label_dict
