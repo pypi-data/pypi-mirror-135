e122d20b0958329d01c24338ebb83ebc585fc64a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:33 +0000

    Add package rolls-blockchain/1.3a1.dev3/rolls-blockchain-1.3a1.dev3.tar.gz

---
+++ packages/rolls-blockchain/rolls-blockchain-1.3a1.dev3.tar.gz/rolls-blockchain-1.3a1.dev3/rolls-blockchain-gui/src/theme/default.ts
@@ -0,0 +1,22 @@
+export default {
+  palette: {
+    primary: {
+      main: '#a56930',
+      contrastText: '#FEFEFE',
+    },
+    secondary: {
+      main: '#000000',
+      contrastText: '#FEFEFE',
+    },
+    danger: {
+      main: '#dc3545',
+      contrastText: '#FEFEFE',
+    },
+    success: {
+      main: '#59A96A'
+    }
+  },
+  drawer: {
+    width: '100px',
+  },
+};
