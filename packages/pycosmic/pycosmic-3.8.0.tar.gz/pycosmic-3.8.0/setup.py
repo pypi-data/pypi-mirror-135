931146f949d42894e23d6131569861694668651b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:55 +0000

    Add package pycosmic/3.8.0/pycosmic-3.8.0.tar.gz

---
+++ packages/pycosmic/pycosmic-3.8.0.tar.gz/pycosmic-3.8.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup ,find_packages
+
+
+
+setup(
+
+name = "pycosmic",
+version="3.8.0",
+description="Packages That Shortens The Code  ",
+author="Rishabh",
+author_email="rishivisionworld@gmail.com",
+install_requires=['numpy','colorama'],
+packages=find_packages(),
+
+
+)
