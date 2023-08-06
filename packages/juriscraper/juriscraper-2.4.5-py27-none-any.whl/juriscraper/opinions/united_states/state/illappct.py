f16683fc391df0dffd4251f3c94489f38184521b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:16 +0000

    Add package juriscraper/2.4.5/juriscraper-2.4.5-py27-none-any.whl

---
+++ packages/juriscraper/juriscraper-2.4.5-py27-none-any.whl/juriscraper/opinions/united_states/state/illappct.py
@@ -0,0 +1,36 @@
+"""
+Author: Krist Jin
+History:
+  2013-08-18: Created.
+  2014-07-17: Updated by mlr to remedy InsanityException.
+  2021-11-02: Updated by satsuki-chan: Updated to new page design.
+"""
+import re
+from typing import List
+
+from juriscraper.opinions.united_states.state import ill
+
+
+class Site(ill.Site):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.court_id = self.__module__
+        self.url = (
+            "https://www.illinoiscourts.gov/top-level-opinions?type=appellate"
+        )
+
+    def _get_docket_numbers(self) -> List[str]:
+        """Get the docket number from citation.
+
+        Extract district and docket number to construct the full docket number.
+
+        Returns: List of docket numbers
+        """
+        dockets_numbers = []
+        for case in self.cases:
+            m = re.search(self.docket_re, case["citation"])
+            raw_docket = m.group("docket")
+            district = m.group("district")
+            docket = f"{district}-{raw_docket[0:2]}-{raw_docket[2:]}"
+            dockets_numbers.append(docket)
+        return dockets_numbers
