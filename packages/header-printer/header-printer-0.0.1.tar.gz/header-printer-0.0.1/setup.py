5dd30121b4cc2fae04c2e5b52733a623dbdc2354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:42 +0000

    Add package header-printer/0.0.1/header-printer-0.0.1.tar.gz

---
+++ packages/header-printer/header-printer-0.0.1.tar.gz/header-printer-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="header-printer",
+    version="0.0.1",
+    description="Python package for printing a nicely formatted header.",
+    author="Michał Wiśniewski",
+    py_modules=["printer"],
+    package_dir={"": "header_printer"},
+)
