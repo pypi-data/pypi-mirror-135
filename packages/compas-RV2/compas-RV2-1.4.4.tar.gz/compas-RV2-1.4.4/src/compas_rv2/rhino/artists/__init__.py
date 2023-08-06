ce8d8e45bb09eb414bc754fb68923dd07facf9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:23 +0000

    Add package compas-RV2/1.4.4/compas-RV2-1.4.4.tar.gz

---
+++ packages/compas-RV2/compas-RV2-1.4.4.tar.gz/compas-RV2-1.4.4/src/compas_rv2/rhino/artists/__init__.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+from compas.datastructures import Mesh
+
+from compas_rv2.datastructures import Skeleton
+from compas_rv2.datastructures import SubdMesh
+from compas_rv2.datastructures import Pattern
+from compas_rv2.datastructures import FormDiagram
+from compas_rv2.datastructures import ForceDiagram
+from compas_rv2.datastructures import ThrustDiagram
+
+from .meshartist import MeshArtist
+from .skeletonartist import SkeletonArtist
+from .formartist import FormArtist
+from .forceartist import ForceArtist
+from .thrustartist import ThrustArtist
+
+MeshArtist.register(Mesh, MeshArtist)
+MeshArtist.register(Skeleton, SkeletonArtist)
+MeshArtist.register(SubdMesh, MeshArtist)
+MeshArtist.register(Pattern, MeshArtist)
+MeshArtist.register(FormDiagram, FormArtist)
+MeshArtist.register(ForceDiagram, ForceArtist)
+MeshArtist.register(ThrustDiagram, ThrustArtist)
