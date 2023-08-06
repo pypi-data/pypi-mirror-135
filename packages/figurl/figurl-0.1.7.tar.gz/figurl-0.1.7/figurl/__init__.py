c969ffc43749e38a7bf67a637d84d9836ca7b36e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:52 +0000

    Add package figurl/0.1.7/figurl-0.1.7.tar.gz

---
+++ packages/figurl/figurl-0.1.7.tar.gz/figurl-0.1.7/figurl/__init__.py
@@ -0,0 +1,5 @@
+from .version import __version__
+from .plugins.builtin.altair import Altair
+from .plugins.builtin.boxlayout import BoxLayout
+from .plugins.builtin.markdown import Markdown
+from .core import Figure, Sync, serialize_wrapper
