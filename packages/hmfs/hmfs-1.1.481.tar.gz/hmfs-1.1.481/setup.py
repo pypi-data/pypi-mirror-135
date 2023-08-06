743b9c32fb37db6c6768350f865ff21e4c7516fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:45 +0000

    Add package hmfs/1.1.481/hmfs-1.1.481.tar.gz

---
+++ packages/hmfs/hmfs-1.1.481.tar.gz/hmfs-1.1.481/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hmfs',
+    version='1.1.481',
+    packages=find_packages(),
+    license="MIT",
+    description='Distributed filesystem for Hamuna AI',
+    long_description='Distributed filesystem for Hamuna AI with multiple third party middleware transfer points',
+    long_description_content_type="text/plain",
+    author='O.Push',
+    author_email='opush.developer@outlook.com',
+    url='https://www.hamuna.club',
+    package_dir={'':'.'},
+    install_requires=['minio', 'redis', 'paho-mqtt', 'gnsq', 'aonsq', 'qiniu', 'boto', 'diskcache', 'httpx', 'aiofile', 'aiofiles', 'aiohttp', 'aiohttp_retry', 'aioredis', 'async_cow']
+)
