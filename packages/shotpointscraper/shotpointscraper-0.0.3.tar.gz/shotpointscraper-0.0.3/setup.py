504147475a8fa761dd739a0d42e9ab538a89a522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:00 +0000

    Add package shotpointscraper/0.0.3/shotpointscraper-0.0.3.tar.gz

---
+++ packages/shotpointscraper/shotpointscraper-0.0.3.tar.gz/shotpointscraper-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="shotpointscraper",
+    version="0.0.3",
+    author="Daiki Azuma",
+    author_email="s1922052@stu.musashino-u.ac.jp",
+    description="A package for scrap shot data from Understats",
+    url ="https://github.com/DaikiAzuma/shot_point_scraper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    py_modules=['shotpointscraper'],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+    entry_points = {
+        'console_scripts': [
+            'shotpointscraper = shotpointscraper:main'
+        ]
+    },
+)
