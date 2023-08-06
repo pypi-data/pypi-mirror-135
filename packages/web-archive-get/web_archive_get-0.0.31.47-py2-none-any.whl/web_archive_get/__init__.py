83d604440e5bb7bac054199ffb55983f0ee131a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:00 +0000

    Add package web-archive-get/0.0.31.47/web_archive_get-0.0.31.47-py2-none-any.whl

---
+++ packages/web-archive-get/web_archive_get-0.0.31.47-py2-none-any.whl/web_archive_get/__init__.py
@@ -0,0 +1,13 @@
+"""
+web_archive_get.
+
+An example python library.
+"""
+
+__version__ = "0.0.31.46"
+__author__ = 'Willdor'
+__credits__ = ''
+from services.archive import archive
+from services.common_crawl_index import common_crawl_index
+from services.web_archive import web_archive
+from services.arquivo import arquivo
