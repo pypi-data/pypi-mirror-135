44cc1ca29104c2c891708248e00adcecc4339f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:01 +0000

    Add package simpdatastructs/0.1.0/simpdatastructs-0.1.0.tar.gz

---
+++ packages/simpdatastructs/simpdatastructs-0.1.0.tar.gz/simpdatastructs-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='simpdatastructs',
+    version='0.1.0',
+    description='Simple Data Structures',
+    url='https://github.com/StephenMal/SimpDataStructs',
+    author='Stephen Maldonado',
+    author_email='simpdatastructs@stephenmal.com',
+    packages=['simpdatastructs'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities'
+    ]
+)
---
+++ packages/simpdatastructs/simpdatastructs-0.1.0.tar.gz/simpdatastructs-0.1.0/simpdatastructs/__init__.py
@@ -0,0 +1,5 @@
+from simpdatastructs.bidict import *
+
+__version__ = 0.1.0
+__author__ = 'Stephen Maldonado'
+__github__ = 'StephenMal'
