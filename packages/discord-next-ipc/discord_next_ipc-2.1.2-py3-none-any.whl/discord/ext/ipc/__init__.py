28d1b3e1d4d93b4791969e2643d5e86996e5f70f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:43 +0000

    Add package discord-next-ipc/2.1.2/discord_next_ipc-2.1.2-py3-none-any.whl

---
+++ packages/discord-next-ipc/discord_next_ipc-2.1.2-py3-none-any.whl/discord/ext/ipc/__init__.py
@@ -0,0 +1,16 @@
+import collections
+
+from discord.ext.ipc.client import Client
+from discord.ext.ipc.server import Server
+from discord.ext.ipc.errors import *
+
+"""
+A fork of the discontinued `discord.ext.ipc` project. This fork will continue it and still be maintained.
+
+Docs will stay the same.
+"""
+
+_VersionInfo = collections.namedtuple("_VersionInfo", "major minor micro release serial")
+
+version = "2.1.2"
+version_info = _VersionInfo(2, 1, 2, "final", 0)
