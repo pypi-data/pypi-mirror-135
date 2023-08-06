c2305528a6095f8cdf0f1c07d6815e213d80f5f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:28 +0000

    Add package stat-distributions-for-testing-only/0.1/stat_distributions_for_testing_only-0.1.tar.gz

---
+++ packages/stat-distributions-for-testing-only/stat_distributions_for_testing_only-0.1.tar.gz/stat_distributions_for_testing_only-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='stat_distributions_for_testing_only',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
