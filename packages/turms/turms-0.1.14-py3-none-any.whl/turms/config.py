32f2a57610d00efcc36aa9e13c24db441501fec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:45 +0000

    Add package turms/0.1.14/turms-0.1.14-py3-none-any.whl

---
+++ packages/turms/turms-0.1.14-py3-none-any.whl/turms/config.py
@@ -0,0 +1,29 @@
+from ast import alias
+from pydantic import AnyHttpUrl, BaseModel, Field
+from typing import Dict, List, Optional
+
+
+class GeneratorConfig(BaseModel):
+    domain: str = "default"
+    out_dir: str = "api"
+    generated_name: str = "schema.py"
+    documents: Optional[str]
+
+    object_bases: List[str] = ["turms.types.object.GraphQLObject"]
+    interface_bases: List[str] = ["turms.types.object.GraphQLObject"]
+
+    scalar_definitions = {
+        "ID": "str",
+        "String": "str",
+        "Int": "int",
+        "Boolean": "bool",
+        "GenericScalar": "Dict",
+    }
+
+    additional_bases = {}
+    extensions: Dict = {}
+
+
+class GraphQLConfig(BaseModel):
+    schema_url: Optional[AnyHttpUrl] = Field(alias="schema")
+    documents: Optional[str]
