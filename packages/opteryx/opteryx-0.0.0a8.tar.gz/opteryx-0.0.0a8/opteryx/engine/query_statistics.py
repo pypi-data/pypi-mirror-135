50cd468e77dc996d3d7795f951252c7347c28561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:40 +0000

    Add package opteryx/0.0.0a8/opteryx-0.0.0a8.tar.gz

---
+++ packages/opteryx/opteryx-0.0.0a8.tar.gz/opteryx-0.0.0a8/opteryx/engine/query_statistics.py
@@ -0,0 +1,31 @@
+class QueryStatistics:
+    def __init__(self):
+        self.count_blobs_found: int = 0
+        self.count_data_blobs_read: int = 0
+        self.bytes_read_data: int = 0
+        self.bytes_processed_data: int = 0
+        self.rows_read: int = 0
+
+        self.time_metadata: int = 0
+        self.time_data_read: int = 0
+
+        # time spent query planning
+        self.time_planning: int = 0
+
+        self.start_time: int = 0
+        self.end_time: int = 0
+
+    def merge(self, stats):
+        pass
+
+    def as_dict(self):
+        return {
+            "count_blobs_found": self.count_blobs_found,
+            "count_data_blobs_read": self.count_data_blobs_read,
+            "bytes_read_data": self.bytes_read_data,
+            "bytes_processed_data": self.bytes_processed_data,
+            "rows_read": self.rows_read,
+            "time_data_read": self.time_data_read,
+            "time_metadata": self.time_metadata,
+            "time_planning": self.time_planning,
+        }
