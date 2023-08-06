d1b12e364fd8bc48ffd48ba49ed5d961d0482422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:34 +0000

    Add package aiounifi/30/aiounifi-30.tar.gz

---
+++ packages/aiounifi/aiounifi-30.tar.gz/aiounifi-30/setup.py
@@ -0,0 +1,20 @@
+"""Setup for aioUnifi"""
+
+from setuptools import setup
+
+setup(
+    name="aiounifi",
+    packages=["aiounifi"],
+    version="30",
+    description="An asynchronous Python library for communicating with Unifi Controller API",
+    author="Robert Svensson",
+    author_email="Kane610@users.noreply.github.com",
+    license="MIT",
+    url="https://github.com/Kane610/aiounifi",
+    download_url="https://github.com/Kane610/aiounifi/archive/v30.tar.gz",
+    install_requires=["aiohttp"],
+    tests_require=["pytest-asyncio", "pytest-aiohttp", "pytest", "aioresponses"],
+    keywords=["unifi", "homeassistant"],
+    classifiers=["Natural Language :: English", "Programming Language :: Python :: 3"],
+    python_requires=">=3.9.0",
+)
