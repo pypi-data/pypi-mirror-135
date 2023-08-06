3ec33d155aab62a88556255c7987d7040c7048e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:33 +0000

    Add package poglossary/0.1.4/poglossary-0.1.4-py3-none-any.whl

---
+++ packages/poglossary/poglossary-0.1.4-py3-none-any.whl/poglossary/config.py
@@ -0,0 +1,102 @@
+import re
+from pathlib import Path
+from typing import Any, Dict, List, Pattern, Union
+
+import yaml
+from pydantic import BaseModel, Extra
+
+from . import logger
+
+DEFAULT_CONFIG_PATH = "./poglossary.yml"
+DEFAULT_SOURCE_PATH = '.'
+DEFAULT_SOURCE_EXCLUDE_PATTERNS = [
+    '.git',
+]
+DEFAULT_IGNORE_RST_TAG_OBJS = [
+    'ref', 'keyword', 'dfn',
+]
+DEFAULT_IGNORE_PATTERNS = [
+    r'{.*?}',
+    r'\".*?\"',
+    r'``.*?``',
+    r'`<.*?>`_',
+    r'`.*?`_',
+    r'\|.*?\|_',
+    r'\*.*?\*',
+    r'\*\*.*?\*\*',
+]
+
+
+class IgnoreSettings(BaseModel):
+    override_default_patterns: bool = False
+    override_default_rst_tags: bool = False
+    patterns: List[str] = []
+    rst_tags: List[Union[Dict, str]] = []
+
+    class Config:
+        extra = Extra.allow
+
+    def __init__(self, **data) -> None:
+        super().__init__(**data)
+        self.ignore_tags: List[str] = self._build_tags(self.rst_tags)
+        self.ignore_pattern: Pattern = self._build_pattern(
+            self.patterns, self.ignore_tags)
+
+    def _build_tags(self, tag_objs: List[Union[Dict, str]]) -> List[str]:
+        if not self.override_default_rst_tags:
+            tag_objs.extend(DEFAULT_IGNORE_RST_TAG_OBJS)
+
+        ignore_tags: List[str] = []
+
+        def _build_tags_recursively(
+            tag_objs: List[Union[Dict, str]],
+            prefix: str = '',
+        ) -> None:
+            for obj in tag_objs:
+                if isinstance(obj, str):
+                    ignore_tags.append(f":{prefix}{obj}:")
+                elif isinstance(obj, dict):
+                    for key, vals in obj.items():
+                        _build_tags_recursively(vals, prefix + f"{key}:")
+
+        _build_tags_recursively(tag_objs)
+        return ignore_tags
+
+    def _build_pattern(self, patterns, ignore_tags=[]) -> Pattern:
+        if not self.override_default_patterns:
+            patterns = DEFAULT_IGNORE_PATTERNS + patterns
+
+        # append the pattern for matching rst tags
+        if ignore_tags:
+            patterns.append(f'({"|".join(ignore_tags)})`.*?`')
+        return re.compile('|'.join(patterns))
+
+
+class Config:
+    def __init__(
+        self,
+        config_file: Union[Path, str],
+    ) -> None:
+        # TODO: need format check for config file
+        config_file_path: Path = Path(config_file)
+        self.config: Dict[str, Any] = {}
+        if config_file_path.is_file():
+            try:
+                self.config = yaml.load(
+                    config_file_path.open(),
+                    Loader=yaml.FullLoader,
+                )
+            except Exception as err:
+                logger.error("Something went wrong when loading config file, "
+                             f"err={err}")
+
+        self.glossary: Dict[str, Any] = self.config.get('glossary', {})
+
+        self.ignore_settings: IgnoreSettings = IgnoreSettings(
+            **self.config.get('ignore', {}),
+        )
+
+    @property
+    def ignore_pattern(self) -> Pattern:
+        # merely a shorthand
+        return self.ignore_settings.ignore_pattern
---
+++ packages/poglossary/poglossary-0.1.4-py3-none-any.whl/poglossary/find_sources.py
@@ -0,0 +1,68 @@
+import glob
+from pathlib import Path
+from typing import List
+
+from pydantic import BaseModel
+
+from . import logger
+from .config import DEFAULT_SOURCE_EXCLUDE_PATTERNS
+
+
+class SourceFinder(BaseModel):
+    path: Path
+    excludes: List[Path] = []
+    po_paths: List[Path] = []
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.po_paths = self._exclude(self._get_po_paths())
+
+        length = len(self.po_paths)
+        if not length:
+            logger.error(f"Cannot found any .po file from '{self.path}'!", err=True)
+        else:
+            logger.info(f"Found {length} po file(s)")
+
+    def _get_po_paths(self) -> List[Path]:
+        """Find all .po files in given path"""
+        if not self.path.exists():
+            logger.error(f"The path '{self.path.absolute()}' does not exist!", err=True)
+
+        # return 1-element list if it's a file
+        if self.path.is_file():
+            return [self.path]
+
+        # find all .po files
+        po_paths = list(self.path.glob("**/*.po"))
+        return po_paths
+
+    def _exclude(self, po_paths: List[Path]) -> List[Path]:
+        """Exclude paths by the given list of paths"""
+        for pattern in DEFAULT_SOURCE_EXCLUDE_PATTERNS:
+            for relative_path in glob.glob(pattern):
+                self.excludes.append(self.path / relative_path)
+
+        excluded_files = []
+        excluded_dirs = []
+        for path in self.excludes:
+            p = path.resolve()
+            if p.is_file():
+                excluded_files.append(p)
+            else:
+                excluded_dirs.append(p)
+
+        paths = []
+        for path in po_paths:
+            p = path.resolve()
+
+            # exclude if matched
+            if p in excluded_files:
+                continue
+
+            # exclude if it's in the given directory
+            if any(e in p.parents for e in excluded_dirs):
+                continue
+
+            paths.append(path)
+
+        return paths
