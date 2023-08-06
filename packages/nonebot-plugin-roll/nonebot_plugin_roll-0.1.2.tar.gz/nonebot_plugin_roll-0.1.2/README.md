88e14146f86f1a5000e493183999957cd50bbe03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:19 +0000

    Add package nonebot-plugin-roll/0.1.2/nonebot_plugin_roll-0.1.2.tar.gz

---
+++ packages/nonebot-plugin-roll/nonebot_plugin_roll-0.1.2.tar.gz/nonebot_plugin_roll-0.1.2/README.md
@@ -0,0 +1,52 @@
+<div align="center">
+
+# Roll
+
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable-next-line MD036 -->
+_🎲 扔骰子 🎲_
+<!-- prettier-ignore-end -->
+
+</div>
+
+<p align="center">
+  
+  <a href="https://github.com/KafCoppelia/nonebot_plugin_roll/blob/main/LICENSE">
+    <img src="https://img.shields.io/badge/license-MIT-informational">
+  </a>
+  
+  <a href="https://github.com/nonebot/nonebot2">
+    <img src="https://img.shields.io/badge/nonebot2-2.0.0alpha.16-green">
+  </a>
+  
+  <a href="">
+    <img src="https://img.shields.io/badge/release-v0.1.2-orange">
+  </a>
+  
+</p>
+
+</p>
+
+## 版本
+
+v0.1.2
+
+⚠ 适配nonebot2-2.0.0alpha.16，适配beta.1版本将会放在dev分支。
+
+## 安装
+
+1. 通过`pip`或`nb`安装；
+
+2. Enjoy:tada:
+
+## 功能
+
+掷骰！扔出指定个数的多面骰子。
+
+## 命令
+
+rd、roll、掷骰，后接“[x]d[y]”， x指定个数，y指骰子面数。
+
+## 本插件改自Omega-Miya roll点抽奖插件
+
+[Omega Miya](https://github.com/Ailitonia/omega-miya)
---
+++ packages/nonebot-plugin-roll/nonebot_plugin_roll-0.1.2.tar.gz/nonebot_plugin_roll-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nonebot_plugin_roll']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'nonebot-plugin-roll',
+    'version': '0.1.2',
+    'description': 'Roll a dice!',
+    'long_description': '<div align="center">\n\n# Roll\n\n<!-- prettier-ignore-start -->\n<!-- markdownlint-disable-next-line MD036 -->\n_🎲 扔骰子 🎲_\n<!-- prettier-ignore-end -->\n\n</div>\n\n<p align="center">\n  \n  <a href="https://github.com/KafCoppelia/nonebot_plugin_roll/blob/main/LICENSE">\n    <img src="https://img.shields.io/badge/license-MIT-informational">\n  </a>\n  \n  <a href="https://github.com/nonebot/nonebot2">\n    <img src="https://img.shields.io/badge/nonebot2-2.0.0alpha.16-green">\n  </a>\n  \n  <a href="">\n    <img src="https://img.shields.io/badge/release-v0.1.2-orange">\n  </a>\n  \n</p>\n\n</p>\n\n## 版本\n\nv0.1.2\n\n⚠ 适配nonebot2-2.0.0alpha.16，适配beta.1版本将会放在dev分支。\n\n## 安装\n\n1. 通过`pip`或`nb`安装；\n\n2. Enjoy:tada:\n\n## 功能\n\n掷骰！扔出指定个数的多面骰子。\n\n## 命令\n\nrd、roll、掷骰，后接“[x]d[y]”， x指定个数，y指骰子面数。\n\n## 本插件改自Omega-Miya roll点抽奖插件\n\n[Omega Miya](https://github.com/Ailitonia/omega-miya)',
+    'author': 'KafCoppelia',
+    'author_email': 'k740677208@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7.3,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
