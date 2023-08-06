fdf075815dac71e3f78c868d750d0ea5239861df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:36 +0000

    Add package liveatlas/0.0.5/liveatlas-0.0.5.tar.gz

---
+++ packages/liveatlas/liveatlas-0.0.5.tar.gz/liveatlas-0.0.5/liveatlas/__init__.py
@@ -0,0 +1,9 @@
+"""
+To update:
+1. Save changes to files
+2. Delete contents of ./dist
+3. increment version in setup.py
+4. run $python setup.py sdist bdist_wheel
+5. run $twine upload dist/*
+
+"""
