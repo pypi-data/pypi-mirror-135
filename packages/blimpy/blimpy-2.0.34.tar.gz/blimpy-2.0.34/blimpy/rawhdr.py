57707c7674a76b2339ea57dc81ffaeedc7f1ba23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:07 +0000

    Add package blimpy/2.0.34/blimpy-2.0.34.tar.gz

---
+++ packages/blimpy/blimpy-2.0.34.tar.gz/blimpy-2.0.34/blimpy/rawhdr.py
@@ -0,0 +1,36 @@
+r'''
+Read the specified raw file.
+Print the entire header.
+Highlight the number of observation channels.
+'''
+
+import blimpy
+from argparse import ArgumentParser
+
+
+def get_obsnchan(filepath):
+    gr = blimpy.GuppiRaw(filepath)
+    header, data_idx = gr.read_header()
+    return header['OBSNCHAN']
+
+
+def cmd_tool(args=None):
+    p = ArgumentParser(description='Show the individual fields of the header for a given raw file.')
+    p.add_argument('filepath', type=str, help='Name of raw guppi file path to access')   
+    if args is None:
+        args = p.parse_args()
+    else:
+        args = p.parse_args(args)
+
+    print('Getting the header from {}.\n'.format(args.filepath))
+    with open(args.filepath, 'rb') as fh:
+        while True:
+            buffer = fh.read(80).decode("utf-8").strip()
+
+            print('\t', buffer)
+            if buffer[0:3] == "END":
+                break
+    print('\nThe number of observation channels:', get_obsnchan(args.filepath))
+
+if __name__ == "__main__":
+    cmd_tool()
