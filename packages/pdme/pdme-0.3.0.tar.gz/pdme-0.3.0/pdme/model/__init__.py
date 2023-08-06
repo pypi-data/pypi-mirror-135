7dade73898d5c2f60d22d91d94e7aa7ae1ca1543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:22 +0000

    Add package pdme/0.3.0/pdme-0.3.0.tar.gz

---
+++ packages/pdme/pdme-0.3.0.tar.gz/pdme-0.3.0/pdme/model/__init__.py
@@ -0,0 +1,5 @@
+from pdme.model.model import Model, Discretisation
+from pdme.model.fixed_z_plane_model import FixedZPlaneModel
+from pdme.model.unrestricted_model import UnrestrictedModel
+
+__all__ = ["Model", "Discretisation", "FixedZPlaneModel", "UnrestrictedModel"]
