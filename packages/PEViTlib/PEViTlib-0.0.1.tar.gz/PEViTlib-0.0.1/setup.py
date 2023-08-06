769e4e3b693862374ebceee40958a330d105a0f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:03 +0000

    Add package PEViTlib/0.0.1/PEViTlib-0.0.1.tar.gz

---
+++ packages/PEViTlib/PEViTlib-0.0.1.tar.gz/PEViTlib-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="PEViTlib", #
+    version='0.0.1',
+    author='xuehaihe',
+    author_email='xuehaihe2008@gmail.com',
+    url='https://github.com/jkooy/KroneckerAdapter',
+    description="Parameter-efficient Finetuning for ViT",
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+    ],
+)
