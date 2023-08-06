84aca6783183710862be1c720bae35129f043fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:08 +0000

    Add package fuzzycorr/0.1.0/fuzzycorr-0.1.0-py3.10.egg

---
+++ packages/fuzzycorr/fuzzycorr-0.1.0-py3.10.egg/fuzzycorr/__init__.py
@@ -0,0 +1,6 @@
+from .correlation_factory import fuzzy_correlation_factory
+
+__author__ = "Tomáš Mikula"
+__email__ = "mail@tomasmikula.cz"
+__version__ = "0.1.0"
+__license__ = "MIT license"
---
+++ packages/fuzzycorr/fuzzycorr-0.1.0-py3.10.egg/fuzzycorr/t_norms.py
@@ -0,0 +1,10 @@
+def lukasiewicz(x, y):
+    return max(0, x + y - 1)
+
+
+def godel(x, y):
+    return min(x, y)
+
+
+def product(x, y):
+    return x * y
