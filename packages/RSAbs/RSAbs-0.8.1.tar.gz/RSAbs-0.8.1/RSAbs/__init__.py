8507e2a342d5b54babbf6f83e49dc0ed3ebe7d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:14 +0000

    Add package RSAbs/0.8.1/RSAbs-0.8.1.tar.gz

---
+++ packages/RSAbs/RSAbs-0.8.1.tar.gz/RSAbs-0.8.1/RSAbs/__init__.py
@@ -0,0 +1,4 @@
+from .absorptions import *
+from .seek_absorptions import *
+from .seek_absorptions_depth import *
+from .absorptions_depth import *
---
+++ packages/RSAbs/RSAbs-0.8.1.tar.gz/RSAbs-0.8.1/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+setup(
+  name = 'RSAbs',       
+  packages = ['RSAbs'],   
+  version = '0.8.1', 
+  license='MIT',  
+  description = 'AUTOMATIC DETECTION OF ABSORPTION FEATURES IN REFLECTANCE SPECTRA', 
+  author = 'MARK RADWIN',  
+  author_email = 'markradwin@gmail.com',     
+  url = 'https://github.com/radwinskis/RSAbs',  
+  download_url = 'https://github.com/radwinskis/RSAbs/archive/refs/tags/v0.8.1.tar.gz',   
+  keywords = ['REFLECTANCE', 'SPECTROSCOPY', 'ABSORPTION', 'DETECTION'],  
+  install_requires=[          
+          'scipy',
+          'pandas',
+          'numpy'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+    'Intended Audience :: Science/Research',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11'
+    
+  ],
+)
