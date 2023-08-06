26d7465f86ed661f7f62d795b61176a201204d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:37 +0000

    Add package masonite-instant-article/0.0.2/masonite-instant-article-0.0.2.tar.gz

---
+++ packages/masonite-instant-article/masonite-instant-article-0.0.2.tar.gz/masonite-instant-article-0.0.2/src/instant_article/providers/instant_article_provider.py
@@ -0,0 +1,23 @@
+from masonite.packages import PackageProvider
+
+from ..commands.instant_command import InstantCommand
+from ..routes import ROUTES
+
+
+class InstantArticleProvider(PackageProvider):
+    
+    def configure(self):
+        (
+            self.root("instant_article")
+            .name("instant_article")
+            .config("config/instant_article.py", publish=True)
+            .views("views", publish=True)
+            
+        )
+
+    def register(self):
+        self.application.make("router").add(ROUTES)
+        self.application.make("commands").add(InstantCommand())
+
+    def boot(self):
+        pass
