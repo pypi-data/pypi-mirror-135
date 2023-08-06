412722d78e3663f7bf17bf10e10b433e4341d382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:32 +0000

    Add package pyEDFlib/0.1.25/pyEDFlib-0.1.25-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyEDFlib/pyEDFlib-0.1.25-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyedflib/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyedflib SETUP.PY
+short_version = '0.1.25'
+version = '0.1.25'
+full_version = '0.1.25'
+git_revision = '74bc387e18a06303613c79c2b227cda7f9773ea8'
+release = True
+
+if not release:
+    version = full_version
