dd78b94655da0e58fa8cc9c7370247e08d773807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:33 +0000

    Add package data-checker1453/0.1/data_checker1453-0.1.tar.gz

---
+++ packages/data-checker1453/data_checker1453-0.1.tar.gz/data_checker1453-0.1/data_checker1453/__init__.py
@@ -0,0 +1,3 @@
+from .Check import Check
+from .Display import print_side_by_side
+from .Display import display_side_by_side
---
+++ packages/data-checker1453/data_checker1453-0.1.tar.gz/data_checker1453-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+long_description = "This is Zainy's package"
+setup(name='data_checker1453',
+      version='0.1',
+      description='package to offer basic previews to the data',
+      packages=['data_checker1453'],
+      author = 'Mohamad AlZainy',
+      author_email = 'zainy1453@gmail.com',
+      long_description=long_description,
+   long_description_content_type="text/markdown", 
+      zip_safe=False)
