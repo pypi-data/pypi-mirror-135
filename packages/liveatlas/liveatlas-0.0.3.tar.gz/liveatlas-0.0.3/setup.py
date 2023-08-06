a8d821789225921ab007b69f407be7456462630d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:49:30 +0000

    Add package liveatlas/0.0.3/liveatlas-0.0.3.tar.gz

---
+++ packages/liveatlas/liveatlas-0.0.3.tar.gz/liveatlas-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'liveatlas-API'
+LONG_DESCRIPTION = 'A package providing methods for interacting with a liveatlas dynamic map'
+
+# Setting up
+setup(
+
+    name="liveatlas",
+    version=VERSION,
+    author="Sam",
+    author_email="",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['datetime', 'requests', 're'],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['python', 'first package'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
