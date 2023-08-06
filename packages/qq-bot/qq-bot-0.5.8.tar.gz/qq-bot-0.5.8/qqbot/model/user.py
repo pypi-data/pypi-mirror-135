d5fa51e8dcfe4b267fa0e7f5366698a7af9c424c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:51 +0000

    Add package qq-bot/0.5.8/qq-bot-0.5.8.tar.gz

---
+++ packages/qq-bot/qq-bot-0.5.8.tar.gz/qq-bot-0.5.8/qqbot/model/user.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+class ReqOption:
+    def __init__(self, before: str = None, after: str = None, limit: str = None):
+        """
+        获取频道列表的请求
+
+        :param before: 读取此 guild id 之前的数据
+        :param after: 读取此 guild id 之后的数据
+        :param limit: 每次拉取多少条数据，最大不超过 100，默认 100
+        """
+
+        self.before = before
+        self.after = after
+        self.limit = limit
