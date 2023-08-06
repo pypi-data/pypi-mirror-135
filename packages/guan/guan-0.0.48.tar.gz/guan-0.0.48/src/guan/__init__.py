4cb0db5ae5fc87c4776fd04349629e915e3c7d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:47 +0000

    Add package guan/0.0.48/guan-0.0.48.tar.gz

---
+++ packages/guan/guan-0.0.48.tar.gz/guan-0.0.48/src/guan/__init__.py
@@ -0,0 +1,5 @@
+# Guan is an open-source python package developed and maintained by https://www.guanjihuan.com/about. The primary location of this package is on website https://py.guanjihuan.com.
+
+# import modules
+
+from . import *
