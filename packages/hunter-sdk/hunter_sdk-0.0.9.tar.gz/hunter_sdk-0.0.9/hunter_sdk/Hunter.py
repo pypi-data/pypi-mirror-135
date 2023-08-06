d1be957b5a977ce2874168544618ba65db52287c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:17 +0000

    Add package hunter-sdk/0.0.9/hunter_sdk-0.0.9.tar.gz

---
+++ packages/hunter-sdk/hunter_sdk-0.0.9.tar.gz/hunter_sdk-0.0.9/hunter_sdk/Hunter.py
@@ -0,0 +1,42 @@
+'''
+Author: 饕餮
+Date: 2022-01-21 10:37:56
+version: 
+LastEditors: 饕餮
+LastEditTime: 2022-01-21 15:37:17
+Description: file content
+'''
+import json
+from hunter_sdk.Common.HunterObject import HunterObject
+from hunter_sdk.HunterApi import HunterApi
+
+class Hunter:
+    def __init__(self,configPath='config.json',keyName='Hunter'):
+        with open(configPath, 'r') as config_f:
+            Config = json.load(config_f)
+            HunterConfig = Config[keyName]
+            _username = HunterConfig["username"]
+            _apikey = HunterConfig["apikey"]
+            self.hunterApi = HunterApi(_username,_apikey)
+
+    def Search(self,queryStr,page=1,page_size=10,days=30):
+        responseData = self.hunterApi.GetData(queryStr,page,page_size,days)
+        hunterObject = HunterObject(responseData)
+        self.NowPage = page
+        self._usePageSize = page_size
+        self._useQuery = queryStr
+        self._useDays = days
+        self.TotalPage = int(hunterObject.Total / 10)
+        if self.TotalPage > 10000: 
+            self.TotalPage = 10000
+        return hunterObject
+
+    def Next(self,page_number=1):
+        page = self.NowPage + page_number
+        if page > self.TotalPage: page = self.TotalPage
+        return self.Search(self._useQuery,page,self._usePageSize,self._useDays)
+
+    def Last(self,page_number=1):
+        page = self.NowPage - page_number
+        if page < 1: page = 1
+        return self.Search(self._useQuery,page,self._usePageSize,self._useDays)
