61e044dbb0093e43fcef3acae335ef394d79d9bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:12 +0000

    Add package deepparse/0.6.4/deepparse-0.6.4-py3-none-any.whl

---
+++ packages/deepparse/deepparse-0.6.4-py3-none-any.whl/tests/tools.py
@@ -0,0 +1,23 @@
+import torch
+
+from deepparse.dataset_container import DatasetContainer
+
+
+def create_file(path: str, content: str):
+    # pylint: disable=unspecified-encoding
+    """ "
+    Function to create a file for test
+    """
+    with open(path, "w") as f:
+        f.write(content)
+
+
+BATCH_SIZE = 32
+
+
+class ADataContainer(DatasetContainer):
+    def __init__(
+        self,
+    ):
+        super().__init__()
+        self.data = (torch.rand(BATCH_SIZE, 1), torch.rand(BATCH_SIZE, 1))
