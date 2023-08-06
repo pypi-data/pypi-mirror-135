1ac8b1660d82e50ac492aa1957614a3447735652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:19 +0000

    Add package rpi-camera-colony/0.4.1.dev0/rpi_camera_colony-0.4.1.dev0-py3-none-any.whl

---
+++ packages/rpi-camera-colony/rpi_camera_colony-0.4.1.dev0-py3-none-any.whl/rpi_camera_colony/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Author: Lars B. Rollik <L.B.Rollik@protonmail.com>
+# License: BSD 3-Clause
+import logging
+
+from rpi_camera_colony.tools.readers import read_session_data
+
+__version__ = "0.4.1.dev0"
+__author__ = "Lars B. Rollik"
