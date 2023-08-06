77419d5fc892876165f997087510cb7203ce770f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:49:40 +0000

    Add package asr-library/0.0.2/asr-library-0.0.2.tar.gz

---
+++ packages/asr-library/asr-library-0.0.2.tar.gz/asr-library-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name="asr-library",
+    version="0.0.2",
+    description="Automatic Speech Recognition inference for wav2vec2 models",
+    url="https://github.com/neerajchhimwal/asr",
+    author="Neeraj Chhimwal",
+    author_email="neerajchhimwal21197@gmail.com",
+    license="MIT",
+
+    platforms=["linux", "unix"],
+    python_requires=">3.6",
+    include_package_data=True,
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
