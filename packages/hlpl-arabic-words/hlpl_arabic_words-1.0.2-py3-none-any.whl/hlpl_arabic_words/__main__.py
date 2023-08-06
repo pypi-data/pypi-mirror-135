e6fb0163127db7453643f6976b70e2855a2055d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:19 +0000

    Add package hlpl-arabic-words/1.0.2/hlpl_arabic_words-1.0.2-py3-none-any.whl

---
+++ packages/hlpl-arabic-words/hlpl_arabic_words-1.0.2-py3-none-any.whl/hlpl_arabic_words/__main__.py
@@ -0,0 +1,23 @@
+﻿
+import configparser
+import sys
+import os
+ 
+ 
+def get_hlpl_arabic_words_version(case):
+    config = configparser.ConfigParser()
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    config_file_path = os.path.join(current_directory, 'setup.cfg')
+    config.read(config_file_path)
+    if case=='version':
+       return config['hlpl_arabic_words']['version']
+    if case=='else':
+       return config['hlpl_arabic_words']['version']+'\n'+config['hlpl_arabic_words']['author']+'\n'+config['hlpl_arabic_words']['email']+'\n'+config['hlpl_arabic_words']['url']+'\n'
+       
+ 
+    
+def main():
+    if 'version' in sys.argv:
+        print('\n'+get_hlpl_arabic_words_version('version'))
+    else:
+        print('\n'+get_hlpl_arabic_words_version('else')+'\n'+'hlpl_arabic_words lists nouns, verbs, articles, nouns prefix, nouns suffix, verbs prefix, and verbs suffix')
