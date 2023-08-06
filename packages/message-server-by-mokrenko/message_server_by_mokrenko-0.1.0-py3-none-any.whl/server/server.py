cee7c5ce048638e7c32a1409bfd6474d046f97d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:24 +0000

    Add package message-server-by-mokrenko/0.1.0/message_server_by_mokrenko-0.1.0-py3-none-any.whl

---
+++ packages/message-server-by-mokrenko/message_server_by_mokrenko-0.1.0-py3-none-any.whl/server/server.py
@@ -0,0 +1,126 @@
+"""Программа-сервер"""
+
+import sys
+import os
+import argparse
+from common.variables import *
+from lesson8.server_dist.server.server import MessageProcessor
+from lesson8.server_dist.server.server import ServerStorage
+import configparser  # https://docs.python.org/3/library/configparser.html
+from server_gui import MainWindow
+from lesson8.server_dist.server.server import MainWindow
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt
+
+# # Флаг, что был подключён новый пользователь, нужен чтобы не мучать BD
+# # постоянными запросами на обновление
+# new_connection = False
+# conflag_lock = threading.Lock()
+
+# Инициализация логирования сервера.
+logger = logging.getLogger('server')
+
+
+def arg_parser(default_port, default_address):
+    """Парсер аргументов коммандной строки."""
+    logger.debug(
+        f'Инициализация парсера аргументов коммандной строки: {sys.argv}')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', default=default_port, type=int, nargs='?')
+    parser.add_argument('-a', default=default_address, nargs='?')
+    parser.add_argument('--no_gui', action='store_true')
+    namespace = parser.parse_args(sys.argv[1:])
+    listen_address = namespace.a
+    listen_port = namespace.p
+    gui_flag = namespace.no_gui
+    logger.debug('Аргументы успешно загружены.')
+    return listen_address, listen_port, gui_flag
+
+
+# Загрузка файла конфигурации
+def config_load():
+    config = configparser.ConfigParser()
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    config.read(f"{dir_path}/{'server.ini'}")
+    # Если конфиг файл загружен правильно, запускаемся, иначе конфиг по умолчанию.
+    if 'SETTINGS' in config:
+        return config
+    else:
+        config.add_section('SETTINGS')
+        config.set('SETTINGS', 'Default_port', str(DEFAULT_PORT))
+        config.set('SETTINGS', 'Listen_Address', '')
+        config.set('SETTINGS', 'Database_path', '')
+        config.set('SETTINGS', 'Database_file', 'server_database.db3')
+        return config
+
+
+def print_help():
+    print('Поддерживаемые комманды:')
+    print('users - список известных пользователей')
+    print('connected - список подключённых пользователей')
+    print('loghist - история входов пользователя')
+    print('exit - завершение работы сервера.')
+    print('help - вывод справки по поддерживаемым командам')
+
+
+def main():
+    # Загрузка файла конфигурации сервера
+    config = config_load()
+
+    # Загрузка параметров командной строки, если нет параметров, то задаём
+    # значения по умоланию.
+    # listen_address, listen_port = Server.arg_parser()
+    #
+    listen_address, listen_port, gui_flag = arg_parser(
+        config['SETTINGS']['Default_port'],
+        config['SETTINGS']['Listen_Address']
+    )
+
+    # Инициализация базы данных
+    database = ServerStorage(
+        os.path.join(
+            config['SETTINGS']['Database_path'],
+            config['SETTINGS']['Database_file']))
+
+    # Создание экземпляра класса - сервера и его запуск:
+    srv = MessageProcessor(listen_address, listen_port, database)
+    # Если не прошли проверку на ValueError выходим из программы
+    if srv == -1:
+        sys.exit(1)
+    # Инициализируем листенер
+    srv.init()
+
+    srv.daemon = True
+    srv.start()
+
+    # # Начинаем принимать сообщения
+    # srv.run()
+    #
+
+    # Если  указан параметр без GUI то запускаем простенький обработчик
+    # консольного ввода
+    if gui_flag:
+        while True:
+            command = input('Введите exit для завершения работы сервера.')
+            if command == 'exit':
+                # Если выход, то завршаем основной цикл сервера.
+                srv.running = False
+                srv.join()
+                break
+
+    # Если не указан запуск без GUI, то запускаем GUI:
+    else:
+        # Создаём графическое окуружение для сервера:
+        server_app = QApplication(sys.argv)
+        server_app.setAttribute(Qt.AA_DisableWindowContextHelpButton)
+        main_window = MainWindow(database, srv, config)
+
+        # Запускаем GUI
+        server_app.exec_()
+
+        # По закрытию окон останавливаем обработчик сообщений
+        srv.running = False
+
+
+if __name__ == '__main__':
+    main()
