2ab275890e7c56b85b0a0e7b37def4836337b8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:47 +0000

    Add package calcmaster/0.0.1/calcmaster-0.0.1.tar.gz

---
+++ packages/calcmaster/calcmaster-0.0.1.tar.gz/calcmaster-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name ="calcmaster",
+    version = "0.0.1",
+    author = "juliorodriguez",
+    author_email = "julio_rz23@hotmail.com",
+    description ="paquete calculadora basica master ",
+    url ="https://github.com/juliorodriguez23/proyectos",
+    License ="MIT",
+    packages = find_packages(),
+    scripts =[]
+    )
