8031acca41dd9503c31415dfc4b1fd82e0f92373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:49:38 +0000

    Add package tensorflow-model-analysis/0.37.0/tensorflow_model_analysis-0.37.0-py3-none-any.whl

---
+++ packages/tensorflow-model-analysis/tensorflow_model_analysis-0.37.0-py3-none-any.whl/tensorflow_model_analysis/extractors/features_extractor.py
@@ -0,0 +1,138 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Features extractor."""
+
+import copy
+from typing import Optional, Tuple
+
+import apache_beam as beam
+import numpy as np
+import pyarrow as pa
+from tensorflow_model_analysis import constants
+from tensorflow_model_analysis import types
+from tensorflow_model_analysis.extractors import extractor
+from tensorflow_model_analysis.proto import config_pb2
+
+_FEATURES_EXTRACTOR_STAGE_NAME = 'ExtractFeatures'
+
+
+def FeaturesExtractor(
+    eval_config: config_pb2.EvalConfig) -> extractor.Extractor:
+  """Creates an extractor for extracting features.
+
+  The extractor's PTransform extracts features from an Arrow RecordBatch stored
+  under tfma.ARROW_RECORD_BATCH_KEY in the incoming extract and adds them to the
+  output extract under the key tfma.FEATURES_KEY. Any extracts that already
+  exist will be merged with the values parsed by this extractor with this
+  extractor's values taking precedence when duplicate keys are detected.
+
+  Args:
+    eval_config: Eval config.
+
+  Returns:
+    Extractor for extracting features.
+  """
+  del eval_config
+  # pylint: disable=no-value-for-parameter
+  return extractor.Extractor(
+      stage_name=_FEATURES_EXTRACTOR_STAGE_NAME, ptransform=_ExtractFeatures())
+
+
+def _IsListLike(arrow_type: pa.DataType) -> bool:
+  return pa.types.is_list(arrow_type) or pa.types.is_large_list(arrow_type)
+
+
+def _IsBinaryLike(arrow_type: pa.DataType) -> bool:
+  return (pa.types.is_binary(arrow_type) or
+          pa.types.is_large_binary(arrow_type) or
+          pa.types.is_string(arrow_type) or
+          pa.types.is_large_string(arrow_type))
+
+
+def _IsSupportedArrowValueType(arrow_type: pa.DataType) -> bool:
+  return (pa.types.is_integer(arrow_type) or pa.types.is_floating(arrow_type) or
+          _IsBinaryLike(arrow_type))
+
+
+def _DropUnsupportedColumnsAndFetchRawDataColumn(
+    record_batch: pa.RecordBatch
+) -> Tuple[pa.RecordBatch, Optional[np.ndarray]]:
+  """Drops unsupported columns and fetches the raw data column.
+
+  Currently, types that are not binary_like or ListArray[primitive types] are
+  dropped.
+
+  Args:
+    record_batch: An Arrow RecordBatch.
+
+  Returns:
+    Arrow RecordBatch with only supported columns.
+  """
+  column_names, column_arrays = [], []
+  serialized_examples = None
+  for column_name, column_array in zip(record_batch.schema.names,
+                                       record_batch.columns):
+    column_type = column_array.type
+    if column_name == constants.ARROW_INPUT_COLUMN:
+      assert (_IsListLike(column_type) and
+              _IsBinaryLike(column_type.value_type)), (
+                  'Invalid type for batched input key: {}. '
+                  'Expected binary like.'.format(column_type))
+      serialized_examples = np.asarray(column_array.flatten())
+    # Currently we only handle columns of type list<primitive|binary_like>.
+    # We ignore other columns as we cannot efficiently convert them into an
+    # instance dict format.
+    elif (_IsListLike(column_type) and
+          _IsSupportedArrowValueType(column_type.value_type)):
+      column_names.append(column_name)
+      column_arrays.append(column_array)
+  return (pa.RecordBatch.from_arrays(column_arrays,
+                                     column_names), serialized_examples)
+
+
+@beam.ptransform_fn
+@beam.typehints.with_input_types(types.Extracts)
+@beam.typehints.with_output_types(types.Extracts)
+def _ExtractFeatures(
+    extracts: beam.pvalue.PCollection) -> beam.pvalue.PCollection:
+  """Extracts features from extracts.
+
+  Args:
+    extracts: PCollection containing features under tfma.FEATURES_KEY.
+
+  Returns:
+    PCollection of extracts with additional features added under the key
+    tfma.FEATURES_KEY.
+  """
+
+  def extract_features(  # pylint: disable=invalid-name
+      batched_extract: types.Extracts) -> types.Extracts:
+    """Extract features from extracts containing arrow table."""
+    result = copy.copy(batched_extract)
+    (record_batch, serialized_examples) = (
+        _DropUnsupportedColumnsAndFetchRawDataColumn(
+            batched_extract[constants.ARROW_RECORD_BATCH_KEY]))
+    # Special case: if record_batch contains 0 column, it will have 0 row, but
+    # we want a list of empty dicts, of the same length as serialized_examples.
+    if record_batch.num_columns == 0:
+      result[constants.FEATURES_KEY] = [dict() for _ in serialized_examples]
+    else:
+      dataframe = record_batch.to_pandas()
+      result[constants.FEATURES_KEY] = dataframe.to_dict(orient='records')
+    # TODO(pachristopher): Consider avoiding setting this key if we don't need
+    # this any further in the pipeline. This can avoid a potentially costly copy
+    result[constants.INPUT_KEY] = serialized_examples
+    return result
+
+  return extracts | 'ExtractFeatures' >> beam.Map(extract_features)
---
+++ packages/tensorflow-model-analysis/tensorflow_model_analysis-0.37.0-py3-none-any.whl/tensorflow_model_analysis/extractors/unbatch_extractor.py
@@ -0,0 +1,73 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Unbatch extractor."""
+
+from typing import Mapping, Sequence
+
+import apache_beam as beam
+import pandas as pd
+from tensorflow_model_analysis import constants
+from tensorflow_model_analysis import types
+from tensorflow_model_analysis.extractors import extractor
+
+UNBATCH_EXTRACTOR_STAGE_NAME = 'ExtractUnbatchedInputs'
+
+
+def UnbatchExtractor() -> extractor.Extractor:
+  """Creates an extractor for unbatching batched extracts.
+
+  This extractor removes Arrow RecordBatch from the batched extract and outputs
+  per-example extracts with the remaining keys. We assume that the remaining
+  keys in the input extract contain list of objects (one per example).
+
+  Returns:
+    Extractor for unbatching batched extracts.
+  """
+  # pylint: disable=no-value-for-parameter
+  return extractor.Extractor(
+      stage_name=UNBATCH_EXTRACTOR_STAGE_NAME, ptransform=_UnbatchInputs())
+
+
+def _extract_unbatched_inputs(  # pylint: disable=invalid-name
+    batched_extract: types.Extracts) -> Sequence[types.Extracts]:
+  """Extract features, predictions, labels and weights from batched extract."""
+  keys_to_retain = set(batched_extract.keys())
+  if constants.ARROW_RECORD_BATCH_KEY in keys_to_retain:
+    keys_to_retain.remove(constants.ARROW_RECORD_BATCH_KEY)
+  dataframe = pd.DataFrame()
+  for key in keys_to_retain:
+    if isinstance(batched_extract[key], Mapping) and not batched_extract[key]:
+      continue
+    try:
+      dataframe[key] = batched_extract[key]
+    except Exception as e:
+      raise RuntimeError(f'Exception encountered while adding key {key} with '
+                         f'batched length {len(batched_extract[key])}') from e
+  return dataframe.to_dict(orient='records')
+
+
+@beam.ptransform_fn
+@beam.typehints.with_input_types(types.Extracts)
+@beam.typehints.with_output_types(types.Extracts)
+def _UnbatchInputs(
+    extracts: beam.pvalue.PCollection) -> beam.pvalue.PCollection:
+  """Extracts unbatched inputs from batched extracts.
+
+  Args:
+    extracts: PCollection containing batched extracts.
+
+  Returns:
+    PCollection of per-example extracts.
+  """
+  return extracts | 'UnbatchInputs' >> beam.FlatMap(_extract_unbatched_inputs)
