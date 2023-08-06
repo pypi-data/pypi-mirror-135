6131f316e6ab64a65b140636bc9fee4484dab7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:45 +0000

    Add package fikkie/0.5.0/fikkie-0.5.0-py3-none-any.whl

---
+++ packages/fikkie/fikkie-0.5.0-py3-none-any.whl/fikkie/config.py
@@ -0,0 +1,26 @@
+import logging
+import os
+import yaml
+
+
+__all__ = ["BASE_DIR", "CONFIG_FILE", "BROKER_DIR", "CONFIG", "DB_FILENAME"]
+
+BASE_DIR = os.getenv("FIKKIE_BASE_DIR", os.path.expanduser("~/.fikkie"))
+CONFIG_FILE = os.getenv("FIKKIE_CONFIG", os.path.join(BASE_DIR, "config.yaml"))
+BROKER_DIR = os.getenv("FIKKIE_BROKER_DIR", os.path.join(BASE_DIR, "broker"))
+DB_FILENAME = os.getenv("FIKKIE_DB_FILENAME", os.path.join(BASE_DIR, "db.json"))
+LOG_FILE = os.getenv("FIKKIE_LOG_FILE", os.path.join(BASE_DIR, "fikkie.log"))
+PID_FILE = os.getenv("FIKKIE_PID_FILE", os.path.join(BASE_DIR, "fikkie.pid"))
+
+
+def load_config() -> dict:  # pragma: no cover - no complexity here
+    """Parse the config from a given file."""
+    try:
+        with open(CONFIG_FILE, "r") as f:
+            return yaml.safe_load(f) or {}
+    except FileNotFoundError as e:
+        logging.warning("Please run `fikkie init`.")
+        exit(1)
+    except yaml.YAMLError as e:
+        logging.error(f"Could not parse config: {e}")
+        exit(1)
