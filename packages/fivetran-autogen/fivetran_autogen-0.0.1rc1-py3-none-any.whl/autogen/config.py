dca4d7a55d6d1afc9bd03363b1837c9d54fcc64f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:03 +0000

    Add package fivetran-autogen/0.0.1rc1/fivetran_autogen-0.0.1rc1-py3-none-any.whl

---
+++ packages/fivetran-autogen/fivetran_autogen-0.0.1rc1-py3-none-any.whl/autogen/config.py
@@ -0,0 +1,26 @@
+import yaml
+from pathlib import Path
+import importlib.resources as pkg_resources
+from autogen import packages
+
+
+class Config:
+    def __init__(self, connector_selection):
+        input = load_configuration(connector_selection)
+        self.connector = connector_selection
+        self.name = input["name"]
+        self.version = input["version"]
+        self.schema_variable = input["source_data_location"]["schema_variable"]
+        self.database_variable = input["source_data_location"]["database_variable"]
+        self.enabled_variables = input.get("enabled_variables", [])
+        self.preset_variables = input.get("preset_variables", [])
+
+
+def load_configuration(name: str):
+    try:
+        file = pkg_resources.read_text(packages, f"{name}.yml")
+        configuration = yaml.load(file, Loader=yaml.FullLoader)
+    except:
+        raise Exception(f"Could not load configuration for {name}")
+
+    return configuration
