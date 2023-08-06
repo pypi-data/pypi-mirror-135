8b393e423e12dc01220ba81a63b375b8850bcecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:40 +0000

    Add package rawtypes/1.0.1/rawtypes-1.0.1.tar.gz

---
+++ packages/rawtypes/rawtypes-1.0.1.tar.gz/rawtypes-1.0.1/src/rawtypes/__init__.py
@@ -0,0 +1,8 @@
+from typing import List, Callable
+from ._version import *
+import logging
+import pathlib
+from .parser import Parser
+from .header import Header
+
+logger = logging.getLogger(__name__)
