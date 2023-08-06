1567b139e9dbfbd3a8791202a9cc7391f2243627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:25 +0000

    Add package taoist/0.5.4/taoist-0.5.4.tar.gz

---
+++ packages/taoist/taoist-0.5.4.tar.gz/taoist-0.5.4/setup.py
@@ -0,0 +1,26 @@
+"""setup.py"""
+from setuptools import setup
+from taoist import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="taoist",
+    version=__version__,
+    author="Daniel Garrigan",
+    author_email="popgendad@gmail.com",
+    description="Command line utility for Todoist",
+    license_files = ('LICENSE',),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/popgendad/taoist",
+    packages=["taoist"],
+    install_requires=[
+        "tabulate>=0.8.9",
+        "todoist_api_python>=1.1.0"
+    ],
+    python_requires=">=3.6",
+    zip_safe=False,
+    entry_points={"console_scripts": ["taoist=taoist.taoist_main:main"]},
+)
