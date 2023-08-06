690ad302ff0b0720039af78eaba4597105ef4eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:22 +0000

    Add package distributions-brc/0.2/distributions-brc-0.2.tar.gz

---
+++ packages/distributions-brc/distributions-brc-0.2.tar.gz/distributions-brc-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions-brc',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author='Bruno Cabral',
+      author_email='cabral.brc@gmail.com',
+      zip_safe=False)
