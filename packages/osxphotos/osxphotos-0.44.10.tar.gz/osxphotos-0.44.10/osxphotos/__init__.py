2b55914b146d2e56e14b1db107c5a573b9c517fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:18 +0000

    Add package osxphotos/0.44.10/osxphotos-0.44.10.tar.gz

---
+++ packages/osxphotos/osxphotos-0.44.10.tar.gz/osxphotos-0.44.10/osxphotos/__init__.py
@@ -0,0 +1,13 @@
+from ._constants import AlbumSortOrder
+from ._version import __version__
+from .exiftool import ExifTool
+from .photoexporter import ExportResults, PhotoExporter
+from .photoinfo import PhotoInfo
+from .photosdb import PhotosDB
+from .photosdb._photosdb_process_comments import CommentInfo, LikeInfo
+from .phototemplate import PhotoTemplate
+from .queryoptions import QueryOptions
+from .utils import _debug, _get_logger, _set_debug
+
+# TODO: Add test for imageTimeZoneOffsetSeconds = None
+# TODO: Add special albums and magic albums
