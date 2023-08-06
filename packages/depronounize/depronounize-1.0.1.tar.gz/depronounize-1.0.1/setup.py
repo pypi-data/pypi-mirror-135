b1cf959b28c930cc9fb00bcbe96a8788ccbe1758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:18 +0000

    Add package depronounize/1.0.1/depronounize-1.0.1.tar.gz

---
+++ packages/depronounize/depronounize-1.0.1.tar.gz/depronounize-1.0.1/setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="depronounize",
+    version='1.0.1',
+    description="Pronoun replacement module",
+    url="https://github.com/NazarTrilisky/PronounReplacement",
+    install_requires=["spacy==2.3.2"],
+    include_package_data=True,
+    zip_safe=False,
+    author_email="",
+    license="MIT",
+    author="Nazar Trilisky",
+    packages=find_packages('src'),
+    package_dir={'': 'src'}
+)
+
---
+++ packages/depronounize/depronounize-1.0.1.tar.gz/depronounize-1.0.1/src/depronounize/libs/masculine.py
@@ -0,0 +1,5 @@
+from libs.male_names import male_names
+from libs.male_nouns import male_nouns
+
+# list of masculine words in lower-case
+masculine = male_names + male_nouns
