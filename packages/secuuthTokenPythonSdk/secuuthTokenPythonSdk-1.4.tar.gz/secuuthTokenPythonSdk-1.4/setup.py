20d6cef471fc0d4c3cee9e1747a478ea5883bf8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:09 +0000

    Add package secuuthTokenPythonSdk/1.4/secuuthTokenPythonSdk-1.4.tar.gz

---
+++ packages/secuuthTokenPythonSdk/secuuthTokenPythonSdk-1.4.tar.gz/secuuthTokenPythonSdk-1.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="secuuthTokenPythonSdk",
+    version="1.4",
+    author="secuuth",
+    author_email="amitoshkumar10@gmail.com",
+    description="secuuth  SDK for python scripts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/engineering-secuuth/secuuth-jwt-python-sdk",
+    packages=["pySdk"],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["requests", "python-jose"]
+)
