5f38ba3fee10d7c1a391f5549b29892ef12c21ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:16 +0000

    Add package iris-emotion/0.0.1/iris_emotion-0.0.1.tar.gz

---
+++ packages/iris-emotion/iris_emotion-0.0.1.tar.gz/iris_emotion-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'iris_emotion',         
+  packages = ['iris_emotion'],   
+  version = '0.0.1',      
+  license='MIT',       
+  description = 'Detect emotions in text.',   
+  author = 'Anjana Valsalan',                   
+  author_email = 'anjana.valsalan@my.uwi.edu',      
+  url = 'https://github.com/Anjanaval/IRIS-Emotion.git', 
+  download_url = 'https://github.com/Anjanaval/IRIS-Emotion/archive/refs/tags/0.0.1.tar.gz',
+  keywords = ['Emotion Detection', 'Sentiment Analysis', 'Natural Language Processing'],
+  install_requires=[            
+          'tensorflow',
+          'numpy',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3.8',
+  ],
+)
