0bbdd5f2b4ea6629f50990e31a2728f72817922e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:12 +0000

    Add package PyMDL/0.1.2/PyMDL-0.1.2.tar.gz

---
+++ packages/PyMDL/PyMDL-0.1.2.tar.gz/PyMDL-0.1.2/PyMDL/__init__.py
@@ -0,0 +1,8 @@
+from .Search import search, SearchResult
+from .Infopage import info, InfoPage
+from .SearchPeople import search_people, PeopleSearchResult
+from .Casts import casts, Cast
+
+__title__ = 'PyMDL'
+__license__ = 'MIT'
+__version__ = '0.1.2'
