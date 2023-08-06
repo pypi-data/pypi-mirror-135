57bc33cec6b0b15a6bc37bba03a38e61a99319c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:48:03 +0000

    Add package scatterbrain/0.1.6/scatterbrain-0.1.6-py3-none-any.whl

---
+++ packages/scatterbrain/scatterbrain-0.1.6-py3-none-any.whl/scatterbrain/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import  # noqa
+
+import os  # noqa
+
+PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
+
+import logging  # noqa
+
+log = logging.getLogger(__name__)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+
+import pickle  # noqa
+
+from .background import ScatteredLightBackground  # noqa
+from .cupy_numpy_imports import load_image  # noqa
+from .scene import StarScene  # noqa
+from .tpf import correct_tpf  # noqa
+from .version import __version__  # noqa
+
+sector_times = pickle.load(open(f"{PACKAGEDIR}/data/tess_sector_times.pkl", "rb"))
