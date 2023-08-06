db8f89e88a4e2568dd86ad798b2b8547d56473e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:23 +0000

    Add package aa-bulletin-board/1.2.0/aa_bulletin_board-1.2.0-py3-none-any.whl

---
+++ packages/aa-bulletin-board/aa_bulletin_board-1.2.0-py3-none-any.whl/aa_bulletin_board/urls.py
@@ -0,0 +1,19 @@
+"""
+Pages url config
+"""
+
+# Django
+from django.urls import path
+
+# AA Bulletin Board
+from aa_bulletin_board import views
+
+app_name: str = "aa_bulletin_board"
+
+urlpatterns = [
+    path("", views.dashboard, name="dashboard"),
+    path("create/", views.create_bulletin, name="create_bulletin"),
+    path("bulletin/<str:slug>/", views.view_bulletin, name="view_bulletin"),
+    path("edit/<str:slug>/", views.edit_bulletin, name="edit_bulletin"),
+    path("remove/<str:slug>/", views.remove_bulletin, name="remove_bulletin"),
+]
