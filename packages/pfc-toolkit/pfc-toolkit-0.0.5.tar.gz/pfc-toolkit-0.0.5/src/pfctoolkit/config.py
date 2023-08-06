c9a775036586eb7c518c89beb02b66208e3b3a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:53 +0000

    Add package pfc-toolkit/0.0.5/pfc-toolkit-0.0.5.tar.gz

---
+++ packages/pfc-toolkit/pfc-toolkit-0.0.5.tar.gz/pfc-toolkit-0.0.5/src/pfctoolkit/config.py
@@ -0,0 +1,34 @@
+"""
+Configuration objects to facilitate using the Precomputed Connectome.
+
+"""
+
+import os
+import json
+import importlib.resources as pkg_resources
+
+
+class Config:
+    def __init__(self, pcc):
+        from . import configs
+
+        with pkg_resources.path(configs, f"{pcc}.json") as configfile:
+            try:
+                with open(str(configfile)) as js:
+                    self.config = json.load(js)
+                if self.check():
+                    print(f"Config {self.config['name']} loaded")
+                else:
+                    raise OSError(f"Config {self.config['name']} unavailable")
+            except FileNotFoundError:
+                raise FileNotFoundError(f"PCC config file {configfile} does not exist!")
+
+    def check(self):
+        checks = ["avgr", "fz", "t", "combo", "std", "norm", "chunk_idx", "type"]
+        return all(list(map(os.path.exists, [self.config[key] for key in checks])))
+
+    def get(self, key):
+        return self.config[key]
+
+    def get_config(self):
+        return self.config
