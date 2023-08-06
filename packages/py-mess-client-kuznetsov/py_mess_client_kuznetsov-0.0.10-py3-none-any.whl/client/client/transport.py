0a5fe5d093b4982c6a381c3253c60d03f2b88618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:03 +0000

    Add package py-mess-client-kuznetsov/0.0.10/py_mess_client_kuznetsov-0.0.10-py3-none-any.whl

---
+++ packages/py-mess-client-kuznetsov/py_mess_client_kuznetsov-0.0.10-py3-none-any.whl/client/client/transport.py
@@ -0,0 +1,307 @@
+import socket
+import sys
+import time
+import logging
+import json
+import threading
+import hashlib
+import hmac
+import binascii
+from PyQt5.QtCore import pyqtSignal, QObject
+
+sys.path.append('../')
+from common.utils import *
+from common.variables import *
+from common.errors import ServerError
+
+# The logger and the lock object for working with the socket.
+logger = logging.getLogger('client')
+socket_lock = threading.Lock()
+
+
+class ClientTransport(threading.Thread, QObject):
+    '''
+    A class that implements the transport subsystem of the client
+    module. Responsible for interacting with the server.
+    '''
+    # Сигналы новое сообщение и потеря соединения
+    new_message = pyqtSignal(dict)
+    message_205 = pyqtSignal()
+    connection_lost = pyqtSignal()
+
+    def __init__(self, port, ip_address, database, username, passwd, keys):
+        # Вызываем конструкторы предков
+        threading.Thread.__init__(self)
+        QObject.__init__(self)
+
+        # Database class - working with the database
+        self.database = database
+        # User name
+        self.username = username
+        # Password
+        self.password = passwd
+        # Socket for working with the server
+        self.transport = None
+        # A set of keys for encryption
+        self.keys = keys
+        # Establishing a connection:
+        self.connection_init(port, ip_address)
+        # Updating tables of known users and contacts
+        try:
+            self.user_list_update()
+            self.contacts_list_update()
+        except OSError as err:
+            if err.errno:
+                logger.critical(f'Потеряно соединение с сервером.')
+                raise ServerError('Потеряно соединение с сервером!')
+            logger.error(
+                'Timeout соединения при обновлении списков пользователей.')
+        except json.JSONDecodeError:
+            logger.critical(f'Потеряно соединение с сервером.')
+            raise ServerError('Потеряно соединение с сервером!')
+            # The flag for the continuation of the transport operation.
+        self.running = True
+
+    def connection_init(self, port, ip):
+        '''The method responsible for establishing a connection to the server.'''
+        # Initializing the socket and informing the server about our appearance
+        self.transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # A timeout is required to release the socket.
+        self.transport.settimeout(5)
+
+        # Connect, 5 connection attempts, set the success flag to True if
+        # succeeded
+        connected = False
+        for i in range(5):
+            logger.info(f'Попытка подключения №{i + 1}')
+            try:
+                self.transport.connect((ip, port))
+            except (OSError, ConnectionRefusedError):
+                pass
+            else:
+                connected = True
+                logger.debug("Connection established.")
+                break
+            time.sleep(1)
+
+        # If the connection failed - an exception
+        if not connected:
+            logger.critical('Не удалось установить соединение с сервером')
+            raise ServerError('Не удалось установить соединение с сервером')
+
+        logger.debug('Starting auth dialog.')
+
+        # Starting the authorization procedure
+        # Getting the password hash
+        passwd_bytes = self.password.encode('utf-8')
+        salt = self.username.lower().encode('utf-8')
+        passwd_hash = hashlib.pbkdf2_hmac('sha512', passwd_bytes, salt, 10000)
+        passwd_hash_string = binascii.hexlify(passwd_hash)
+
+        logger.debug(f'Passwd hash ready: {passwd_hash_string}')
+
+        # We get the public key and decode it from bytes
+        pubkey = self.keys.publickey().export_key().decode('ascii')
+
+        # Log in to the server
+        with socket_lock:
+            presense = {
+                ACTION: PRESENCE,
+                TIME: time.time(),
+                USER: {
+                    ACCOUNT_NAME: self.username,
+                    PUBLIC_KEY: pubkey
+                }
+            }
+            logger.debug(f"Presense message = {presense}")
+            # We send a welcome message to the server.
+            try:
+                send_message(self.transport, presense)
+                ans = get_message(self.transport)
+                logger.debug(f'Server response = {ans}.')
+                # If the server returned an error, we throw an exception.
+                if RESPONSE in ans:
+                    if ans[RESPONSE] == 400:
+                        raise ServerError(ans[ERROR])
+                    elif ans[RESPONSE] == 511:
+                        # If everything is fine, then we continue the procedure
+                        # authorization.
+                        ans_data = ans[DATA]
+                        hash = hmac.new(passwd_hash_string, ans_data.encode('utf-8'), 'MD5')
+                        digest = hash.digest()
+                        my_ans = RESPONSE_511
+                        my_ans[DATA] = binascii.b2a_base64(
+                            digest).decode('ascii')
+                        send_message(self.transport, my_ans)
+                        self.process_server_ans(get_message(self.transport))
+            except (OSError, json.JSONDecodeError) as err:
+                logger.debug(f'Connection error.', exc_info=err)
+                raise ServerError('Сбой соединения в процессе авторизации.')
+
+    def process_server_ans(self, message):
+        '''The method is a handler for incoming messages from the server.'''
+        logger.debug(f'Разбор сообщения от сервера: {message}')
+
+        # If this is a confirmation of something
+        if RESPONSE in message:
+            if message[RESPONSE] == 200:
+                return
+            elif message[RESPONSE] == 400:
+                raise ServerError(f'{message[ERROR]}')
+            elif message[RESPONSE] == 205:
+                self.user_list_update()
+                self.contacts_list_update()
+                self.message_205.emit()
+            else:
+                logger.error(
+                    f'Принят неизвестный код подтверждения {message[RESPONSE]}')
+
+        # If we add this message from the user to the database, we give a signal about
+        # new message
+        elif ACTION in message and message[ACTION] == MESSAGE and SENDER in message and DESTINATION in message \
+                and MESSAGE_TEXT in message and message[DESTINATION] == self.username:
+            logger.debug(
+                f'Получено сообщение от пользователя {message[SENDER]}:{message[MESSAGE_TEXT]}')
+            self.new_message.emit(message)
+
+    def contacts_list_update(self):
+        '''A method that updates the contact list from the server.'''
+        self.database.contacts_clear()
+        logger.debug(f'Запрос контакт листа для пользователся {self.name}')
+        req = {
+            ACTION: GET_CONTACTS,
+            TIME: time.time(),
+            USER: self.username
+        }
+        logger.debug(f'Сформирован запрос {req}')
+        with socket_lock:
+            send_message(self.transport, req)
+            ans = get_message(self.transport)
+        logger.debug(f'Получен ответ {ans}')
+        if RESPONSE in ans and ans[RESPONSE] == 202:
+            for contact in ans[LIST_INFO]:
+                self.database.add_contact(contact)
+        else:
+            logger.error('Не удалось обновить список контактов.')
+
+    def user_list_update(self):
+        '''A method that updates the list of users from the server.'''
+        logger.debug(f'Запрос списка известных пользователей {self.username}')
+        req = {
+            ACTION: USERS_REQUEST,
+            TIME: time.time(),
+            ACCOUNT_NAME: self.username
+        }
+        with socket_lock:
+            send_message(self.transport, req)
+            ans = get_message(self.transport)
+        if RESPONSE in ans and ans[RESPONSE] == 202:
+            self.database.add_users(ans[LIST_INFO])
+        else:
+            logger.error('Не удалось обновить список известных пользователей.')
+
+    def key_request(self, user):
+        '''A method requesting the user's public key from the server.'''
+        logger.debug(f'Запрос публичного ключа для {user}')
+        req = {
+            ACTION: PUBLIC_KEY_REQUEST,
+            TIME: time.time(),
+            ACCOUNT_NAME: user
+        }
+        with socket_lock:
+            send_message(self.transport, req)
+            ans = get_message(self.transport)
+        if RESPONSE in ans and ans[RESPONSE] == 511:
+            return ans[DATA]
+        else:
+            logger.error(f'Не удалось получить ключ собеседника{user}.')
+
+    def add_contact(self, contact):
+        '''A method that sends information about adding a contact to the server.'''
+        logger.debug(f'Создание контакта {contact}')
+        req = {
+            ACTION: ADD_CONTACT,
+            TIME: time.time(),
+            USER: self.username,
+            ACCOUNT_NAME: contact
+        }
+        with socket_lock:
+            send_message(self.transport, req)
+            self.process_server_ans(get_message(self.transport))
+
+    def remove_contact(self, contact):
+        '''A method that sends information about deleting a contact to the server.'''
+        logger.debug(f'Удаление контакта {contact}')
+        req = {
+            ACTION: REMOVE_CONTACT,
+            TIME: time.time(),
+            USER: self.username,
+            ACCOUNT_NAME: contact
+        }
+        with socket_lock:
+            send_message(self.transport, req)
+            self.process_server_ans(get_message(self.transport))
+
+    def transport_shutdown(self):
+        '''A method that notifies the server of the client shutdown.'''
+        self.running = False
+        message = {
+            ACTION: EXIT,
+            TIME: time.time(),
+            ACCOUNT_NAME: self.username
+        }
+        with socket_lock:
+            try:
+                send_message(self.transport, message)
+            except OSError:
+                pass
+        logger.debug('Транспорт завершает работу.')
+        time.sleep(0.5)
+
+    def send_message(self, to, message):
+        '''A method that sends messages to the server for the user.'''
+        message_dict = {
+            ACTION: MESSAGE,
+            SENDER: self.username,
+            DESTINATION: to,
+            TIME: time.time(),
+            MESSAGE_TEXT: message
+        }
+        logger.debug(f'Сформирован словарь сообщения: {message_dict}')
+        # Необходимо дождаться освобождения сокета для отправки сообщения
+        with socket_lock:
+            send_message(self.transport, message_dict)
+            self.process_server_ans(get_message(self.transport))
+            logger.info(f'Отправлено сообщение для пользователя {to}')
+
+    def run(self):
+        '''A method containing the main cycle of the transport flow.'''
+        logger.debug('Запущен процесс - приёмник собщений с сервера.')
+        while self.running:
+            # We rest for a second and try to grab the socket again.
+            # if you don't make a delay here, then sending can take quite a long time
+            # wait for the socket to be released.
+            time.sleep(1)
+            message = None
+            with socket_lock:
+                try:
+                    self.transport.settimeout(0.5)
+                    message = get_message(self.transport)
+                except OSError as err:
+                    if err.errno:
+                        logger.critical(f'Потеряно соединение с сервером.')
+                        self.running = False
+                        self.connection_lost.emit()
+                # Connection problems
+                except (ConnectionError, ConnectionAbortedError, ConnectionResetError, json.JSONDecodeError, TypeError):
+                    logger.debug(f'Потеряно соединение с сервером.')
+                    self.running = False
+                    self.connection_lost.emit()
+                finally:
+                    self.transport.settimeout(5)
+
+            # If the message is received, then we call the handler function:
+            if message:
+                logger.debug(f'Принято сообщение с сервера: {message}')
+                self.process_server_ans(message)
