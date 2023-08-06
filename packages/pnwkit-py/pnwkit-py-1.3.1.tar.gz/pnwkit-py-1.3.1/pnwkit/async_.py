5b63475f8464a768f7449c2aed3be581d2e12ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:15 +0000

    Add package pnwkit-py/1.3.1/pnwkit-py-1.3.1.tar.gz

---
+++ packages/pnwkit-py/pnwkit-py-1.3.1.tar.gz/pnwkit-py-1.3.1/pnwkit/async_.py
@@ -0,0 +1,289 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, cast, overload
+
+import aiohttp
+
+from .base import KitBase
+from .data import (
+    Alliance,
+    Color,
+    Data,
+    Nation,
+    PaginatorInfo,
+    Trade,
+    Tradeprice,
+    Treasure,
+    War,
+)
+from .errors import GraphQLError
+from .paginator import AlliancePaginator, NationPaginator, Paginator
+
+if TYPE_CHECKING:
+    from typing import (
+        Any,
+        Dict,
+        Literal,
+        Mapping,
+        MutableMapping,
+        Optional,
+        Sequence,
+        Tuple,
+        Type,
+        Union,
+    )
+
+
+class AsyncKit(KitBase):
+    async def _query(
+        self,
+        endpoint: str,
+        params: MutableMapping[str, Any],
+        args: Sequence[Union[str, Any]],
+        *,
+        is_paginator: bool = False,
+    ) -> Dict[str, Any]:
+        query = query = self._format_query(endpoint, params, args, is_paginator)
+        async with aiohttp.request(
+            "GET", self.graphql_url(), json={"query": query}
+        ) as response:
+            data: Any = await response.json()
+            try:
+                if "errors" in data[0]:
+                    error = (
+                        "\n".join(i["message"] for i in data[0]["errors"])
+                        if len(data[0]["errors"]) > 1
+                        else data[0]["errors"][0]["message"]
+                    )
+                    raise GraphQLError(error)
+            except KeyError:
+                pass
+            try:
+                if "errors" in data:
+                    error = (
+                        "\n".join(i["message"] for i in data["errors"])
+                        if len(data["errors"]) > 1
+                        else data["errors"][0]["message"]
+                    )
+                    raise GraphQLError(error)
+            except KeyError:
+                pass
+            return data
+
+    async def _data_query(
+        self,
+        endpoint: str,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        paginator: bool = False,
+        is_paginator: bool = False,
+        type_: Type[Data],
+        paginator_type: Optional[Type[Paginator]] = None,
+        **kwargs: Any,
+    ) -> Union[Tuple[Data, ...], Paginator]:
+        args = (arg, *args)
+        params = params or kwargs
+        if "first" not in params and endpoint in {"alliance", "nations"}:
+            params["first"] = 5
+        response = await self._query(endpoint, params, args, is_paginator=is_paginator)
+        if is_paginator and paginator_type:
+            data = tuple(type_(i) for i in response["data"][endpoint]["data"])
+            if paginator:
+                return paginator_type(
+                    data, PaginatorInfo(response["data"][endpoint]["paginatorInfo"])
+                )
+            return data
+        data = tuple(type_(i) for i in response["data"][endpoint])
+        return data
+
+    @overload
+    async def alliance_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        paginator: Literal[False] = False,
+        **kwargs: Any,
+    ) -> Tuple[Alliance, ...]:
+        ...
+
+    @overload
+    async def alliance_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        paginator: Literal[True] = True,
+        **kwargs: Any,
+    ) -> AlliancePaginator:
+        ...
+
+    async def alliance_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        paginator: bool = False,
+        **kwargs: Any,
+    ) -> Union[Tuple[Alliance, ...], AlliancePaginator]:
+        data = await self._data_query(
+            "alliances",
+            params,
+            arg,
+            *args,
+            **kwargs,
+            type_=Alliance,
+            paginator=paginator,
+            is_paginator=True,
+            paginator_type=AlliancePaginator,
+        )
+        if TYPE_CHECKING:
+            if isinstance(data, tuple):
+                data = cast(Tuple[Alliance, ...], data)
+            else:
+                data = cast(AlliancePaginator, data)
+        return data
+
+    async def color_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        **kwargs: Any,
+    ) -> Tuple[Color, ...]:
+        data = await self._data_query(
+            "colors",
+            params,
+            arg,
+            *args,
+            **kwargs,
+            type_=Color,
+        )
+        if TYPE_CHECKING:
+            data = cast(Tuple[Color, ...], data)
+        return data
+
+    @overload
+    async def nation_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        paginator: Literal[False] = False,
+        **kwargs: Any,
+    ) -> Tuple[Nation, ...]:
+        ...
+
+    @overload
+    async def nation_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        paginator: Literal[True] = True,
+        **kwargs: Any,
+    ) -> NationPaginator:
+        ...
+
+    async def nation_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        paginator: bool = False,
+        **kwargs: Any,
+    ) -> Union[Tuple[Nation, ...], NationPaginator]:
+        data = await self._data_query(
+            "nations",
+            params,
+            arg,
+            *args,
+            **kwargs,
+            type_=Nation,
+            paginator=paginator,
+            is_paginator=True,
+            paginator_type=NationPaginator,
+        )
+        if TYPE_CHECKING:
+            if isinstance(data, tuple):
+                data = cast(Tuple[Nation, ...], data)
+            else:
+                data = cast(NationPaginator, data)
+        return data
+
+    async def trade_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        **kwargs: Any,
+    ) -> Tuple[Trade, ...]:
+        data = await self._data_query(
+            "trades",
+            params,
+            arg,
+            *args,
+            **kwargs,
+            type_=Trade,
+        )
+        if TYPE_CHECKING:
+            data = cast(Tuple[Trade, ...], data)
+        return data
+
+    async def trade_price_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        **kwargs: Any,
+    ) -> Tuple[Tradeprice, ...]:
+        data = await self._data_query(
+            "tradeprices",
+            params,
+            arg,
+            *args,
+            **kwargs,
+            type_=Tradeprice,
+        )
+        if TYPE_CHECKING:
+            data = cast(Tuple[Tradeprice, ...], data)
+        return data
+
+    async def treasure_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        **kwargs: Any,
+    ) -> Tuple[Treasure, ...]:
+        data = await self._data_query(
+            "treasures",
+            params,
+            arg,
+            *args,
+            **kwargs,
+            type_=Treasure,
+        )
+        if TYPE_CHECKING:
+            data = cast(Tuple[Treasure, ...], data)
+        return data
+
+    async def war_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        **kwargs: Any,
+    ) -> Tuple[War, ...]:
+        data = await self._data_query(
+            "wars",
+            params,
+            arg,
+            *args,
+            **kwargs,
+            type_=War,
+        )
+        if TYPE_CHECKING:
+            data = cast(Tuple[War, ...], data)
+        return data
---
+++ packages/pnwkit-py/pnwkit-py-1.3.1.tar.gz/pnwkit-py-1.3.1/pnwkit/base.py
@@ -0,0 +1,355 @@
+from __future__ import annotations
+
+from abc import ABCMeta, abstractmethod
+from typing import TYPE_CHECKING, overload
+
+from .api_key import API_KEY
+
+__all__ = ("KitBase",)
+
+if TYPE_CHECKING:
+    from typing import (
+        Any,
+        List,
+        Literal,
+        Mapping,
+        MutableMapping,
+        Optional,
+        Sequence,
+        Tuple,
+        Union,
+    )
+
+    from .data import *
+    from .paginator import *
+
+    SubQuery = Mapping[str, Union[str, "SubQuery", Sequence[Union[str, "SubQuery"]]]]
+
+
+class KitBase(metaclass=ABCMeta):
+    def __init__(self, api_key: Optional[str] = None, **kwargs: Any) -> None:
+        self.api_key: str = api_key or API_KEY
+
+    def graphql_url(self) -> str:
+        return f"https://api.politicsandwar.com/graphql?api_key={self.api_key}"
+
+    def set_key(self, api_key: str) -> None:
+        """Sets the API key for this instance.
+
+        Parameters
+        ----------
+        api_key : str
+            A Politics and War API Key.
+        """
+        self.api_key: str = api_key
+
+    @classmethod
+    def _format_sub_query(
+        cls,
+        query: SubQuery,
+    ) -> str:
+        key, query_string = list(query.items())[0]
+        if not isinstance(query_string, str):
+            query_arguments: List[Any] = []
+            for value in query_string:
+                if isinstance(value, str):
+                    query_arguments.append(value)
+                else:
+                    query_arguments.append(cls._format_sub_query(value))
+            query_string = " ".join(query_arguments)
+
+        return f"{key}{{{query_string}}}"
+
+    @classmethod
+    def _format_query(
+        cls,
+        endpoint: str,
+        params: MutableMapping[str, Any],
+        query: Union[
+            str,
+            Sequence[Union[str, Mapping[str, Union[str, Mapping[str, Any]]]]],
+        ],
+        is_paginator: bool,
+    ) -> str:
+        if isinstance(params, str):
+            params_string = params
+        else:
+            params_string = ",".join(
+                name
+                + ":"
+                + (
+                    f'"{value}"'
+                    if isinstance(value, str)
+                    else str(value).lower()
+                    if isinstance(value, bool)
+                    else str(value)
+                )
+                for name, value in params.items()
+            )
+        if isinstance(query, str):
+            query_string = query
+        else:
+            query_arguments: List[Any] = []
+            for value in query:
+                if isinstance(value, str):
+                    query_arguments.append(value)
+                else:
+                    query_arguments.append(cls._format_sub_query(value))
+            query_string = " ".join(query_arguments)
+        if is_paginator:
+            if params:
+                return f"{{{endpoint}({params_string}){{paginatorInfo{{count currentPage firstItem hasMorePages lastItem lastPage perPage total}}data{{{query_string}}}}}}}"
+            return f"{{{endpoint}{{paginatorInfo{{count currentPage firstItem hasMorePages lastItem lastPage perPage total}}data{{{query_string}}}}}}}"
+        if params_string:
+            return f"{{{endpoint}({params_string}){{{query_string}}}}}"
+        return f"{{{endpoint}{{{query_string}}}}}"
+
+    @overload
+    @abstractmethod
+    def alliance_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        paginator: Literal[False],
+        **kwargs: Any,
+    ) -> Tuple[Alliance, ...]:
+        ...
+
+    @overload
+    @abstractmethod
+    def alliance_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        paginator: Literal[True],
+        **kwargs: Any,
+    ) -> AlliancePaginator:
+        ...
+
+    @abstractmethod
+    def alliance_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        paginator: bool = False,
+        **kwargs: Any,
+    ) -> Union[Tuple[Alliance, ...], AlliancePaginator]:
+        """Makes a query to the alliances endpoint.
+
+        Parameters
+        ----------
+        params : Union[str, Mapping[str, Any]]
+            A string, dict, or other mapping of the parameters to pass in the query to filter results.
+        arg : Union[str, Mapping[str, Any]]
+            A string, dict, or other mapping of the data to retrieve.
+        *args: Union[str, Mapping[str, Any]]
+            Will be concatenated with arg to form the string of data to retrieve.
+        paginator : bool
+            Whether to return the result as a Paginator, by default False.
+        **kwargs: Any
+            If params is falsy, then extra kwargs will be used as params.
+
+        Returns
+        -------
+        Union[Tuple[Alliance], AlliancePaginator]
+            A tuple of Alliances representing the data retrived. If ``paginator`` is True then will return an AlliancePaginator.
+        """
+        ...
+
+    @abstractmethod
+    def color_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        **kwargs: Any,
+    ) -> Tuple[Color, ...]:
+        """Makes a query to the colors endpoint.
+
+        Parameters
+        ----------
+        params : Union[str, Mapping[str, Any]]
+            A string, dict, or other mapping of the parameters to pass in the query to filter results.
+        arg : Union[str, Mapping[str, Any]]
+            A string, dict, or other mapping of the data to retrieve.
+        *args: Union[str, Mapping[str, Any]]
+            Will be concatenated with arg to form the string of data to retrieve.
+        **kwargs: Any
+            If params is falsy, then extra kwargs will be used as params.
+
+        Returns
+        -------
+        Tuple[Color]
+            A tuple of Colors representing the data retrived.
+        """
+        ...
+
+    @overload
+    @abstractmethod
+    def nation_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        paginator: Literal[False] = False,
+        **kwargs: Any,
+    ) -> Tuple[Nation, ...]:
+        ...
+
+    @overload
+    @abstractmethod
+    def nation_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        paginator: Literal[True] = True,
+        **kwargs: Any,
+    ) -> NationPaginator:
+        ...
+
+    @abstractmethod
+    def nation_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        paginator: bool = False,
+        **kwargs: Any,
+    ) -> Union[Tuple[Nation, ...], NationPaginator]:
+        """Makes a query to the nations endpoint.
+
+        Parameters
+        ----------
+        params : Union[str, Mapping[str, Any]]
+            A string, dict, or other mapping of the parameters to pass in the query to filter results.
+        arg : Union[str, Mapping[str, Any]]
+            A string, dict, or other mapping of the data to retrieve.
+        *args: Union[str, Mapping[str, Any]]
+            Will be concatenated with arg to form the string of data to retrieve.
+        paginator : bool
+            Whether to return the result as a Paginator, by default False.
+        **kwargs: Any
+            If params is falsy, then extra kwargs will be used as params.
+
+        Returns
+        -------
+        Union[Tuple[Nation], NationPaginator]
+            A tuple of Nations representing the data retrived. If ``paginator`` is True then will return a NationPaginator.
+        """
+        ...
+
+    @abstractmethod
+    def trade_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        **kwargs: Any,
+    ) -> Tuple[Trade, ...]:
+        """Makes a query to the trades endpoint.
+
+        Parameters
+        ----------
+        params : Union[str, Mapping[str, Any]]
+            A string, dict, or other mapping of the parameters to pass in the query to filter results.
+        arg : Union[str, Mapping[str, Any]]
+            A string, dict, or other mapping of the data to retrieve.
+        *args: Union[str, Mapping[str, Any]]
+            Will be concatenated with arg to form the string of data to retrieve.
+        **kwargs: Any
+            If params is falsy, then extra kwargs will be used as params.
+
+        Returns
+        -------
+        Tuple[Trade]
+            A tuple of Trades representing the data retrived.
+        """
+        ...
+
+    @abstractmethod
+    def trade_price_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        **kwargs: Any,
+    ) -> Tuple[Tradeprice, ...]:
+        """Makes a query to the tradeprices endpoint.
+
+        Parameters
+        ----------
+        params : Union[str, Mapping[str, Any]]
+            A string, dict, or other mapping of the parameters to pass in the query to filter results.
+        arg : Union[str, Mapping[str, Any]]
+            A string, dict, or other mapping of the data to retrieve.
+        *args: Union[str, Mapping[str, Any]]
+            Will be concatenated with arg to form the string of data to retrieve.
+        **kwargs: Any
+            If params is falsy, then extra kwargs will be used as params.
+
+        Returns
+        -------
+        Tuple[Tradeprice]
+            A tuple of Tradeprices representing the data retrived.
+        """
+        ...
+
+    @abstractmethod
+    def treasure_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        **kwargs: Any,
+    ) -> Tuple[Treasure, ...]:
+        """Makes a query to the treasures endpoint.
+
+        Parameters
+        ----------
+        params : Union[str, Mapping[str, Any]]
+            A string, dict, or other mapping of the parameters to pass in the query to filter results.
+        arg : Union[str, Mapping[str, Any]]
+            A string, dict, or other mapping of the data to retrieve.
+        *args: Union[str, Mapping[str, Any]]
+            Will be concatenated with arg to form the string of data to retrieve.
+        **kwargs: Any
+            If params is falsy, then extra kwargs will be used as params.
+
+        Returns
+        -------
+        Tuple[Treasure]
+            A tuple of Treasures representing the data retrived.
+        """
+        ...
+
+    @abstractmethod
+    def war_query(
+        self,
+        params: MutableMapping[str, Any],
+        arg: Union[str, Mapping[str, Any]],
+        *args: Union[str, Mapping[str, Any]],
+        **kwargs: Any,
+    ) -> Tuple[War, ...]:
+        """Makes a query to the wars endpoint.
+
+        Parameters
+        ----------
+        params : Union[str, Mapping[str, Any]]
+            A string, dict, or other mapping of the parameters to pass in the query to filter results.
+        arg : Union[str, Mapping[str, Any]]
+            A string, dict, or other mapping of the data to retrieve.
+        *args: Union[str, Mapping[str, Any]]
+            Will be concatenated with arg to form the string of data to retrieve.
+        **kwargs: Any
+            If params is falsy, then extra kwargs will be used as params.
+
+        Returns
+        -------
+        Tuple[War]
+            A tuple of Wars representing the data retrived.
+        """
+        ...
