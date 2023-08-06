0b07f8d6e9bb8a14bf08cee3d256d09d43f1ec4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:59 +0000

    Add package robyn/0.12.0/robyn-0.12.0.tar.gz

---
+++ packages/robyn/robyn-0.12.0.tar.gz/robyn-0.12.0/integration_tests/test_base_url.py
@@ -0,0 +1,20 @@
+import requests
+
+
+def test_local_index_request(session):
+    BASE_URL = "http://127.0.0.1:5000"
+    res = requests.get(f"{BASE_URL}")
+    assert(res.status_code == 200)
+
+
+def test_global_index_request(global_session):
+    BASE_URL = "http://0.0.0.0:5000"
+    res = requests.get(f"{BASE_URL}")
+    assert(res.status_code == 200)
+
+
+def test_dev_index_request(dev_session):
+    BASE_URL = "http://0.0.0.0:5000"
+    res = requests.get(f"{BASE_URL}")
+    assert(res.status_code == 200)
+
