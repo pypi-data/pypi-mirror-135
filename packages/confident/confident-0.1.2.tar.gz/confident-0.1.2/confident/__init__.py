739fadad8206f16f238006732f434ddf069263c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:28 +0000

    Add package confident/0.1.2/confident-0.1.2.tar.gz

---
+++ packages/confident/confident-0.1.2.tar.gz/confident-0.1.2/confident/__init__.py
@@ -0,0 +1,2 @@
+from confident.confident import Confident, ConfidentConfigSpecs
+from confident.deployment_field import DeploymentField
---
+++ packages/confident/confident-0.1.2.tar.gz/confident-0.1.2/confident/config_property.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from typing import Any, Union
+
+from config_source import ConfigSource
+from pydantic.main import BaseModel
+
+
+class ConfigProperty(BaseModel):
+    """
+    Holds details of a single configuration variable and its value.
+    """
+    name: str
+    value: Any
+    origin_value: Any
+    source_name: str
+    source_type: ConfigSource
+    source_location: Union[str, Path]
+
+    def __init__(self, value: Any, **kwargs):
+        try:
+            origin_value = kwargs.pop('origin_value')
+        except KeyError:
+            origin_value = value
+        super().__init__(value=value, origin_value=origin_value, **kwargs)
