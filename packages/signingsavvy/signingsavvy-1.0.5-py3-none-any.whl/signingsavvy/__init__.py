ed4a50f093ab8ffe3dc9b5d7333145b8bd931dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:36 +0000

    Add package signingsavvy/1.0.5/signingsavvy-1.0.5-py3-none-any.whl

---
+++ packages/signingsavvy/signingsavvy-1.0.5-py3-none-any.whl/signingsavvy/__init__.py
@@ -0,0 +1,16 @@
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+# type: ignore[has-type]
+
+from signingsavvy import __version__
+from signingsavvy import api
+
+
+"""
+Submodules
+==========
+
+.. autosummary::
+    :toctree: _autosummary
+
+    api
+"""
