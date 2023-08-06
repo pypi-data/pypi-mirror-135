7ec4fe505e246115974eca92f6522f353049aa57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:37 +0000

    Add package featureclass/0.3.0/featureclass-0.3.0-py3-none-any.whl

---
+++ packages/featureclass/featureclass-0.3.0-py3-none-any.whl/featureclass/__init__.py
@@ -0,0 +1,3 @@
+from featureclass.core import as_dataclass, asdict, feature, feature_annotations, feature_names, featureclass
+
+__all__ = ["featureclass", "feature", "feature_names", "feature_annotations", "asdict", "as_dataclass"]
