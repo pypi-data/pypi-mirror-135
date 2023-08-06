083007b13fcfa0a153b8e0e3304d7fbf54fa5ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:53 +0000

    Add package cornflow-client/0.30.0/cornflow_client-0.30.0-py3-none-any.whl

---
+++ packages/cornflow-client/cornflow_client-0.30.0-py3-none-any.whl/cornflow_client/core/instance.py
@@ -0,0 +1,10 @@
+from .instance_solution import InstanceSolutionCore
+from abc import ABC
+
+
+class InstanceCore(InstanceSolutionCore, ABC):
+    """
+    The instance template.
+    """
+
+    pass
---
+++ packages/cornflow-client/cornflow_client-0.30.0-py3-none-any.whl/cornflow_client/core/solution.py
@@ -0,0 +1,10 @@
+from .instance_solution import InstanceSolutionCore
+from abc import ABC
+
+
+class SolutionCore(InstanceSolutionCore, ABC):
+    """
+    The solution template.
+    """
+
+    pass
