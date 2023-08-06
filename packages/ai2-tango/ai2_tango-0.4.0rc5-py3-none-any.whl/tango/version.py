cb0aefa2ce704a75b3e1ebd5d38b5b259a75bcb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:57 +0000

    Add package ai2-tango/0.4.0rc5/ai2_tango-0.4.0rc5-py3-none-any.whl

---
+++ packages/ai2-tango/ai2_tango-0.4.0rc5-py3-none-any.whl/tango/version.py
@@ -0,0 +1,10 @@
+_MAJOR = "0"
+_MINOR = "4"
+# On main and in a nightly release the patch should be one ahead of the last
+# released build.
+_PATCH = "0"
+# This is mainly for pre-releases which have the suffix "rc[0-9]+".
+_SUFFIX = "rc5"
+
+VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
+VERSION = "{0}.{1}.{2}{3}".format(_MAJOR, _MINOR, _PATCH, _SUFFIX)
