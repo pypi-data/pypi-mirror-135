dfc56dcccb795ea21d2681eea436a03e9598ea38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:32 +0000

    Add package aibro/1.1.1/aibro-1.1.1.tar.gz

---
+++ packages/aibro/aibro-1.1.1.tar.gz/aibro-1.1.1/aibro/scripts/call_predict.sh
@@ -0,0 +1,8 @@
+. repo_venv/bin/activate
+cd $1
+python3 -c """
+from predict import *
+model = load_model()
+result = run(model)
+"""
+echo "Prediction finished without error"
---
+++ packages/aibro/aibro-1.1.1.tar.gz/aibro-1.1.1/aibro/scripts/infer_command.sh
@@ -0,0 +1,4 @@
+python -m venv repo_venv
+. repo_venv/bin/activate
+pip install -r $1/requirements.txt
+echo "Dependencies installed!"
