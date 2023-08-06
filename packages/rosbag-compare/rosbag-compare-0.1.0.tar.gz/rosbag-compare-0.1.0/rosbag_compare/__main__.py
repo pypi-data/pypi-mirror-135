3dfbe90840e8479dcc09a0b2589b5a7833e02e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:54:01 +0000

    Add package rosbag-compare/0.1.0/rosbag-compare-0.1.0.tar.gz

---
+++ packages/rosbag-compare/rosbag-compare-0.1.0.tar.gz/rosbag-compare-0.1.0/rosbag_compare/__main__.py
@@ -0,0 +1,67 @@
+"""Rosbags topic comparator
+
+Usage:
+------
+
+ $ rosbag-compare [OPTIONS] -b BAGFOLDER
+
+Compare the topics in rosbags inside BAGFOLDER and summarize in a JSON file:
+
+ $ rosbag-compare -b path/to/BAGFOLDER
+
+Compare the topics in rosbags inside BAGFOLDER, summarize in a JSON file
+    and plot missing topics in each rosbag:
+
+ $ rosbag-compare -b path/to/BAGFOLDER -p
+
+Available options are:
+
+ -h, --help                                Show this help
+ -b BAGFOLDER, --bagfolder BAGFOLDER       Path to folder with rosbags
+ -p, --plot                                Optional. Plot missing topics
+
+Version:
+--------
+
+- rosbag-compare v0.1.0
+"""
+
+
+import argparse
+from pathlib import Path
+
+from .comparator import RosbagComparator
+
+
+def parse_arguments():
+    """Parse bagfile name"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-b",
+        "--bagfolder",
+        help="""Path to the folder that contains rosbags""",
+        required=True,
+    )
+    parser.add_argument(
+        "-p",
+        "--plot",
+        help="Flag for plotting and showing the result",
+        action="store_true",
+    )
+    return parser.parse_args()
+
+
+def main():
+    """Main function"""
+    args = parse_arguments()
+    data_path = Path(args.bagfolder)
+    is_plot = args.plot
+    rosbag_comp = RosbagComparator(data_path)
+    rosbag_comp.extract_data()
+    rosbag_comp.to_json()
+    if is_plot:
+        rosbag_comp.plot()
+
+
+if __name__ == "__main__":
+    main()
