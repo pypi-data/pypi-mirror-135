89cf50b8ffdc7ad17e5d54e3c032c290e9f384cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:18 +0000

    Add package danitonapi/1.3/danitonapi-1.3-py3-none-any.whl

---
+++ packages/danitonapi/danitonapi-1.3-py3-none-any.whl/danitonapi/api.py
@@ -0,0 +1,39 @@
+import requests
+import time
+
+token = ''
+
+#testRequest: wait for request
+class testRequest:
+  def __init__(self):
+    self.text = 'Thinking'
+
+def login(token):
+  global token
+  token = ''
+
+class ChatBot:
+  #getAnswer: wait for answer and return it
+  def __init__(self, id, name, language):
+    self.thinking = False
+    self.id = id
+    self.name = name
+    self.language = language
+  def getAnswer(self, message):
+    if self.thinking:
+      return False
+    try:
+      x = testRequest()
+      self.thinking = True
+      while x.text == 'Thinking':
+        x = requests.get('https://api.daniton999.ml/chatbot', json = {'token': token, 'id': self.id, 'language': self.language, 'name': self.name, 'message': message}, stream = True)
+        time.sleep(3)
+      x = x.json()['message']
+    except Exception as es:
+      print(es)
+      x = ''
+    self.thinking = False
+    return x
+    #reset: reset the chatbot
+    def reset(self):
+      requests.patch('https://api.daniton999.ml/chatbot', json = {'token': token, 'id': self.id, 'language': self.language, 'name': self.name})
