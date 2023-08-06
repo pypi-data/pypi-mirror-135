c82ad76887004d0a5ccb1e18dd9dbb98d9418ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:24 +0000

    Add package arcfutil/0.6.4/arcfutil-0.6.4-py3-none-any.whl

---
+++ packages/arcfutil/arcfutil-0.6.4-py3-none-any.whl/arcfutil/exception.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+# (c)2021 .direwolf <kururinmiracle@outlook.com>
+# Licensed under the MIT License.
+
+class AffNoteIndexError(Exception):
+    pass
+
+
+class AffNoteValueError(Exception):
+    pass
+
+
+class AffSceneTypeError(Exception):
+    pass
+
+
+class AffReadError(Exception):
+    pass
