ac4d3d772fae22809256a130b3dcb7a6150de0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:14 +0000

    Add package BeeDrive/0.1.0.2/BeeDrive-0.1.0.2.tar.gz

---
+++ packages/BeeDrive/BeeDrive-0.1.0.2.tar.gz/BeeDrive-0.1.0.2/setup.py
@@ -0,0 +1,38 @@
+import sys
+import os
+
+import setuptools
+
+sys.path.append("../../")
+import beedrive
+
+
+setuptools.setup(
+    name=beedrive.__name__,
+    version=beedrive.__version__,
+    description="BeeDrive: Open Source Privacy File Transfering System for Teams and Individual Developers",
+    long_description=open("../../README.md", encoding="utf8").read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/JacksonWuxs/BeeDrive",
+    author="Xuansheng Wu",
+    maintainer="Xuansheng Wu",
+    platforms=["all"],
+    python_requires='>=3',
+    package_dir={"": os.path.abspath("../../")},
+    packages=setuptools.find_packages(where=os.path.abspath("../../"),
+                                      exclude=("distribute_exe",
+                                               "distribute_pip",)),
+    license="GPL v3",
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        "Programming Language :: Python :: 3",
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        ],
+    entry_points={"console_scripts": ["beedrive=beedrive.__main__:main"],
+                  "gui_scripts": ["beedrive-gpu=beedrive.app:main"]}
+    )
+    
+        
+
