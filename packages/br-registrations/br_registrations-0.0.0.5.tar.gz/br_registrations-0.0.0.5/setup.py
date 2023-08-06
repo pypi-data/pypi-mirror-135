a43a4000cf214ccb67726ecafbe5dc6250e3bd15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:50 +0000

    Add package br-registrations/0.0.0.5/br_registrations-0.0.0.5.tar.gz

---
+++ packages/br-registrations/br_registrations-0.0.0.5.tar.gz/br_registrations-0.0.0.5/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+name='br_registrations'
+git_repo = "https://github.com/sliatecinos/br_registrations"
+
+with open('README.rst') as _readme_file:
+    readme = _readme_file.read()
+
+setuptools.setup(
+    name=name,  # Required
+    url=git_repo,   # Optional
+    version='0.0.0.5',  # Required
+    author='Sliatecinos',   # Optional
+    author_email='sliatecinos@hotmail.com',   # Optional
+    package_dir={'': 'br_registrations'},
+    packages=setuptools.find_packages(where='br_registrations'),
+    license='MIT',
+    description=(
+        "A simple Python library containing functions that check Brazilian documents values"
+    ),
+    long_description=readme,
+    classifiers=[
+		'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+		'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.6',
+    keywords="cpf,cnpj,br,brazil,brasil,validate",  # Optional
+)
