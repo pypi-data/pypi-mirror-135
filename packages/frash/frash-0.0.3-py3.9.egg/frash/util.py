77f1e5a5af65fc3c6944e7b9ca941aac3a498354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:27 +0000

    Add package frash/0.0.3/frash-0.0.3-py3.9.egg

---
+++ packages/frash/frash-0.0.3-py3.9.egg/frash/util.py
@@ -0,0 +1,28 @@
+import sys
+
+
+def normalize_to_hex(input):
+    if input in [".0", "0."]:
+        return "0x00"
+    else:
+        try:
+            return hex(int(input))
+        except ValueError:
+            try:
+                output = float.hex(float(input))
+                out_arr = output.split("p")
+                out_arr[0] = out_arr[0].rstrip("0")
+                out_arr[1] = out_arr[1].lstrip("+")
+                output = out_arr[0] + "p" + out_arr[1]
+                return output
+            except ValueError:
+                print("Wrong input!")
+                sys.exit(-1)
+
+
+def from_hex(input):
+    try:
+        output = float.fromhex(input)
+        return f"{output:f}".rstrip("0").rstrip(".")
+    except ValueError:
+        sys.exit(-1)
