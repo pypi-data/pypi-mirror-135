4f820e9e73aa8a6b018ec229f5bed8219fba6632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:46 +0000

    Add package nanome/0.33.4/nanome-0.33.4.tar.gz

---
+++ packages/nanome/nanome-0.33.4.tar.gz/nanome-0.33.4/nanome/_internal/_util/_proc_data.py
@@ -0,0 +1,11 @@
+from nanome.util import IntEnum, auto
+
+
+class _DataType(IntEnum):
+    process = auto()
+
+
+class _ProcData():
+    def __init__(self):
+        self._type = _DataType.process
+        self._data = None
