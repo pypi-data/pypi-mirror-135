e6667e1c1c69328304299dc8cfdf64928e62edb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:27 +0000

    Add package optiz/0.0.1/optiz-0.0.1.tar.gz

---
+++ packages/optiz/optiz-0.0.1.tar.gz/optiz-0.0.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="optiz",
+    version="0.0.1",
+    license="MIT",
+    description="Optimization library for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Lucas Marandat",
+    author_email="lucas.mrdt@gmail.com",
+    url="https://github.com/lucasmrdt/optiz",
+    keywords=["optimisation"],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    py_modules=["optiz"],
+)
