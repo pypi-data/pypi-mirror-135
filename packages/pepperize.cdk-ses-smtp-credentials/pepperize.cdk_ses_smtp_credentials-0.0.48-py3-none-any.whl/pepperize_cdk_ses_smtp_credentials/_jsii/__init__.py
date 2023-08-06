97767671e2287c54ca974e684d3d518965e0ded2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:00 +0000

    Add package pepperize.cdk-ses-smtp-credentials/0.0.48/pepperize.cdk_ses_smtp_credentials-0.0.48-py3-none-any.whl

---
+++ packages/pepperize.cdk-ses-smtp-credentials/pepperize.cdk_ses_smtp_credentials-0.0.48-py3-none-any.whl/pepperize_cdk_ses_smtp_credentials/_jsii/__init__.py
@@ -0,0 +1,31 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_nodejs._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.aws_secretsmanager._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@pepperize/cdk-ses-smtp-credentials",
+    "0.0.48",
+    __name__[0:-6],
+    "cdk-ses-smtp-credentials@0.0.48.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
