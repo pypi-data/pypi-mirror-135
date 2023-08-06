3d3edaaf93b41090dd82b7d00c6778ebd69fe1e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:52 +0000

    Add package cpkg/0.0.10/cpkg-0.0.10.tar.gz

---
+++ packages/cpkg/cpkg-0.0.10.tar.gz/cpkg-0.0.10/cpkg/__init__.py
@@ -0,0 +1,12 @@
+from .createPkg import *
+from cpkgtab import *
+import argparse
+__author__ = 'xiongtianshuo'
+__version__ = '0.0.10'
+__all__ = [
+    'NameNotSpecificationError',
+    'PathNotFoundError',
+    'PathNotFoundError',
+    'create'
+]
+ctab(__version__, create)
