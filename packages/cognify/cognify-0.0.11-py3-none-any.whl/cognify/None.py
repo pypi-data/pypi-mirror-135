2add21630bb2ddae1a7e038d8e6c5e2109ee2ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:12 +0000

    Add package cognify/0.0.11/cognify-0.0.11-py3-none-any.whl

---
+++ packages/cognify/cognify-0.0.11-py3-none-any.whl/cognify/None.py
@@ -0,0 +1,111 @@
+
+
+# Cell
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import time
+import os
+import mne
+import psycopg2
+from cognify import dataset
+
+# Cell
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import time
+import os
+import mne
+import psycopg2
+from cognify import dataset
+
+# Cell
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import time
+import os
+import mne
+import psycopg2
+from cognify import dataset
+
+# Cell
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import time
+import os
+import mne
+import psycopg2
+from cognify import dataset
+
+# Cell
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import time
+import os
+import mne
+import psycopg2
+from cognify import dataset
+
+# Cell
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import time
+import os
+import mne
+import psycopg2
+from cognify import dataset
+
+# Cell
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import time
+import os
+import mne
+import psycopg2
+from cognify import dataset
+
+# Cell
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import time
+import os
+import mne
+import psycopg2
+from cognify import dataset
+
+# Cell
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import time
+import os
+import mne
+import psycopg2
+from cognify import dataset
+
+# Cell
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import time
+import os
+import mne
+import psycopg2
+from cognify import dataset
