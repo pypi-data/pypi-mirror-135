27781060a85219c908ed2d5934935c0817346211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:42 +0000

    Add package limits/2.3.1/limits-2.3.1.tar.gz

---
+++ packages/limits/limits-2.3.1.tar.gz/limits-2.3.1/doc/source/conf.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+#
+
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath("../../"))
+sys.path.insert(0, os.path.abspath("./"))
+
+import limits
+from theme_config import *
+
+project = "limits"
+description = "limits is a python library to perform rate limiting with commonly used storage backends"
+copyright = "2021, Ali-Akber Saifee"
+version = release = limits.__version__.split("+")[0]
+
+html_theme_options["description"] = description
+html_theme_options["github_repo"] = "limits"
+
+html_static_path = ["./_static"]
+html_css_files = [
+    "custom.css",
+    "https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;700&family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+]
+html_sidebars = {
+    "**": [
+        "about.html",
+        "searchbox.html",
+        "navigation.html",
+        "relations.html",
+        "donate.html",
+    ]
+}
+
+extensions = [
+    "alabaster",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinxcontrib.programoutput",
+    "sphinx_autodoc_typehints",
+    "sphinx_panels",
+    "sphinx_paramlinks",
+]
+
+autodoc_default_options = {
+    "members": True,
+    "inherited-members": True,
+    "inherit-docstrings": True,
+    "member-order": "bysource",
+}
+
+add_module_names = False
+autoclass_content = "both"
+
+autosectionlabel_maxdepth = 3
+autosectionlabel_prefix_document = True
+
+extlinks = {"pypi": ("https://pypi.org/project/%s", "%s")}
+
+intersphinx_mapping = {
+    "python": ("http://docs.python.org/", None),
+    "coredis": ("https://coredis.readthedocs.io/en/latest/", None),
+    "emcache": ("https://emcache.readthedocs.io/en/latest/", None),
+    "motor": ("https://motor.readthedocs.io/en/stable/", None),
+    "redis-py-cluster": ("https://redis-py-cluster.readthedocs.io/en/latest/", None),
+    "redis-py": ("https://redis-py.readthedocs.io/en/latest/", None),
+    "pymemcache": ("https://pymemcache.readthedocs.io/en/latest/", None),
+    "pymongo": ("https://pymongo.readthedocs.io/en/stable/", None),
+}
