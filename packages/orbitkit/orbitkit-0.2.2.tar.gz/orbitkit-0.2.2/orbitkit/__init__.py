16c833e1f4528ad781325c3f5d5c887e59a2db76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:27 +0000

    Add package orbitkit/0.2.2/orbitkit-0.2.2.tar.gz

---
+++ packages/orbitkit/orbitkit-0.2.2.tar.gz/orbitkit-0.2.2/orbitkit/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from orbitkit import util
+from orbitkit.file_extractor.dispatcher import FileDispatcher
+from orbitkit import id_srv
+from orbitkit.lark_send import FeiShuTalkChatBot
+
+name = 'orbitkit'
+
+__version__ = '0.2.2'
+VERSION = __version__
+
+__all__ = [
+    'util',
+    'FileDispatcher',
+    'FeiShuTalkChatBot',
+    'id_srv',
+]
