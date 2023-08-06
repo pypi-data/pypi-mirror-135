12e61b941bbda4f1759c7ff9fa2ec6e6fc0ffe24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:30 +0000

    Add package ruuvi-lapio/0.2.0/ruuvi-lapio-0.2.0.tar.gz

---
+++ packages/ruuvi-lapio/ruuvi-lapio-0.2.0.tar.gz/ruuvi-lapio-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ruuvi_lapio']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.8.1,<4.0.0', 'ruuvitag-sensor>=1.2.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'ruuvi-lapio',
+    'version': '0.2.0',
+    'description': 'A simple app that sends sensory data over http',
+    'long_description': None,
+    'author': 'Panu Oksiala',
+    'author_email': 'panu@oksiala.fi',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/poksiala/ruuvi-lapio',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
