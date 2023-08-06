2a6ad1c3290712ea89573efcfbb8d2cb1eeb54d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:00 +0000

    Add package niceshell/1.1.0/niceshell-1.1.0.tar.gz

---
+++ packages/niceshell/niceshell-1.1.0.tar.gz/niceshell-1.1.0/niceshell/__init__.py
@@ -0,0 +1,31 @@
+from .core import (
+    expose_tilde,
+    normalize_short_and_long_args,
+    quotes_wrapper,
+    shell,
+    Shell,
+    ShortArgsOption
+)
+from .extra import (
+    force_sudo_password_promt,
+    get_root_privileges,
+    get_root_privileges_or_exit,
+    has_root_privileges,
+    list_dirs,
+    list_files
+)
+from .gnu_coreutils import cd, cp, ln, ls, mv, rm
+
+__all__ = ["cd", "cp", "expose_tilde", "force_sudo_password_promt",
+           "get_root_privileges", "get_root_privileges_or_exit", "GID",
+           "GROUP", "has_root_privileges", "HOME", "list_dirs", "list_files",
+           "ln", "ls", "mv", "normalize_short_and_long_args", "quotes_wrapper",
+           "rm", "shell", "Shell", "ShortArgsOption", "UID", "USER"]
+__author__ = "Andrew Voynov"
+__version__ = "1.1.0"
+
+GID = Shell("id -g").output()[:-1]
+GROUP = Shell("id -gn").output()[:-1]
+HOME = Shell("printf ~").output()
+UID = Shell("id -u").output()[:-1]
+USER = Shell("id -un").output()[:-1]
