bf19c8c4d4a324e46eebcbc850a1cce88a5a6e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:14 +0000

    Add package discord-rpc/3.0/discord-rpc-3.0.tar.gz

---
+++ packages/discord-rpc/discord-rpc-3.0.tar.gz/discord-rpc-3.0/DiscordRPC/__init__.py
@@ -0,0 +1,10 @@
+from .presence import *
+from .button import button
+from .exceptions import *
+from .get_current_app import GCAR
+
+__title__ = "Discord-RPC"
+__version__ = "3.0"
+__authors__ = "LyQuid"
+__license__ = "Apache License 2.0"
+__copyright__ = "Copyright 2021-present LyQuid"
