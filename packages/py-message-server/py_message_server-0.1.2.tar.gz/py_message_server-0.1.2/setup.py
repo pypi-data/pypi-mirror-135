d30bf19286beaa9f28ec60d37f7b56b0a91bb7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:49 +0000

    Add package py-message-server/0.1.2/py_message_server-0.1.2.tar.gz

---
+++ packages/py-message-server/py_message_server-0.1.2.tar.gz/py_message_server-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="py_message_server",
+      version="0.1.2",
+      description="Message Server",
+      author="User",
+      author_email="shtyrov89@gmail.com",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex'],
+      scripts=['server/server_run']
+      )
