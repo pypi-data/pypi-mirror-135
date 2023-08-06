589609265150ebed1d6504edd3abad1aac7537f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:41 +0000

    Add package py-mess-server-kuznetsov/3.3.9/py_mess_server_kuznetsov-3.3.9.tar.gz

---
+++ packages/py-mess-server-kuznetsov/py_mess_server_kuznetsov-3.3.9.tar.gz/py_mess_server_kuznetsov-3.3.9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name="py_mess_server_kuznetsov",
+      version="3.3.9",
+      description="Mess Server",
+      author="Sergey Kuznetsov",
+      author_email="ksn1974@mail.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex'],
+      scripts=['server/server_run']
+      )
+
