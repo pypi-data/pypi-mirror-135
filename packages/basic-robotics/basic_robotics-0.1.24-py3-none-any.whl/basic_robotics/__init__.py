c3aaddf5bbb0086cd86e59b9f8bb586e34eaa218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:41 +0000

    Add package basic-robotics/0.1.24/basic_robotics-0.1.24-py3-none-any.whl

---
+++ packages/basic-robotics/basic_robotics-0.1.24-py3-none-any.whl/basic_robotics/__init__.py
@@ -0,0 +1,11 @@
+# __init__.py
+
+# Version
+__version__ = "0.1.24"
+from . import faser_interfaces
+from . import modern_robotics_numba
+from . import faser_math
+#from . import faser_plotting
+from . import faser_robot_kinematics
+#from . import faser_utils
+#from . import robot_collisions
