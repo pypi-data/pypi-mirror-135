1c0dcc5bceb14b915d1fb1495238a9cbf258a2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:08 +0000

    Add package jupyterlab-myst/0.1.2/jupyterlab-myst-0.1.2.tar.gz

---
+++ packages/jupyterlab-myst/jupyterlab-myst-0.1.2.tar.gz/jupyterlab-myst-0.1.2/jupyterlab_myst/labextension/static/style_index_js.5f43024f5bdd0bfd4808.js
@@ -0,0 +1,73 @@
+"use strict";
+(self["webpackChunkjupyterlab_myst"] = self["webpackChunkjupyterlab_myst"] || []).push([["style_index_js"],{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./style/base.css":
+/*!**************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./style/base.css ***!
+  \**************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/cssWithMappingToString.js */ "./node_modules/css-loader/dist/runtime/cssWithMappingToString.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// Imports
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, ".jp-RenderedMarkdown aside.callout {\n  width: 95%;\n  margin: 1.5625em auto;\n  padding: 0 .6rem .8rem!important;\n  overflow: hidden;\n  page-break-inside: avoid;\n  border-left: .2rem solid #007bff;\n  border-radius: .1rem;\n  box-shadow: 0 0.2rem 0.5rem rgba(0,0,0,.05), 0 0 0.05rem rgba(0,0,0,.1);\n}\n\n.jp-RenderedMarkdown aside.callout > header {\n  position: relative;\n  margin: 0 -.6rem!important;\n  /* Changed the final from 2rem, without icon */\n  padding: .4rem .6rem .4rem 1rem;\n  font-weight: 700;\n  background-color: #e7f2fa;\n}\n\n.jp-RenderedMarkdown aside.callout.attention {border-left-color: #fd7e14;}\n.jp-RenderedMarkdown aside.callout.attention > header {background-color: #ffedcc;}\n.jp-RenderedMarkdown aside.callout.caution {border-left-color: #fd7e14;}\n.jp-RenderedMarkdown aside.callout.caution > header {background-color: #ffedcc;}\n.jp-RenderedMarkdown aside.callout.warning {border-left-color: #dc3545;}\n.jp-RenderedMarkdown aside.callout.warning > header {background-color: #fdf3f2;}\n.jp-RenderedMarkdown aside.callout.danger {border-left-color: #dc3545;}\n.jp-RenderedMarkdown aside.callout.danger > header {background-color: #fdf3f2;}\n.jp-RenderedMarkdown aside.callout.error {border-left-color: #dc3545;}\n.jp-RenderedMarkdown aside.callout.error > header {background-color: #fdf3f2;}\n.jp-RenderedMarkdown aside.callout.hint {border-left-color: #ffc107;}\n.jp-RenderedMarkdown aside.callout.hint > header {background-color: #fff6dd;}\n.jp-RenderedMarkdown aside.callout.important {border-left-color: #007bff;}\n.jp-RenderedMarkdown aside.callout.important > header {background-color: #e7f2fa;}\n.jp-RenderedMarkdown aside.callout.note {border-left-color: #007bff;}\n.jp-RenderedMarkdown aside.callout.note > header {background-color: #e7f2fa;}\n.jp-RenderedMarkdown aside.callout.tip {border-left-color: #ffc107;}\n.jp-RenderedMarkdown aside.callout.tip > header {background-color: #fff6dd;}\n\n.jp-RenderedMarkdown aside.callout > p {\n  padding: 0 1.4rem;\n}\n\n.jp-RenderedMarkdown abbr {\n  text-decoration: none;\n  border-bottom: 1px solid #ddd;\n  cursor: help;\n}\n\n.jp-RenderedMarkdown a {\n  color: #0071bc;\n  text-decoration: none;\n}\n\n.jp-RenderedMarkdown a:hover {\n  text-decoration: underline;\n}\n\n.jp-RenderedMarkdown code {\n  font-size: 95%;\n  font-family: monospace;\n  color: #e83e8c;\n  word-wrap: break-word;\n}\n\n.jp-RenderedMarkdown pre {\n  width: calc(100% - 20px - 3em);\n  padding: 10px;\n  background-color: #fafafa;\n  color: #222;\n  line-height: 1.2em;\n  border: 1px solid #c9c9c9;\n  margin: 1.5em 0;\n  box-shadow: 1px 1px 1px #d8d8d8;\n}\n.jp-RenderedMarkdown pre > code {\n  font-size: 87.5%;\n  font-family: monospace;\n  color: #222;\n}\n\n.jp-RenderedMarkdown hr {\n  width: 80%;\n  margin: 20px auto;\n}\n\n.jp-RenderedMarkdown blockquote {\n  color: rgb(90, 90, 90);\n  padding: 0.1rem 1rem;\n  border-left: .2rem solid #aaa;\n}\n\n.jp-RenderedMarkdown figure {\n  text-align: center;\n}\n\n.jp-RenderedMarkdown figure.numbered > figcaption {\n  color: rgb(90, 90, 90);\n  font-size: 90%;\n}\n\n.jp-RenderedMarkdown figure.numbered > figcaption:before {\n  content: 'Figure ' attr(number) ':';\n  font-weight: 700;\n  user-select: text;\n}\n\n.jp-RenderedMarkdown figure.numbered > figcaption > p:first-child {\n  display: inline;\n}\n\n.math .MathJax {\n  outline: none;\n}\n.jp-RenderedMarkdown div.math.numbered {\n  width: calc(100% - 50px);\n  margin: 0 auto;\n}\n.jp-RenderedMarkdown div.math.numbered:before {\n  content: '(' attr(number) ')';\n  float: right;\n}\n", "",{"version":3,"sources":["webpack://./style/base.css"],"names":[],"mappings":"AAAA;EACE,UAAU;EACV,qBAAqB;EACrB,gCAAgC;EAChC,gBAAgB;EAChB,wBAAwB;EACxB,gCAAgC;EAChC,oBAAoB;EACpB,uEAAuE;AACzE;;AAEA;EACE,kBAAkB;EAClB,0BAA0B;EAC1B,8CAA8C;EAC9C,+BAA+B;EAC/B,gBAAgB;EAChB,yBAAyB;AAC3B;;AAEA,8CAA8C,0BAA0B,CAAC;AACzE,uDAAuD,yBAAyB,CAAC;AACjF,4CAA4C,0BAA0B,CAAC;AACvE,qDAAqD,yBAAyB,CAAC;AAC/E,4CAA4C,0BAA0B,CAAC;AACvE,qDAAqD,yBAAyB,CAAC;AAC/E,2CAA2C,0BAA0B,CAAC;AACtE,oDAAoD,yBAAyB,CAAC;AAC9E,0CAA0C,0BAA0B,CAAC;AACrE,mDAAmD,yBAAyB,CAAC;AAC7E,yCAAyC,0BAA0B,CAAC;AACpE,kDAAkD,yBAAyB,CAAC;AAC5E,8CAA8C,0BAA0B,CAAC;AACzE,uDAAuD,yBAAyB,CAAC;AACjF,yCAAyC,0BAA0B,CAAC;AACpE,kDAAkD,yBAAyB,CAAC;AAC5E,wCAAwC,0BAA0B,CAAC;AACnE,iDAAiD,yBAAyB,CAAC;;AAE3E;EACE,iBAAiB;AACnB;;AAEA;EACE,qBAAqB;EACrB,6BAA6B;EAC7B,YAAY;AACd;;AAEA;EACE,cAAc;EACd,qBAAqB;AACvB;;AAEA;EACE,0BAA0B;AAC5B;;AAEA;EACE,cAAc;EACd,sBAAsB;EACtB,cAAc;EACd,qBAAqB;AACvB;;AAEA;EACE,8BAA8B;EAC9B,aAAa;EACb,yBAAyB;EACzB,WAAW;EACX,kBAAkB;EAClB,yBAAyB;EACzB,eAAe;EACf,+BAA+B;AACjC;AACA;EACE,gBAAgB;EAChB,sBAAsB;EACtB,WAAW;AACb;;AAEA;EACE,UAAU;EACV,iBAAiB;AACnB;;AAEA;EACE,sBAAsB;EACtB,oBAAoB;EACpB,6BAA6B;AAC/B;;AAEA;EACE,kBAAkB;AACpB;;AAEA;EACE,sBAAsB;EACtB,cAAc;AAChB;;AAEA;EACE,mCAAmC;EACnC,gBAAgB;EAChB,iBAAiB;AACnB;;AAEA;EACE,eAAe;AACjB;;AAEA;EACE,aAAa;AACf;AACA;EACE,wBAAwB;EACxB,cAAc;AAChB;AACA;EACE,6BAA6B;EAC7B,YAAY;AACd","sourcesContent":[".jp-RenderedMarkdown aside.callout {\n  width: 95%;\n  margin: 1.5625em auto;\n  padding: 0 .6rem .8rem!important;\n  overflow: hidden;\n  page-break-inside: avoid;\n  border-left: .2rem solid #007bff;\n  border-radius: .1rem;\n  box-shadow: 0 0.2rem 0.5rem rgba(0,0,0,.05), 0 0 0.05rem rgba(0,0,0,.1);\n}\n\n.jp-RenderedMarkdown aside.callout > header {\n  position: relative;\n  margin: 0 -.6rem!important;\n  /* Changed the final from 2rem, without icon */\n  padding: .4rem .6rem .4rem 1rem;\n  font-weight: 700;\n  background-color: #e7f2fa;\n}\n\n.jp-RenderedMarkdown aside.callout.attention {border-left-color: #fd7e14;}\n.jp-RenderedMarkdown aside.callout.attention > header {background-color: #ffedcc;}\n.jp-RenderedMarkdown aside.callout.caution {border-left-color: #fd7e14;}\n.jp-RenderedMarkdown aside.callout.caution > header {background-color: #ffedcc;}\n.jp-RenderedMarkdown aside.callout.warning {border-left-color: #dc3545;}\n.jp-RenderedMarkdown aside.callout.warning > header {background-color: #fdf3f2;}\n.jp-RenderedMarkdown aside.callout.danger {border-left-color: #dc3545;}\n.jp-RenderedMarkdown aside.callout.danger > header {background-color: #fdf3f2;}\n.jp-RenderedMarkdown aside.callout.error {border-left-color: #dc3545;}\n.jp-RenderedMarkdown aside.callout.error > header {background-color: #fdf3f2;}\n.jp-RenderedMarkdown aside.callout.hint {border-left-color: #ffc107;}\n.jp-RenderedMarkdown aside.callout.hint > header {background-color: #fff6dd;}\n.jp-RenderedMarkdown aside.callout.important {border-left-color: #007bff;}\n.jp-RenderedMarkdown aside.callout.important > header {background-color: #e7f2fa;}\n.jp-RenderedMarkdown aside.callout.note {border-left-color: #007bff;}\n.jp-RenderedMarkdown aside.callout.note > header {background-color: #e7f2fa;}\n.jp-RenderedMarkdown aside.callout.tip {border-left-color: #ffc107;}\n.jp-RenderedMarkdown aside.callout.tip > header {background-color: #fff6dd;}\n\n.jp-RenderedMarkdown aside.callout > p {\n  padding: 0 1.4rem;\n}\n\n.jp-RenderedMarkdown abbr {\n  text-decoration: none;\n  border-bottom: 1px solid #ddd;\n  cursor: help;\n}\n\n.jp-RenderedMarkdown a {\n  color: #0071bc;\n  text-decoration: none;\n}\n\n.jp-RenderedMarkdown a:hover {\n  text-decoration: underline;\n}\n\n.jp-RenderedMarkdown code {\n  font-size: 95%;\n  font-family: monospace;\n  color: #e83e8c;\n  word-wrap: break-word;\n}\n\n.jp-RenderedMarkdown pre {\n  width: calc(100% - 20px - 3em);\n  padding: 10px;\n  background-color: #fafafa;\n  color: #222;\n  line-height: 1.2em;\n  border: 1px solid #c9c9c9;\n  margin: 1.5em 0;\n  box-shadow: 1px 1px 1px #d8d8d8;\n}\n.jp-RenderedMarkdown pre > code {\n  font-size: 87.5%;\n  font-family: monospace;\n  color: #222;\n}\n\n.jp-RenderedMarkdown hr {\n  width: 80%;\n  margin: 20px auto;\n}\n\n.jp-RenderedMarkdown blockquote {\n  color: rgb(90, 90, 90);\n  padding: 0.1rem 1rem;\n  border-left: .2rem solid #aaa;\n}\n\n.jp-RenderedMarkdown figure {\n  text-align: center;\n}\n\n.jp-RenderedMarkdown figure.numbered > figcaption {\n  color: rgb(90, 90, 90);\n  font-size: 90%;\n}\n\n.jp-RenderedMarkdown figure.numbered > figcaption:before {\n  content: 'Figure ' attr(number) ':';\n  font-weight: 700;\n  user-select: text;\n}\n\n.jp-RenderedMarkdown figure.numbered > figcaption > p:first-child {\n  display: inline;\n}\n\n.math .MathJax {\n  outline: none;\n}\n.jp-RenderedMarkdown div.math.numbered {\n  width: calc(100% - 50px);\n  margin: 0 auto;\n}\n.jp-RenderedMarkdown div.math.numbered:before {\n  content: '(' attr(number) ')';\n  float: right;\n}\n"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./style/base.css":
+/*!************************!*\
+  !*** ./style/base.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../node_modules/css-loader/dist/cjs.js!./base.css */ "./node_modules/css-loader/dist/cjs.js!./style/base.css");
+
+            
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"], options);
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"].locals || {});
+
+/***/ }),
+
+/***/ "./style/index.js":
+/*!************************!*\
+  !*** ./style/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _base_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base.css */ "./style/base.css");
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=style_index_js.5f43024f5bdd0bfd4808.js.map
