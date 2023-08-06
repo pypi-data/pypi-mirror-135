1036bbfcb800b18a114e17a5bcd617d9827d6281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:23 +0000

    Add package nonebot-plugin-emojimix/0.1.2/nonebot_plugin_emojimix-0.1.2.tar.gz

---
+++ packages/nonebot-plugin-emojimix/nonebot_plugin_emojimix-0.1.2.tar.gz/nonebot_plugin_emojimix-0.1.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nonebot_plugin_emojimix']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.19.0',
+ 'nonebot-adapter-onebot>=2.0.0-beta.1,<3.0.0',
+ 'nonebot2>=2.0.0-beta.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'nonebot-plugin-emojimix',
+    'version': '0.1.2',
+    'description': 'Nonebot2 plugin for emojimix',
+    'long_description': None,
+    'author': 'meetwq',
+    'author_email': 'meetwq@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.3,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
