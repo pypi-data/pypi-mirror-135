0b42320383dd88fb5322bbff5afda873936f0943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:44 +0000

    Add package joker.py/0.0.1/joker.py-0.0.1.tar.gz

---
+++ packages/joker.py/joker.py-0.0.1.tar.gz/joker.py-0.0.1/src/joker/joker.py
@@ -0,0 +1,29 @@
+""" joker package """
+import random
+from .jokes import joke_list, pun_list
+
+
+class Joker:
+    """Joker class
+        joke() -> returns a random joke
+        jokes(n) -> returns a list of n jokes
+        pun() -> returns a random joke
+        puns(n) -> returns a list of n jokes
+    """
+    def joke(self) -> str:
+        """ returns a random joke """
+        return random.choice(joke_list)
+
+    def jokes(self, num:int = 10) -> list:
+        """ returns a list of n jokes """
+        random.shuffle(joke_list)
+        return joke_list[0:num]
+
+    def pun(self) -> str:
+        """ returns a random pun """
+        return random.choice(pun_list)
+
+    def puns(self, num:int = 10) -> list:
+        """ returns a list of n puns """
+        random.shuffle(pun_list)
+        return pun_list[0:num]
