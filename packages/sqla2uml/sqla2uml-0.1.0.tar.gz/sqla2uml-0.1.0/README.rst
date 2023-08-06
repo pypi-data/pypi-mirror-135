7b8ef404e3c402cb40ceb904c4352ccaefe7b726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:11 +0000

    Add package sqla2uml/0.1.0/sqla2uml-0.1.0.tar.gz

---
+++ packages/sqla2uml/sqla2uml-0.1.0.tar.gz/sqla2uml-0.1.0/README.rst
@@ -0,0 +1,49 @@
+==============
+SqlAlchemy2UML
+==============
+
+
+.. image:: https://img.shields.io/pypi/v/sqla2uml.svg
+        :target: https://pypi.python.org/pypi/sqla2uml
+
+.. image:: https://img.shields.io/travis/sfermigier/sqla2uml.svg
+        :target: https://travis-ci.com/sfermigier/sqla2uml
+
+.. image:: https://readthedocs.org/projects/sqla2uml/badge/?version=latest
+        :target: https://sqla2uml.readthedocs.io/en/latest/?version=latest
+        :alt: Documentation Status
+
+
+
+Usage
+-----
+
+Install as a development dependency in your project, then type `sqla2uml` for help.
+
+
+* Free software: MIT license
+* Documentation: https://sqla2uml.readthedocs.io. (not working yet)
+
+
+Features
+--------
+
+* Generate UML diagrams from SQLAlchemy models.
+* One must have PlantUML installed.
+* More features / more flexibility to come later.
+
+
+Development
+-----------
+
+* Pull-requests accepted.
+* Participants must adhere to the Python COC (<https://www.python.org/psf/>)
+
+
+Credits
+-------
+
+This package was created with Cruft_ and the `abilian/cookiecutter-abilian-python`_ project template.
+
+.. _Cruft: https://github.com/cruft/cruft
+.. _`abilian/cookiecutter-abilian-python`: https://github.com/abilian/cookiecutter-abilian-python
---
+++ packages/sqla2uml/sqla2uml-0.1.0.tar.gz/sqla2uml-0.1.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['sqla2uml', 'tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['SQLAlchemy>=1.4,<2.0']
+
+entry_points = \
+{'console_scripts': ['sqla2uml = sqla2uml.main:main']}
+
+setup_kwargs = {
+    'name': 'sqla2uml',
+    'version': '0.1.0',
+    'description': 'Top-level package for SqlAlchemy2UML.',
+    'long_description': '==============\nSqlAlchemy2UML\n==============\n\n\n.. image:: https://img.shields.io/pypi/v/sqla2uml.svg\n        :target: https://pypi.python.org/pypi/sqla2uml\n\n.. image:: https://img.shields.io/travis/sfermigier/sqla2uml.svg\n        :target: https://travis-ci.com/sfermigier/sqla2uml\n\n.. image:: https://readthedocs.org/projects/sqla2uml/badge/?version=latest\n        :target: https://sqla2uml.readthedocs.io/en/latest/?version=latest\n        :alt: Documentation Status\n\n\n\nUsage\n-----\n\nInstall as a development dependency in your project, then type `sqla2uml` for help.\n\n\n* Free software: MIT license\n* Documentation: https://sqla2uml.readthedocs.io. (not working yet)\n\n\nFeatures\n--------\n\n* Generate UML diagrams from SQLAlchemy models.\n* One must have PlantUML installed.\n* More features / more flexibility to come later.\n\n\nDevelopment\n-----------\n\n* Pull-requests accepted.\n* Participants must adhere to the Python COC (<https://www.python.org/psf/>)\n\n\nCredits\n-------\n\nThis package was created with Cruft_ and the `abilian/cookiecutter-abilian-python`_ project template.\n\n.. _Cruft: https://github.com/cruft/cruft\n.. _`abilian/cookiecutter-abilian-python`: https://github.com/abilian/cookiecutter-abilian-python\n',
+    'author': 'Abilian SAS',
+    'author_email': 'sf@abilian.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/sfermigier/sqla2uml',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4',
+}
+
+
+setup(**setup_kwargs)
