40857686557387fdf5294813be86f799b4378bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:33 +0000

    Add package eaibat/0.0.11/eaibat-0.0.11.tar.gz

---
+++ packages/eaibat/eaibat-0.0.11.tar.gz/eaibat-0.0.11/setup.py
@@ -0,0 +1,46 @@
+# -*- Product under GNU GPL v3 -*-
+# -*- Author: E.Aivayan -*-
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open("readme.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name="eaibat",
+    version="0.0.11",
+    description="Basic classes to ease BDD evidence creation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://eaibat.readthedocs.io/",
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        # 'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.8',
+    ],
+    install_requires=[
+        'behave',
+        'requests',
+        'python-docx',
+        'unidecode'
+    ],
+    python_requires='>=3.7, !=2.*',
+    packages=find_packages(),
+    include_package_data=True,
+    # package_dir={'': 'eaiBat'},
+    author="Eric Aïvayan",
+    author_email="eric.aivayan@free.fr"
+)
