26750f8c9e194a355070851084e467358c69ef2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:37 +0000

    Add package FastLine/1.0/FastLine-1.0.tar.gz

---
+++ packages/FastLine/FastLine-1.0.tar.gz/FastLine-1.0/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup, Extension
+import os
+
+# change compiler to gcc
+os.environ['CC'] = 'gcc'
+
+src_dir = 'src'
+ext_modules = [Extension('FastLine',
+                         [src_dir + '/FastLine.cpp'],
+                         language='c++',
+                         ), ]
+
+setup(name='FastLine',
+      version='1.0',
+      ext_modules=ext_modules,
+      author='M.Ali Zarrinzade',
+      author_email="ali.zarrinzadeh@gmail.com",
+      description="FastLine",
+      long_description="FastLine is a module that provides line operations implemented in C++",
+      license="MIT",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Programming Language :: Python :: 3',
+      ],
+      url="https://github.com/MrGolden1/FastLine",
+      project_urls={
+          "Bug Tracker": "https://github.com/MrGolden1/FastLine/issues",
+      },
+      )
