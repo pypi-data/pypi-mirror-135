9be3024b098afd597333719036d266bd92c97321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:12 +0000

    Add package masterful/0.3.6/masterful-0.3.6.tar.gz

---
+++ packages/masterful/masterful-0.3.6.tar.gz/masterful-0.3.6/masterful/version.py
@@ -0,0 +1,7 @@
+# Current Masterful version.
+# 0.x.x means we are still in beta.
+# x.3.x means we are on our third autopolicy version
+#   (v1: epislon greedy, v2: RA, v3: beam search)
+# x.x.4 means we are on our fifth release of beam search
+#   (bravo, sierra, tango, uniform, victor)
+__version__ = "0.3.6"
