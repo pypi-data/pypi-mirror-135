ab322dd33df95719557a18f9c92a466defacdeb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:39 +0000

    Add package chatnoir-api/0.1.5/chatnoir-api-0.1.5.tar.gz

---
+++ packages/chatnoir-api/chatnoir-api-0.1.5.tar.gz/chatnoir-api-0.1.5/chatnoir_api/__init__.py
@@ -0,0 +1,28 @@
+__version__ = "0.1.5"
+
+from logging import getLogger
+from uuid import UUID
+
+from requests import get, Response
+
+from chatnoir_api.constants import BASE_URL
+from chatnoir_api.model import Index
+
+logger = getLogger("chatnoir-api")
+
+
+def html_contents(
+        uuid: UUID,
+        index: Index,
+        plain: bool = False,
+) -> str:
+    response: Response = get(
+        f"{BASE_URL}/cache",
+        params={
+            "uuid": str(uuid),
+            "index": index.value,
+            "raw": True,
+            "plain": plain,
+        }
+    )
+    return response.text
