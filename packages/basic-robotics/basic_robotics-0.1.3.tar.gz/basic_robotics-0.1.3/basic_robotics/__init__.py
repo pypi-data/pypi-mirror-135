b66259a11057d37e16328a83d652db1b84c242c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:21 +0000

    Add package basic-robotics/0.1.3/basic_robotics-0.1.3.tar.gz

---
+++ packages/basic-robotics/basic_robotics-0.1.3.tar.gz/basic_robotics-0.1.3/basic_robotics/__init__.py
@@ -0,0 +1,14 @@
+# __init__.py
+
+# Version
+__version__ = "0.1.2"
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+import modern_robotics_numba
+import faser_interfaces
+import faser_math
+import faser_plotting
+import faser_robot_kinematics
+import faser_utils
+import robot_collisions
