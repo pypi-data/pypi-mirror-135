c868cf4199fa38ac60da127d3bb24f79bf7fe646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:26 +0000

    Add package starlite/0.6.0/starlite-0.6.0-py3-none-any.whl

---
+++ packages/starlite/starlite-0.6.0-py3-none-any.whl/starlite/__init__.py
@@ -0,0 +1,106 @@
+# flake8: noqa
+from starlite.datastructures import File, Redirect, State, Stream
+
+from .app import Starlite
+from .config import CORSConfig, OpenAPIConfig, StaticFilesConfig
+from .controller import Controller
+from .dto import DTOFactory
+from .enums import (
+    HttpMethod,
+    MediaType,
+    OpenAPIMediaType,
+    RequestEncodingType,
+    ScopeType,
+)
+from .exceptions import (
+    HTTPException,
+    ImproperlyConfiguredException,
+    InternalServerException,
+    MissingDependencyException,
+    NotAuthorizedException,
+    NotFoundException,
+    PermissionDeniedException,
+    ServiceUnavailableException,
+    StarLiteException,
+)
+from .handlers import (
+    BaseRouteHandler,
+    HTTPRouteHandler,
+    WebsocketRouteHandler,
+    delete,
+    get,
+    patch,
+    post,
+    put,
+    route,
+    websocket,
+)
+from .logging import LoggingConfig
+from .middleware import AbstractAuthenticationMiddleware, AuthenticationResult
+from .openapi.controller import OpenAPIController
+from .params import Body, Parameter
+from .plugins import PluginProtocol
+from .provide import Provide
+from .request import Request, WebSocket
+from .response import Response
+from .routing import BaseRoute, HTTPRoute, Router, WebSocketRoute
+from .testing import TestClient, create_test_client, create_test_request
+from .types import MiddlewareProtocol, Partial, ResponseHeader
+
+__all__ = [
+    "AbstractAuthenticationMiddleware",
+    "AuthenticationResult",
+    "BaseRoute",
+    "BaseRouteHandler",
+    "Body",
+    "CORSConfig",
+    "Controller",
+    "DTOFactory",
+    "File",
+    "HTTPException",
+    "HTTPRoute",
+    "HTTPRouteHandler",
+    "HttpMethod",
+    "ImproperlyConfiguredException",
+    "InternalServerException",
+    "LoggingConfig",
+    "MediaType",
+    "MiddlewareProtocol",
+    "MissingDependencyException",
+    "NotAuthorizedException",
+    "NotFoundException",
+    "OpenAPIConfig",
+    "OpenAPIController",
+    "OpenAPIMediaType",
+    "Parameter",
+    "Partial",
+    "PermissionDeniedException",
+    "PluginProtocol",
+    "Provide",
+    "Redirect",
+    "Request",
+    "RequestEncodingType",
+    "Response",
+    "ResponseHeader",
+    "Router",
+    "ScopeType",
+    "ServiceUnavailableException",
+    "StarLiteException",
+    "Starlite",
+    "State",
+    "StaticFilesConfig",
+    "Stream",
+    "TestClient",
+    "WebSocket",
+    "WebSocketRoute",
+    "WebsocketRouteHandler",
+    "create_test_client",
+    "create_test_request",
+    "delete",
+    "get",
+    "patch",
+    "post",
+    "put",
+    "route",
+    "websocket",
+]
---
+++ packages/starlite/starlite-0.6.0-py3-none-any.whl/starlite/enums.py
@@ -0,0 +1,47 @@
+from enum import Enum
+from typing import Any, cast
+
+from starlite.exceptions import ImproperlyConfiguredException
+
+
+class HttpMethod(str, Enum):
+    GET = "get"
+    POST = "post"
+    PUT = "put"
+    PATCH = "patch"
+    DELETE = "delete"
+
+    @classmethod
+    def is_http_method(cls, value: Any) -> bool:
+        """Validates that a given value is a member of the HttpMethod enum"""
+        return isinstance(value, str) and value.lower() in list(cls)
+
+    @classmethod
+    def from_str(cls, value: Any) -> "HttpMethod":
+        """Given a string value, return an enum member or raise a ValueError"""
+
+        if cls.is_http_method(value):
+            return cast(HttpMethod, value.lower())
+        raise ImproperlyConfiguredException(f"value {value} is not a supported http method")
+
+
+class MediaType(str, Enum):
+    JSON = "application/json"
+    HTML = "text/html"
+    TEXT = "text/plain"
+
+
+class OpenAPIMediaType(str, Enum):
+    OPENAPI_YAML = "application/vnd.oai.openapi"
+    OPENAPI_JSON = "application/vnd.oai.openapi+json"
+
+
+class RequestEncodingType(str, Enum):
+    JSON = "application/json"
+    MULTI_PART = "multipart/form-data"
+    URL_ENCODED = "application/x-www-form-urlencoded"
+
+
+class ScopeType(str, Enum):
+    HTTP = "http"
+    WEBSOCKET = "websocket"
