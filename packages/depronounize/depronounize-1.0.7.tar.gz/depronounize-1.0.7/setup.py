01caa1fdb31100c5c220f987a1014aac80874a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:09 +0000

    Add package depronounize/1.0.7/depronounize-1.0.7.tar.gz

---
+++ packages/depronounize/depronounize-1.0.7.tar.gz/depronounize-1.0.7/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="depronounize",
+    version='1.0.7',
+    description="Pronoun replacement module",
+    url="https://github.com/NazarTrilisky/PronounReplacement",
+    install_requires=["spacy==2.3.2"],
+    include_package_data=True,
+    zip_safe=False,
+    author_email="",
+    license="MIT",
+    author="Nazar Trilisky",
+    packages=["depronounize"],
+    package_dir={
+        "": ".",
+        "depronounize": "./depronounize"
+    }
+)
+
