7af690ac3cd051e35a3966a74372256134db7a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:25 +0000

    Add package cosabot/0.0.4/cosabot-0.0.4.tar.gz

---
+++ packages/cosabot/cosabot-0.0.4.tar.gz/cosabot-0.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='cosabot',
+    version='0.0.4',
+    description='Computer Optic Semantics',
+    packages=['cosa'],
+    author = 'Alfredo Lozano',
+    author_email = 'lozanoa94@gmail.com',
+    install_requires=[
+   'scipy',
+   'scikit-image',
+   'scikit-learn',
+   'numpy'
+   ],
+   zip_safe=False,
+)
