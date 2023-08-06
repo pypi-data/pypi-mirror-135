edcaca3720b52de06bb0d6a79252f9ef8180d74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:16 +0000

    Add package python-blockchain-explorer/1.0.1/python-blockchain-explorer-1.0.1.tar.gz

---
+++ packages/python-blockchain-explorer/python-blockchain-explorer-1.0.1.tar.gz/python-blockchain-explorer-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="python-blockchain-explorer",
+    version="1.0.1",
+    description="A minimal, yet complete, python API for etherscan.io. arbiscan.io ...",
+    url="https://github.com/orthogonalglobal/python-blockchain-explorer",
+    author="Dion and Robert",
+    license="MIT",
+    packages=[
+        "explorer",
+        "explorer.configs",
+        "explorer.enums",
+        "explorer.modules",
+        "explorer.utils",
+    ],
+    install_requires=["requests"],
+    include_package_data=True,
+    zip_safe=False,
+)
