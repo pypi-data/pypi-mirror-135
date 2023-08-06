6a626f261b0dab762af8094ae783e8af95d97489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:05 +0000

    Add package amshan/1.1.0/amshan-1.1.0-py3-none-any.whl

---
+++ packages/amshan/amshan-1.1.0-py3-none-any.whl/amshan/serial_connection_factory.py
@@ -0,0 +1,65 @@
+"""Serial connection factory."""
+from __future__ import annotations
+from asyncio import AbstractEventLoop, get_event_loop
+from asyncio import Queue  # pylint: disable=unused-import
+from typing import cast
+
+import serial_asyncio  # type: ignore
+
+from amshan.hdlc import HdlcFrame  # pylint: disable=unused-import
+from amshan.meter_connection import (
+    MeterTransportProtocol,
+    SmartMeterFrameContentProtocol,
+    SmartMeterFrameProtocol,
+)
+
+
+async def create_serial_frame_connection(
+    queue: "Queue[HdlcFrame]",
+    loop: AbstractEventLoop | None,
+    *args,
+    **kwargs,
+) -> MeterTransportProtocol:
+    """
+    Create serial connection using SmartMeterFrameProtocol.
+
+    :param queue: Queue for received frames
+    :param loop: The event handler
+    :param args: Passed to serial_asyncio.create_serial_connection and further to serial.Serial init function
+    :param kwargs: Passed to serial_asyncio.create_serial_connection and further the serial.Serial init function
+    :return: Tuple of transport and protocol
+    """
+    return cast(
+        MeterTransportProtocol,
+        await serial_asyncio.create_serial_connection(
+            loop if loop else get_event_loop(),
+            lambda: SmartMeterFrameProtocol(queue),
+            *args,
+            **kwargs,
+        ),
+    )
+
+
+async def create_serial_frame_content_connection(
+    queue: "Queue[bytes]",
+    loop: AbstractEventLoop | None,
+    *args,
+    **kwargs,
+) -> MeterTransportProtocol:
+    """Create serial connection using SmartMeterFrameContentProtocol.
+
+    :param queue: Queue for received frames
+    :param loop: The event handler
+    :param args: Passed to serial_asyncio.create_serial_connection and further to serial.Serial init function
+    :param kwargs: Passed to serial_asyncio.create_serial_connection and further the serial.Serial init function
+    :return: Tuple of transport and protocol
+    """
+    return cast(
+        MeterTransportProtocol,
+        await serial_asyncio.create_serial_connection(
+            loop if loop else get_event_loop(),
+            lambda: SmartMeterFrameContentProtocol(queue),
+            *args,
+            **kwargs,
+        ),
+    )
