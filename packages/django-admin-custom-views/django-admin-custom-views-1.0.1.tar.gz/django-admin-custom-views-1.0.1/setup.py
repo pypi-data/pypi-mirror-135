96047d1779913ae4bb92308496617e6ef9263ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:32 +0000

    Add package django-admin-custom-views/1.0.1/django-admin-custom-views-1.0.1.tar.gz

---
+++ packages/django-admin-custom-views/django-admin-custom-views-1.0.1.tar.gz/django-admin-custom-views-1.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+from custom_admin_views import __version__
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup(
+    author='Ludovic Delsol',
+    author_email='ludel47@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities'
+    ],
+    description='Create custom view for Django admin.',
+    include_package_data=True,
+    install_requires=['django'],
+    license='APL',
+    long_description=README,
+    name='django-admin-custom-views',
+    packages=find_packages(exclude=['sample_project']),
+    url='https://github.com/ludel/django-admin-views',
+    version=__version__,
+)
