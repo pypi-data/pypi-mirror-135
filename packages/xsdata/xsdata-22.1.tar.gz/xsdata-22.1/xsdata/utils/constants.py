eeddcffb16932499a4179d41bf8b23bb46306138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:07 +0000

    Add package xsdata/22.1/xsdata-22.1.tar.gz

---
+++ packages/xsdata/xsdata-22.1.tar.gz/xsdata-22.1/xsdata/utils/constants.py
@@ -0,0 +1,22 @@
+import sys
+from typing import Any
+from typing import Dict
+from typing import Sequence
+from typing import Tuple
+
+EMPTY_MAP: Dict = {}
+EMPTY_SEQUENCE: Sequence = []
+EMPTY_TUPLE: Tuple = ()
+
+XML_FALSE = sys.intern("false")
+XML_TRUE = sys.intern("true")
+
+
+def return_true(*_: Any) -> bool:
+    """A dummy function that always returns true."""
+    return True
+
+
+def return_input(obj: Any) -> Any:
+    """A dummy function that always returns input."""
+    return obj
