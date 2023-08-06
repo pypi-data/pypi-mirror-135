432a2fd316e18fab35f63a4120d69920ce0e5700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:43 +0000

    Add package ApiPasswordGenerator/1/ApiPasswordGenerator-1.tar.gz

---
+++ packages/ApiPasswordGenerator/ApiPasswordGenerator-1.tar.gz/ApiPasswordGenerator-1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='ApiPasswordGenerator',
+	version='1',
+	author='Chelovek228',
+	author_email='artemsharap@yandex.ru',
+	description='api',
+	packages=['ApiPasswordGenerator'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
