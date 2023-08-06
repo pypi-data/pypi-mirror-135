327802892bfac14331383800fab133322d7f6a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:55 +0000

    Add package pylinearalg/0.2.0/pylinearalg-0.2.0-py3-none-any.whl

---
+++ packages/pylinearalg/pylinearalg-0.2.0-py3-none-any.whl/pylinearalg/__init__.py
@@ -0,0 +1,8 @@
+from .gram_schmidt import gram_schmidt
+from .vector import BasisVector
+from .vector import Constant
+from .vector import Vector
+from .vector import VectorSpace
+
+__version__ = "0.2.0"
+__all__ = ['gram_schmidt', 'BasisVector', 'Constant', 'Vector', 'VectorSpace']
