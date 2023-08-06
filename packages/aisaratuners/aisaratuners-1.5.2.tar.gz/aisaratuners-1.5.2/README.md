34a6a5fec4cc7468ae8a944e131ea19f0061d4e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:05 +0000

    Add package aisaratuners/1.5.2/aisaratuners-1.5.2.tar.gz

---
+++ packages/aisaratuners/aisaratuners-1.5.2.tar.gz/aisaratuners-1.5.2/README.md
@@ -0,0 +1,3 @@
+- **aisaratuners is an open-source optimization library to automate and expedite hyperparameter search. aisaratuners has two modules, aisara_general_tuner.py which is a framework agnostic, and aisara_keras_tuner.py which is tailored for Karas API.**
+
+- **aisaratuners leverages AiSara algorithm, Latin hypercube sampling, and the concept of search space reduction for fast reach of the optimum hyperparameters.**
---
+++ packages/aisaratuners/aisaratuners-1.5.2.tar.gz/aisaratuners-1.5.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='aisaratuners',
+    version='1.5.2',
+    description='leveraging aisara algorithm for effective hyperparameter tuning',
+    license='LICENSE.txt',
+    long_description=open('README.md').read() + '\n\n' +
+    open('CHANGELOG.txt').read(),
+    project_urls={
+        'Source Code': 'https://github.com/aisara-hub/aisaratuners',
+        'Documentation': 'https://github.com/aisara-hub/aisaratuners/blob/master/docs/user%20guide.md',
+        'Tutorials': 'https://github.com/aisara-hub/aisaratuners/blob/master/docs/Tutorials.md'
+    },
+    author='AiSara Artificial Intelligence',
+    author_email='devs@aisara.ai',
+    classifiers=classifiers,
+    python_requires='>=3.6',
+    packages=find_packages(exclude=['tests', 'data', 'docs']),
+    install_requires=['requests', 'pandas', 'numpy',
+                      'plotly >= 4.6.0', 'tensorflow >= 1.15']
+)
