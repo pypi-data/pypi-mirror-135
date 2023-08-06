622f91af09e8446057b739822b7725387d983125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:15 +0000

    Add package discord-custom-help/0.0.7/discord-custom-help-0.0.7.tar.gz

---
+++ packages/discord-custom-help/discord-custom-help-0.0.7.tar.gz/discord-custom-help-0.0.7/README.md
@@ -0,0 +1,25 @@
+## discord-custom-help
+Making help commands easier
+
+## Installation
+`pip install discord-custom-help`
+
+## Usage
+```py
+bot.load_extension('dch')
+```
+
+## Customization
+To change the color of embeds
+```py
+import os
+os.environ['DCH_COLOR'] = '34ffcd' # Any hex color code
+```
+
+Using disnake?
+```py
+import os
+os.environ["DISCORD_LIB"] = "disnake"
+```
+
+[Join Discord](https://discord.gg/7SaE8v2) For any kind of help
---
+++ packages/discord-custom-help/discord-custom-help-0.0.7.tar.gz/discord-custom-help-0.0.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+# See note below for more information about classifiers
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Education",
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: MacOS",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+]
+
+setup(
+    name="discord-custom-help",
+    version="0.0.7",
+    description="A help cog for discord bots",
+    long_description=open("README.md", encoding="utf-8", mode="r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/shahprog/discord-custom-help/",
+    author="Md Shahriyar Alam",
+    author_email="mdshahriyaralam552@gmail.com",
+    license="MIT",
+    classifiers=classifiers,
+    keywords="discord-custom-help dch discord-help",
+    packages=find_packages(where='.', include=["dch"]),
+    install_requires=[],
+    include_package_data=True,
+)
