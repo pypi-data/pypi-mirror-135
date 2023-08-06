ad0ccf201a4f0a003895f4aa6142beb67f482c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:11 +0000

    Add package turms/0.1.3/turms-0.1.3.tar.gz

---
+++ packages/turms/turms-0.1.3.tar.gz/turms-0.1.3/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['turms',
+ 'turms.cli',
+ 'turms.parser',
+ 'turms.plugins',
+ 'turms.processor',
+ 'turms.wards']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['graphql-core>=3.2.0,<4.0.0', 'pydantic>=1.9.0,<2.0.0', 'rich>=11.0.0,<12.0.0']
+
+entry_points = \
+{'console_scripts': ['turms = turms.cli.main:entrypoint']}
+
+setup_kwargs = {
+    'name': 'turms',
+    'version': '0.1.3',
+    'description': 'graphql-codegen powered by pydantic',
+    'long_description': '# turms\n\n[![codecov](https://codecov.io/gh/jhnnsrs/turms/branch/master/graph/badge.svg?token=UGXEA2THBV)](https://codecov.io/gh/jhnnsrs/turms)\n\n### DEVELOPMENT\n\n# Insipiration\n\nTurms is a pure python implementation of the awesome graphql-codegen library, following a simliar extensible design.\nIt makes heavy use of pydantic and its serialization capablities and provides fully typed querys, mutations and subscriptions\n\n## Supports\n\n- Documents\n- Fragments\n- Enums\n- Query Functions\n\n## Features\n\n- Fully Modular (agnostic of graphql engine)\n- Specify type mixins, baseclasses...\n- Fully Support type hints for variables (Pylance)\n\n## Installation\n\n```bash\npip install turms\n```\n\n## Usage\n\nOpen your workspace (create a virtual env), in the root folder\n\n```bash\nturms init\n```\n\nThis creates a configuration file in the working directory, edit this to reflect your\nsettings (see Configuration)\n\n```bash\nturms gen\n```\n\nGenerate beautifully typed Operations, Enums,...\n\n## Why Turms\n\nIn Etruscan religion, Turms (usually written as 𐌕𐌖𐌓𐌌𐌑 Turmś in the Etruscan alphabet) was the equivalent of Roman Mercury and Greek Hermes, both gods of trade and the **messenger** god between people and gods.\n',
+    'author': 'jhnnsrs',
+    'author_email': 'jhnnsrs@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
