6bab0d34649a33e7049843ede00f31aea908ff85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:06 +0000

    Add package algotradingPythonSalem/0.0.1/algotradingPythonSalem-0.0.1.tar.gz

---
+++ packages/algotradingPythonSalem/algotradingPythonSalem-0.0.1.tar.gz/algotradingPythonSalem-0.0.1/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="algotradingPythonSalem",
+    version="0.0.1",
+    author="AlgoTrading",
+    author_email="AlgoTrading@gmail.com",
+    description="Find and tag Git commits based on version numbers in commit messages.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://initialcommit.com/projects/algotradingPythonSalem.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'gitpython'
+    ],
+    keywords='git tag git-tagup tagup tag-up version autotag auto-tag commit message',
+    project_urls={
+        'Homepage': 'https://initialcommit.com/projects/algotradingPythonSalem.com',
+    },
+    entry_points={
+        'console_scripts': [
+            'git-tagup=src.__main__:main',
+            'gtu=src.__main__:main',
+        ],
+    },
+)
