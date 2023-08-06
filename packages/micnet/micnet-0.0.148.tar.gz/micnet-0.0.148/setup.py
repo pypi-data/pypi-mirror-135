4993cd653b63b97bb6a017685bb1f1a5c53ee73d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:41 +0000

    Add package micnet/0.0.148/micnet-0.0.148.tar.gz

---
+++ packages/micnet/micnet-0.0.148.tar.gz/micnet-0.0.148/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+# Setting up
+setuptools.setup(
+        name="micnet", 
+        version="0.0.148",
+        author="Natalia Favila, David Madrigal-Trejo, Daniel Legorreta",
+        author_email="natalia.favila.v@gmail.com",
+        description="Toolbox for the visualization and analysis of microbial datasets",
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+	install_requires=['pandas', 
+			  'numpy == 1.20.3',
+		          'umap-learn == 0.5.1',
+                          'typing',
+                          'webcolors',
+                          'matplotlib',
+                          'bokeh == 2.2.2',
+	],
+        classifiers= [
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+        ],
+	include_package_data=True,
+	packages=setuptools.find_packages(),
+	package_data={'': ['data/*.txt']},
+)
