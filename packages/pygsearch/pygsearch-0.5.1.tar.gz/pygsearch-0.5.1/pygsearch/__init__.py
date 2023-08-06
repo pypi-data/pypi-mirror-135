7dc0c1c1d7f80162c8b24c6d98c8b483a33751ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:27 +0000

    Add package pygsearch/0.5.1/pygsearch-0.5.1.tar.gz

---
+++ packages/pygsearch/pygsearch-0.5.1.tar.gz/pygsearch-0.5.1/pygsearch/__init__.py
@@ -0,0 +1,23 @@
+"""
+pygsearch
+~~~~~~~~~~
+Python library to get google search results.
+:copyright: (c) 2022-present Vitaman02
+:license: MIT, see LICENSE for more details.
+"""
+
+from collections import namedtuple
+
+from .gsearch import *
+
+
+__title__ = "pygsearch"
+__author__ = "Vitaman02"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022-present Vitaman02"
+__version__ = "0.5.1"
+
+
+VersionInfo = namedtuple("VersionInfo", "major minor patch")
+
+version_info = VersionInfo(*map(int, __version__.split(".")))
