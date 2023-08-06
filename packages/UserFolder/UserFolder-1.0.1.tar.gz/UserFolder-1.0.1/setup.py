5ffb7ad7645bafdc1b6a30f1d90228403d143f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:18 +0000

    Add package UserFolder/1.0.1/UserFolder-1.0.1.tar.gz

---
+++ packages/UserFolder/UserFolder-1.0.1.tar.gz/UserFolder-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+    name='UserFolder',
+    packages=['UserFolder'],
+    version='1.0.1',
+    license='MIT',
+    description='This library allows you to write and save files to the users folder. Useful for when you convert this script to a onefile exe program.',
+    author='Legopitstop',
+    author_email='officiallegopitstop@gmail.com',
+    url='https://legopitstop.weebly.com/media.html',
+    download_url='https://github.com/legopitstop/UserFolder/archive/v1.0.1.tar.gz',
+    keywords=['filemanagement', 'user folder', 'easy'],
+    classifiers=[
+        'Development Status :: 4 - Beta', # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
