19e0967efe95bd2a92a497a3a2eeb4e6d54f6cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:56 +0000

    Add package turms/0.1.10/turms-0.1.10.tar.gz

---
+++ packages/turms/turms-0.1.10.tar.gz/turms-0.1.10/turms/config.py
@@ -0,0 +1,23 @@
+from pydantic import AnyHttpUrl, BaseModel
+from typing import List, Optional
+
+
+class GeneratorConfig(BaseModel):
+    domain: str = "default"
+    schema_url: Optional[AnyHttpUrl]
+    schema_file: Optional[str]
+    out_dir: str = "api"
+    generated_name: str = "schema.py"
+
+    object_bases: List[str] = ["turms.types.object.GraphQLObject"]
+    interface_bases: List[str] = ["turms.types.object.GraphQLObject"]
+
+    scalar_definitions = {
+        "ID": "str",
+        "String": "str",
+        "Int": "int",
+        "Boolean": "bool",
+        "GenericScalar": "Dict",
+    }
+
+    additional_bases = {}
