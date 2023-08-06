550602ee4fd05879b6871841e1f88892c6ea5555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:02 +0000

    Add package azet/0/azet-0.tar.gz

---
+++ packages/azet/azet-0.tar.gz/azet-0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='azet',
+    version='0',
+    description='A local zettelkasten tool',
+    packages=['azet'],
+    install_requires=[],
+)
