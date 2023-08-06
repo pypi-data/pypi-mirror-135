237759a5e985df7845c81b1143c7854127722516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:15 +0000

    Add package nestshredder/0.4/nestshredder-0.4.tar.gz

---
+++ packages/nestshredder/nestshredder-0.4.tar.gz/nestshredder-0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("readme.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='nestshredder',
+   version='0.4',
+   license='MIT',
+   description='A useful thing that will take nested JSONS and output something a touch more SQL-sensible',
+   long_description_content_type='text/markdown',
+   long_description=long_description,
+   author='Chris Woodward',
+   author_email='chriswoodward77@googlemail.com',
+   packages=['nestshredder'],  
+   install_requires=['pandas','pyarrow'],
+   download_url='https://github.com/Gadz00ks77/nestshredder/archive/refs/tags/v0.4.tar.gz'
+)
