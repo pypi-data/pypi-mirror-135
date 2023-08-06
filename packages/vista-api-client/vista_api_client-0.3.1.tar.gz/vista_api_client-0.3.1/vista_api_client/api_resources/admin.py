3946f845476b9676edd33ec3cd11c6362941518a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:27 +0000

    Add package vista-api-client/0.3.1/vista_api_client-0.3.1.tar.gz

---
+++ packages/vista-api-client/vista_api_client-0.3.1.tar.gz/vista_api_client-0.3.1/vista_api_client/api_resources/admin.py
@@ -0,0 +1,12 @@
+
+from vista_api_client.api_resources.api_resource import ApiResource, HttpMethods
+
+
+class Admin(ApiResource):
+    def create_branch(self, branch):
+        return self.dispatch('/v1/companies/branches', HttpMethods.POST, {
+            'branch': branch,
+        })
+
+    def create_read_tokens(self):
+        return self.dispatch('/v1/auth/readTokens', HttpMethods.GET)
