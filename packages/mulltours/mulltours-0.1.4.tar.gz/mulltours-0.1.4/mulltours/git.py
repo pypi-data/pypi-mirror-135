6b5bdbb27e5fada307c65a7ecff8cdf563e98272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:13 +0000

    Add package mulltours/0.1.4/mulltours-0.1.4.tar.gz

---
+++ packages/mulltours/mulltours-0.1.4.tar.gz/mulltours-0.1.4/mulltours/git.py
@@ -0,0 +1,20 @@
+import os
+import pathlib
+
+import git
+
+
+def add_all(path, repo):
+    lst = []
+    for p in path.rglob("*"):
+        print(p.name)
+        if not str(p).lower().startswith(".git/"):
+            lst.append(str(p.resolve()))
+
+    repo.index.add(lst)
+    repo.index.commit("initial commit")
+
+
+def init(path):
+    r = git.Repo.init(path)
+    return r
