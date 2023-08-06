15f1a55cc110c51122ef1b709e38ae167efc9009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:28 +0000

    Add package tankobon/2022.1.20/tankobon-2022.1.20-py3-none-any.whl

---
+++ packages/tankobon/tankobon-2022.1.20-py3-none-any.whl/tankobon/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf8
+"""Yet another manga downloader.
+
+.. include:: ./documentation.md
+"""
+
+from .__version__ import __version__
+
+from .core import Cache, Downloader
+from .sources.base import Parser
+
+__pdoc__ = {"ui": False}
