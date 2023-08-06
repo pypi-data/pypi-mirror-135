341f1010a4379db2f169b51a72fe1054d07ec124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:48:10 +0000

    Add package RockyRoad/0.0.165/RockyRoad-0.0.165-py3-none-any.whl

---
+++ packages/RockyRoad/RockyRoad-0.0.165-py3-none-any.whl/rockyroad/modules/warranty_pip.py
@@ -0,0 +1,30 @@
+from .module_imports import *
+
+
+@headers({"Ocp-Apim-Subscription-Key": key})
+class Product_Improvements(Consumer):
+    """Inteface to Warranties Product Improvement resource for the RockyRoad API."""
+
+    def __init__(self, Resource, *args, **kw):
+        self._base_url = Resource._base_url
+        super().__init__(base_url=Resource._base_url, *args, **kw)
+
+    @returns.json
+    @http_get("warranties/product-improvements")
+    def list(self, site: Query(type=str) = None):
+        """This call will return list of product improvements."""
+
+    @delete("warranties/product-improvements/{uid}")
+    def delete(self, uid: str):
+        """This call will delete the product improvement for the specified uid."""
+
+    @returns.json
+    @json
+    @post("warranties/product-improvements")
+    def insert(self, product_improvement: Body):
+        """This call will create the product improvement with the specified parameters."""
+
+    @json
+    @patch("warranties/product-improvements/{uid}")
+    def update(self, uid: str, product_improvement: Body):
+        """This call will update the product improvement with the specified parameters."""
