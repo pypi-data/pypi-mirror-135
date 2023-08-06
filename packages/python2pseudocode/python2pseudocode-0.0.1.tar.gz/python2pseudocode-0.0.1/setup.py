c8ea20f258ecc0998d93150c0cda1793e4a14807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:17 +0000

    Add package python2pseudocode/0.0.1/python2pseudocode-0.0.1.tar.gz

---
+++ packages/python2pseudocode/python2pseudocode-0.0.1.tar.gz/python2pseudocode-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Converting Python code snippets to Pseudocode effectively.'
+LONG_DESCRIPTION = 'Converting Python code snippets to Pseudocode effectively.'
+
+# Setting up
+setup(
+    name="python2pseudocode",
+    version=VERSION,
+    author="anonymouscoolguy",
+    author_email="<anonymouscoolguy05@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['pseudocode', 'python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
