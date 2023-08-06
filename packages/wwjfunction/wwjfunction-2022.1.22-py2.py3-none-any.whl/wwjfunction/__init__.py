fbe88b507e7d70df0dbd39e77663d1dc699da67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:39 +0000

    Add package wwjfunction/2022.1.22/wwjfunction-2022.1.22-py2.py3-none-any.whl

---
+++ packages/wwjfunction/wwjfunction-2022.1.22-py2.py3-none-any.whl/wwjfunction/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from .wwjfunction import *
+ 
+__version__ = '1.0.0'
+__license__ = ''
+
+#其中第一句 from __future__ import absolute_import 必不可少，from .模块名 import * 是导入你所写的模块(所有写的模块都要导入)，否则这个模块就不能被识别。
