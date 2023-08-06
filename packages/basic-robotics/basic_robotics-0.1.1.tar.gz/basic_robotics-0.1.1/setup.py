c1d7ec444372f69ff705b4a4dc3d6bd3a144cc3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:13 +0000

    Add package basic-robotics/0.1.1/basic_robotics-0.1.1.tar.gz

---
+++ packages/basic-robotics/basic_robotics-0.1.1.tar.gz/basic_robotics-0.1.1/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(name='basic_robotics',
+      version='0.1.1',
+      description='Basic Robotics Toolbox Developed in FASER Lab',
+      url='http://github.com/storborg/funniest',
+      author='William Chapin',
+      author_email='liam@64b1t.com',
+      license='MIT',
+      packages=['basic_robotics'],
+      install_requires=[
+          'numpy',
+          'scipy',
+          'numba',
+          'modern_robotics',
+          'numpy-stl',
+          'descartes',
+          'alphashape',
+          'trimesh',
+      ],
+      zip_safe=False)
