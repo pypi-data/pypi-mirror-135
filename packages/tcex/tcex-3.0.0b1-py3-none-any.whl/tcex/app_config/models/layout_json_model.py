371b7fc51ab28f41de7d0e0f656ed153aac50beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:42 +0000

    Add package tcex/3.0.0b1/tcex-3.0.0b1-py3-none-any.whl

---
+++ packages/tcex/tcex-3.0.0b1-py3-none-any.whl/tcex/app_config/models/layout_json_model.py
@@ -0,0 +1,98 @@
+"""Layout JSON Model"""
+# standard library
+from typing import Dict, List, Optional, Union
+
+# third-party
+from pydantic import BaseModel
+from pydantic.types import constr
+
+# first-party
+from tcex.pleb.none_model import NoneModel
+
+__all__ = ['LayoutJsonModel']
+
+
+def snake_to_camel(snake_string: str) -> str:
+    """Convert snake_case to camelCase"""
+    components = snake_string.split('_')
+    return components[0] + ''.join(x.title() for x in components[1:])
+
+
+class ParametersModel(BaseModel):
+    """Model for layout_json.inputs.{}"""
+
+    display: Optional[str]
+    name: str
+
+    class Config:
+        """DataModel Config"""
+
+        alias_generator = snake_to_camel
+        validate_assignment = True
+
+
+class InputsModel(BaseModel):
+    """Model for layout_json.inputs"""
+
+    parameters: List[ParametersModel]
+    sequence: int
+    title: constr(min_length=3, max_length=100)
+
+    class Config:
+        """DataModel Config"""
+
+        alias_generator = snake_to_camel
+        validate_assignment = True
+
+
+class OutputsModel(BaseModel):
+    """Model for layout_json.outputs"""
+
+    display: Optional[str]
+    name: str
+
+    class Config:
+        """DataModel Config"""
+
+        alias_generator = snake_to_camel
+        validate_assignment = True
+
+
+class LayoutJsonModel(BaseModel):
+    """Layout JSON Model"""
+
+    inputs: List[InputsModel]
+    outputs: Optional[List[OutputsModel]]
+
+    class Config:
+        """DataModel Config"""
+
+        alias_generator = snake_to_camel
+        validate_assignment = True
+
+    def get_param(self, name: str) -> Union[NoneModel, ParametersModel]:
+        """Return the param or a None Model."""
+        return self.params.get(name) or NoneModel()
+
+    def get_output(self, name) -> Union[NoneModel, OutputsModel]:
+        """Return layout.json outputs in a flattened dict with name param as key."""
+        return self.outputs_.get(name) or NoneModel
+
+    @property
+    def outputs_(self) -> Dict[str, OutputsModel]:
+        """Return layout.json outputs in a flattened dict with name param as key."""
+        return {o.name: o for o in self.outputs}
+
+    @property
+    def param_names(self) -> list:
+        """Return all param names in a single list."""
+        return list(self.params.keys())
+
+    @property
+    def params(self) -> Dict[str, ParametersModel]:
+        """Return layout.json params in a flattened dict with name param as key."""
+        parameters = {}
+        for i in self.inputs:
+            for p in i.parameters:
+                parameters.setdefault(p.name, p)
+        return parameters
---
+++ packages/tcex/tcex-3.0.0b1-py3-none-any.whl/tcex/bin/__init__.py
@@ -0,0 +1,8 @@
+"""Bin module for TcEx Framework"""
+# flake8: noqa
+# first-party
+from tcex.bin.dep import Dep
+from tcex.bin.deploy import Deploy
+from tcex.bin.package import Package
+from tcex.bin.template import Template
+from tcex.bin.validate import Validate
