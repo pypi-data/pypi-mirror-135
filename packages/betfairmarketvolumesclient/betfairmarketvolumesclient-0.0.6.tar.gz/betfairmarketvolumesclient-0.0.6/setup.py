6a7f2dc86979204f9f8deb89ecd4e0bf2ae9f718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:11 +0000

    Add package betfairmarketvolumesclient/0.0.6/betfairmarketvolumesclient-0.0.6.tar.gz

---
+++ packages/betfairmarketvolumesclient/betfairmarketvolumesclient-0.0.6.tar.gz/betfairmarketvolumesclient-0.0.6/setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open(os.path.join(os.path.dirname(__file__), "requirements.txt")) as f:
+    INSTALL_REQUIRES = f.read().splitlines()
+
+setup(
+    name="betfairmarketvolumesclient",
+    version="0.0.6",
+    author="Oliver Varney",
+    author_email="oliverashleyvarney@gmail.com",
+    description="Simple file downloader for the betfair daily sp files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="core packages",
+    url="https://github.com/varneyo/betfairmarketvolumesclient",
+    package_dir={"betfairmarketvolumesclient": "betfairmarketvolumesclient"},
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(exclude=["examples"]),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+    test_suite="tests",
+)
