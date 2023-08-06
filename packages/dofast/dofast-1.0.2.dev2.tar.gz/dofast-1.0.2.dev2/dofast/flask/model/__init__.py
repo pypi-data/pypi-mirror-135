99944d46ef0c3f84c25ea9d2d2c88c3dfca79de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:35 +0000

    Add package dofast/1.0.2.dev2/dofast-1.0.2.dev2.tar.gz

---
+++ packages/dofast/dofast-1.0.2.dev2.tar.gz/dofast-1.0.2.dev2/dofast/flask/model/__init__.py
@@ -0,0 +1,17 @@
+from dofast.flask.core import produce_cn_app
+import subprocess
+app = produce_cn_app('linux_work')
+
+
+@app.task
+def unlock_device() -> str:
+    cmd = "/bin/loginctl unlock-session"
+    subprocess.Popen(cmd, shell=True)
+    return 'DONE'
+
+
+@app.task
+def lock_device() -> str:
+    cmd = "/bin/loginctl lock-session"
+    subprocess.Popen(cmd, shell=True)
+    return 'DONE'
