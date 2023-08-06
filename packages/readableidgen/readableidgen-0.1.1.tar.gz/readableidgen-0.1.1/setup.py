2df10b9aa35c3b8fc0d96af9b0f70a255b0a1aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:16 +0000

    Add package readableidgen/0.1.1/readableidgen-0.1.1.tar.gz

---
+++ packages/readableidgen/readableidgen-0.1.1.tar.gz/readableidgen-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.1'
+DESCRIPTION = 'Human-readable phrase id generator'
+LONG_DESCRIPTION = 'A human-readable id generator that randomly generates nouns or adjective-noun pairs'
+
+setup(
+    name='readableidgen',
+    version=VERSION,
+    license='MIT',
+    author='Stefan/Yuzhao Heng',
+    author_email='stefan.hg@outlook.com',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url='https://github.com/StefanHeng/Readable-Id-Generator',
+    download_url='https://github.com/StefanHeng/Readable-Id-Generator/archive/refs/tags/v0.1.tar.gz',
+    packages=find_packages(),
+    install_requires=['colorama', 'numpy'],
+    keywords=['python', 'id', 'generator', 'nlp'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: MacOS X',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ]
+)
