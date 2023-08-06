17362e76e4764f6c4292ceb11b2bff707cb21f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:06 +0000

    Add package EpikCord.py/0.4.3/EpikCord.py-0.4.3.tar.gz

---
+++ packages/EpikCord.py/EpikCord.py-0.4.3.tar.gz/EpikCord.py-0.4.3/EpikCord/__init__.py
@@ -0,0 +1,16 @@
+# from .abc import * This annoyed me so much istg if this works without this...
+from .application import *
+from .client import *
+from .components import *
+from .exceptions import *
+from .guild import *
+from .member import *
+from .message import *
+from .partials import *
+from .role import *
+from .route import *
+from .team import *
+from .user import *
+from .ws import *
+
+from .managers import *
