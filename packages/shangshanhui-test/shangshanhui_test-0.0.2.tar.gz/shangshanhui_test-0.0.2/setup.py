fc82e7af311c730ea91efe33a34db667239412b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:32 +0000

    Add package shangshanhui-test/0.0.2/shangshanhui_test-0.0.2.tar.gz

---
+++ packages/shangshanhui-test/shangshanhui_test-0.0.2.tar.gz/shangshanhui_test-0.0.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='shangshanhui_test',
+    version='0.0.2',
+    author='huiliyi',
+    author_email='1900010748@pku.edu.cn',
+    url='',
+    description=u'吃枣药丸',
+    packages=['shangshanhui_test'],
+    install_requires=["hdmap_base"],
+)
---
+++ packages/shangshanhui-test/shangshanhui_test-0.0.2.tar.gz/shangshanhui_test-0.0.2/shangshanhui_test/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+import hdmap_base
+def jujube():
+    print('吃枣')
+
+
+def pill():
+    print('药丸')
