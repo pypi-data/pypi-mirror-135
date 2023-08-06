53ff89660648df12b4818cfdcfedd8b8adc88685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:25 +0000

    Add package nicks-client/0.0.2/nicks_client-0.0.2.tar.gz

---
+++ packages/nicks-client/nicks_client-0.0.2.tar.gz/nicks_client-0.0.2/client/common/utils.py
@@ -0,0 +1,44 @@
+"""Утилиты"""
+
+import json
+import os
+import sys
+sys.path.append('../')
+from common.variables import MAX_PACKAGE_LENGTH, ENCODING
+from common.errors import IncorrectDataReceivedError, NonDictInputError
+from common.decos import log
+
+
+
+@log
+def get_message(client):
+    """
+    Утилита приёма и декодирования сообщения принимает байты выдаёт словарь,
+    если приняточто-то другое отдаёт ошибку значения
+    :param client:
+    :return:
+    """
+    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+    if isinstance(encoded_response, bytes):
+        json_response = encoded_response.decode(ENCODING)
+        response = json.loads(json_response)
+        if isinstance(response, dict):
+            return response
+        raise IncorrectDataReceivedError
+    raise IncorrectDataReceivedError
+
+
+@log
+def send_message(sock, message):
+    """
+    Утилита кодирования и отправки сообщения
+    принимает словарь и отправляет его
+    :param sock:
+    :param message:
+    :return:
+    """
+    if not isinstance(message, dict):
+        raise NonDictInputError
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode(ENCODING)
+    sock.send(encoded_message)
