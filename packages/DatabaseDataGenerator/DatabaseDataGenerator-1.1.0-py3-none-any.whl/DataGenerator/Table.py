f288f80d784e46918c8e1a1e7108c3f5a83dcabe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:01 +0000

    Add package DatabaseDataGenerator/1.1.0/DatabaseDataGenerator-1.1.0-py3-none-any.whl

---
+++ packages/DatabaseDataGenerator/DatabaseDataGenerator-1.1.0-py3-none-any.whl/DataGenerator/Table.py
@@ -0,0 +1,19 @@
+from . import Column
+
+class Table:
+	def __init__(self, name):
+		self.name = name
+		self.columns = []
+
+	def addColumn(self, column: Column):
+		self.columns.append(column)
+
+	def addColumns(self, columns):
+		for i in columns:
+			self.addColumn(i)
+
+	def getPkColumnName(self):
+		for i in self.columns:
+			if i.pk:
+				return i.name
+		return ""
