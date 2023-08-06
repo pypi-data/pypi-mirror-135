c78a23de070472c7ad19fa27ebe00da906867ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:29 +0000

    Add package cohere/1.1.0/cohere-1.1.0-py3-none-any.whl

---
+++ packages/cohere/cohere-1.1.0-py3-none-any.whl/cohere/__init__.py
@@ -0,0 +1,12 @@
+from .client import Client
+from .error import CohereError
+
+COHERE_API_URL = 'https://api.cohere.ai'
+COHERE_VERSION = '2021-11-08'
+GENERATE_URL = 'generate'
+EMBED_URL = 'embed'
+CHOOSE_BEST_URL = 'choose-best'
+LIKELIHOOD_URL = 'likelihood'
+
+CHECK_API_KEY_URL = 'check-api-key'
+TOKENIZE_URL = 'tokenize'
