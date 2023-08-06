be9835935bae76d829896d76e07542770add3786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:48 +0000

    Add package aio-rom/0.1.2/aio-rom-0.1.2.tar.gz

---
+++ packages/aio-rom/aio-rom-0.1.2.tar.gz/aio-rom-0.1.2/aio_rom/types.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from typing import Any, TypeVar, Union
+
+from aioredis.client import FieldT, KeyT
+from typing_extensions import Protocol, runtime_checkable
+
+Key = KeyT
+T = TypeVar("T", bound="IModel")
+
+
+@runtime_checkable
+class IModel(Protocol):
+    def db_id(self) -> Key:
+        ...
+
+    async def save(self, optimistic: bool) -> None:
+        ...
+
+    @classmethod
+    async def get(cls: type[T], id: Key, **kwargs: Any) -> T:
+        ...
+
+    async def total_count(self) -> int:
+        ...
+
+    async def delete(self) -> None:
+        ...
+
+
+RedisValue = FieldT
+Serializable = Union[RedisValue, IModel]
+Serialized = Union[RedisValue, IModel, None]
