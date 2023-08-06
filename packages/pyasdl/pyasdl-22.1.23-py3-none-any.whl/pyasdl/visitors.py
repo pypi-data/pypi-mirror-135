20fd7835b4d9f620970c160719b75ba686d744ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:45 +0000

    Add package pyasdl/22.1.23/pyasdl-22.1.23-py3-none-any.whl

---
+++ packages/pyasdl/pyasdl-22.1.23-py3-none-any.whl/pyasdl/visitors.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from typing import Any, List, cast
+
+from pyasdl.grammar import AST
+
+
+class Visitor:
+    def __call__(self, node: AST, *args, **kwargs) -> Any:
+        ...
+
+
+class ASDLVisitor:
+    def visit(self, node: AST, *args, **kwargs) -> Any:
+        visitor = self.find_visitor(type(node).__name__)
+        return visitor(node, *args, **kwargs)
+
+    def visit_all(self, nodes: List[AST], *args, **kwags) -> List[Any]:
+        return [self.visit(node, *args, **kwags) for node in nodes]
+
+    def generic_visit(self, node: AST, *args, **kwags) -> AST:
+        for value in vars(node).values():
+            if isinstance(value, AST):
+                self.visit(value)
+            elif isinstance(value, list):
+                self.visit_all(value)
+        return node
+
+    def find_visitor(self, name: str) -> Visitor:
+        visitor = f"visit_{name}"
+        if hasattr(self, visitor):
+            func = getattr(self, visitor)
+        else:
+            func = self.generic_visit
+        return cast(Visitor, func)
