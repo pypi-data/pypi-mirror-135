ab3c5a225f91f9a9ccce38d172ac616d3a5d4e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:38 +0000

    Add package yea/0.7.10/yea-0.7.10.tar.gz

---
+++ packages/yea/yea-0.7.10.tar.gz/yea-0.7.10/src/yea/cli.py
@@ -0,0 +1,52 @@
+"""Command line entrypoint."""
+
+import argparse
+import sys
+
+from yea import context, runner
+
+
+def cli_list(yc):
+    print("Tests:")
+    yc._args.action = "list"
+    tr = runner.TestRunner(yc=yc)
+    tests = tr.get_tests()
+    test_ids = [len(t.test_id) for t in tests]
+    tlen = max(test_ids) if test_ids else 0
+    for t in tests:
+        print("  {:<{}s}: {}".format(t.test_id, tlen, t.name))
+    tr.clean()
+
+
+def cli_run(yc):
+    yc._args.action = "run"
+    tr = runner.TestRunner(yc=yc)
+    tr.run()
+
+
+def cli():
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(dest="action", title="action", description="Action to perform")
+    parser.add_argument("--dryrun", action="store_true", help="Dont do anything")
+    parser.add_argument("--live", action="store_true", help="Run against real server")
+    parser.add_argument("--debug", action="store_true", help="Print out extra debug info")
+    parser.add_argument("--shard", help="Specify testing shard")
+    parser.add_argument("--suite", help="Specify testing suite")
+    parser.add_argument("--docs-only", help="Read tests from docstrings only.", action="store_true")
+
+    parse_list = subparsers.add_parser("list", aliases=["l"])
+    parse_list.set_defaults(func=cli_list)
+    parse_list.add_argument("tests", nargs="*")
+
+    parse_run = subparsers.add_parser("run", aliases=["r"])
+    parse_run.add_argument("-a", "--all", action="store_true", help="Run all")
+    parse_run.add_argument("tests", nargs="*")
+    parse_run.set_defaults(func=cli_run)
+    args = parser.parse_args()
+    if not args.action:
+        parser.print_help()
+        sys.exit(1)
+
+    yc = context.YeaContext(args=args)
+    args.func(yc)
---
+++ packages/yea/yea-0.7.10.tar.gz/yea-0.7.10/src/yea/context.py
@@ -0,0 +1,76 @@
+"""yea context."""
+
+import datetime
+import logging
+import os
+
+from yea import config, plugins
+
+
+class YeaContext:
+    def __init__(self, *, args):
+        self._args = args
+        self._cachedir = None
+        self._covfile = None
+        self._now = datetime.datetime.now()
+        self._ts = self._now.strftime("%Y%m%dT%H%M%S")
+        self._pid = os.getpid()
+        self._setup_env()
+        self._cfg = config.Config()
+        self._setup_cachedir()
+        self._setup_logging()
+        self._plugs = plugins.Plugins(yc=self)
+
+    def _setup_env(self):
+        self._covfile = os.environ.get("COVERAGE_FILE")
+
+    def _setup_cachedir(self):
+        root = self._cfg._cfroot
+        p = root.joinpath(".yea_cache")
+        if not p.exists():
+            p.mkdir()
+        self._cachedir = p
+
+    def _setup_logging(self):
+        logger = logging.getLogger("yea")
+        logger.propogate = False
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(fmt="%(asctime)s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+        logfname = "debug-{}-{}.log".format(self._ts, self._pid)
+        lf = self._cachedir.joinpath(logfname)
+        fh = logging.FileHandler(lf)
+        fh.setFormatter(formatter)
+        fh.setLevel(logging.DEBUG)
+        logger.addHandler(fh)
+        logger.info("started")
+
+    def is_live(self):
+        return self._args.live
+
+    def monitors_inform(self, tlist):
+        self._plugs.monitors_inform(tlist)
+
+    def monitors_init(self):
+        self._plugs.monitors_init()
+
+    def monitors_start(self):
+        self._plugs.monitors_start()
+
+    def monitors_stop(self):
+        self._plugs.monitors_stop()
+
+    def monitors_reset(self):
+        self._plugs.monitors_reset()
+
+    def test_prep(self, yt):
+        # wandb_dir_safe_cleanup()
+        self._plugs.test_prep(yt)
+
+    def test_done(self, yt):
+        # wandb_dir_safe_cleanup()
+        self._plugs.test_done(yt)
+
+    def test_check(self, yt):
+        # ctx = self._backend.get_state()
+        result_list = self._plugs.test_check(yt)
+        return result_list
