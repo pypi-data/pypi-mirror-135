b84f5f8267c0b557491da395f88a181a7922871f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:50 +0000

    Add package je-editor/0.0.20/je_editor-0.0.20.tar.gz

---
+++ packages/je-editor/je_editor-0.0.20.tar.gz/je_editor-0.0.20/je_editor/utils/exception/je_editor_exception_tag.py
@@ -0,0 +1,20 @@
+"""
+default exception
+"""
+je_editor_error = "JEditor error"
+"""
+exec exception
+"""
+je_editor_exec_error = "JEditor exec error"
+file_not_fond_error = "file not fond"
+compiler_not_found_error = "compiler not found"
+"""
+shell exception
+"""
+je_editor_shell_error = "JEditor run on shell error"
+"""
+file exception
+"""
+je_editor_save_file_error = "JEditor save file error"
+je_editor_open_file_error = "JEditor open file error"
+je_editor_content_file_error = "JEditor content file error"
---
+++ packages/je-editor/je_editor-0.0.20.tar.gz/je_editor-0.0.20/je_editor/utils/exception/je_editor_exceptions.py
@@ -0,0 +1,26 @@
+class JEditorException(Exception):
+    pass
+
+
+class JEditorExecException(JEditorException):
+    pass
+
+
+class JEditorRunOnShellException(JEditorException):
+    pass
+
+
+class JEditorSaveFileException(JEditorException):
+    pass
+
+
+class JEditorOpenFileException(JEditorException):
+    pass
+
+
+class JEditorContentFileException(JEditorException):
+    pass
+
+
+class JEditorCantFindLanguageException(JEditorException):
+    pass
