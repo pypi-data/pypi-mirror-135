b7d2c77813cf551819fa5e92f8820cb57ee45793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:27 +0000

    Add package crosshair-tool/0.0.21/crosshair_tool-0.0.21-py3-none-any.whl

---
+++ packages/crosshair-tool/crosshair_tool-0.0.21-py3-none-any.whl/crosshair/__init__.py
@@ -0,0 +1,32 @@
+"""Analyze Python code for correctness using symbolic execution."""
+
+from crosshair.core import deep_realize
+from crosshair.core import realize
+from crosshair.core import with_realized_args
+from crosshair.core import register_patch
+from crosshair.core import register_type
+from crosshair.core import SymbolicFactory
+from crosshair.statespace import StateSpace
+from crosshair.util import IgnoreAttempt
+from crosshair.util import debug
+from crosshair.tracers import NoTracing
+from crosshair.tracers import ResumedTracing
+
+__version__ = "0.0.21"  # Do not forget to update in setup.py!
+__author__ = "Phillip Schanely"
+__license__ = "MIT"
+__status__ = "Alpha"
+
+__all__ = [
+    "debug",
+    "IgnoreAttempt",
+    "NoTracing",
+    "deep_realize",
+    "realize",
+    "register_patch",
+    "register_type",
+    "ResumedTracing",
+    "StateSpace",
+    "SymbolicFactory",
+    "with_realized_args",
+]
