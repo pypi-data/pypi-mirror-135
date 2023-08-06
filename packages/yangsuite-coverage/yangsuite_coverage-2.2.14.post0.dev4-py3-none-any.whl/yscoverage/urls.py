236216822e5f9b6143b8a486842e997c1a9b274c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:38 +0000

    Add package yangsuite-coverage/2.2.14.post0.dev4/yangsuite_coverage-2.2.14.post0.dev4-py3-none-any.whl

---
+++ packages/yangsuite-coverage/yangsuite_coverage-2.2.14.post0.dev4-py3-none-any.whl/yscoverage/urls.py
@@ -0,0 +1,14 @@
+# Copyright 2016 to 2021, Cisco Systems, Inc., all rights reserved.
+from django.conf.urls import url
+from . import views
+
+app_name = 'yscoverage'
+urlpatterns = [
+    url(r'^coverage/', views.render_main_page, name="main"),
+    url(r'^getconfig/', views.getconfig, name="getconfig"),
+    url(r'^getreleases/', views.getreleases, name="getreleases"),
+    url(r'^getcoverage/', views.getcoverage, name='getcoverage'),
+    url(r'^datasets/', views.render_datasets_page, name='datasets'),
+    url(r'^getdataset/', views.get_dataset, name='getdataset'),
+    url(r'^getdiff/', views.get_diff, name='getdiff'),
+]
