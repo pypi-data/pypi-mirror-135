94dddfaf2b0510cb934b00bc4c3a40f651f37ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:49:36 +0000

    Add package govuk-bank-holidays/0.10/govuk_bank_holidays-0.10-py3-none-any.whl

---
+++ packages/govuk-bank-holidays/govuk_bank_holidays-0.10-py3-none-any.whl/govuk_bank_holidays/i18n.py
@@ -0,0 +1,31 @@
+def gettext(s):
+    return s
+
+
+gettext('Bank holiday')
+gettext('Bank holidays')
+
+gettext('England')
+gettext('Wales')
+gettext('England and Wales')
+gettext('Scotland')
+gettext('Northern Ireland')
+gettext('United Kingdom')
+
+gettext('2nd January')
+gettext('Battle of the Boyne (Orangemen’s Day)')
+gettext('Boxing Day')
+gettext('Christmas Day')
+gettext('Early May bank holiday')
+gettext('Early May bank holiday (VE day)')
+gettext('Easter Monday')
+gettext('Good Friday')
+gettext('New Year’s Day')
+gettext('Queen’s Diamond Jubilee')
+gettext('Spring bank holiday')
+gettext('St Andrew’s Day')
+gettext('St Patrick’s Day')
+gettext('Summer bank holiday')
+
+gettext('Extra bank holiday')
+gettext('Substitute day')
