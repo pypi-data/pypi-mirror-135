86dcfa00907822e9a9b5325b6b3bba76be33ddfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:37 +0000

    Add package nester-learn/1.0.0/nester_learn-1.0.0.tar.gz

---
+++ packages/nester-learn/nester_learn-1.0.0.tar.gz/nester_learn-1.0.0/nester_learn.py
@@ -0,0 +1,15 @@
+"""
+This is the “nester_learn.py" module, and it provides one function called
+print_nested_lists() which prints lists that may or may not include nested lists.
+"""
+
+
+def print_nested_lists(inp_list):
+    """ Prints each item from a nested list
+    :param inp_list: any list (maybe nested)
+    """
+    for item in inp_list:
+        if isinstance(item, list):
+            print_nested_lists(item)
+        else:
+            print(item)
