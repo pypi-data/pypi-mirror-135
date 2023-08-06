3183576b8a4f7388c2b6e1daabb5eee3eb353d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:03 +0000

    Add package project-semantic-release/0.2.1/project_semantic_release-0.2.1-py3-none-any.whl

---
+++ packages/project-semantic-release/project_semantic_release-0.2.1-py3-none-any.whl/semantic_release/__init__.py
@@ -0,0 +1,5 @@
+"""Semantic Release."""
+__version__ = "0.2.1"
+
+from .errors import UnknownCommitMessageStyleError  # noqa
+from .errors import ImproperConfigurationError, SemanticReleaseBaseError
