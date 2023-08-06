f7e25683c32e4f75c7bfda4e05286d18e76a4eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:35 +0000

    Add package pyrill/0.1.9b17/pyrill-0.1.9b17-py3-none-any.whl

---
+++ packages/pyrill/pyrill-0.1.9b17-py3-none-any.whl/pyrill/strings.py
@@ -0,0 +1,88 @@
+from .base import BaseIndependentConsumerStage, BaseSource
+from .chunks import (BaseChunksFirstSeparatorProducer,
+                     BaseChunksSeparatorProducer, BaseChunksSlowStartProducer,
+                     BaseSizedChunksProducer)
+from .mappers import make_map
+
+__all__ = ['StringSizedChunksSource', 'StringChunksSlowStartSource', 'StringChunksSeparatorSource',
+           'StringChunksFirstSeparatorSource',
+           'StringSizedChunks', 'StringChunksSlowStart', 'StringChunksSeparator', 'StringChunksFirstSeparator',
+           'Encode', 'Lower', 'Upper']
+
+
+class StringChunksMixin:
+    @classmethod
+    def empty_buffer(cls) -> str:
+        return ''
+
+
+class StringSeparatorMixin(StringChunksMixin):
+    @classmethod
+    def default_separator(cls) -> str:
+        return '\n'
+
+
+# Sources
+
+class StringSizedChunksSource(StringChunksMixin, BaseSizedChunksProducer[str], BaseSource[str]):
+    pass
+
+
+class StringChunksSlowStartSource(StringChunksMixin, BaseChunksSlowStartProducer[str], BaseSource[str]):
+    pass
+
+
+class StringChunksSeparatorSource(StringSeparatorMixin, BaseChunksSeparatorProducer[str], BaseSource[str]):
+    pass
+
+
+class StringChunksFirstSeparatorSource(StringSeparatorMixin, BaseChunksFirstSeparatorProducer[str], BaseSource[str]):
+    pass
+
+
+# Middle stages
+
+class StringSizedChunks(StringChunksMixin,
+                        BaseSizedChunksProducer[str],
+                        BaseIndependentConsumerStage[str]):
+    pass
+
+
+class StringChunksSlowStart(StringChunksMixin,
+                            BaseChunksSlowStartProducer[str],
+                            BaseIndependentConsumerStage[str]):
+    pass
+
+
+class StringChunksSeparator(StringSeparatorMixin,
+                            BaseChunksSeparatorProducer[str],
+                            BaseIndependentConsumerStage[str]):
+
+    async def _consume_frame(self) -> str:
+        try:
+            return await super(StringChunksSeparator, self)._consume_frame()
+        except StopAsyncIteration:
+            self._open_buffer = False
+            await self._notify()
+            raise
+
+
+class StringChunksFirstSeparator(StringSeparatorMixin,
+                                 BaseChunksFirstSeparatorProducer[str],
+                                 BaseIndependentConsumerStage[str]):
+    pass
+
+
+@make_map
+def Lower(frame: str, **kwargs) -> str:
+    return frame.lower()
+
+
+@make_map
+def Upper(frame: str, **kwargs) -> str:
+    return frame.upper()
+
+
+@make_map
+def Encode(frame: str, *, encoding: str = 'utf-8', errors: str = 'strict', **kwargs) -> bytes:
+    return frame.encode(encoding=encoding, errors=errors)
