c94185ccc3acdf30c10e437438199a60dd7b2142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:05 +0000

    Add package hardxor/0.0.1/hardxor-0.0.1.tar.gz

---
+++ packages/hardxor/hardxor-0.0.1.tar.gz/hardxor-0.0.1/README.md
@@ -0,0 +1,15 @@
+## Hardxor
+
+`hardxor` is a tool to xor a file byte by byte. 
+
+```bash
+bash> hexdump inputfile.hex
+$ 0000000 69 6d 70 6f 72 74 20 70 61 74 68 6c 69 62 0a 66
+  0000010 72 6f 6d 20 73 65 74 75 70 74 6f 6f 6c 73 20 69
+
+bash> hxor inputfile.hex -k 0xe4 -o outputfile.hex
+
+bash> hexdump outputfile.hex
+$ 0000000 8d 89 94 8b 96 90 c4 94 85 90 8c 88 8d 86 ee 82
+  0000010 96 8b 89 c4 97 81 90 91 94 90 8b 8b 88 97 c4 8d
+```
---
+++ packages/hardxor/hardxor-0.0.1.tar.gz/hardxor-0.0.1/hardxor/__main__.py
@@ -0,0 +1,29 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="XOR any file")
+    parser.add_argument("input", help="Input file")
+    parser.add_argument("-k", default="0x00", action="store", help="key")
+    parser.add_argument("-o", default="system.out", action="store", help="output file")
+    args = parser.parse_args()
+
+    try:
+        key = int(args.k, 0x10)
+        if key > 255:
+            print('Key must be [0x00..0xff]')
+        else:
+            with open(args.input, "rb") as f:
+                f2 = open(args.o, "wb")
+                while True:
+                    current_byte = f.read(1)
+                    if(not current_byte):
+                        break
+                    hexa = hex(current_byte[0] ^ key)[2:]
+                    f2.write(bytes.fromhex(hexa if len(hexa) > 1 else '0' + hexa))
+                f2.close()
+    except ValueError as ve:
+        print(ve)
+
+
+if __name__ == "__main__":
+    main()
