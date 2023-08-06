ae184935fdfabcc629092c36b9363716374b6033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:51 +0000

    Add package nonebot-plugin-gocqhttp/0.1.0/nonebot_plugin_gocqhttp-0.1.0-py3-none-any.whl

---
+++ packages/nonebot-plugin-gocqhttp/nonebot_plugin_gocqhttp-0.1.0-py3-none-any.whl/nonebot_plugin_gocqhttp/__init__.py
@@ -0,0 +1,28 @@
+from typing import Dict
+
+from nonebot import get_driver
+
+import nonebot_plugin_gocqhttp.plugin  # noqa:F401
+
+from .log import logger
+from .plugin_config import config
+from .process import GoCQProcess
+from .process.download import BINARY_PATH, download_gocq
+
+PROCESSES: Dict[int, GoCQProcess] = {}
+
+driver = get_driver()
+
+
+@driver.on_startup
+async def startup():
+    if config.FORCE_DOWNLOAD or not BINARY_PATH.is_file():
+        await download_gocq()
+
+    for account in config.ACCOUNTS:
+        logger.info(f"Starting GoCQ process for <e>{account.uin}</e>")
+        process = GoCQProcess(account)
+        PROCESSES[account.uin] = process
+        await process.start()
+
+    return
