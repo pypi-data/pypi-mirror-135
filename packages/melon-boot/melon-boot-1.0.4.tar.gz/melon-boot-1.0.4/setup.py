2d00664e8c9d610dfe62b989d5111636309d25a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:21 +0000

    Add package melon-boot/1.0.4/melon-boot-1.0.4.tar.gz

---
+++ packages/melon-boot/melon-boot-1.0.4.tar.gz/melon-boot-1.0.4/setup.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+"""
+@Author  : zh_o
+"""
+from setuptools import setup, find_packages
+
+
+setup(
+    name='melon-boot',
+    version='1.0.4',
+    author='zh_o',
+    author_email='a1763041057@gmail.com',
+    description='The uiautomation framework',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=['toml', 'selenium', 'pytest', 'allure-pytest']
+)
