c716e9dbb234928de384aa5b1ed35ec5ede5199e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:45 +0000

    Add package estd/0.0.1/estd-0.0.1.tar.gz

---
+++ packages/estd/estd-0.0.1.tar.gz/estd-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="estd",
+    license="MIT",
+    version="0.0.1",
+    description="Extended standard library",
+    author="SingChen",
+    author_email="xhqsm@qq.com",
+    url="https://gitee.com/xhqsm/estd/",
+    packages=find_packages(),
+)
