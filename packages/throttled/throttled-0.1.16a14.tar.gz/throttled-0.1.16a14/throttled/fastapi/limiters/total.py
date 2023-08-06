6bf362ccdb739e0fac91173ead40d045d2234f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:42 +0000

    Add package throttled/0.1.16a14/throttled-0.1.16a14.tar.gz

---
+++ packages/throttled/throttled-0.1.16a14.tar.gz/throttled-0.1.16a14/throttled/fastapi/limiters/total.py
@@ -0,0 +1,12 @@
+from starlette.requests import Request
+
+from throttled.limiter import Limiter
+from throttled.starlette.limiters.base import Middleware
+
+
+class TotalLimiter(Limiter, Middleware):
+    def _maybe_block_request(self, request: Request):
+        self()
+
+    def __call__(self):  # pylint: disable=arguments-differ
+        self.limit(key="total")
