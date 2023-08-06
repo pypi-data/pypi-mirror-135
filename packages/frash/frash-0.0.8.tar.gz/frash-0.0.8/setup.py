f9e3636ff864b56010c098d5631cd94c79118b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:58 +0000

    Add package frash/0.0.8/frash-0.0.8.tar.gz

---
+++ packages/frash/frash-0.0.8.tar.gz/frash-0.0.8/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="frash",
+    version="0.0.8",
+    description="Juggling with hex numbers. Convert integers and floats to hex.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/0xflotus/frash",
+    author="0xflotus",
+    author_email="0xflotus+pypi@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries ",
+    ],
+    packages=["frash"],
+    include_package_data=True,
+    install_requires=["argparse"],
+    tests_require=["pytest"],
+    entry_points={
+        'console_scripts': [
+            'frash=frash.__main__:main'
+        ]
+    }
+)
+
