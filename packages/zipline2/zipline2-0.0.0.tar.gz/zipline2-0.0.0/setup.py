4c4aacb53e2366d2fdcacc3e8ac7ebd6e2eee916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:17 +0000

    Add package zipline2/0.0.0/zipline2-0.0.0.tar.gz

---
+++ packages/zipline2/zipline2-0.0.0.tar.gz/zipline2-0.0.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name="zipline2",
+    version="0.0.0",
+    author="André Bienemann",
+    author_email="andre.bienemann@gmail.com",
+    packages=setuptools.find_packages(),
+)
