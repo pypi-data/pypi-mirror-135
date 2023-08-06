96efdf60e9959477803c580c8835bb399989e47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:22 +0000

    Add package pydrawer/0.0.1/pydrawer-0.0.1.tar.gz

---
+++ packages/pydrawer/pydrawer-0.0.1.tar.gz/pydrawer-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+
+setup(
+    name="pydrawer",
+    packages=["pydrawer"],
+    version="0.0.1",
+    license="MIT",
+    description="The Python package for visualizing curves and linear transformations in a super simple way.",
+    author="Dylan Tintenfich",
+    author_email="tintenfichdylan05@gmail.com",
+    url="https://github.com/dylannalex/pydrawer",
+    keywords=[
+        "pydrawer",
+    ],
+    install_requires=["PyYAML==6.0", "turtle==0.0.1"],
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
