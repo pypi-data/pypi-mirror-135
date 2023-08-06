7ac928450d07dc0a569ca497310d2b4021616b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:20 +0000

    Add package mypackage-divyamtalreja/0.0.2/mypackage-divyamtalreja-0.0.2.tar.gz

---
+++ packages/mypackage-divyamtalreja/mypackage-divyamtalreja-0.0.2.tar.gz/mypackage-divyamtalreja-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+Project_NAME="mypackage"
+USER_NAME="divyamtalreja"
+
+setuptools.setup(
+    name=f"{Project_NAME}-{USER_NAME}",
+    version="0.0.2",
+    author=USER_NAME,
+    author_email="divyamtalreja16@gmail.com",
+    description="implementation of  perceptron",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USER_NAME}/{Project_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{USER_NAME}/{Project_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "pandas"
+    ]
+)
