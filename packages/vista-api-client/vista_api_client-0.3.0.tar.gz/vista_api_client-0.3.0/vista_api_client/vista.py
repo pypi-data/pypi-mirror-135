9c9f3b01babe235d1e5276d4e7194751106932cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:18 +0000

    Add package vista-api-client/0.3.0/vista_api_client-0.3.0.tar.gz

---
+++ packages/vista-api-client/vista_api_client-0.3.0.tar.gz/vista_api_client-0.3.0/vista_api_client/vista.py
@@ -0,0 +1,21 @@
+
+from vista_api_client.api_resources.admin import Admin
+from vista_api_client.api_resources.resource_types import ResourceTypes
+from vista_api_client.api_resources.roles import Roles
+from vista_api_client.api_resources.users import Users
+from vista_api_client.api_resources.usersets import Usersets
+
+from vista_api_client.config.config import config
+
+class VistaClient(object):
+    ALL = '*'
+
+    def __init__(self, secret, branch, hostname=None):
+        if not hostname:
+            hostname = config['VistaAPIHostname']
+
+        self.admin = Admin(secret, branch, hostname)
+        self.resource_types = ResourceTypes(secret, branch, hostname)
+        self.roles = Roles(secret, branch, hostname)
+        self.users = Users(secret, branch, hostname)
+        self.usersets = Usersets(secret, branch, hostname)
