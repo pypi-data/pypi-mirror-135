251beb50f7e5f6434db7e5e2685ca319f8359172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:49:16 +0000

    Add package homeline/0.0.2/homeline-0.0.2.tar.gz

---
+++ packages/homeline/homeline-0.0.2.tar.gz/homeline-0.0.2/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="homeline",
+    version="0.0.2",
+    author="Magnus Eldén",
+    description="Wrapper for the Compare It Homeline Api",
+    packages=["Compare_It"]
+)   
