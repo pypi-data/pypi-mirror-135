5ed06ed6801cd245dde97968ad79eb492835ba27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:41 +0000

    Add package pcap-ct/1.2.3b12/pcap-ct-1.2.3b12.zip

---
+++ packages/pcap-ct/pcap-ct-1.2.3b12.zip/pcap-ct-1.2.3b12/tests/test_main.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2016-2022, Adam Karpierz
+# Licensed under the BSD license
+# https://opensource.org/licenses/BSD-3-Clause
+
+import unittest
+
+import pcap
+
+
+class MainTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
