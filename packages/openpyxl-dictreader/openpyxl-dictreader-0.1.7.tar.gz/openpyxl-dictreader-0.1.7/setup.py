0e2078fe6791fbb7fafafc2fc10c709cfddf0f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:50 +0000

    Add package openpyxl-dictreader/0.1.7/openpyxl-dictreader-0.1.7.tar.gz

---
+++ packages/openpyxl-dictreader/openpyxl-dictreader-0.1.7.tar.gz/openpyxl-dictreader-0.1.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+requirements = [
+    "openpyxl~=3.0.3"
+]
+
+setup(
+    name='openpyxl-dictreader',
+    version='0.1.7',
+    description='A simple package to read openpyxl worksheets like a csv DictReader',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Frank Oprel',
+    author_email="oprel.fj@gmail.com",
+    url='https://github.com/foprel/openpyxl-dictreader',
+    install_requires=requirements,
+    license=license,
+    py_modules=["openpyxl_dictreader"]
+)
