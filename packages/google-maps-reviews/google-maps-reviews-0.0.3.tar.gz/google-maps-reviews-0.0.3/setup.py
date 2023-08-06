7bc531dd2157516fc0a74a6e57538833ae7c6997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:38 +0000

    Add package google-maps-reviews/0.0.3/google-maps-reviews-0.0.3.tar.gz

---
+++ packages/google-maps-reviews/google-maps-reviews-0.0.3.tar.gz/google-maps-reviews-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='google-maps-reviews',
+    version='0.0.3',
+    description='Google Maps Reviews API SDK',
+    long_description=readme(),
+    classifiers = ['Programming Language :: Python',
+                    'License :: OSI Approved :: MIT License',
+                    'Operating System :: OS Independent',
+                    'Intended Audience :: Developers',
+                    'Topic :: Utilities',
+    ],
+    keywords='google maps reviews api sdk scraper parser',
+    url='https://github.com/outscraper/google-maps-reviews-python',
+    author='Outscraper',
+    author_email='developers@outscraper.com',
+    license='MIT',
+    packages=['google_maps_reviews'],
+    install_requires=['requests'],
+    include_package_data=True,
+    zip_safe=False,
+    long_description_content_type='text/x-rst',
+)
