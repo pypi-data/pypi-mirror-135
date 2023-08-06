02f11840b81e14aebb15fa43ee088c19aa1e6f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:38 +0000

    Add package ckan/2.9.5/ckan-2.9.5.tar.gz

---
+++ packages/ckan/ckan-2.9.5.tar.gz/ckan-2.9.5/ckan/public/base/i18n/fa_IR.js
@@ -0,0 +1,20 @@
+{
+  "": {
+    "domain": "ckan", 
+    "lang": "fa_IR", 
+    "plural-forms": "nplurals=2; plural=(n > 1);"
+  }, 
+  "Filters": [
+    null, 
+    "فیلترها"
+  ], 
+  "Input is too short, must be at least one character": [
+    "Input is too short, must be at least %(num)d characters", 
+    "", 
+    ""
+  ], 
+  "Map": [
+    null, 
+    "نقشه"
+  ]
+}
