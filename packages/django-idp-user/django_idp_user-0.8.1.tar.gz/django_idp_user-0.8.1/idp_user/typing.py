cb07cf5e8f4a59400a077329e9d74b87fed84aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:34 +0000

    Add package django-idp-user/0.8.1/django_idp_user-0.8.1.tar.gz

---
+++ packages/django-idp-user/django_idp_user-0.8.1.tar.gz/django_idp_user-0.8.1/idp_user/typing.py
@@ -0,0 +1,100 @@
+from typing import TypedDict, Union, List, Any, Optional
+
+
+class JwtData(TypedDict):
+    iat: int
+    nbf: int
+    jti: str
+    exp: str
+    type: str
+    fresh: str
+    user_id: int
+    email: str
+    username: str
+
+
+class UserFeaturesPermissions(TypedDict):
+    dod_manager: Union[List, bool]
+    cash_flow_projection: Union[List, bool]
+    notes_manager: Union[List, bool]
+
+
+class AppSpecificConfigs(TypedDict):
+    app_config: Any
+    permission_restrictions: dict[str, Union[bool, Any]]
+
+
+Role = str
+UserAppSpecificConfigs = dict[Role, AppSpecificConfigs]
+
+
+class UserTenantData(TypedDict):
+    idp_user_id: int
+    first_name: str
+    last_name: str
+    username: str
+    email: str
+    app_specific_configs: UserAppSpecificConfigs
+
+
+"""
+"data": [
+   {
+        "idp_user_id": 12,
+        "first_name": "str",
+        "last_name": "str",
+        "username": "str",
+        "email": "str",
+        "app_specific_configs": {
+            "app_identifier": {
+                "Servicer": {
+                    "app_config": {"vehicle_ids": [1, 2]},
+                    "permission_restrictions": {
+                        "viewDoD": {"vehicle_ids": [1]},
+                        "synchronizeDoD": false
+                    }
+                }
+            }
+        }
+    }
+]
+"""
+
+# ===
+AppIdentifier = str
+TenantIdentifier = str
+
+UserRecordAppSpecificConfigs = dict[AppIdentifier, dict[TenantIdentifier, AppSpecificConfigs]]
+
+
+class UserRecordDict(TypedDict):
+    idp_user_id: int
+    first_name: Optional[str]
+    last_name: Optional[str]
+    username: Optional[str]
+    email: Optional[str]
+    app_specific_configs: UserRecordAppSpecificConfigs
+
+
+"""
+Example of a user record from kafka:
+{
+    "first_name": "str",
+    "last_name": "str",
+    "username": "str",
+    "email": "str",
+    "app_specific_configs": {
+        "app_identifier": {
+            "tenant": {
+                "Servicer": {
+                    "app_config": {"vehicle_ids": [1, 2]},
+                    "permission_restrictions": {
+                        "viewDoD": {"vehicle_ids": [1]},
+                        "synchronizeDoD": false
+                    }
+                }
+            }
+        }
+    }
+}
+"""
