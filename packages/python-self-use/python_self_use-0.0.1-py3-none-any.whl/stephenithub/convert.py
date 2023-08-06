370b9508c88202e34956402cd10aeb1b738f6997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:01 +0000

    Add package python-self-use/0.0.1/python_self_use-0.0.1-py3-none-any.whl

---
+++ packages/python-self-use/python_self_use-0.0.1-py3-none-any.whl/stephenithub/convert.py
@@ -0,0 +1,29 @@
+"""
+将ocr识别后的文本转换为DataFrame
+
+"""
+import pandas as pd
+import numpy as np
+
+
+class Convert(object):
+
+    """
+    将ocr识别后的文本转换为DataFrame
+
+    """
+
+    # 定义个函数
+    def ocrtext2df(self, text, column_size, ignore_na=False):
+        """
+            便于ocr截图识别后转表格
+            text: 带换行的text
+            column_size: 需要转成几列
+            ignore_na: 是否删除中间空行
+        """
+
+        str_list = text.strip().splitlines()
+        if ignore_na:
+            str_list = [x for x in str_list if len(x)]
+        nd_arr = np.array(str_list).reshape(len(str_list) // column_size, column_size)
+        return pd.DataFrame(nd_arr[1:], columns=list(nd_arr[:1].flatten()))
