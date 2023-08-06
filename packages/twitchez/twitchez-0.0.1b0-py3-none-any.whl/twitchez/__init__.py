fa0c2ee1404ff583a84645e70a55c36b1d86049c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:15 +0000

    Add package twitchez/0.0.1b0/twitchez-0.0.1b0-py3-none-any.whl

---
+++ packages/twitchez/twitchez-0.0.1b0-py3-none-any.whl/twitchez/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+from pathlib import Path
+
+__version__ = "0.0.1b0"
+__license__ = "GPLv3"
+__description__ = "TUI client for twitch with thumbnails"
+__url_repository__ = "https://github.com/WANDEX/twitchez"
+__url_bug_reports__ = "https://github.com/WANDEX/twitchez/issues"
+__url_project__ = __url_repository__
+__author__ = "WANDEX"
+
+# Constants
+TWITCHEZDIR = Path(__file__).parent.resolve()
