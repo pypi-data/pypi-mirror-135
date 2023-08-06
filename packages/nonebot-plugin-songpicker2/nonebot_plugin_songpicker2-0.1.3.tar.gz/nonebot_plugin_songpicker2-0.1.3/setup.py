5519fa62d4ffd39eb3d817d67cae57e7bad44640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:43 +0000

    Add package nonebot-plugin-songpicker2/0.1.3/nonebot_plugin_songpicker2-0.1.3.tar.gz

---
+++ packages/nonebot-plugin-songpicker2/nonebot_plugin_songpicker2-0.1.3.tar.gz/nonebot_plugin_songpicker2-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nonebot_plugin_songpicker2']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['build>=0.1.0,<0.2.0', 'nonebot2>=2.0.0-beta.1,<3.0.0', 'twine>=3.3.0,<4.0.0']
+
+setup_kwargs = {
+    'name': 'nonebot-plugin-songpicker2',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Maximilian Wu',
+    'author_email': 'me@maxng.cc',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
