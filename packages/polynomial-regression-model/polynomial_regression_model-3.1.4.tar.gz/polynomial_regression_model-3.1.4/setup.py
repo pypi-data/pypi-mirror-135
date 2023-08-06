f3479c6822b691dd50b7081b442d10e58977a40e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:44 +0000

    Add package polynomial-regression-model/3.1.4/polynomial_regression_model-3.1.4.tar.gz

---
+++ packages/polynomial-regression-model/polynomial_regression_model-3.1.4.tar.gz/polynomial_regression_model-3.1.4/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="polynomial_regression_model",
+    version="3.1.4",
+    description="Python package that analyses the given datasets and comes up with the best regression representation with either the smallest polynomial degree possible, to be the most reliable without overfitting or other models such as exponentials and logarithms",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nikolas-virionis/polynomial-regression",
+    author="Nikolas B Virionis",
+    author_email="nikolas.virionis@bandtec.com.br",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["polynomial_regression"],
+    install_requires=["numpy", "matplotlib", "sklearn"],
+)
