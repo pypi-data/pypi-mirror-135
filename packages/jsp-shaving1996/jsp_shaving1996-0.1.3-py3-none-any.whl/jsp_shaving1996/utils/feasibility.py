05294a50b07378d80f179d41586d9f93a86ad485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:12 +0000

    Add package jsp-shaving1996/0.1.3/jsp_shaving1996-0.1.3-py3-none-any.whl

---
+++ packages/jsp-shaving1996/jsp_shaving1996-0.1.3-py3-none-any.whl/jsp_shaving1996/utils/feasibility.py
@@ -0,0 +1,10 @@
+from jsp_exp import Job
+from jsp_shaving1990 import iterated_full_shaving
+
+
+def has_schedule(job: Job, UB: int) -> bool:
+    result_list, _, _ = iterated_full_shaving(job.attrtolist('r'), job.attrtolist('p'), job.attrtolist('q'), UB)
+    if result_list:
+        return True
+    else:
+        return False
