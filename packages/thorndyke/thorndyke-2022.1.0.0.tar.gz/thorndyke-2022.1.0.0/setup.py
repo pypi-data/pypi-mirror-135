977badcb0ec3963495f4adaa8c187939be11d2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:05 +0000

    Add package thorndyke/2022.1.0.0/thorndyke-2022.1.0.0.tar.gz

---
+++ packages/thorndyke/thorndyke-2022.1.0.0.tar.gz/thorndyke-2022.1.0.0/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="thorndyke",
+    version="2022.1.0.0",
+    author="Richard Mwewa",
+    author_email="richardmwewa@duck.com",
+    packages=["thorndyke"],
+    description="Username enumeration tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rlyonheart/thorndyke",
+    license="GNU General Public License v3 (GPLv3)",
+    install_requires=["requests==2.26.0","tqdm==4.60.0"],
+    classifiers=[
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',  
+        'Operating System :: POSIX :: Linux',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    entry_points={
+        "console_scripts": [
+            "thorndyke=thorndyke.main:Thorndyke",
+        ]
+    }
+)
