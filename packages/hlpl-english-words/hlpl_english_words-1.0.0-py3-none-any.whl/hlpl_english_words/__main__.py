872091c9658c502643e06c5efbc9b7910a284b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:20 +0000

    Add package hlpl-english-words/1.0.0/hlpl_english_words-1.0.0-py3-none-any.whl

---
+++ packages/hlpl-english-words/hlpl_english_words-1.0.0-py3-none-any.whl/hlpl_english_words/__main__.py
@@ -0,0 +1,23 @@
+﻿
+import configparser
+import sys
+import os
+ 
+ 
+def get_hlpl_english_words_version(case):
+    config = configparser.ConfigParser()
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    config_file_path = os.path.join(current_directory, 'setup.cfg')
+    config.read(config_file_path)
+    if case=='version':
+       return config['hlpl_english_words']['version']
+    if case=='else':
+       return config['hlpl_english_words']['version']+'\n'+config['hlpl_english_words']['author']+'\n'+config['hlpl_english_words']['email']+'\n'+config['hlpl_english_words']['url']+'\n'
+       
+ 
+    
+def main():
+    if 'version' in sys.argv:
+        print('\n'+get_hlpl_english_words_version('version'))
+    else:
+        print('\n'+get_hlpl_english_words_version('else')+'\n'+'hlpl_english_words lists english words list (or english words database)')
