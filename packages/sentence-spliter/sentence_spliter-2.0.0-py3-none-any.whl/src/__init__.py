9a546fffaa2d23ed0b65dd0c3e47b4f3b7209c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:48 +0000

    Add package sentence-spliter/2.0.0/sentence_spliter-2.0.0-py3-none-any.whl

---
+++ packages/sentence-spliter/sentence_spliter-2.0.0-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Author  :JIANWEI ZHAO
+# @Time    :2021/12/2  下午7:20
+# @Describe:
---
+++ packages/sentence-spliter/sentence_spliter-2.0.0-py3-none-any.whl/test_old/debug.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# CREATED BY: bohuai jiang 
+# CREATED ON: 2020/8/19 11:22 AM
+# LAST MODIFIED ON:
+# AIM:
+
+from sentence_spliter_old.logic_graph import long_short_cuter, simple_cuter
+from sentence_spliter_old.automata.state_machine import StateMachine
+from sentence_spliter_old.automata.sequence import StrSequence
