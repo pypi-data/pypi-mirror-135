3eb412f03b6b48c737c1f9b0668286dba3a4812e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:30 +0000

    Add package capturejob/0.1.0/capturejob-0.1.0-py3-none-any.whl

---
+++ packages/capturejob/capturejob-0.1.0-py3-none-any.whl/capturejob/__init__.py
@@ -0,0 +1,39 @@
+import os
+
+from loguru import logger
+from dyndebug import Debug
+from typing import Callable, Any
+from dotmap import DotMap
+from capturejob.service.fileservice import FileService
+
+
+from datetime import datetime
+
+
+def CaptureJob():
+    debug = Debug('capturejob')
+    config = DotMap({
+        "connectionString": os.environ.get("CAPTURE_CONNECTION_STRING"),
+        "containerName": os.environ.get("CAPTURE_CONTAINER_NAME"),
+    })
+
+    print(config.containerName)
+    fileservice =FileService(config)
+
+    job = os.environ.get("JOB")
+
+    now = datetime.now() 
+    date_time = now.strftime("%Y%m%d_%H%M%S")    
+
+    cwd = os.getcwd()
+    
+    folder = f"{date_time}_{job}"
+    fileservice.upload(os.path.join(cwd,"stdout.txt"),folder,"stdout.txt")
+    fileservice.upload(os.path.join(cwd,"stderr.txt"),folder,"stderr.txt")
+    
+    
+
+
+
+    
+            
