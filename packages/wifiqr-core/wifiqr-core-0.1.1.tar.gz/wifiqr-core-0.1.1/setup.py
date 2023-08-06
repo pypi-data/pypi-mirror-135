ebb00837721f508cfbb01f7f0c01fe9c4a456297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:49 +0000

    Add package wifiqr-core/0.1.1/wifiqr-core-0.1.1.tar.gz

---
+++ packages/wifiqr-core/wifiqr-core-0.1.1.tar.gz/wifiqr-core-0.1.1/setup.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""Setup module."""
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+MINIMAL_DESCRIPTION = """WiFiQR-Core Generate a QR code to share WiFi creds (SSID+Pass). This is the core functionality can be used as library, API or a CLI tools. It's used by the WiFiQR WebApp."""
+
+
+def read_description():
+    """Read README.md and CHANGELOG.md."""
+    try:
+        with open("README.md", "r") as r:
+            description = "\n"
+            description += r.read()
+        return description
+    except Exception:
+        return MINIMAL_DESCRIPTION
+
+
+setup(
+    name='wifiqr-core',
+
+    version='0.1.1',
+
+    description='An API to generate a QR-code for your WiFI to let others quickly connect.',
+    long_description=read_description(),
+
+    url='https://github.com/MoElaSec/WiFiQR_Core',
+
+    author='Mo Eltahir',
+    author_email='mohd.debrecen@gmail.com',
+
+    license='GPLv3+',
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+
+        'Environment :: Console',
+
+        'Intended Audience :: End Users/Desktop',
+
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        'Operating System :: OS Independent',
+
+        'Topic :: Communications',
+
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering'
+    ],
+
+    keywords=['WiFi', 'qrcode', 'pillow'],
+
+    py_modules=['wifiqr-core'],
+
+    install_requires=['Pillow', 'qrcode'],
+
+    entry_points={
+        'console_scripts': [
+            'wifiqr-core=wifi_qr:main',
+        ]
+    },
+
+    test_suite='setup.test_suite'
+)
