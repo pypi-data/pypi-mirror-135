52f8ae6ac559cfd59794a8f26716af4843c13eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:54 +0000

    Add package QUIfixed/0.5/QUIfixed-0.5.tar.gz

---
+++ packages/QUIfixed/QUIfixed-0.5.tar.gz/QUIfixed-0.5/setup.py
@@ -0,0 +1,24 @@
+# Author: Acer Zhang
+# Datetime: 2021/9/17 
+# Copyright belongs to the author.
+# Please indicate the source for reprinting.
+
+from setuptools import setup
+from setuptools import find_packages
+
+__version__ = "0.5"
+
+# python setup.py sdist bdist_wheel
+setup(
+    name='QUIfixed',
+    version=__version__,
+    packages=find_packages(),
+    url='https://github.com/QPT-Family/QGUI',
+    license='MIT',
+    author='fred',
+    author_email='3365424882@qq.com',
+    description='/',
+    install_requires=["ttkbootstrap", "pillow"],
+    python_requires='>3.5',
+    include_package_data=True,
+)
