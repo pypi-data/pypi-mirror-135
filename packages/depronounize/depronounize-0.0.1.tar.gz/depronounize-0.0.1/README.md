a7044b4e3d735c51acfff5002ae755c37d1319f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:08 +0000

    Add package depronounize/0.0.1/depronounize-0.0.1.tar.gz

---
+++ packages/depronounize/depronounize-0.0.1.tar.gz/depronounize-0.0.1/README.md
@@ -0,0 +1,14 @@
+
+## Summary
+Replace pronouns with the nouns they represent.
+
+## Example
+```
+from depronounize import replace_pronouns
+
+print(replace_pronouns("I went to buy jam, but they were out of it."))
+```
+
+## Python Version
+Python 3.8.1 was used for testing
+
---
+++ packages/depronounize/depronounize-0.0.1.tar.gz/depronounize-0.0.1/setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup
+
+
+setup(
+      name="depronounize",
+      version='0.0.1',
+      description="Pronoun replacement module",
+      url="https://github.com/NazarTrilisky/PronounReplacement",
+      install_requires=["spacy==2.3.2"],
+      include_package_data=True,
+      zip_safe=False,
+      author_email="",
+      license="MIT",
+      author="Nazar Trilisky"
+)
+
