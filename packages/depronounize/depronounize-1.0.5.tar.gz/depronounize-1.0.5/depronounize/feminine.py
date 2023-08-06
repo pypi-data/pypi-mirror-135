673b364c3594772e7d74942efecd861ef22401cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:02 +0000

    Add package depronounize/1.0.5/depronounize-1.0.5.tar.gz

---
+++ packages/depronounize/depronounize-1.0.5.tar.gz/depronounize-1.0.5/depronounize/feminine.py
@@ -0,0 +1,5 @@
+from female_nouns import female_nouns
+from female_names import female_names
+
+# list of female words in lower-case
+feminine = female_nouns + female_names
---
+++ packages/depronounize/depronounize-1.0.5.tar.gz/depronounize-1.0.5/depronounize/masculine.py
@@ -0,0 +1,5 @@
+from male_names import male_names
+from male_nouns import male_nouns
+
+# list of masculine words in lower-case
+masculine = male_names + male_nouns
