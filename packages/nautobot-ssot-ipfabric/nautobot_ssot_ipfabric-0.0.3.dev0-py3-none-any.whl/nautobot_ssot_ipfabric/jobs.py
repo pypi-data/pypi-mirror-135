e1d32d3af1ea743bae91964b1df6a4a184adf8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:08 +0000

    Add package nautobot-ssot-ipfabric/0.0.3.dev0/nautobot_ssot_ipfabric-0.0.3.dev0-py3-none-any.whl

---
+++ packages/nautobot-ssot-ipfabric/nautobot_ssot_ipfabric-0.0.3.dev0-py3-none-any.whl/nautobot_ssot_ipfabric/jobs.py
@@ -0,0 +1,87 @@
+"""IP Fabric Data Target Job."""
+from diffsync.exceptions import ObjectNotCreated
+from django.conf import settings
+from django.templatetags.static import static
+from django.urls import reverse
+
+from nautobot.extras.jobs import BooleanVar, Job
+from nautobot_ssot.jobs.base import DataMapping, DataSource
+
+from nautobot_ssot_ipfabric.diffsync.adapter_ipfabric import IPFabricDiffSync
+from nautobot_ssot_ipfabric.diffsync.adapter_nautobot import NautobotDiffSync
+from nautobot_ssot_ipfabric.utilities.ipfabric_client import IpFabricClient
+
+CONFIG = settings.PLUGINS_CONFIG.get("nautobot_ssot_ipfabric", {})
+IPFABRIC_HOST = CONFIG["ipfabric_host"]
+IPFABRIC_API_TOKEN = CONFIG["ipfabric_api_token"]
+
+
+# pylint:disable=too-few-public-methods
+class IpFabricDataSource(DataSource, Job):
+    """Job syncing data from IP Fabric to Nautobot."""
+
+    debug = BooleanVar(description="Enable for more verbose debug logging")
+
+    class Meta:
+        """Metadata about this Job."""
+
+        name = "IP Fabric"
+        data_source = "IP Fabric"
+        data_source_icon = static("nautobot_ssot_ipfabric/ipfabric.png")
+        description = "Synchronize data from IP Fabric into Nautobot."
+
+    @classmethod
+    def data_mappings(cls):
+        """List describing the data mappings involved in this DataSource."""
+        return (
+            DataMapping("Device", None, "Device", reverse("dcim:device_list")),
+            DataMapping("Site", None, "Site", reverse("dcim:site_list")),
+            DataMapping("Interfaces", None, "Interfaces", reverse("dcim:interface_list")),
+            DataMapping("IP Addresses", None, "IP Addresses", reverse("ipam:ipaddress_list")),
+            DataMapping("VLANs", None, "VLANs", reverse("ipam:vlan_list")),
+            DataMapping("Network Diagram", None, "Graph", f"{IPFABRIC_HOST}/graph"),
+        )
+
+    @classmethod
+    def config_information(cls):
+        """Dictionary describing the configuration of this DataSource."""
+        return {
+            "IP Fabric host": CONFIG["ipfabric_host"],
+        }
+
+    def sync_data(self):
+        """Sync a device data from IP Fabric into Nautobot."""
+        client = IpFabricClient(IPFABRIC_HOST, IPFABRIC_API_TOKEN)
+
+        ipfabric_source = IPFabricDiffSync(job=self, sync=self.sync, client=client)
+        self.log_info(message="Loading current data from IP Fabric...")
+        ipfabric_source.load()
+
+        dest = NautobotDiffSync(job=self, sync=self.sync)
+        self.log_info(message="Loading current data from Nautobot.")
+        dest.load()
+
+        self.log_info(message="Calculating diffs...")
+        diff = dest.diff_from(ipfabric_source)
+        self.log_debug(message=f"Diff: {diff.dict()}")
+
+        self.sync.diff = diff.dict()
+        self.sync.save()
+        create = diff.summary().get("create")
+        update = diff.summary().get("update")
+        delete = diff.summary().get("delete")
+        no_change = diff.summary().get("no-change")
+        self.log_info(
+            message=f"DiffSync Summary: Create: {create}, Update: {update}, Delete: {delete}, No Change: {no_change}"
+        )
+
+        if not self.kwargs["dry_run"]:
+            self.log_info(message="Syncing from IP Fabric to Nautobot")
+            try:
+                dest.sync_from(ipfabric_source)
+            except ObjectNotCreated as err:
+                self.log_debug(f"Unable to create object. {err}")
+            self.log_success(message="Sync complete.")
+
+
+jobs = [IpFabricDataSource]
---
+++ packages/nautobot-ssot-ipfabric/nautobot_ssot_ipfabric-0.0.3.dev0-py3-none-any.whl/nautobot_ssot_ipfabric/workers.py
@@ -0,0 +1,87 @@
+# Disable dispatcher from chatops unused. # pylint: disable=unused-argument
+"""Chat Ops Worker."""
+import uuid
+
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django_rq import job
+from nautobot.core.settings_funcs import is_truthy
+from nautobot.extras.models import JobResult
+from nautobot_chatops.choices import CommandStatusChoices
+from nautobot_chatops.workers import handle_subcommands, subcommand_of
+
+from nautobot_ssot_ipfabric.jobs import IpFabricDataSource
+
+CONFIG = settings.PLUGINS_CONFIG.get("nautobot_ssot_ipfabric", {})
+NAUTOBOT_HOST = CONFIG.get("nautobot_host")
+
+BASE_CMD = "ipfabric"
+IPFABRIC_LOGO_PATH = "nautobot_ssot_ipfabric/ipfabric_logo.png"
+IPFABRIC_LOGO_ALT = "IPFabric Logo"
+
+
+def prompt_for_dry_run(dispatcher, action_id, help_text):
+    """Prompt the user to select a Network name."""
+    choices = [("Yes", "True"), ("No", "False")]
+    return dispatcher.prompt_from_menu(action_id, help_text, choices, default=("Yes", "True"))
+
+
+def ipfabric_logo(dispatcher):
+    """Construct an image_element containing the locally hosted IP Fabric logo."""
+    return dispatcher.image_element(dispatcher.static_url(IPFABRIC_LOGO_PATH), alt_text=IPFABRIC_LOGO_ALT)
+
+
+@job("default")
+def ipfabric(subcommand, **kwargs):
+    """Interact with ipfabric plugin."""
+    return handle_subcommands("ipfabric", subcommand, **kwargs)
+
+
+@subcommand_of("ipfabric")
+def ssot_sync_to_nautobot(dispatcher, dry_run=None):
+    """Start an SSoT sync from IPFabric to Nautobot."""
+    if not dry_run:
+        prompt_for_dry_run(dispatcher, f"{BASE_CMD} ssot-sync-to-nautobot", "Do you want to run a dry run?")
+        return False
+
+    dry_run = is_truthy(dry_run)
+    data = {"dry_run": dry_run}
+    sync_job = IpFabricDataSource()
+    sync_job.job_result = JobResult(
+        name=sync_job.class_path,
+        obj_type=ContentType.objects.get(
+            app_label="extras",
+            model="job",
+        ),
+        job_id=uuid.uuid4(),
+    )
+    sync_job.job_result.validated_save()
+
+    dispatcher.send_markdown(
+        f"Stand by {dispatcher.user_mention()}, I'm running your sync with dry run set to {dry_run}.",
+        ephemeral=True,
+    )
+
+    sync_job.run(data, commit=True)
+    sync_job.post_run()
+    sync_job.job_result.status = "completed" if not sync_job.failed else "failed"
+    sync_job.job_result.save()
+    blocks = [
+        *dispatcher.command_response_header(
+            "ipfabric",
+            "ssot-sync-to-nautobot",
+            [("Dry Run", str(dry_run))],
+            "sync job",
+            ipfabric_logo(dispatcher),
+        ),
+    ]
+    dispatcher.send_blocks(blocks)
+    if sync_job.job_result.status == "completed":
+        dispatcher.send_markdown(
+            f"Sync completed succesfully. Here is the link to your job: {NAUTOBOT_HOST}{sync_job.sync.get_absolute_url()}."
+        )
+    else:
+        dispatcher.send_warning(
+            f"Sync failed. Here is the link to your job: {NAUTOBOT_HOST}{sync_job.sync.get_absolute_url()}"
+        )
+    return CommandStatusChoices.STATUS_SUCCEEDED
