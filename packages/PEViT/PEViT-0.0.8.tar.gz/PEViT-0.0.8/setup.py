d0d09ebaa12f31cda7e043a6523c3f26d8f54e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:27 +0000

    Add package PEViT/0.0.8/PEViT-0.0.8.tar.gz

---
+++ packages/PEViT/PEViT-0.0.8.tar.gz/PEViT-0.0.8/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="PEViT", #
+    version='0.0.8',
+    author='xuehaihe',
+    author_email='xuehaihe2008@gmail.com',
+    url='https://github.com/jkooy/KroneckerAdapter',
+    description="Parameter-efficient Finetuning for ViT",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6'
+)
